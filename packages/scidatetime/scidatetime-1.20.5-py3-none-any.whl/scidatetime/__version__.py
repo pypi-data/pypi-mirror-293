01f27b1fa35f47c925bc6c431fdc7b4445e2337a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:31 +0000

    Add package scidatetime/1.20.5/scidatetime-1.20.5-py3-none-any.whl

---
+++ packages/scidatetime/scidatetime-1.20.5-py3-none-any.whl/scidatetime/__version__.py
@@ -0,0 +1,10 @@
+__title__ = "scidatetime"
+__description__ = "scidatetime is python module for DateTime handle."
+__keywords__ = ['lib', 'utils']
+__url__ = "https://github.com/serfend/scidatetime"
+__version__ = "1.20.5"
+__author__ = "serfend"
+__author_email__ = "serfend@foxmail.com"
+__license__ = "MIT Licence"
+
+__public_path__ = './dist'
