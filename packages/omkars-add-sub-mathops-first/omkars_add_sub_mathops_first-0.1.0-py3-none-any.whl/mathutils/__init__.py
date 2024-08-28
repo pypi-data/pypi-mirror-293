e5b3908c32c45620023b8b9025e482aa6e2cdadd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:31 +0000

    Add package omkars-add-sub-mathops-first/0.1.0/omkars_add_sub_mathops_first-0.1.0-py3-none-any.whl

---
+++ packages/omkars-add-sub-mathops-first/omkars_add_sub_mathops_first-0.1.0-py3-none-any.whl/mathutils/__init__.py
@@ -0,0 +1,5 @@
+# mathutils/__init__.py
+
+from .operations import add, subtract
+
+__all__ = ["add", "subtract"]
---
+++ packages/omkars-add-sub-mathops-first/omkars_add_sub_mathops_first-0.1.0-py3-none-any.whl/mathutils/operations.py
@@ -0,0 +1,13 @@
+# mathutils/operations.py
+
+def add(a, b):
+    """
+    Return the sum of two numbers.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Return the difference of two numbers.
+    """
+    return a - b
