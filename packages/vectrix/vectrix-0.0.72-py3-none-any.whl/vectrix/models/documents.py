c8c3b8e4a2b5cdc41dcd7092e85b06c073907705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 16:03:13 +0000

    Add package vectrix/0.0.72/vectrix-0.0.72-py3-none-any.whl

---
+++ packages/vectrix/vectrix-0.0.72-py3-none-any.whl/vectrix/models/documents.py
@@ -0,0 +1,56 @@
+from typing import Optional, List
+from pydantic import BaseModel, Field
+
+class VectorDocument(BaseModel):
+    """
+    Data model for storing chunks of text and their metadata.
+    Can be ingested by the Vectordb class.
+    """
+    title: str
+    url: str
+    content: str
+    type: str
+    NER: Optional[str] = None
+
+class Documents(BaseModel):
+    """
+    A list of VectorDocument objects.
+    """
+    documents: List[VectorDocument]
+
+
+class FileObject(BaseModel):
+    '''
+    A physical file object that can be processed for chunking and processing.
+    '''
+    file_path: str
+    file_type: str
+    file_name: str
+    url: Optional[str] = None
+
+class WebPageData(BaseModel):
+    '''
+    Content of an extracted webpage.
+    '''
+    content: dict = {
+        "title": Optional[str],
+        "hostname": Optional[str],
+        "date": Optional[str],
+        "fingerprint": Optional[str],
+        "id": Optional[str],
+        "license": Optional[str],
+        "comments": Optional[str],
+        "raw_text": Optional[str],
+        "text": Optional[str],
+        "language": Optional[str],
+        "image": Optional[str],
+        "pagetype": Optional[str],
+        "filedate": Optional[str],
+        "source": Optional[str],
+        "source_hostname": Optional[str],
+        "excerpt": Optional[str],
+        "categories": Optional[str],
+        "tags": Optional[str]
+    }
+    metadata: dict = {}
+    source_hostname: Optional[str] = Field(None, alias='source-hostname')
