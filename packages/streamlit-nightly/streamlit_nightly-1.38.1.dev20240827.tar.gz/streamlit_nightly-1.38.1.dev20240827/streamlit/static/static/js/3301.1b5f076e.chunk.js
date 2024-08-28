ef9e4df1b6c7a8ab7236a1078cc0eb4b885a1043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:49 +0000

    Add package streamlit-nightly/1.38.1.dev20240827/streamlit_nightly-1.38.1.dev20240827.tar.gz

---
+++ packages/streamlit-nightly/streamlit_nightly-1.38.1.dev20240827.tar.gz/streamlit_nightly-1.38.1.dev20240827/streamlit/static/static/js/3301.1b5f076e.chunk.js
@@ -0,0 +1,17 @@
+"use strict";(self.webpackChunk_streamlit_app=self.webpackChunk_streamlit_app||[]).push([[3301],{76039:(t,e,a)=>{a.d(e,{K:()=>s});var n=a(66845),r=a(17664),i=a(9332),o=a(40864);const s=t=>{var e;let{children:a}=t;const s=null===(e=(0,n.useContext)(i.w))||void 0===e?void 0:e();return s?(0,r.createPortal)(a,s):(0,o.jsx)(o.Fragment,{children:a})}},70738:(t,e,a)=>{a.d(e,{Z:()=>m});var n=a(66845),r=a(7974),i=a.n(r),o=a(23175),s=a.n(o);const c=(0,a(1515).Z)("div",{target:"ecnfqzf0"})((()=>({"@media print":{display:"none"}})),"");var d=a(40864);const l=t=>{let{className:e,scriptRunId:a,numParticles:n,numParticleTypes:r,ParticleComponent:o}=t;return(0,d.jsx)(c,{className:s()(e,"stHidden"),"data-testid":`${e}`,children:i()(n).map((t=>{const e=Math.floor(Math.random()*r);return(0,d.jsx)(o,{particleType:e},a+t)}))})},m=(0,n.memo)(l)},69436:(t,e,a)=>{a.r(e),a.d(e,{NUM_FLAKES:()=>f,default:()=>k});var n=a(66845);const r=a.p+"static/media/flake-0.beded754e8024c73d9d2.png",i=a.p+"static/media/flake-1.8077dc154e0bf900aa73.png",o=a.p+"static/media/flake-2.e3f07d06933dd0e84c24.png";var s=a(70738),c=a(76039),d=a(1515),l=a(7865);const m=function(t){let e=arguments.length>1&&void 0!==arguments[1]?arguments[1]:0;return Math.random()*(t-e)+e},p=(0,d.Z)("img",{target:"ekdfb790"})((t=>{let{theme:e}=t;return{position:"fixed",top:"-150px",marginLeft:"-75px",zIndex:e.zIndices.balloons,left:`${m(90,10)}vw`,animationDelay:`${m(4e3)}ms`,height:"150px",width:"150px",pointerEvents:"none",animationDuration:"3000ms",animationName:l.F4`
+  from {
+    transform:
+      translateY(0)
+      rotateX(${m(360)}deg)
+      rotateY(${m(360)}deg)
+      rotateZ(${m(360)}deg);
+  }
+
+  to {
+    transform:
+      translateY(calc(100vh + ${150}px))
+      rotateX(0)
+      rotateY(0)
+      rotateZ(0);
+  }
+`,animationTimingFunction:"ease-in",animationDirection:"normal",animationIterationCount:1,opacity:1}}),"");var u=a(40864);const f=100,h=[r,i,o],g=h.length,v=t=>{let{particleType:e}=t;return(0,u.jsx)(p,{src:h[e]})},x=function(t){let{scriptRunId:e}=t;return(0,u.jsx)(c.K,{children:(0,u.jsx)(s.Z,{className:"stSnow","data-testid":"stSnow",scriptRunId:e,numParticleTypes:g,numParticles:f,ParticleComponent:v})})},k=(0,n.memo)(x)}}]);
