d50ca9a7b2c2ca8d1a83ece52fb614fa17d9c754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:25 +0000

    Add package crypto-exchange-api/0.1.8/crypto_exchange_api-0.1.8.tar.gz

---
+++ packages/crypto-exchange-api/crypto_exchange_api-0.1.8.tar.gz/crypto_exchange_api-0.1.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crypto_exchange_api',
+    version='0.1.8',
+    description='A Python framework for accessing multiple cryptocurrency exchanges APIs',
+    author='hedeqiang',
+    author_email='laravel_code@163.com',
+    url='https://github.com/hedeqiang/python_crypto_exchange_api',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
