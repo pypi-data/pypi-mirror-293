e40d536e280dc7c10af6571daefab95e17eae901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:30 +0000

    Add package micro-editor/2.0.14/micro_editor-2.0.14.tar.gz

---
+++ packages/micro-editor/micro_editor-2.0.14.tar.gz/micro_editor-2.0.14/micro/go.mod
@@ -0,0 +1,30 @@
+module github.com/zyedidia/micro/v2
+
+require (
+	github.com/blang/semver v3.5.1+incompatible
+	github.com/dustin/go-humanize v1.0.0
+	github.com/go-errors/errors v1.0.1
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
+	github.com/mattn/go-isatty v0.0.11
+	github.com/mattn/go-runewidth v0.0.7
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/sergi/go-diff v1.1.0
+	github.com/stretchr/testify v1.4.0
+	github.com/yuin/gopher-lua v0.0.0-20191220021717-ab39c6098bdb
+	github.com/zyedidia/clipper v0.1.1
+	github.com/zyedidia/glob v0.0.0-20170209203856-dd4023a66dc3
+	github.com/zyedidia/json5 v0.0.0-20200102012142-2da050b1a98d
+	github.com/zyedidia/tcell/v2 v2.0.10 // indirect
+	github.com/zyedidia/terminal v0.0.0-20230315200948-4b3bcf6dddef
+	golang.org/x/text v0.3.8
+	gopkg.in/yaml.v2 v2.2.8
+	layeh.com/gopher-luar v1.0.7
+)
+
+replace github.com/kballard/go-shellquote => github.com/zyedidia/go-shellquote v0.0.0-20200613203517-eccd813c0655
+
+replace github.com/mattn/go-runewidth => github.com/zyedidia/go-runewidth v0.0.12
+
+replace layeh.com/gopher-luar => github.com/layeh/gopher-luar v1.0.7
+
+go 1.16
---
+++ packages/micro-editor/micro_editor-2.0.14.tar.gz/micro_editor-2.0.14/pdm_build.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+
+NAME = "micro"
+VERSION = "2.0.14"
+
+
+def is_windows():
+    if "GOOS" in os.environ:
+        return os.environ["GOOS"] == "windows"
+    return sys.platform == "win32"
+
+
+def build(output: str) -> None:
+    if shutil.which("go") is None:
+        msg = "golang is required and 'go' should be in $PATH"
+        raise RuntimeError(msg)
+
+    make = shutil.which("make")
+    if make is None:
+        msg = "make is required and 'make' should be in $PATH"
+        raise RuntimeError(msg)
+
+    args = [make, "build"]
+    submodule = Path(__file__).parent.joinpath(NAME)
+    subprocess.run(args, check=True, cwd=submodule)
+
+    binary = submodule.joinpath(NAME)
+    if is_windows():
+        binary = binary.with_suffix(".exe")
+
+    Path(output).parent.mkdir(exist_ok=True, parents=True)
+    shutil.move(binary, Path(output).parent)
+    Path(output).chmod(0o777)
+
+
+def pdm_build_hook_enabled(context):
+    return context.target != "sdist"
+
+
+def pdm_build_initialize(context) -> None:
+    add = {"--python-tag": "py3", "--py-limited-api": "none"}
+    context.builder.config_settings = {**add, **context.builder.config_settings}
+
+    context.ensure_build_dir()
+    output_path = Path(context.build_dir, "bin", NAME)
+    if is_windows():
+        output_path = output_path.with_suffix(".exe")
+    build(str(output_path))
