99f616804fac65e363d04842b4b578abec1b0f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 15:03:12 +0000

    Add package simple-print/2.2.1/simple_print-2.2.1-py3-none-any.whl

---
+++ packages/simple-print/simple_print-2.2.1-py3-none-any.whl/simple_print/__init__.py
@@ -0,0 +1,11 @@
+from .sprint import sprint
+from .spprint import spprint
+from .art_enum import ArtEnum
+from .sprint_err import SprintErr
+
+__all__ = [
+    "sprint",
+    "spprint",
+    "ArtEnum",
+    "SprintErr",
+]
