49ee5f20d66bbe74a1e76563f6866e04581ee0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:32 +0000

    Add package mtmai-client/0.3.506/mtmai_client-0.3.506-py3-none-any.whl

---
+++ packages/mtmai-client/mtmai_client-0.3.506-py3-none-any.whl/mtmai_client/models/agent.py
@@ -0,0 +1,77 @@
+from typing import Any, Dict, List, Type, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="Agent")
+
+
+@_attrs_define
+class Agent:
+    """
+    Attributes:
+        id (str):
+        title (str):
+        type (Union[Unset, str]):  Default: 'default'.
+    """
+
+    id: str
+    title: str
+    type: Union[Unset, str] = "default"
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        id = self.id
+
+        title = self.title
+
+        type = self.type
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "id": id,
+                "title": title,
+            }
+        )
+        if type is not UNSET:
+            field_dict["type"] = type
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        id = d.pop("id")
+
+        title = d.pop("title")
+
+        type = d.pop("type", UNSET)
+
+        agent = cls(
+            id=id,
+            title=title,
+            type=type,
+        )
+
+        agent.additional_properties = d
+        return agent
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
