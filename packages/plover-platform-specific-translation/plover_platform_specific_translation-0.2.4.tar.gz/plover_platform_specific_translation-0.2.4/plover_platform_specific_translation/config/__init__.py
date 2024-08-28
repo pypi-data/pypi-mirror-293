860eec0a602e07d29a3323c8147268f1a050b877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:12 +0000

    Add package plover-platform-specific-translation/0.2.4/plover_platform_specific_translation-0.2.4.tar.gz

---
+++ packages/plover-platform-specific-translation/plover_platform_specific_translation-0.2.4.tar.gz/plover_platform_specific_translation-0.2.4/plover_platform_specific_translation/config/__init__.py
@@ -0,0 +1,15 @@
+"""
+# Config
+
+A package dealing with:
+    - loading and saving config containing platform-specific outline values
+"""
+from .actions import (
+    load,
+    save
+)
+
+__all__ = [
+    "load",
+    "save"
+]
