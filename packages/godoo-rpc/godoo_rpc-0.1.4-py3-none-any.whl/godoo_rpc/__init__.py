23ea656dc0f436fdda5d122e8657d0daa15d7a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 16:03:10 +0000

    Add package godoo-rpc/0.1.4/godoo_rpc-0.1.4-py3-none-any.whl

---
+++ packages/godoo-rpc/godoo_rpc-0.1.4-py3-none-any.whl/godoo_rpc/__init__.py
@@ -0,0 +1,7 @@
+"""gOdoo-rpc."""
+__version__ = "0.1.4"
+from . import odoo2odoo_mapper  # noqa
+from .api import OdooApiWrapper  # noqa
+from .file_2_odoo import import_data, import_dataset  # noqa
+from .login import login_odoo, login_odoo_env  # noqa
+from .odoo_dataset import OdooDataset  # noqa
