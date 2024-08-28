c6bcfd8047f048d37f0ff38629932378b56e3dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 15:03:07 +0000

    Add package pygcap/1.2.6/pygcap-1.2.6.tar.gz

---
+++ packages/pygcap/pygcap-1.2.6.tar.gz/pygcap-1.2.6/pygcap/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '1.2.6'
+
+from ._accessory import *
+from ._blast import *
+from ._cluster import *
+from ._count import *
+from ._parse import *
+from ._probe import *
+from ._profile import *
+from ._search import *
+from ._seqlib import *
+from ._utils import *
+from ._visualize import *
+
+import pkg_resources
+
+DATA_PATH = 'data/probe_sample.tsv'
+
+def get_data_path():
+    return pkg_resources.resource_filename(__name__, DATA_PATH)
---
+++ packages/pygcap/pygcap-1.2.6.tar.gz/pygcap-1.2.6/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='pygcap',
+    version='1.2.6',
+    description='Python package for probe-based gene cluster finding in large microbial genome database',
+	long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='jsrim',
+    author_email='comfortindex@naver.com',
+    url='https://github.com/CompSynBioLab-KoreaUniv/pyGCAP',
+    install_requires=[
+        'pandas >= 2.1', 
+        'matplotlib >= 3.7, < 3.9',  
+        'requests', 
+        'setuptools'
+        ],
+    packages=find_packages(),
+    package_data={
+        'pygcap': ['data/*.tsv'],
+    },
+    keywords=['gene', 
+              'cluster', 
+              'genomics', 
+              'bioinformatics'],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pygcap=pygcap.cli:main',
+        ],
+    },
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
