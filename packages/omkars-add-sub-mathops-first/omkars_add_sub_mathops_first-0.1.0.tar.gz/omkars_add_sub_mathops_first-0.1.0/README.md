d0cd15ff9b4365b327cd1dc73f07870bed6ae964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:31 +0000

    Add package omkars-add-sub-mathops-first/0.1.0/omkars_add_sub_mathops_first-0.1.0.tar.gz

---
+++ packages/omkars-add-sub-mathops-first/omkars_add_sub_mathops_first-0.1.0.tar.gz/omkars_add_sub_mathops_first-0.1.0/README.md
@@ -0,0 +1,10 @@
+# mathutils
+
+`mathutils` is a simple Python package that provides basic mathematical operations, including addition and subtraction.
+
+## Installation
+
+You can install the package via pip:
+
+```bash
+pip install mathutils
---
+++ packages/omkars-add-sub-mathops-first/omkars_add_sub_mathops_first-0.1.0.tar.gz/omkars_add_sub_mathops_first-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="omkars_add_sub_mathops_first",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Omkar Singh",
+    author_email="singhomkar20.1995@gmail.com",
+    description="A simple math operations package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/omkars20/mathutils_packagae.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
