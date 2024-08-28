8dbf83b4652d2358c3d94796469d4b9a2fc01386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:45 +0000

    Add package yaylib/1.5.0.dev3/yaylib-1.5.0.dev3.tar.gz

---
+++ packages/yaylib/yaylib-1.5.0.dev3.tar.gz/yaylib-1.5.0.dev3/yaylib/__init__.py
@@ -0,0 +1,39 @@
+"""
+
+Yay! (nanameue, Inc.) API Client
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A powerful API wrapper for Yay! (yay.space) written in Python.
+
+:copyright: (c) 2023 ekkx
+:license: MIT, see LICENSE for more details.
+
+https://github.com/ekkx/yaylib
+
+"""
+
+__title__ = "yaylib"
+__author__ = "ekkx"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2023 ekkx"
+__version__ = "1.5.0.dev3"
+
+from .client import Client
+from .constants import *
+from .errors import *
+from .models import *
+from .responses import *
+from .state import State
+from .utils import mention
+from .ws import *
+
+__all__ = (
+    "Client",
+    "constants",
+    "errors",
+    "models",
+    "responses",
+    "State",
+    "mention",
+    "ws",
+)
