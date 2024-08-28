8b28d8708ee1f8b5466b7915f2859fbdd068edad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:03:25 +0000

    Add package autonomize-autorag/0.1.7/autonomize_autorag-0.1.7.tar.gz

---
+++ packages/autonomize-autorag/autonomize_autorag-0.1.7.tar.gz/autonomize_autorag-0.1.7/autorag/embedding/base.py
@@ -0,0 +1,67 @@
+"Embedding abstraction module"
+
+# pylint: disable=line-too-long
+
+from abc import ABC, abstractmethod
+from typing import List, Union
+
+from autorag.utilities.concurrency import run_async
+
+
+class Embedding(ABC):
+    """
+    Abstract base class for Embedding.
+
+    This class defines the interface for embedding model implementations. It provides
+    a structure to create embeddings using various models, making it easier to switch
+    between different embedding models.
+    """
+
+    def create_embedding(self, texts: Union[str, List[str]]) -> List[List[float]]:
+        """
+        Creates embeddings for the provided text(s).
+
+        Args:
+            texts (Union[str, List[str]]): The input text or list of texts to generate embeddings for.
+
+        Returns:
+            List[List[float]]: A list of embedding vectors for the input text(s).
+        """
+
+        if isinstance(texts, str):
+            texts = [texts]
+
+        return self._create_embedding(texts=texts)
+
+    @abstractmethod
+    def _create_embedding(self, texts: List[str]) -> List[List[float]]:
+        """
+        Creates embeddings for the provided text(s).
+
+        Args:
+            texts (List[str]): The input list of texts to generate embeddings for.
+
+        Returns:
+            List[List[float]]: A list of embedding vectors for the input texts.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a subclass.
+        """
+        raise NotImplementedError(  # pragma: no cover
+            f"{self.__class__.__name__} does not implement '_create_embedding'."
+        )
+
+    async def acreate_embedding(
+        self, texts: Union[str, List[str]]
+    ) -> List[List[float]]:
+        """
+        Asynchronously creates embeddings for the provided text(s).
+
+        Args:
+            texts (Union[str, List[str]]): The input text or list of texts to generate embeddings for.
+
+        Returns:
+            List[List[float]]: A list of embedding vectors for the input text(s).
+        """
+
+        return await run_async(lambda: self.create_embedding(texts=texts))
