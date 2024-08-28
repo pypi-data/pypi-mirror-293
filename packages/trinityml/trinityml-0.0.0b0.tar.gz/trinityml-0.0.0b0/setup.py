26746448716a20756a9478023b8cb4e9a1493030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 16:02:57 +0000

    Add package trinityml/0.0.0b0/trinityml-0.0.0b0.tar.gz

---
+++ packages/trinityml/trinityml-0.0.0b0.tar.gz/trinityml-0.0.0b0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trinityml',
+    version='0.0.0-beta',
+    description='A Python SDK for record the LLM Experiment Execution', 
+    author='Team Giggso',
+    author_email='support@giggso.com',
+    url='https://github.com/roosterhr/gg_python_sdk',
+    py_modules=['trinity'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
+)
+
+
