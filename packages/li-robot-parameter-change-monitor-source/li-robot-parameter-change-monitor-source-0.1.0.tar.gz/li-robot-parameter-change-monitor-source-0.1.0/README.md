0736d93884298385eb8e0bb7a24b91d404f8f889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 15:03:03 +0000

    Add package li-robot-parameter-change-monitor-source/0.1.0/li-robot-parameter-change-monitor-source-0.1.0.tar.gz

---
+++ packages/li-robot-parameter-change-monitor-source/li-robot-parameter-change-monitor-source-0.1.0.tar.gz/li-robot-parameter-change-monitor-source-0.1.0/README.md
@@ -0,0 +1,1 @@
+# li-robot-parameter-change-monitor-source
---
+++ packages/li-robot-parameter-change-monitor-source/li-robot-parameter-change-monitor-source-0.1.0.tar.gz/li-robot-parameter-change-monitor-source-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="li-robot-parameter-change-monitor-source",
+    version="0.1.0",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+    
+    ],
+    author="Juncheng Zhang",
+    author_email="zhangjunchengbh@163.com",
+    description="A package for industrial robot parameter change monitoring using ZDT API",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.example.com/your-group/robot-parameter-change-monitor-source",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
