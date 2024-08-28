5504668869b739eacfb51226a9387c266db1e14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 16:03:14 +0000

    Add package idds-monitor/2.1.35/idds_monitor-2.1.35.tar.gz

---
+++ packages/idds-monitor/idds_monitor-2.1.35.tar.gz/idds_monitor-2.1.35/data/conf.js
@@ -0,0 +1,9 @@
+
+var appConfig = {
+    'iddsAPI_request': "https://fv-az1499-457.mb0m5sqlh0wutl0wi0kcl1bndc.bx.internal.cloudapp.net:443/idds/monitor_request/null/null",
+    'iddsAPI_transform': "https://fv-az1499-457.mb0m5sqlh0wutl0wi0kcl1bndc.bx.internal.cloudapp.net:443/idds/monitor_transform/null/null",
+    'iddsAPI_processing': "https://fv-az1499-457.mb0m5sqlh0wutl0wi0kcl1bndc.bx.internal.cloudapp.net:443/idds/monitor_processing/null/null",
+    'iddsAPI_request_detail': "https://fv-az1499-457.mb0m5sqlh0wutl0wi0kcl1bndc.bx.internal.cloudapp.net:443/idds/monitor/null/null/true/false/false",
+    'iddsAPI_transform_detail': "https://fv-az1499-457.mb0m5sqlh0wutl0wi0kcl1bndc.bx.internal.cloudapp.net:443/idds/monitor/null/null/false/true/false",
+    'iddsAPI_processing_detail': "https://fv-az1499-457.mb0m5sqlh0wutl0wi0kcl1bndc.bx.internal.cloudapp.net:443/idds/monitor/null/null/false/false/true"
+}
