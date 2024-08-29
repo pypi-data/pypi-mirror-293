3c84ce14b3f07c08da233b52af0314d8b0044693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:10 +0000

    Add package pycoze/0.1.9/pycoze-0.1.9.tar.gz

---
+++ packages/pycoze/pycoze-0.1.9.tar.gz/pycoze-0.1.9/pycoze/utils/arg.py
@@ -0,0 +1,15 @@
+import argparse
+import sys
+
+
+def read_arg(param, is_path=False):
+    args = sys.argv[1:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(param, nargs='?', help=f'Parameter {param}')
+    args = parser.parse_known_args(args)[0]
+    value = getattr(args, param.lstrip('-'))
+    # 如果是路径并且有引号，去掉引号
+    if is_path and value and value.startswith('"') and value.endswith('"'):
+        value = value[1:-1]
+
+    return value
