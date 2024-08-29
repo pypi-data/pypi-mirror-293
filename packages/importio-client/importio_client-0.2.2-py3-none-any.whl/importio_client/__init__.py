00f3d4c1afe935503a49bdbf6136fb1b789a898b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 06:03:54 +0000

    Add package importio-client/0.2.2/importio_client-0.2.2-py3-none-any.whl

---
+++ packages/importio-client/importio_client-0.2.2-py3-none-any.whl/importio_client/__init__.py
@@ -0,0 +1,22 @@
+"""
+ImportIO Client
+
+This package provides a client for interacting with the ImportIO API.
+"""
+
+from .client import ImportIOClient, ExtractorEndpoint, CrawlRunEndpoint, FileType
+from .exceptions import ImportIOError, ExtractorNotFound, StatusCodeError
+from .utils import build_input_payload
+
+__all__ = [
+    'ImportIOClient',
+    'ExtractorEndpoint',
+    'CrawlRunEndpoint',
+    'FileType',
+    'ImportIOError',
+    'ExtractorNotFound',
+    'StatusCodeError',
+    'build_input_payload'
+]
+
+__version__ = "0.2.2"  # Update this version number
