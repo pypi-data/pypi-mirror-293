92c49924081e2bf58a34697bc596b0c6c368032f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 07:03:52 +0000

    Add package openpaperwork-gtk/2.2.5/openpaperwork_gtk-2.2.5.tar.gz

---
+++ packages/openpaperwork-gtk/openpaperwork_gtk-2.2.5.tar.gz/openpaperwork_gtk-2.2.5/src/openpaperwork_gtk/deps.py
@@ -0,0 +1,39 @@
+GDK_PIXBUF = {
+    'debian': 'gir1.2-gdkpixbuf-2.0',
+    'linuxmint': 'gir1.2-gdkpixbuf-2.0',
+    'raspbian': 'gir1.2-gdkpixbuf-2.0',
+    'ubuntu': 'gir1.2-gdkpixbuf-2.0',
+}
+
+GTK = {
+    'debian': 'gir1.2-gtk-3.0',
+    'fedora': 'gtk3',
+    'gentoo': 'x11-libs/gtk+',
+    'linuxmint': 'gir1.2-gtk-3.0',
+    'raspbian': 'gir1.2-gtk-3.0',
+    'suse': 'python-gtk',
+    'ubuntu': 'gir1.2-gtk-3.0',
+}
+
+HDY = {
+    'debian': 'gir1.2-handy-1',
+    'fedora': 'libhandy1',
+    'gentoo': 'gui-libs/libhandy',
+    'linuxmint': 'gir1.2-handy-1',
+    'raspbian': 'gir1.2-handy-1',
+    'suse': 'libhandy-1-0',
+    'ubuntu': 'gir1.2-handy-1',
+}
+
+NOTIFY = {
+    'debian': 'gir1.2-notify-0.7',
+    'fedora': 'libnotify',
+    'ubuntu': 'gir1.2-notify-0.7',
+}
+
+PANGO = {
+    'debian': 'gir1.2-pango-1.0',
+    'linuxmint': 'gir1.2-pango-1.0',
+    'raspbian': 'gir1.2-pango-1.0',
+    'ubuntu': 'gir1.2-pango-1.0',
+}
