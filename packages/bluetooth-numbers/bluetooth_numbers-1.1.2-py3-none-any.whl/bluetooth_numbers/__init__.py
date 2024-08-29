daa700a988e31a0c9d2fb7e3425cf918200f7d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:10 +0000

    Add package bluetooth-numbers/1.1.2/bluetooth_numbers-1.1.2-py3-none-any.whl

---
+++ packages/bluetooth-numbers/bluetooth_numbers-1.1.2-py3-none-any.whl/bluetooth_numbers/__init__.py
@@ -0,0 +1,62 @@
+# pylint: skip-file
+"""bluetooth-numbers: Python package with a wide set of numbers related to Bluetooth.
+
+This project offers a Python package with company IDs, service, characteristic and
+descriptor UUIDs and OUIs, so Python projects can easily use these numbers.
+
+Get the description of a company ID:
+
+>>> from bluetooth_numbers import company
+>>> company[0x0499]
+'Ruuvi Innovations Ltd.'
+
+Get the description of a service UUID:
+
+>>> from bluetooth_numbers import service
+>>> from uuid import UUID
+>>> service[0x180F]
+'Battery Service'
+>>> service[UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")]
+'Nordic UART Service'
+
+Get the description of a characteristic UUID:
+
+>>> from bluetooth_numbers import characteristic
+>>> from uuid import UUID
+>>> characteristic[0x2A37]
+'Heart Rate Measurement'
+>>> characteristic[UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")]
+'UART RX Characteristic'
+
+Get the description of a descriptor UUID:
+
+>>> from bluetooth_numbers import descriptor
+>>> descriptor[0x2901]
+'Characteristic User Descriptor'
+
+Get the description of an OUI:
+
+>>> from bluetooth_numbers import oui
+>>> oui["58:2D:34"]
+'Qingping Electronics (Suzhou) Co., Ltd'
+"""
+
+# Public API for easier importing
+from ._characteristics import characteristic
+from ._companies import company
+from ._descriptors import descriptor
+from ._ouis import oui
+from ._services import service
+
+__all__ = ["characteristic", "company", "descriptor", "oui", "service"]
+
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = "bluetooth-numbers"
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
---
+++ packages/bluetooth-numbers/bluetooth_numbers-1.1.2-py3-none-any.whl/bluetooth_numbers/dicts.py
@@ -0,0 +1,157 @@
+"""Module with specialized dictionary classes for UUIDs, CICs and OUIs."""
+from __future__ import annotations
+
+from typing import Dict, Union
+from uuid import UUID
+
+from bluetooth_numbers.exceptions import (
+    No16BitIntegerError,
+    NonStandardUUIDError,
+    UnknownCICError,
+    UnknownOUIError,
+    UnknownUUIDError,
+)
+from bluetooth_numbers.utils import (
+    is_normalized_oui,
+    is_uint16,
+    normalize_oui,
+    uuid128_to_uuid16,
+)
+
+
+class CICDict(Dict[int, str]):
+    """Dictionary class to hold 16-bit company codes and their names.
+
+    You can use this class as a dict with the following differences:
+
+    - If you check for a key that doesn't exist, this raises an
+      :class:`~bluetooth_numbers.exceptions.UnknownCICError`.
+    - If you check for a key that isn't a 16-bit unsigned integer, this raises a
+      :class:`~bluetooth_numbers.exceptions.No16BitIntegerError`.
+
+    Examples:
+        >>> from bluetooth_numbers import company
+        >>> company[0x004C]
+        'Apple, Inc.'
+        >>> company[-1]
+        Traceback (most recent call last):
+        bluetooth_numbers.exceptions.No16BitIntegerError: -1
+        >>> company[65534]
+        Traceback (most recent call last):
+        bluetooth_numbers.exceptions.UnknownCICError: 65534
+    """
+
+    def __missing__(self, key: int) -> str:
+        """Try the key and raise exception when it's invalid.
+
+        Args:
+            key (int): The key to check.
+
+        Raises:
+            No16BitIntegerError: If ``key`` isn't a 16-bit unsigned integer.
+            UnknownCICError: If ``key`` isn't in this CICDict instance.
+        """
+        if is_uint16(key):
+            raise UnknownCICError(key)
+
+        raise No16BitIntegerError(key)
+
+
+class OUIDict(Dict[str, str]):
+    """Dictionary class to hold OUIs and their names.
+
+    You can use this class as a dict with the following differences:
+
+    - You can check for an OUI in the formats "xx:yy:zz", "xx-yy-zz" or "xxyyzz".
+      Both lowercase and uppercase letters are supported.
+    - If you check for a key that doesn't exist, this raises an
+      :class:`~bluetooth_numbers.exceptions.UnknownOUIError`.
+    - If you check for a key that doesn't have one of the supported formats, this
+      raises a :class:`~bluetooth_numbers.exceptions.WrongOUIFormatError`.
+
+    Examples:
+        >>> from bluetooth_numbers import oui
+        >>> oui["98:E7:43"]
+        'Dell Inc.'
+        >>> oui["c4-29-96"]
+        'Signify B.V.'
+        >>> oui["A44519"]
+        'Xiaomi Communications Co Ltd'
+        >>> oui["FOOBAR"]
+        Traceback (most recent call last):
+        bluetooth_numbers.exceptions.WrongOUIFormatError: 'FOOBAR'
+        >>> oui["AB:CD:EF"]
+        Traceback (most recent call last):
+        bluetooth_numbers.exceptions.UnknownOUIError: AB:CD:EF
+    """
+
+    def __missing__(self, key: str) -> str:
+        """Try the key and raise exception when it's invalid.
+
+        Args:
+            key (str): The key to check.
+
+        Raises:
+            UnknownOUIError: If ``key`` isn't in this OUIDict instance.
+            WrongOUIFormatError: If ``key`` doesn't have one of the supported
+              formats.
+
+        Returns:
+            str: The name corresponding to ``key``.
+        """
+        if is_normalized_oui(key):
+            raise UnknownOUIError(key)
+
+        return self[normalize_oui(key)]
+
+
+class UUIDDict(Dict[Union[UUID, int], str]):
+    """Dictionary class to hold 16-bit and 128-bit standard UUID keys and descriptions.
+
+    You can use this class as a dict for Bluetooth UUIDs, with the following
+    differences:
+
+    - If you check for a 128-bit standard UUID and this UUID doesn't exist in the
+      dictionary, it will check for the corresponding 16-bit UUID.
+    - If you check for a UUID that doesn't exist, this raises an
+      :class:`~bluetooth_numbers.exceptions.UnknownUUIDError`.
+    - If you check for a key that isn't a 16-bit unsigned integer, this raises a
+      :class:`~bluetooth_numbers.exceptions.No16BitIntegerError`.
+
+    Examples:
+        >>> from bluetooth_numbers import service
+        >>> from uuid import UUID
+        >>> service[UUID("0000180F-0000-1000-8000-00805F9B34FB")]
+        'Battery Service'
+        >>> service[0x180F]
+        'Battery Service'
+        >>> service[0]
+        Traceback (most recent call last):
+        bluetooth_numbers.exceptions.UnknownUUIDError: 0
+        >>> service[6.5]
+        Traceback (most recent call last):
+        bluetooth_numbers.exceptions.No16BitIntegerError: 6.5
+    """
+
+    def __missing__(self, key: UUID | int) -> str:
+        """Try the key converted to 16-bit UUID.
+
+        Args:
+            key (UUID | int): The 128-bit or 16-bit UUID to check.
+
+        Raises:
+            No16BitIntegerError: If ``key`` isn't a 16-bit unsigned integer.
+            UnknownUUIDError: If ``key`` isn't in this UUIDDict instance.
+
+        Returns:
+            str: The name corresponding to ``key``.
+        """
+        if isinstance(key, UUID):
+            try:
+                return self[uuid128_to_uuid16(key)]
+            except NonStandardUUIDError as error:
+                raise UnknownUUIDError(key) from error
+        elif is_uint16(key):
+            raise UnknownUUIDError(key)
+
+        raise No16BitIntegerError(key)
