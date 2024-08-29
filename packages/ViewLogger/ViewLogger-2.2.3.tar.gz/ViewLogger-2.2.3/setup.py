5cfc1695738042855bd7c33baef48fa6c57d5ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:10 +0000

    Add package ViewLogger/2.2.3/ViewLogger-2.2.3.tar.gz

---
+++ packages/ViewLogger/ViewLogger-2.2.3.tar.gz/ViewLogger-2.2.3/setup.py
@@ -0,0 +1,56 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='ViewLogger',
+    version='2.2.3',
+    packages=find_packages(exclude=['docs','tests*']),
+    include_package_data=True,
+    license='MIT',
+    description='Log view hits over time so that you know who enter this view and when.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mahmoodnasr/ViewLogger/',
+    author='Mahmood Nasr',
+    author_email='mahmood.nasr.fcis@gmail.com',
+    install_requires=[
+        'Django>=1.7',
+        'simplejson',
+        'django_tables2'
+      ],
+classifiers=[
+        "Framework :: Django",
+        "Framework :: Django :: 1.7",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 1.9",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
