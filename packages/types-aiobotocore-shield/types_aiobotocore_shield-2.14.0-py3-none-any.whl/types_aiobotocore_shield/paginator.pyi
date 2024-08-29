2cd69d1e11e36431ddafeace19b99fe61a130e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:04:20 +0000

    Add package types-aiobotocore-shield/2.14.0/types_aiobotocore_shield-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-shield/types_aiobotocore_shield-2.14.0-py3-none-any.whl/types_aiobotocore_shield/paginator.pyi
@@ -0,0 +1,83 @@
+"""
+Type annotations for shield service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_shield/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_shield.client import ShieldClient
+    from types_aiobotocore_shield.paginator import (
+        ListAttacksPaginator,
+        ListProtectionsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("shield") as client:
+        client: ShieldClient
+
+        list_attacks_paginator: ListAttacksPaginator = client.get_paginator("list_attacks")
+        list_protections_paginator: ListProtectionsPaginator = client.get_paginator("list_protections")
+    ```
+"""
+
+from typing import AsyncIterator, Generic, Iterator, Sequence, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    InclusionProtectionFiltersTypeDef,
+    ListAttacksResponseTypeDef,
+    ListProtectionsResponseTypeDef,
+    PaginatorConfigTypeDef,
+    TimeRangeUnionTypeDef,
+)
+
+__all__ = ("ListAttacksPaginator", "ListProtectionsPaginator")
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListAttacksPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/shield.html#Shield.Paginator.ListAttacks)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_shield/paginators/#listattackspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ResourceArns: Sequence[str] = ...,
+        StartTime: TimeRangeUnionTypeDef = ...,
+        EndTime: TimeRangeUnionTypeDef = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListAttacksResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/shield.html#Shield.Paginator.ListAttacks.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_shield/paginators/#listattackspaginator)
+        """
+
+class ListProtectionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/shield.html#Shield.Paginator.ListProtections)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_shield/paginators/#listprotectionspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        InclusionFilters: InclusionProtectionFiltersTypeDef = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListProtectionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/shield.html#Shield.Paginator.ListProtections.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_shield/paginators/#listprotectionspaginator)
+        """
