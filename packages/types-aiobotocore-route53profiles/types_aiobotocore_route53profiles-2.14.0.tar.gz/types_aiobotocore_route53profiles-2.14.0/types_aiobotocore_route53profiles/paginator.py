dd15cb14869e46ba0e9b66587e27616d0735edd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:03:59 +0000

    Add package types-aiobotocore-route53profiles/2.14.0/types_aiobotocore_route53profiles-2.14.0.tar.gz

---
+++ packages/types-aiobotocore-route53profiles/types_aiobotocore_route53profiles-2.14.0.tar.gz/types_aiobotocore_route53profiles-2.14.0/types_aiobotocore_route53profiles/paginator.py
@@ -0,0 +1,106 @@
+"""
+Type annotations for route53profiles service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_route53profiles/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_route53profiles.client import Route53ProfilesClient
+    from types_aiobotocore_route53profiles.paginator import (
+        ListProfileAssociationsPaginator,
+        ListProfileResourceAssociationsPaginator,
+        ListProfilesPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("route53profiles") as client:
+        client: Route53ProfilesClient
+
+        list_profile_associations_paginator: ListProfileAssociationsPaginator = client.get_paginator("list_profile_associations")
+        list_profile_resource_associations_paginator: ListProfileResourceAssociationsPaginator = client.get_paginator("list_profile_resource_associations")
+        list_profiles_paginator: ListProfilesPaginator = client.get_paginator("list_profiles")
+    ```
+"""
+
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    ListProfileAssociationsResponseTypeDef,
+    ListProfileResourceAssociationsResponseTypeDef,
+    ListProfilesResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "ListProfileAssociationsPaginator",
+    "ListProfileResourceAssociationsPaginator",
+    "ListProfilesPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListProfileAssociationsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/route53profiles.html#Route53Profiles.Paginator.ListProfileAssociations)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_route53profiles/paginators/#listprofileassociationspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ProfileId: str = ...,
+        ResourceId: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListProfileAssociationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/route53profiles.html#Route53Profiles.Paginator.ListProfileAssociations.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_route53profiles/paginators/#listprofileassociationspaginator)
+        """
+
+
+class ListProfileResourceAssociationsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/route53profiles.html#Route53Profiles.Paginator.ListProfileResourceAssociations)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_route53profiles/paginators/#listprofileresourceassociationspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ProfileId: str,
+        ResourceType: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListProfileResourceAssociationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/route53profiles.html#Route53Profiles.Paginator.ListProfileResourceAssociations.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_route53profiles/paginators/#listprofileresourceassociationspaginator)
+        """
+
+
+class ListProfilesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/route53profiles.html#Route53Profiles.Paginator.ListProfiles)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_route53profiles/paginators/#listprofilespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListProfilesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/route53profiles.html#Route53Profiles.Paginator.ListProfiles.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_route53profiles/paginators/#listprofilespaginator)
+        """
