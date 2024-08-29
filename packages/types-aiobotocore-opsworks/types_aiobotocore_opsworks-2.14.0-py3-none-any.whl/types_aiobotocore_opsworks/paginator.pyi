4180a771c52c834ec9c5127b0124045b76f96f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:04:20 +0000

    Add package types-aiobotocore-opsworks/2.14.0/types_aiobotocore_opsworks-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-opsworks/types_aiobotocore_opsworks-2.14.0-py3-none-any.whl/types_aiobotocore_opsworks/paginator.pyi
@@ -0,0 +1,57 @@
+"""
+Type annotations for opsworks service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_opsworks/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_opsworks.client import OpsWorksClient
+    from types_aiobotocore_opsworks.paginator import (
+        DescribeEcsClustersPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("opsworks") as client:
+        client: OpsWorksClient
+
+        describe_ecs_clusters_paginator: DescribeEcsClustersPaginator = client.get_paginator("describe_ecs_clusters")
+    ```
+"""
+
+from typing import AsyncIterator, Generic, Iterator, Sequence, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import DescribeEcsClustersResultTypeDef, PaginatorConfigTypeDef
+
+__all__ = ("DescribeEcsClustersPaginator",)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class DescribeEcsClustersPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks.html#OpsWorks.Paginator.DescribeEcsClusters)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_opsworks/paginators/#describeecsclusterspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        EcsClusterArns: Sequence[str] = ...,
+        StackId: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[DescribeEcsClustersResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/opsworks.html#OpsWorks.Paginator.DescribeEcsClusters.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_opsworks/paginators/#describeecsclusterspaginator)
+        """
