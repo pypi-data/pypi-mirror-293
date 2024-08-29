cc334e5e3102afec350dda9e199b7324fa777dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:03:20 +0000

    Add package skleearn/0.0/skleearn-0.0.tar.gz

---
+++ packages/skleearn/skleearn-0.0.tar.gz/skleearn-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='skleearn',
+      version='0.0',
+      description='skleearn',
+      packages=['skleearn'],
+      author_email='applepro285@gmail.com',
+      zip_safe=False)
