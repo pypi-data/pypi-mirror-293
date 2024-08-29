330a4948b5dace0193904489278447898dfd964f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 05:03:38 +0000

    Add package dataforseo-client/1.0.28/dataforseo_client-1.0.28-py3-none-any.whl

---
+++ packages/dataforseo-client/dataforseo_client-1.0.28-py3-none-any.whl/dataforseo_client/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+import re
+import importlib
+
+def camel_to_snake(name):
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+def __getattr__(name):
+    file_name = camel_to_snake(name)
+    module = importlib.import_module(f'dataforseo_client.api.{file_name}')
+    model = getattr(module, name)
+    globals()[name] = model
+    return model
