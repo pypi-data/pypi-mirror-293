88baa0565b13aaa6707dda5f198a6e35b9634688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:00 +0000

    Add package types-aiobotocore-s3control/2.14.0/types_aiobotocore_s3control-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-s3control/types_aiobotocore_s3control-2.14.0-py3-none-any.whl/types_aiobotocore_s3control/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for s3control service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_s3control import (
+        Client,
+        ListAccessPointsForObjectLambdaPaginator,
+        S3ControlClient,
+    )
+
+    session = get_session()
+    async with session.create_client("s3control") as client:
+        client: S3ControlClient
+        ...
+
+
+    list_access_points_for_object_lambda_paginator: ListAccessPointsForObjectLambdaPaginator = client.get_paginator("list_access_points_for_object_lambda")
+    ```
+"""
+
+from .client import S3ControlClient
+from .paginator import ListAccessPointsForObjectLambdaPaginator
+
+Client = S3ControlClient
+
+__all__ = ("Client", "ListAccessPointsForObjectLambdaPaginator", "S3ControlClient")
