0e10da6717f6f069af92fdf23bb7f5860aaebafd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:58 +0000

    Add package obx/11/obx-11.tar.gz

---
+++ packages/obx/obx-11.tar.gz/obx-11/obx/runtime.py
@@ -0,0 +1,33 @@
+# This file is placed in the Public Domain.
+# pylint: disable=R0903,W0201
+
+
+"runtime"
+
+
+import getpass
+import os
+import time
+
+
+from .default import Default
+from .persist import Persist
+
+
+STARTTIME   = time.time()
+
+
+class Config(Default):
+
+    "configuration"
+
+
+Cfg         = Config()
+Cfg.name    = Config.__module__.rsplit(".", maxsplit=1)[-2]
+Cfg.user    = getpass.getuser()
+Cfg.mod     = "cmd,skl,req,srv"
+Cfg.wdr     = os.path.expanduser(f"~/.{Cfg.name}")
+Cfg.pidfile = os.path.join(Cfg.wdr, f"{Cfg.name}.pid")
+
+
+Persist.workdir = Cfg.wdr
