f7c8f90b6826ab4f065fd9cb56543e7103966555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:01 +0000

    Add package tyme-config-loader/0.2/tyme_config_loader-0.2.tar.gz

---
+++ packages/tyme-config-loader/tyme_config_loader-0.2.tar.gz/tyme_config_loader-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="tyme_config_loader",  # The name of your library
+    version="0.2",
+    description="A simple Python library",
+    author="Bao Tran",
+    author_email="bao.tr0401@gmail.com",
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=[]
+)
