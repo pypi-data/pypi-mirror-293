3b0b3c748d6f1275026a213458e4c42da808d9e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:03:35 +0000

    Add package odoo-pos-driver/0.4.2/odoo_pos_driver-0.4.2.tar.gz

---
+++ packages/odoo-pos-driver/odoo_pos_driver-0.4.2.tar.gz/odoo_pos_driver-0.4.2/odoo_pos_driver/devices/__init__.py
@@ -0,0 +1,5 @@
+from .device_display import DeviceDisplay  # noqa: F401
+from .device_list import get_device_type  # noqa: F401
+from .device_payment import DevicePayment  # noqa: F401
+from .device_printer import DevicePrinter  # noqa: F401
+from .device_scale import DeviceScale  # noqa: F401
