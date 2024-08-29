39f4b270605d322ef4a6013587991e0adb6c8401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:03 +0000

    Add package types-aiobotocore-scheduler/2.14.0/types_aiobotocore_scheduler-2.14.0.tar.gz

---
+++ packages/types-aiobotocore-scheduler/types_aiobotocore_scheduler-2.14.0.tar.gz/types_aiobotocore_scheduler-2.14.0/types_aiobotocore_scheduler/client.py
@@ -0,0 +1,285 @@
+"""
+Type annotations for scheduler service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_scheduler.client import EventBridgeSchedulerClient
+
+    session = get_session()
+    async with session.create_client("scheduler") as client:
+        client: EventBridgeSchedulerClient
+    ```
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Sequence, Type, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .literals import ActionAfterCompletionType, ScheduleStateType
+from .paginator import ListScheduleGroupsPaginator, ListSchedulesPaginator
+from .type_defs import (
+    CreateScheduleGroupOutputTypeDef,
+    CreateScheduleOutputTypeDef,
+    FlexibleTimeWindowTypeDef,
+    GetScheduleGroupOutputTypeDef,
+    GetScheduleOutputTypeDef,
+    ListScheduleGroupsOutputTypeDef,
+    ListSchedulesOutputTypeDef,
+    ListTagsForResourceOutputTypeDef,
+    TagTypeDef,
+    TargetUnionTypeDef,
+    TimestampTypeDef,
+    UpdateScheduleOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("EventBridgeSchedulerClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class EventBridgeSchedulerClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        EventBridgeSchedulerClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.exceptions)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.can_paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#can_paginate)
+        """
+
+    async def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.close)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#close)
+        """
+
+    async def create_schedule(
+        self,
+        *,
+        FlexibleTimeWindow: FlexibleTimeWindowTypeDef,
+        Name: str,
+        ScheduleExpression: str,
+        Target: TargetUnionTypeDef,
+        ActionAfterCompletion: ActionAfterCompletionType = ...,
+        ClientToken: str = ...,
+        Description: str = ...,
+        EndDate: TimestampTypeDef = ...,
+        GroupName: str = ...,
+        KmsKeyArn: str = ...,
+        ScheduleExpressionTimezone: str = ...,
+        StartDate: TimestampTypeDef = ...,
+        State: ScheduleStateType = ...,
+    ) -> CreateScheduleOutputTypeDef:
+        """
+        Creates the specified schedule.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.create_schedule)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#create_schedule)
+        """
+
+    async def create_schedule_group(
+        self, *, Name: str, ClientToken: str = ..., Tags: Sequence[TagTypeDef] = ...
+    ) -> CreateScheduleGroupOutputTypeDef:
+        """
+        Creates the specified schedule group.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.create_schedule_group)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#create_schedule_group)
+        """
+
+    async def delete_schedule(
+        self, *, Name: str, ClientToken: str = ..., GroupName: str = ...
+    ) -> Dict[str, Any]:
+        """
+        Deletes the specified schedule.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.delete_schedule)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#delete_schedule)
+        """
+
+    async def delete_schedule_group(self, *, Name: str, ClientToken: str = ...) -> Dict[str, Any]:
+        """
+        Deletes the specified schedule group.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.delete_schedule_group)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#delete_schedule_group)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.generate_presigned_url)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#generate_presigned_url)
+        """
+
+    async def get_schedule(self, *, Name: str, GroupName: str = ...) -> GetScheduleOutputTypeDef:
+        """
+        Retrieves the specified schedule.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.get_schedule)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#get_schedule)
+        """
+
+    async def get_schedule_group(self, *, Name: str) -> GetScheduleGroupOutputTypeDef:
+        """
+        Retrieves the specified schedule group.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.get_schedule_group)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#get_schedule_group)
+        """
+
+    async def list_schedule_groups(
+        self, *, MaxResults: int = ..., NamePrefix: str = ..., NextToken: str = ...
+    ) -> ListScheduleGroupsOutputTypeDef:
+        """
+        Returns a paginated list of your schedule groups.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.list_schedule_groups)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#list_schedule_groups)
+        """
+
+    async def list_schedules(
+        self,
+        *,
+        GroupName: str = ...,
+        MaxResults: int = ...,
+        NamePrefix: str = ...,
+        NextToken: str = ...,
+        State: ScheduleStateType = ...,
+    ) -> ListSchedulesOutputTypeDef:
+        """
+        Returns a paginated list of your EventBridge Scheduler schedules.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.list_schedules)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#list_schedules)
+        """
+
+    async def list_tags_for_resource(self, *, ResourceArn: str) -> ListTagsForResourceOutputTypeDef:
+        """
+        Lists the tags associated with the Scheduler resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.list_tags_for_resource)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#list_tags_for_resource)
+        """
+
+    async def tag_resource(self, *, ResourceArn: str, Tags: Sequence[TagTypeDef]) -> Dict[str, Any]:
+        """
+        Assigns one or more tags (key-value pairs) to the specified EventBridge
+        Scheduler
+        resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.tag_resource)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#tag_resource)
+        """
+
+    async def untag_resource(self, *, ResourceArn: str, TagKeys: Sequence[str]) -> Dict[str, Any]:
+        """
+        Removes one or more tags from the specified EventBridge Scheduler schedule
+        group.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.untag_resource)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#untag_resource)
+        """
+
+    async def update_schedule(
+        self,
+        *,
+        FlexibleTimeWindow: FlexibleTimeWindowTypeDef,
+        Name: str,
+        ScheduleExpression: str,
+        Target: TargetUnionTypeDef,
+        ActionAfterCompletion: ActionAfterCompletionType = ...,
+        ClientToken: str = ...,
+        Description: str = ...,
+        EndDate: TimestampTypeDef = ...,
+        GroupName: str = ...,
+        KmsKeyArn: str = ...,
+        ScheduleExpressionTimezone: str = ...,
+        StartDate: TimestampTypeDef = ...,
+        State: ScheduleStateType = ...,
+    ) -> UpdateScheduleOutputTypeDef:
+        """
+        Updates the specified schedule.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.update_schedule)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#update_schedule)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_schedule_groups"]
+    ) -> ListScheduleGroupsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.get_paginator)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["list_schedules"]) -> ListSchedulesPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client.get_paginator)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> "EventBridgeSchedulerClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/scheduler.html#EventBridgeScheduler.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_scheduler/client/)
+        """
