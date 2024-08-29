55c5bafefae52aacbac7cb86aa463a658a6cfa7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:26 +0000

    Add package ihunt/0.1.4/ihunt-0.1.4.tar.gz

---
+++ packages/ihunt/ihunt-0.1.4.tar.gz/ihunt-0.1.4/src/ihunt/apis/anubis.py
@@ -0,0 +1,33 @@
+# Docs: https://github.com/jonluca/anubis
+
+import requests
+from threading import Lock
+from ..models import Ihunt
+from ..stdout import echo
+from ..utils import is_empty
+
+BASE_URL = "https://jonlu.ca/anubis/subdomains"
+
+
+# Query: Domain
+# Return: Subdomains
+def req_anubis_domain(ihunt: Ihunt, lock: Lock) -> None:
+    echo("[*] Fetching Anubis...", ihunt.verbose)
+
+    url = BASE_URL + f"/{ihunt.query.value}"
+
+    try:
+        resp = requests.get(url, timeout=ihunt.timeout)
+        if resp.status_code == 200:
+            with lock:
+                subdomains = resp.json()
+                for subdomain in subdomains:
+                    if is_empty(ihunt.data.subdomains):
+                        ihunt.data.subdomains = [subdomain]
+                    else:
+                        if subdomain not in ihunt.data.subdomains:
+                            ihunt.data.subdomains.append(subdomain)
+    except Exception as e:
+        echo(f"[x] Anubis API error: {e}", ihunt.verbose)
+
+    echo("[*] Finished fetching Anubis...", ihunt.verbose)
