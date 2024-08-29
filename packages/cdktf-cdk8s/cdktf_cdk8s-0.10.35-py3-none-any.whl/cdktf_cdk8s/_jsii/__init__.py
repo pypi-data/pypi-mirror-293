01db06c960dfc98c63b00999439e612030928542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:03:17 +0000

    Add package cdktf-cdk8s/0.10.35/cdktf_cdk8s-0.10.35-py3-none-any.whl

---
+++ packages/cdktf-cdk8s/cdktf_cdk8s-0.10.35-py3-none-any.whl/cdktf_cdk8s/_jsii/__init__.py
@@ -0,0 +1,29 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import cdk8s._jsii
+import cdktf._jsii
+import cdktf_cdktf_provider_kubernetes._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdktf-cdk8s", "0.10.35", __name__[0:-6], "cdktf-cdk8s@0.10.35.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
