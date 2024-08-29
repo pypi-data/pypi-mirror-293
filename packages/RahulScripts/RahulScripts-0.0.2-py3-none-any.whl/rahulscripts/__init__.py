4eb020f1549b9b697ba0f454b4e430275e627291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:39 +0000

    Add package RahulScripts/0.0.2/RahulScripts-0.0.2-py3-none-any.whl

---
+++ packages/RahulScripts/RahulScripts-0.0.2-py3-none-any.whl/rahulscripts/__init__.py
@@ -0,0 +1,7 @@
+from .__version__ import __version__
+from .download import *
+from .external import *
+from .utils import *
+
+print(f"Package v: {__version__}")
+print("===============================================")
