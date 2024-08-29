9beca4d5758f4abff438f64f24bf805eab4c7816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:46 +0000

    Add package vinayaga/0.1/vinayaga-0.1.tar.gz

---
+++ packages/vinayaga/vinayaga-0.1.tar.gz/vinayaga-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vinayaga",
+    version="0.1",
+    packages=find_packages(),
+    description="A simple package for multiplication operations",
+    author="arasu",
+    author_email="arasum6262@gmail.com",
+    
+)
