33e2bc6a7ab7b4784d9d838dcf4a09f00876f36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:23 +0000

    Add package vdk-logging-format/0.1.1431637373/vdk_logging_format-0.1.1431637373.tar.gz

---
+++ packages/vdk-logging-format/vdk_logging_format-0.1.1431637373.tar.gz/vdk_logging_format-0.1.1431637373/setup.py
@@ -0,0 +1,34 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+
+__version__ = "0.1.1431637373"
+
+setuptools.setup(
+    name="vdk-logging-format",
+    version=__version__,
+    url="https://github.com/vmware/versatile-data-kit",
+    description="Versatile Data Kit SDK plugin that configures logging output format.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=["vdk-core", "ecs-logging"],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    entry_points={
+        "vdk.plugin.run": [
+            "vdk-logging-format = vdk.plugin.logging_format.logging_format"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
