d6e40e458e4ea488bdbac0c5bf7c147219d206bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:05:08 +0000

    Add package funcnodes-numpy/0.2.3/funcnodes_numpy-0.2.3.tar.gz

---
+++ packages/funcnodes-numpy/funcnodes_numpy-0.2.3.tar.gz/funcnodes_numpy-0.2.3/README.md
@@ -0,0 +1,1 @@
+Numpy implementation for funcnodes
---
+++ packages/funcnodes-numpy/funcnodes_numpy-0.2.3.tar.gz/funcnodes_numpy-0.2.3/funcnodes_numpy/_core/_defchararray.py
@@ -0,0 +1,58 @@
+import numpy
+from typing import Optional, TYPE_CHECKING
+from exposedfunctionality import controlled_wrapper as wraps
+import funcnodes as fn
+
+from .._types import str_array
+
+
+@fn.NodeDecorator(
+    node_id="np.char.array",
+    name="array",
+    outputs=[],
+)
+@wraps(numpy.char.array, wrapper_attribute="__fnwrapped__")
+def chararray(
+    obj: str_array,
+    itemsize: Optional[int] = None,
+    # copy: bool = True,
+    # unicode: Optional[bool] = None,
+    # order: OrderACF = None,
+):  # params ['obj'] ['itemsize'] []
+    res = numpy.char.array(
+        obj,
+        itemsize,
+        # copy=copy,
+        # unicode=unicode,
+        # order=order
+    )
+    return res
+
+
+@fn.NodeDecorator(
+    node_id="np.char.asarray",
+    name="asarray",
+    outputs=[],
+)
+@wraps(numpy.char.asarray, wrapper_attribute="__fnwrapped__")
+def aschararray(
+    obj: str_array,
+    itemsize: Optional[int],
+    # unicode: Optional[bool] = None,
+    # order: OrderCF = None,
+):  # params ['obj'] ['itemsize'] []
+    res = numpy.char.asarray(
+        obj,
+        itemsize,
+        # unicode=unicode,
+        # order=order
+    )
+    return res
+
+
+NODE_SHELF = fn.Shelf(
+    name="char arrays",
+    description="char",
+    nodes=[chararray, aschararray],
+    subshelves=[],
+)
