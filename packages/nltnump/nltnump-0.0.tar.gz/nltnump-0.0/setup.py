d5a3e909e44c18ca1b98fcf2f86f4ecf67b4a430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:46 +0000

    Add package nltnump/0.0/nltnump-0.0.tar.gz

---
+++ packages/nltnump/nltnump-0.0.tar.gz/nltnump-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nltnump',
+      version='0.0',
+      description='nltnump',
+      packages=['nltnump'],
+      author_email='applepro285@gmail.com',
+      zip_safe=False)
