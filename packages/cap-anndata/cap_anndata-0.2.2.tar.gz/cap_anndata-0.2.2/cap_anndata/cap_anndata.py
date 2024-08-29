9d55d6b100506514911a5b82dc0669166746ccb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:03:38 +0000

    Add package cap-anndata/0.2.2/cap_anndata-0.2.2.tar.gz

---
+++ packages/cap-anndata/cap_anndata-0.2.2.tar.gz/cap_anndata-0.2.2/cap_anndata/cap_anndata.py
@@ -0,0 +1,287 @@
+import logging
+import anndata as ad
+import numpy as np
+import h5py
+from typing import List, Union, Dict, Tuple, Final
+from anndata._io.specs import read_elem, write_elem
+
+from cap_anndata import CapAnnDataDF, CapAnnDataUns
+
+
+logger = logging.getLogger(__name__)
+
+X_NOTATION = Union[h5py.Dataset, ad.experimental.CSRDataset, ad.experimental.CSCDataset]
+OBSM_NOTATION = Dict[str, X_NOTATION]
+
+NotLinkedObject: Final = "__NotLinkedObject"
+
+
+class BaseLayerMatrixAndDf:
+    def __init__(self, file: h5py.File, path_to_content: str = "/") -> None:
+        self._file = file
+        self._path_to_content = path_to_content
+        self._X: X_NOTATION = None
+
+    @property
+    def X(self) -> X_NOTATION:
+        if self._X is None:
+            self._link_x()
+        return self._X
+
+    def _link_x(self) -> None:
+        x = self._file[self._path_to_content + "X"]
+        if isinstance(x, h5py.Dataset):
+            # dense X
+            self._X = x
+        else:
+            # sparse dataset
+            self._X = ad.experimental.sparse_dataset(x)
+
+    @property
+    def shape(self) -> Tuple[int, int]:
+        if self.X is not None:
+            shape = tuple(map(int, self.X.shape))
+        else:
+            shape = None
+        return shape
+
+    def _lazy_df_load(self, key: str) -> CapAnnDataDF:
+        df = CapAnnDataDF()
+        attribute = self._path_to_content + key
+        column_order = self._read_attr(self._file[attribute], "column-order")
+        df.column_order = column_order
+        if df.column_order.dtype != object:
+            # empty DataFrame will have column_order as float64
+            # which leads to failure in overwrite method
+            df.column_order = df.column_order.astype(object)
+        return df
+
+    @staticmethod
+    def _read_attr(obj: Union[h5py.Group, h5py.Dataset], attr_name: str) -> any:
+        attrs = dict(obj.attrs)
+        if attr_name not in attrs.keys():
+            raise KeyError(f"The {attr_name} doesn't exist!")
+        return attrs[attr_name]
+
+    def _read_df(self, key: str, columns: List[str]) -> CapAnnDataDF:
+        group_path = self._path_to_content + key
+        if group_path not in self._file.keys():
+            raise ValueError(f"The group {group_path} doesn't exist in the file!")
+
+        h5_group = self._file[group_path]
+
+        column_order = self._read_attr(h5_group, "column-order")
+
+        if columns is None:
+            # read whole df
+            df = CapAnnDataDF.from_df(read_elem(h5_group), column_order=column_order)
+        else:
+            cols_to_read = [c for c in columns if c in column_order]
+            df = CapAnnDataDF()
+            df.column_order = column_order
+            index_col = self._read_attr(h5_group, "_index")
+            df.index = read_elem(h5_group[index_col])
+
+            for col in cols_to_read:
+                df[col] = read_elem(h5_group[col])
+
+        if df.column_order.dtype != object:
+            # empty DataFrame will have column_order as float64
+            # which leads to failure in overwrite method
+            df.column_order = df.column_order.astype(object)
+        return df
+
+    def _write_elem(self, dest_key: str, elem: any, compression: str) -> None:
+        write_elem(self._file, dest_key, elem, dataset_kwargs={"compression": compression})
+
+    def _validate_cap_df(self, cap_df: CapAnnDataDF, axis: int) -> None:
+        if not isinstance(cap_df, CapAnnDataDF):
+            raise TypeError(
+                f"The input should be an instance of CapAnnDataDF class but {type(cap_df)} given!"
+            )
+
+        if axis not in [0, 1]:
+            raise ValueError("The axis should be either 0 or 1!")
+
+        if cap_df.shape[0] != self.shape[axis]:
+            items = "cells" if axis == 0 else "genes"
+            raise ValueError(
+                f"The number of rows in the input DataFrame should be equal to the number of {items} in the "
+                "AnnData object!"
+            )
+
+
+class RawLayer(BaseLayerMatrixAndDf):
+    def __init__(self, h5_file: h5py.File):
+        super().__init__(h5_file, path_to_content="/raw/")
+        self._var: CapAnnDataDF = None
+
+    @property
+    def var(self) -> CapAnnDataDF:
+        if self._var is None:
+            self._var = self._lazy_df_load("var")
+        return self._var
+
+    @var.setter
+    def var(self, cap_df: CapAnnDataDF) -> None:
+        self._validate_cap_df(cap_df, axis=1)
+        self._var = cap_df
+
+    def read_var(self, columns: List[str] = None, reset: bool = False) -> None:
+        df = self._read_df(key="var", columns=columns)
+        if self.var.empty or reset:
+            self._var = df
+        else:
+            for col in df.columns:
+                self._var[col] = df[col]
+
+
+class CapAnnData(BaseLayerMatrixAndDf):
+    def __init__(self, h5_file: h5py.File) -> None:
+        super().__init__(h5_file, path_to_content="/")
+        self._file: h5py.File = h5_file
+        self._obs: CapAnnDataDF = None
+        self._var: CapAnnDataDF = None
+        self._X: X_NOTATION = None
+        self._obsm: OBSM_NOTATION = None
+        self._uns: CapAnnDataUns = None
+        self._raw: RawLayer = None
+        self._shape: Tuple[int, int] = None
+
+    @property
+    def obs(self) -> CapAnnDataDF:
+        if self._obs is None:
+            self._obs = self._lazy_df_load("obs")
+        return self._obs
+
+    @obs.setter
+    def obs(self, cap_df: CapAnnDataDF) -> None:
+        self._validate_cap_df(cap_df, axis=0)
+        self._obs = cap_df
+
+    @property
+    def var(self) -> CapAnnDataDF:
+        if self._var is None:
+            self._var = self._lazy_df_load("var")
+        return self._var
+
+    @var.setter
+    def var(self, cap_df: CapAnnDataDF) -> None:
+        self._validate_cap_df(cap_df, axis=1)
+        self._var = cap_df
+
+    @property
+    def obsm(self) -> OBSM_NOTATION:
+        if self._obsm is None:
+            self._link_obsm()
+        return self._obsm
+
+    @property
+    def raw(self) -> RawLayer:
+        if self._raw is None:
+            if "raw" not in self._file.keys():
+                logger.warning("Can't read raw.var since raw layer doesn't exist!")
+                return
+
+            self._raw = RawLayer(self._file)
+        return self._raw
+
+    @property
+    def uns(self) -> CapAnnDataUns:
+        if self._uns is None:
+            self._uns = CapAnnDataUns(
+                {k: NotLinkedObject for k in self._file["uns"].keys()}
+            )
+        return self._uns
+
+    def read_obs(self, columns: List[str] = None, reset: bool = False) -> None:
+        df = self._read_df("obs", columns=columns)
+        if self.obs.empty or reset:
+            self._obs = df
+        else:
+            for col in df.columns:
+                self._obs[col] = df[col]
+
+    def read_var(self, columns: List[str] = None, reset: bool = False) -> None:
+        df = self._read_df("var", columns=columns)
+        if self.var.empty or reset:
+            self._var = df
+        else:
+            for col in df.columns:
+                self._var[col] = df[col]
+
+    def overwrite(self, fields: List[str] = None, compression: str = "lzf") -> None:
+        field_to_entity = {
+            "obs": self.obs,
+            "var": self.var,
+            "raw.var": self.raw.var if self.raw is not None else None,
+            "uns": self.uns,
+        }
+
+        if fields is None:
+            fields = list(field_to_entity.keys())
+        else:
+            for f in fields:
+                if f not in field_to_entity.keys():
+                    raise KeyError(
+                        f"The field {f} is not supported! The list of supported fields are equal to supported "
+                        f"attributes of the CapAnnData class: obs, var, raw.var and uns."
+                    )
+
+        for key in ["obs", "var", "raw.var"]:
+            if key in fields:
+                entity: CapAnnDataDF = field_to_entity[key]
+                if entity is None:
+                    continue
+
+                key = key.replace(".", "/") if key == "raw.var" else key
+
+                for col in entity.columns:
+                    self._write_elem(f"{key}/{col}", entity[col].values, compression=compression)
+
+                column_order = entity.column_order
+                if (
+                    column_order.size == 0
+                ):  # Refs https://github.com/cellannotation/cap-anndata/issues/6
+                    column_order = np.array([], dtype=np.float64)
+                self._file[key].attrs["column-order"] = column_order
+
+        if "uns" in fields:
+            for key in self.uns.keys():
+                if self.uns[key] is not NotLinkedObject:
+                    dest = f"uns/{key}"
+                    self._write_elem(dest, self.uns[key], compression=compression)
+            for key in self.uns.keys_to_remove:
+                del self._file[f"uns/{key}"]
+
+    def read_uns(self, keys: List[str] = None) -> None:
+        if keys is None:
+            keys = list(self.uns.keys())
+
+        for key in keys:
+            existing_keys = self.uns.keys()
+            if key in existing_keys:
+                source = self._file[f"uns/{key}"]
+                self.uns[key] = read_elem(source)
+
+    def _link_obsm(self) -> None:
+        self._obsm = {}
+        if "obsm" in self._file.keys():
+            obsm_group = self._file["obsm"]
+            for entity_name in obsm_group.keys():
+                entity = obsm_group[entity_name]
+                if isinstance(entity, h5py.Dataset):
+                    # dense array
+                    self._obsm[entity_name] = entity
+                else:
+                    # sparse array
+                    self._obsm[entity_name] = ad.experimental.sparse_dataset(entity)
+
+    def obsm_keys(self) -> List[str]:
+        return list(self.obsm.keys())
+
+    def obs_keys(self) -> List[str]:
+        return self.obs.column_order.tolist()
+
+    def var_keys(self) -> List[str]:
+        return self.var.column_order.tolist()
---
+++ packages/cap-anndata/cap_anndata-0.2.2.tar.gz/cap_anndata-0.2.2/test/test_cap_anndata.py
@@ -0,0 +1,433 @@
+import anndata as ad
+import numpy as np
+import tempfile
+import os
+import pandas as pd
+import pytest
+
+from cap_anndata import CapAnnDataDF
+from test.context import get_base_anndata
+from cap_anndata.reader import read_h5ad
+
+
+def get_filled_anndata(n_rows: int = 10, n_genes: int = 10, sparse=False) -> ad.AnnData:
+    adata = get_base_anndata(n_rows, n_genes, sparse)
+
+    adata.obs["cell_type"] = [f"cell_{i%3}" for i in range(adata.shape[0])]
+    adata.obs["number"] = [i / 10 for i in range(adata.shape[0])]
+    adata.obs.index = [f"obs_{i}" for i in range(adata.shape[0])]
+
+    adata.var.index = [f"gene_{i}" for i in range(adata.shape[1])]
+    adata.var["filtered"] = [i > 4 for i in range(adata.shape[1])]
+    adata.var["gene_names"] = [f"gene_name_{i}" for i in range(adata.shape[1])]
+    adata.var["dispersion"] = [i / 100 for i in range(adata.shape[1])]
+
+    adata.raw = adata
+    return adata
+
+
+def save_filled_anndata(
+    file_name: str, n_rows: int = 10, n_genes: int = 10, sparse=False
+) -> str:
+    adata = get_filled_anndata(n_rows, n_genes, sparse)
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, file_name)
+    adata.write_h5ad(file_path)
+    return file_path
+
+
+def test_read_shape():
+    n_rows = 10
+    n_genes = 20
+    adata = get_base_anndata(n_rows, n_genes)
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_read_shape.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path) as cap_adata:
+        shape = cap_adata.shape
+
+    os.remove(file_path)
+
+    assert shape == (n_rows, n_genes), "Shape axis size is incorrect!"
+
+    for i in [0, 1]:
+        assert type(shape[i]) == int, "Shape axis type is wrong!"
+
+
+def test_read_df():
+    adata = get_filled_anndata()
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_read_obs.h5ad")
+
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path) as cap_adata:
+        cap_adata.read_obs()
+        cap_adata.read_var()
+        cap_adata.raw.read_var()
+
+    os.remove(file_path)
+    pd.testing.assert_frame_equal(adata.obs, cap_adata.obs, check_frame_type=False)
+    pd.testing.assert_frame_equal(adata.var, cap_adata.var, check_frame_type=False)
+    pd.testing.assert_frame_equal(
+        adata.raw.var, cap_adata.raw.var, check_frame_type=False
+    )
+
+
+def test_partial_read():
+    adata = get_filled_anndata()
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_partial_read.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path) as cap_adata:
+        cap_adata.read_obs(columns=["cell_type"])
+        cap_adata.read_obs(columns=["cell_type"])
+        cap_adata.read_var(columns=["dispersion"])
+        cap_adata.raw.read_var(columns=["dispersion"])
+
+    os.remove(file_path)
+
+    assert len(adata.obs.columns) == len(cap_adata.obs.column_order)
+    assert len(adata.var.columns) == len(cap_adata.var.column_order)
+    assert len(adata.raw.var.columns) == len(cap_adata.raw.var.column_order)
+
+    assert len(cap_adata.obs.columns) == 1
+    assert len(cap_adata.var.columns) == 1
+    assert len(cap_adata.raw.var.columns) == 1
+
+    pd.testing.assert_index_equal(adata.obs.index, cap_adata.obs.index)
+    pd.testing.assert_index_equal(adata.var.index, cap_adata.var.index)
+    pd.testing.assert_index_equal(adata.raw.var.index, cap_adata.raw.var.index)
+
+
+@pytest.mark.parametrize("compression", ["gzip", "lzf"])
+def test_overwrite_df(compression):
+    adata = get_filled_anndata()
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_overwrite_df.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path, edit=True) as cap_adata:
+        cap_adata.read_obs(columns=["cell_type"])
+        cap_adata.obs["cell_type"] = [
+            f"new_cell_type_{i%2}" for i in range(cap_adata.shape[0])
+        ]
+        cap_adata.obs["const_str"] = "some string"
+        ref_obs = cap_adata.obs.copy()
+
+        # Modify 'var'
+        cap_adata.read_var()
+        cap_adata.var["gene_names"] = [
+            f"new_gene_{i}" for i in range(cap_adata.shape[1])
+        ]
+        cap_adata.var["extra_info"] = np.random.rand(cap_adata.shape[1])
+        ref_var = cap_adata.var.copy()
+
+        # Modify 'raw.var', assuming 'raw' is also a CapAnnData
+        cap_adata.raw.read_var()
+        cap_adata.raw.var["gene_names"] = [
+            f"raw_new_gene_{i}" for i in range(cap_adata.raw.shape[1])
+        ]
+        cap_adata.raw.var["extra_info"] = np.random.rand(cap_adata.shape[1])
+        ref_raw_var = cap_adata.raw.var.copy()
+
+        cap_adata.overwrite(["obs", "var", "raw.var"], compression=compression)
+
+    adata = ad.read_h5ad(file_path)
+    os.remove(file_path)
+
+    # Assert changes in 'obs'
+    assert all([c in adata.obs.columns for c in ref_obs.columns])
+    pd.testing.assert_frame_equal(
+        ref_obs, adata.obs[ref_obs.columns.to_list()], check_frame_type=False
+    )
+
+    # Assert changes in 'var'
+    assert all([c in adata.var.columns for c in ref_var.columns])
+    pd.testing.assert_frame_equal(
+        ref_var, adata.var[ref_var.columns.to_list()], check_frame_type=False
+    )
+
+    # Assert changes in 'raw.var'
+    assert all([c in adata.raw.var.columns for c in ref_raw_var.columns])
+    pd.testing.assert_frame_equal(
+        ref_raw_var,
+        adata.raw.var[ref_raw_var.columns.to_list()],
+        check_frame_type=False,
+    )
+
+
+@pytest.mark.parametrize("sparse", [False, True])
+@pytest.mark.parametrize("vertical_slice", [None, False, True, "mask"])
+def test_link_x(sparse, vertical_slice):
+    adata = get_filled_anndata(sparse=sparse)
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_link_x.h5ad")
+    adata.write_h5ad(file_path)
+
+    if vertical_slice is None:
+        s_ = np.s_[:]
+    elif vertical_slice == "mask":
+        mask = np.array([i < 5 for i in range(adata.shape[0])])
+        s_ = np.s_[mask, :5]
+    else:
+        # slice over var or obs
+        s_ = np.s_[:, 0:5] if vertical_slice else np.s_[0:5, :]
+
+    with read_h5ad(file_path) as cap_adata:
+        x = cap_adata.X[s_]
+        raw_x = cap_adata.raw.X[s_]
+
+    os.remove(file_path)
+    if sparse:
+        assert np.allclose(adata.X.A[s_], x.A)
+        assert np.allclose(adata.raw.X.A[s_], raw_x.A)
+    else:
+        assert np.allclose(adata.X[s_], x)
+        assert np.allclose(adata.raw.X[s_], raw_x)
+
+
+@pytest.mark.parametrize("sparse", [False, True])
+def test_shape(sparse):
+    n_rows = 15
+    n_genes = 25
+
+    adata = get_filled_anndata(n_rows, n_genes, sparse)
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_shape.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path) as cap_adata:
+        shape = cap_adata.shape
+        shape_raw = cap_adata.raw.shape
+
+    os.remove(file_path)
+    for sh in [shape, shape_raw]:
+        assert sh == (n_rows, n_genes)
+
+
+def test_read_obsm():
+    adata = get_filled_anndata()
+    obsm_names = [f"X_test{i}" for i in range(2)]
+
+    for emb in obsm_names:
+        adata.obsm[emb] = np.random.random(size=(adata.shape[0], 2))
+
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_read_obsm.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path) as cap_adata:
+        for emb in obsm_names:
+            assert emb in cap_adata.obsm_keys()
+            assert cap_adata.obsm[emb].shape == adata.obsm[emb].shape
+
+        x_1 = cap_adata.obsm[obsm_names[0]][:]
+        x_2 = cap_adata.obsm[obsm_names[1]][:]
+
+    os.remove(file_path)
+    assert np.allclose(adata.obsm[obsm_names[0]], x_1)
+    assert np.allclose(adata.obsm[obsm_names[1]], x_2)
+
+
+def test_read_uns():
+    adata = get_base_anndata()
+    key1, key2 = "key1", "key2"
+    keys = (key1, key2)
+
+    adata.uns = {k: {k: k} for k in keys}
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_read_uns.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path) as cap_adata:
+        for k in keys:
+            assert k in cap_adata.uns
+
+        cap_adata.read_uns(keys=[key1])
+
+        assert cap_adata.uns[key1] == adata.uns[key1]  # connected
+        assert cap_adata.uns[key2] != adata.uns[key2]  # not connected
+
+    os.remove(file_path)
+
+
+@pytest.mark.parametrize("compression", ["gzip", "lzf"])
+def test_modify_uns(compression):
+    adata = get_base_anndata()
+    adata.uns = {
+        "field_to_ignore": list(range(100)),
+        "field_to_rename": "value",
+        "field_to_expand": {"key1": {}},
+        "field_to_modify": {"a": "b"},
+    }
+    new_name = "renamed_field"
+    d_to_exp = {"sub_key1": "v1", "sub_key2": "v2"}
+    v_to_mod = "value"
+
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_modify_uns.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path, edit=True) as cap_adata:
+        cap_adata.read_uns(
+            keys=["field_to_rename", "field_to_expand", "field_to_modify"]
+        )
+
+        cap_adata.uns[new_name] = cap_adata.uns.pop("field_to_rename")
+        cap_adata.uns["field_to_expand"]["key1"] = d_to_exp
+        cap_adata.uns["field_to_modify"] = v_to_mod
+
+        cap_adata.overwrite(["uns"], compression=compression)
+
+    adata = ad.read_h5ad(file_path)
+
+    assert adata.uns is not None
+    assert len(adata.uns.keys()) == 4
+    assert new_name in adata.uns.keys()
+    assert adata.uns["field_to_expand"]["key1"] == d_to_exp
+    assert adata.uns["field_to_modify"] == v_to_mod
+
+
+def test_empty_obs_override():
+    """
+    especially for solving the issue:
+    https://github.com/cellannotation/cap-anndata/pull/5
+    """
+    adata = get_base_anndata()
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_modify_uns.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path, edit=True) as cap_adata:
+        cap_adata.read_obs()
+
+        cap_adata.obs["cell_type_1"] = pd.Series(
+            data=np.nan, index=cap_adata.obs.index, dtype="category"
+        )
+        cap_adata.obs["cell_type_new"] = pd.Series(
+            data=np.nan, index=cap_adata.obs.index, dtype="category"
+        )
+        cap_adata.overwrite(fields=["obs"])
+
+
+def test_obs_var_keys():
+    adata = get_filled_anndata()
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_obs_keys.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path) as cap_adata:
+        obs_keys_before_read = cap_adata.obs_keys()
+        var_keys_before_read = cap_adata.var_keys()
+        cap_adata.read_obs()
+        cap_adata.read_var()
+        obs_keys_after_read = cap_adata.obs_keys()
+        var_keys_after_read = cap_adata.var_keys()
+
+    assert obs_keys_before_read == adata.obs_keys()
+    assert obs_keys_after_read == adata.obs_keys()
+    assert var_keys_before_read == adata.var_keys()
+    assert var_keys_after_read == adata.var_keys()
+    os.remove(file_path)
+
+
+def test_reset_read():
+    adata = get_filled_anndata()
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_flush_read.h5ad")
+    adata.write_h5ad(file_path)
+
+    with read_h5ad(file_path) as cap_adata:
+        cap_adata.read_obs(columns=["cell_type"])
+        cap_adata.obs["cell_type"] = list(range(cap_adata.shape[0]))
+
+        cap_adata.read_obs(columns=["number"])
+        assert cap_adata.obs.columns.size == 2
+        assert all(cap_adata.obs.cell_type.values == list(range(cap_adata.shape[0])))
+
+        cap_adata.read_obs(columns=["cell_type"])
+        pd.testing.assert_series_equal(cap_adata.obs.cell_type, adata.obs.cell_type)
+
+        cap_adata.read_obs(columns=["number"], reset=True)
+        assert cap_adata.obs.columns.size == 1
+    os.remove(file_path)
+
+
+def test_obs_last_column_removal():
+    col_name = "cell_type"
+    adata = ad.AnnData(X=np.ones(shape=(10, 10), dtype=np.float32))
+    adata.obs[col_name] = col_name
+
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_obs_last_column_removal.h5ad")
+    adata.write(filename=file_path)
+
+    # Remove last column
+    with read_h5ad(file_path, edit=True) as cap_adata:
+        cap_adata.read_obs()
+        cap_adata.obs.remove_column(col_name=col_name)
+        cap_adata.overwrite(["obs"])
+
+    # Check no any issues on read with updated file
+    with read_h5ad(file_path) as cap_adata:
+        cap_adata.read_obs()
+        assert col_name not in cap_adata.obs.columns
+
+    # Check compatability with anndata
+    adata = ad.read_h5ad(file_path)
+    os.remove(file_path)
+
+
+@pytest.mark.parametrize("field", ["obs", "var", "raw.var"])
+def test_df_setter(field):
+    def set_field(field, new_df, cap_adata):
+        if field == "obs":
+            cap_adata.obs = new_df
+        elif field == "var":
+            cap_adata.var = new_df
+        else:
+            cap_adata.raw.var = new_df
+
+    n_rows, n_genes = 10, 5
+    adata = get_filled_anndata(n_rows=n_rows, n_genes=n_genes)
+    temp_folder = tempfile.mkdtemp()
+    file_path = os.path.join(temp_folder, "test_obs_setter.h5ad")
+    adata.write(filename=file_path)
+
+    new_df = pd.DataFrame(
+        index=range(n_rows if field == "obs" else n_genes), columns=["new_column"]
+    )
+
+    with read_h5ad(file_path, edit=True) as cap_adata:
+        cap_adata.read_obs()
+        cap_adata.read_var()
+        cap_adata.raw.read_var()
+
+        # test bad format
+        try:
+            set_field(field, new_df, cap_adata)
+        except TypeError:
+            pass
+        except Exception as e:
+            assert False, f"Unexpected exception: {e}"
+        else:
+            assert False, "Expected TypeError"
+
+        # test good format
+        new_df = CapAnnDataDF.from_df(new_df)
+        set_field(field, new_df, cap_adata)
+
+        # test bad shape
+        new_df = CapAnnDataDF.from_df(new_df[: new_df.shape[0] // 2])
+
+        try:
+            set_field(field, new_df, cap_adata)
+        except ValueError:
+            pass
+        except Exception as e:
+            assert False, f"Unexpected exception: {e}"
+        else:
+            assert False, "Expected ValueError"
