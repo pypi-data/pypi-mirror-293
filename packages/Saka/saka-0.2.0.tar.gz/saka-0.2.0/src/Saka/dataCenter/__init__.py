0aa9ee5efaefb8dcb47cbe8a91d5a5f6b62b3310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:47 +0000

    Add package Saka/0.2.0/saka-0.2.0.tar.gz

---
+++ packages/Saka/saka-0.2.0.tar.gz/saka-0.2.0/src/Saka/dataCenter/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：Plome_api 
+@File    ：__init__.py.py
+@Author  ：zhenxi_zhang@cx
+@Date    ：2024/8/19 下午2:53 
+@explain : 文件说明
+"""
+from WindPy import w
+from .cerebrum import DataCerebrum
+import logging
+
+# 屏蔽InfluxDB写入时候产生的timeout
+logger = logging.getLogger("Rx")
+logger.setLevel(logging.WARNING)
+logger.propagate = False
+
+
+if not w.isconnected():
+    w.start()
+
+__all__ = ["DataCerebrum"]
