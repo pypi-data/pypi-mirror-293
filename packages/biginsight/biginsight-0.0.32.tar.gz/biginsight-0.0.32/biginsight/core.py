9256e48c15c17eb6c9251b091c98c78d7bb0860e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:29 +0000

    Add package biginsight/0.0.32/biginsight-0.0.32.tar.gz

---
+++ packages/biginsight/biginsight-0.0.32.tar.gz/biginsight-0.0.32/biginsight/core.py
@@ -0,0 +1,45 @@
+import requests, json
+
+prod = True
+routeUrl = "https://api.biginsight.ca/api" if prod == True else "http://10.0.0.60:5002/api"
+
+def fetch(url, info):
+  resp = requests.post(
+    url,
+    json=info["body"],
+    headers=info["headers"]
+  )
+
+  return resp
+
+class BigInsight:
+  def __init__(self, key):
+    self.key = key
+  
+  def track(self, info):
+    # page visits, user actions
+    userInfo = info["userInfo"] # email or user id or any other id to identify a specific user
+    page = info["page"] # optional
+    action = info["action"]
+    body = { "userInfo": userInfo, "page": page, "action": action }
+    
+    if self.key == "":
+      return "Project Token is missing"
+    
+    if "log_error" in info:
+      body["log_error"] = True
+
+    resp = fetch(routeUrl + "/track", {
+      "headers": { "Content-Type": "application/json" },
+      "body": { 
+        "key": self.key,
+        **body
+      }
+    })
+
+    if resp.status_code == 200:
+      return resp.json()
+    else:
+      return resp.json(), 400
+  
+biginsight = BigInsight(__name__)
