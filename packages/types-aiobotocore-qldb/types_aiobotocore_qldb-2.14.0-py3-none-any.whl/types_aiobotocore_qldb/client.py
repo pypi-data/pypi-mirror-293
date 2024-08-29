72783a2c8fba151c014bcd774a40cf4debab473b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:02 +0000

    Add package types-aiobotocore-qldb/2.14.0/types_aiobotocore_qldb-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-qldb/types_aiobotocore_qldb-2.14.0-py3-none-any.whl/types_aiobotocore_qldb/client.py
@@ -0,0 +1,358 @@
+"""
+Type annotations for qldb service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_qldb.client import QLDBClient
+
+    session = get_session()
+    async with session.create_client("qldb") as client:
+        client: QLDBClient
+    ```
+"""
+
+from typing import Any, Dict, Mapping, Sequence, Type
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .literals import OutputFormatType, PermissionsModeType
+from .type_defs import (
+    CancelJournalKinesisStreamResponseTypeDef,
+    CreateLedgerResponseTypeDef,
+    DescribeJournalKinesisStreamResponseTypeDef,
+    DescribeJournalS3ExportResponseTypeDef,
+    DescribeLedgerResponseTypeDef,
+    EmptyResponseMetadataTypeDef,
+    ExportJournalToS3ResponseTypeDef,
+    GetBlockResponseTypeDef,
+    GetDigestResponseTypeDef,
+    GetRevisionResponseTypeDef,
+    KinesisConfigurationTypeDef,
+    ListJournalKinesisStreamsForLedgerResponseTypeDef,
+    ListJournalS3ExportsForLedgerResponseTypeDef,
+    ListJournalS3ExportsResponseTypeDef,
+    ListLedgersResponseTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    S3ExportConfigurationTypeDef,
+    StreamJournalToKinesisResponseTypeDef,
+    TimestampTypeDef,
+    UpdateLedgerPermissionsModeResponseTypeDef,
+    UpdateLedgerResponseTypeDef,
+    ValueHolderTypeDef,
+)
+
+__all__ = ("QLDBClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    ClientError: Type[BotocoreClientError]
+    InvalidParameterException: Type[BotocoreClientError]
+    LimitExceededException: Type[BotocoreClientError]
+    ResourceAlreadyExistsException: Type[BotocoreClientError]
+    ResourceInUseException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ResourcePreconditionNotMetException: Type[BotocoreClientError]
+
+
+class QLDBClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        QLDBClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.exceptions)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.can_paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#can_paginate)
+        """
+
+    async def cancel_journal_kinesis_stream(
+        self, *, LedgerName: str, StreamId: str
+    ) -> CancelJournalKinesisStreamResponseTypeDef:
+        """
+        Ends a given Amazon QLDB journal stream.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.cancel_journal_kinesis_stream)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#cancel_journal_kinesis_stream)
+        """
+
+    async def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.close)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#close)
+        """
+
+    async def create_ledger(
+        self,
+        *,
+        Name: str,
+        PermissionsMode: PermissionsModeType,
+        Tags: Mapping[str, str] = ...,
+        DeletionProtection: bool = ...,
+        KmsKey: str = ...,
+    ) -> CreateLedgerResponseTypeDef:
+        """
+        Creates a new ledger in your Amazon Web Services account in the current Region.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.create_ledger)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#create_ledger)
+        """
+
+    async def delete_ledger(self, *, Name: str) -> EmptyResponseMetadataTypeDef:
+        """
+        Deletes a ledger and all of its contents.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.delete_ledger)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#delete_ledger)
+        """
+
+    async def describe_journal_kinesis_stream(
+        self, *, LedgerName: str, StreamId: str
+    ) -> DescribeJournalKinesisStreamResponseTypeDef:
+        """
+        Returns detailed information about a given Amazon QLDB journal stream.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.describe_journal_kinesis_stream)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#describe_journal_kinesis_stream)
+        """
+
+    async def describe_journal_s3_export(
+        self, *, Name: str, ExportId: str
+    ) -> DescribeJournalS3ExportResponseTypeDef:
+        """
+        Returns information about a journal export job, including the ledger name,
+        export ID, creation time, current status, and the parameters of the original
+        export creation
+        request.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.describe_journal_s3_export)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#describe_journal_s3_export)
+        """
+
+    async def describe_ledger(self, *, Name: str) -> DescribeLedgerResponseTypeDef:
+        """
+        Returns information about a ledger, including its state, permissions mode,
+        encryption at rest settings, and when it was
+        created.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.describe_ledger)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#describe_ledger)
+        """
+
+    async def export_journal_to_s3(
+        self,
+        *,
+        Name: str,
+        InclusiveStartTime: TimestampTypeDef,
+        ExclusiveEndTime: TimestampTypeDef,
+        S3ExportConfiguration: S3ExportConfigurationTypeDef,
+        RoleArn: str,
+        OutputFormat: OutputFormatType = ...,
+    ) -> ExportJournalToS3ResponseTypeDef:
+        """
+        Exports journal contents within a date and time range from a ledger into a
+        specified Amazon Simple Storage Service (Amazon S3)
+        bucket.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.export_journal_to_s3)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#export_journal_to_s3)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.generate_presigned_url)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#generate_presigned_url)
+        """
+
+    async def get_block(
+        self,
+        *,
+        Name: str,
+        BlockAddress: ValueHolderTypeDef,
+        DigestTipAddress: ValueHolderTypeDef = ...,
+    ) -> GetBlockResponseTypeDef:
+        """
+        Returns a block object at a specified address in a journal.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.get_block)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#get_block)
+        """
+
+    async def get_digest(self, *, Name: str) -> GetDigestResponseTypeDef:
+        """
+        Returns the digest of a ledger at the latest committed block in the journal.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.get_digest)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#get_digest)
+        """
+
+    async def get_revision(
+        self,
+        *,
+        Name: str,
+        BlockAddress: ValueHolderTypeDef,
+        DocumentId: str,
+        DigestTipAddress: ValueHolderTypeDef = ...,
+    ) -> GetRevisionResponseTypeDef:
+        """
+        Returns a revision data object for a specified document ID and block address.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.get_revision)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#get_revision)
+        """
+
+    async def list_journal_kinesis_streams_for_ledger(
+        self, *, LedgerName: str, MaxResults: int = ..., NextToken: str = ...
+    ) -> ListJournalKinesisStreamsForLedgerResponseTypeDef:
+        """
+        Returns all Amazon QLDB journal streams for a given ledger.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.list_journal_kinesis_streams_for_ledger)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#list_journal_kinesis_streams_for_ledger)
+        """
+
+    async def list_journal_s3_exports(
+        self, *, MaxResults: int = ..., NextToken: str = ...
+    ) -> ListJournalS3ExportsResponseTypeDef:
+        """
+        Returns all journal export jobs for all ledgers that are associated with the
+        current Amazon Web Services account and
+        Region.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.list_journal_s3_exports)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#list_journal_s3_exports)
+        """
+
+    async def list_journal_s3_exports_for_ledger(
+        self, *, Name: str, MaxResults: int = ..., NextToken: str = ...
+    ) -> ListJournalS3ExportsForLedgerResponseTypeDef:
+        """
+        Returns all journal export jobs for a specified ledger.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.list_journal_s3_exports_for_ledger)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#list_journal_s3_exports_for_ledger)
+        """
+
+    async def list_ledgers(
+        self, *, MaxResults: int = ..., NextToken: str = ...
+    ) -> ListLedgersResponseTypeDef:
+        """
+        Returns all ledgers that are associated with the current Amazon Web Services
+        account and
+        Region.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.list_ledgers)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#list_ledgers)
+        """
+
+    async def list_tags_for_resource(
+        self, *, ResourceArn: str
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        Returns all tags for a specified Amazon QLDB resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.list_tags_for_resource)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#list_tags_for_resource)
+        """
+
+    async def stream_journal_to_kinesis(
+        self,
+        *,
+        LedgerName: str,
+        RoleArn: str,
+        InclusiveStartTime: TimestampTypeDef,
+        KinesisConfiguration: KinesisConfigurationTypeDef,
+        StreamName: str,
+        Tags: Mapping[str, str] = ...,
+        ExclusiveEndTime: TimestampTypeDef = ...,
+    ) -> StreamJournalToKinesisResponseTypeDef:
+        """
+        Creates a journal stream for a given Amazon QLDB ledger.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.stream_journal_to_kinesis)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#stream_journal_to_kinesis)
+        """
+
+    async def tag_resource(self, *, ResourceArn: str, Tags: Mapping[str, str]) -> Dict[str, Any]:
+        """
+        Adds one or more tags to a specified Amazon QLDB resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.tag_resource)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#tag_resource)
+        """
+
+    async def untag_resource(self, *, ResourceArn: str, TagKeys: Sequence[str]) -> Dict[str, Any]:
+        """
+        Removes one or more tags from a specified Amazon QLDB resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.untag_resource)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#untag_resource)
+        """
+
+    async def update_ledger(
+        self, *, Name: str, DeletionProtection: bool = ..., KmsKey: str = ...
+    ) -> UpdateLedgerResponseTypeDef:
+        """
+        Updates properties on a ledger.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.update_ledger)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#update_ledger)
+        """
+
+    async def update_ledger_permissions_mode(
+        self, *, Name: str, PermissionsMode: PermissionsModeType
+    ) -> UpdateLedgerPermissionsModeResponseTypeDef:
+        """
+        Updates the permissions mode of a ledger.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client.update_ledger_permissions_mode)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/#update_ledger_permissions_mode)
+        """
+
+    async def __aenter__(self) -> "QLDBClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/qldb.html#QLDB.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_qldb/client/)
+        """
