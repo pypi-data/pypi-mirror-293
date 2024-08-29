82dd99d608752e2c84389b01e97c1827a99840bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:04:05 +0000

    Add package m3u8-analyzer/1.0.3.6/m3u8_analyzer-1.0.3.6.tar.gz

---
+++ packages/m3u8-analyzer/m3u8_analyzer-1.0.3.6.tar.gz/m3u8_analyzer-1.0.3.6/CHANGELOG.md
@@ -0,0 +1,17 @@
+![Versão](https://img.shields.io/badge/version-1.0.3.5-orange)
+
+-
+---
+![Correção](https://img.shields.io/badge/status-correção-brightgreen)
+
+- [x] image-ffmpeg erros de configuração
+---
+### ![Bugs](https://img.shields.io/badge/status-bugs-red)
+- Para reportar bugs, [clique aqui](https://github.com/PauloCesar-dev404/M3u8_Analyzer/issues).
+---
+### ![Melhorias](https://img.shields.io/badge/status-melhorias-yellow)
+- 
+---
+![Novas Funcionalidades](https://img.shields.io/badge/status-novas_funcionalidades-blue)
+
+- 
---
+++ packages/m3u8-analyzer/m3u8_analyzer-1.0.3.6.tar.gz/m3u8_analyzer-1.0.3.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Lê o conteúdo do README.md
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+# Lê o conteúdo do CHANGELOG.md
+with open('CHANGELOG.md', 'r', encoding='utf-8') as file:
+    changelog = file.read()
+
+# Concatena o CHANGELOG.md ao README.md
+long_description += "\n\n# CHANGELOG\n\n" + changelog
+
+setup(
+    name="m3u8-analyzer",
+    version="1.0.3.6",
+    description="Análise de dados de HLS m3u8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="PauloCesar0073-dev404",
+    author_email="paulocesar0073dev404@gmail.com",
+    url='https://paulocesar-dev404.github.io/M3u8_Analyzer/',
+    license="MIT",
+    keywords=["hls", "m3u8", "m3u8_analyzer", "M3u8Analyzer"],
+    packages=find_packages(),
+    install_requires=['cryptography', 'requests', 'python-dotenv', 'colorama'],
+    include_package_data=True
+)
