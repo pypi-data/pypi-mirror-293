25112e9dbbf9f39969b2d66d8e0e9788a9be6925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:05:00 +0000

    Add package log-abstractor/1.1.0/log_abstractor-1.1.0.tar.gz

---
+++ packages/log-abstractor/log_abstractor-1.1.0.tar.gz/log_abstractor-1.1.0/abstract_logger/__init__.py
@@ -0,0 +1,3 @@
+# logging_abstraction/__init__.py
+from .logger import Logger
+#from .config_loader import LogConfig
---
+++ packages/log-abstractor/log_abstractor-1.1.0.tar.gz/log_abstractor-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="log_abstractor",
+    version="1.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "structlog>=24.4.0", "anonypy>=0.1.7",
+    ],
+    description="A configurable logging abstraction using structlog.",
+    author="Dipanjan Mazumder",
+    author_email="javahub@yahoo.com",
+    url="https://github.com/SMART2016/logger",  # Optional: Link to your project repository
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Specify Python version compatibility
+)
