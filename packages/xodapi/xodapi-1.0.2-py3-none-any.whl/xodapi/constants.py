6716be9f3b745b4a73f1664a7f9d3fa0ebd6fa19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 05:03:54 +0000

    Add package xodapi/1.0.2/xodapi-1.0.2-py3-none-any.whl

---
+++ packages/xodapi/xodapi-1.0.2-py3-none-any.whl/xodapi/constants.py
@@ -0,0 +1,8 @@
+# URLS
+XODA_LOGIN_URL = "https://app.xoda.com/auth/login"
+XODA_POS_URL = "https://app.xoda.com/app/pos"
+XODA_MEMBER_DASHBOARD_URL = "https://app.xoda.com/app/facility/member-details"
+
+
+XODA_API_LOGIN_URL = "https://api.xoda.com/api/auth/login"
+XODA_API_BASE_URL = "https://api.xoda.com"
