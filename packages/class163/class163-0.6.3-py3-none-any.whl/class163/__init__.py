f0d66c24af24e7c3d8b6030b2a13d252db332492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:03:14 +0000

    Add package class163/0.6.3/class163-0.6.3-py3-none-any.whl

---
+++ packages/class163/class163-0.6.3-py3-none-any.whl/class163/__init__.py
@@ -0,0 +1,10 @@
+"""
+class163/__init__.py
+Version: 0.3.9
+Author: CooooldWind_
+E-Mail: 3091868003@qq.com
+Copyright @CooooldWind_ / Following GNU_AGPLV3+ License
+"""
+
+from class163.playlist_old import Playlist
+from class163.music import Music
