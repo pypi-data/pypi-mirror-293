86b3b8ee42a090f0ba71a19475965843f0d46ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:03:45 +0000

    Add package skibiditoilet230/0.0/skibiditoilet230-0.0.tar.gz

---
+++ packages/skibiditoilet230/skibiditoilet230-0.0.tar.gz/skibiditoilet230-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='skibiditoilet230',
+      version='0.0',
+      description='skibiditoilet230',
+      packages=['skibiditoilet230'],
+      author_email='applepro285@gmail.com',
+      zip_safe=False)
