54607d93ad6f0cad925902d5d6dbd83c8c780873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:00 +0000

    Add package Indox/0.1.26/indox-0.1.26.tar.gz

---
+++ packages/Indox/indox-0.1.26.tar.gz/indox-0.1.26/indox/llms/__init__.py
@@ -0,0 +1,7 @@
+from .Mistral import Mistral
+from .OpenAi import OpenAi
+# from .Dspy_Cot import DspyCot
+from .IndoxApi import IndoxApi
+from .HuggingFace import HuggingFaceModel
+from .GoogleAi import GoogleAi
+from .Ollama import Ollama
