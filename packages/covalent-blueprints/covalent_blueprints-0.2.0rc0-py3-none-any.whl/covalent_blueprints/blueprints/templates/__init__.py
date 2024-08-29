4ee9fd8258a3ce91a134aef17a3a52c083c3f70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 05:03:13 +0000

    Add package covalent-blueprints/0.2.0rc0/covalent_blueprints-0.2.0rc0-py3-none-any.whl

---
+++ packages/covalent-blueprints/covalent_blueprints-0.2.0rc0-py3-none-any.whl/covalent_blueprints/blueprints/templates/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2024 Agnostiq Inc.
+"""A collection of reusable blueprint types for creating installed blueprints."""
+
+from .single_service_deployment import SingleServiceBlueprint
+from .single_service_workflow import ServiceWorkflowBlueprint
