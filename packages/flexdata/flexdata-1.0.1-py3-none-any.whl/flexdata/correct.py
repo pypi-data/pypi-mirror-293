de775fb39ef1e72466e179ebfe422f81a853204d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:02:53 +0000

    Add package flexdata/1.0.1/flexdata-1.0.1-py3-none-any.whl

---
+++ packages/flexdata/flexdata-1.0.1-py3-none-any.whl/flexdata/correct.py
@@ -0,0 +1,195 @@
+import logging
+from copy import deepcopy
+
+
+###############################################################################
+#                             Correction profiles                             #
+###############################################################################
+#
+# Once a profile has been added here, it should never be changed to ensure
+# reproducibility of existing scripts using the profile.
+# If an existing profile needs retroactive corrections, add a new profile
+# with a -v2 (-v3, ...) suffix.
+# If the physical setup changed, create a new profile with an updated date.
+
+profiles = {
+    'cwi-flexray-2024-07-25': {
+        'description': """
+Correction profile deduced from a calibration experiment with metal spheres followed by contrast-based fine tuning.
+""",
+        'det_tan': 24.0042,
+        'src_ort': -6.2879,
+        'axs_tan': -0.5143
+    },
+    'cwi-flexray-2023-08-21': {
+        'description': """
+Correction profile deduced from Acquila hc/vc/cor settings after the exchange of the detector motor.
+""",
+        'det_tan': 24.1215,
+        'src_ort': -6.2917,
+        'axs_tan': -0.5405
+    },
+    'cwi-flexray-2022-10-28': {
+        'description': """
+Correction profile deduced from markers after the October 2022 maintenance.
+""",
+        'det_tan': 24.0485,
+        'src_ort': -5.7730,
+        'axs_tan': -0.5010
+    },
+    'cwi-flexray-2022-05-31': {
+        'description': """
+correction profile deduced from acquila hc/vc/cor settings after the 31 may 2022 re-calibration. Includes det_roll determined by Robert using markers.
+""",
+        'det_tan': 24.4203,
+        'src_ort': -6.2281,
+        'axs_tan': -0.5010,
+        'det_roll': -0.262,
+    },
+    'cwi-flexray-2022-05-31-norotation': {
+        'description': """
+correction profile deduced from acquila hc/vc/cor settings after the 31 may 2022 re-calibration.
+""",
+        'det_tan': 24.4203,
+        'src_ort': -6.2281,
+        'axs_tan': -0.5010,
+    },
+    'cwi-flexray-2020-03-26': {
+        'description': """
+Correction profile deduced from Acquila HC/VC/COR settings after the March 2020 maintenance. Includes empirically determined det_roll.
+""",
+        'det_tan': 24.300,
+        'src_ort': -6.086,
+        'axs_tan': -0.524,
+        'det_roll': -0.175,
+    },
+    'cwi-flexray-2020-03-26-norotation': {
+        'description': """
+Correction profile deduced from Acquila HC/VC/COR settings after the March 2020 maintenance.
+""",
+        'det_tan': 24.300,
+        'src_ort': -6.086,
+        'axs_tan': -0.524,
+    },
+    'cwi-flexray-2019-04-24': {
+        'description': """
+This profile was last updated by Alex Kostenko on 24 April 2019.
+
+It was concurrently updated with the documentation and some other changes in the flexdata codebase.
+See:
+https://github.com/cicwi/flexDATA/commit/8859bc8073880efcb32cc57b152ef23746993ec1#diff-08f83b989c80a05906f380a66964d8d3L393
+""",
+        'det_tan': 24,
+        'src_ort': -7,
+        'axs_tan': -0.5,
+    }
+}
+
+###############################################################################
+#                             Correction functions                            #
+###############################################################################
+
+
+def print_profiles():
+    print("Profiles")
+    for k, profile in profiles.items():
+        print(f" - {k}")
+        description = profile.get('description')
+        if description is not None:
+            for l in description.split('\n'):
+                print(f"   {l}")
+
+
+def correct(geometry, profile=None, do_print_changes=True):
+    """Apply a correction profile to projection geometry.
+
+
+    :param geometry:
+    :param profile:
+        A string describing the calibration profile that should be applied.
+        Use `correct.print_profiles()' to view available profiles.
+    :returns:
+    :rtype:
+
+    """
+    profile_names = "\n".join(profiles.keys())
+
+    prof = profiles.get(profile)
+    if prof is None:
+        raise ValueError(
+            f"Correction profile was not correctly specified. Choose one of:\n"
+            f"{profile_names}"
+        )
+
+    geometry = deepcopy(geometry)
+
+    for k, v in prof.items():
+        # Skip description, it is purely to describe correction profiles.
+        if k == 'description':
+            continue
+
+        prev = geometry[k]
+        if do_print_changes:
+            logging.info(f"For {k}: adjusting {prev} by {v}.")
+        geometry[k] += v
+
+    geometry.log(f"Applied correction profile '{profile}'")
+
+    return geometry
+
+
+def correct_roi(geometry):
+    """Set the detector ort and tan to be in line with the detector ROI settings
+
+    This function is flexray-specific.
+
+    :param geometry:
+    :returns:
+    :rtype:
+
+    """
+    geometry = deepcopy(geometry)
+
+    # Fix roi:
+    roi = geometry.description['roi']
+
+    # ROI is written for a pixel in 1x1 binning, so the centre of the detector
+    # is at (767,971), and detector pixel size 74.8 um
+    centre = [(roi[0] + roi[2]) // 2 - 971, (roi[1] + roi[3]) // 2 - 767]
+    detector_pixel_size = 0.0748
+
+    d_ort = centre[1] * detector_pixel_size
+    d_tan = centre[0] * detector_pixel_size
+
+    geometry.parameters['det_ort'] += d_ort
+    geometry.parameters['det_tan'] += d_tan
+
+    if abs(d_ort) > 1e-6 or abs(d_tan) > 1e-6:
+        msg = f"Adjusted detector center to ROI window {roi}: {(d_ort, d_tan)}."
+        geometry.log(msg)
+        logging.info(msg)
+
+    return geometry
+
+
+def correct_vol_center(geometry):
+    """Move the volume center vertically in-between the source and detector center.
+
+    :param geometry:
+    :returns:
+    :rtype:
+
+    """
+
+    geometry = deepcopy(geometry)
+    vol_tra_z_old = geometry.parameters['vol_tra'][0]
+    vol_tra_z_new = (geometry.parameters['det_ort'] * geometry.src2obj +
+                   geometry.parameters['src_ort'] * geometry.det2obj) / geometry.src2det
+    geometry.parameters['vol_tra'][0] = vol_tra_z_new
+
+    if abs(vol_tra_z_old - vol_tra_z_new) > 1e-6:
+        msg = f"Adjusted vertical volume center to source/detector pos: z = {vol_tra_z_new}."
+        geometry.log(msg)
+        logging.info(msg)
+
+    return geometry
---
+++ packages/flexdata/flexdata-1.0.1-py3-none-any.whl/flexdata/display.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module contains a few simple routines for displaying data:
+* 2D displays like: slice, projection, etc.
+* Interactive slicer: pyqt_graph
+* Other displays: mesh, color_project
+"""
+
+""" * Imports * """
+
+import numpy
+
+
+from . import data
+
+""" * Methods * """
+def plot3d(x, y, z, connected = False, title = None):
+    '''
+    Plot a 3D line or a scatter plot.
+    '''
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    ax.scatter(x, y, z)
+
+    if connected:
+        ax.plot(x, y, z)
+
+    _after_plot_(title, None)
+
+def plot2d(x, y=None, semilogy=False, title=None, legend=None):
+    '''
+    A standard 2D plot.
+    '''
+    import matplotlib.pyplot as plt
+
+    if y is None:
+        y = x
+        x = numpy.arange(numpy.size(x))
+
+    x = numpy.squeeze(x)
+    y = numpy.squeeze(y)
+
+    plt.figure()
+    if semilogy:
+        plt.semilogy(x, y)
+    else:
+        plt.plot(x, y)
+
+    if title:
+        plt.title(title)
+
+    if legend:
+        plt.legend(legend)
+
+    plt.show()
+
+def plot(x, y=None, semilogy=False, title=None, legend=None):
+    '''
+    A standard 2D plot.
+    '''
+    plot2d(x, y, semilogy, title, legend)
+
+def pyqt_graph(array, dim = 0, title=None):
+    '''
+    Create a PYQT window to display a 3D arrayset.
+    '''
+    import pyqtgraph as pq
+
+    # create pyqtgraph app:
+    app = pq.mkQApp()
+
+    pq.image(numpy.rot90(numpy.rollaxis(array, dim), axes = (1,2)), title = title)
+
+    app.exec_()
+
+def slice(array, index=None, dim=0, bounds=None, title=None, cmap="gray", file=None, cbar = True):
+
+    import matplotlib.pyplot as plt
+    from matplotlib import ticker
+
+    # Just in case squeeze:
+    array = numpy.squeeze(array)
+
+    # If the image is 2D:
+    if array.ndim == 2:
+        img = array
+
+    else:
+        if index is None:
+            index = array.shape[dim] // 2
+
+        sl = data.anyslice(array, index, dim)
+
+        img = numpy.squeeze(array[sl])
+
+        # There is a bug in plt. It doesn't like float16
+        if img.dtype == "float16":
+            img = numpy.float32(img)
+
+    fig = plt.figure()
+
+    if bounds:
+        imsh = plt.imshow(img[::-1, :], vmin=bounds[0], vmax=bounds[1], cmap=cmap)
+    else:
+        imsh = plt.imshow(img[::-1, :], cmap=cmap)
+
+    # plt.colorbar()
+    if cbar:
+        cbar = fig.colorbar(imsh, ticks=ticker.MaxNLocator(nbins=6))
+        cbar.ax.tick_params(labelsize=15)
+
+    _after_plot_(title, file)
+
+def _after_plot_(title, file):
+
+    import matplotlib.pyplot as plt
+    #plt.axis("off")
+
+    if title:
+        plt.title(title)
+
+    plt.show()
+
+    if file:
+        plt.savefig(file, dpi=300, bbox_inches="tight")
+
+def mesh(stl_mesh):
+    """
+    Display an stl mesh. Use flexcalc.generate_stl(volume) to generate mesh.
+    """
+    import matplotlib.pyplot as plt
+    from mpl_toolkits import mplot3d
+    from mpl_toolkits.mplot3d import Axes3D
+
+    figure = plt.figure()
+    axes = mplot3d.Axes3D(figure)
+
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(stl_mesh.vectors))
+
+    # Auto scale to the mesh size
+    scale = stl_mesh.points.flatten(-1)
+    axes.auto_scale_xyz(scale, scale, scale)
+    # Show the plot to the screen
+    plt.show()
+
+
+def projection(array, dim=1, bounds=None, title=None, cmap="gray", file=None):
+    '''
+    A simple projection of the volume along one of the dimensions.
+    '''
+    import matplotlib.pyplot as plt
+
+    img = array.sum(dim)
+
+    # There is a bug in plt. It doesn't like float16
+    img = numpy.float32(img)
+
+    plt.figure()
+
+    if bounds:
+        plt.imshow(img[::-1, :], vmin=bounds[0], vmax=bounds[1], cmap=cmap)
+    else:
+        plt.imshow(img[::-1, :], cmap=cmap)
+
+    plt.colorbar()
+
+    _after_plot_(title, file)
+
+
+def color_project(array, dim=1, sample = 2, bounds=[0.01, 0.1], title=None, cmap='nipy_spectral', file=None):
+    '''
+    Create a pseudo color projection of a 3D volume.
+    '''
+    import matplotlib.pyplot as plt
+
+    # Sample array:
+    array = array[::sample,::sample,::sample]
+
+    # Initialize colormap:
+    cmap_ = plt.get_cmap(cmap)
+
+    # Shape of the final image:
+    shape = list(array.shape)
+    shape.remove(shape[dim])
+    shape.append(3)
+
+    # Output image:
+    rgb_total = numpy.zeros(shape, dtype = 'float32')
+
+    print('Applying colormap...')
+
+    for ii in range(array.shape[dim]):
+
+        sl = data.anyslice(array, ii, dim)
+        img = numpy.squeeze(array[sl].copy())
+
+        img[img > bounds[1]] = bounds[1]
+        img[img < bounds[0]] = bounds[0]
+        img -= bounds[0]
+        img /= bounds[1] - bounds[0]
+
+        rgba_img = cmap_(img)
+        rgb_img = numpy.delete(rgba_img, 3, 2)
+
+        rgb_total += rgb_img# / array.shape[dim]
+        #rgb_total = numpy.max([rgb_img, rgb_total], axis = 0)
+
+    #rgb_total /= rgb_total.max()
+    #rgb_total = numpy.log(rgb_total)
+    rgb_total = numpy.sqrt(rgb_total)
+
+    plt.figure()
+
+    plt.imshow(rgb_total[::-1, :] / rgb_total.max(), cmap = cmap)
+
+    plt.colorbar()
+
+    _after_plot_(title, file)
+
+def max_projection(array, dim=0, bounds=None, title=None, cmap="gray", file=None):
+    '''
+    Projection of maximum values.
+    '''
+    import matplotlib.pyplot as plt
+
+    img = array.max(dim)
+
+    # There is a bug in plt. It doesn't like float16
+    img = numpy.float32(img)
+
+    plt.figure()
+
+    if bounds:
+        plt.imshow(img[::-1, :], vmin=bounds[0], vmax=bounds[1], cmap=cmap)
+    else:
+        plt.imshow(img[::-1, :], cmap=cmap)
+
+    plt.colorbar()
+    _after_plot_(title, file)
+
+
+def min_projection(array, dim=0, title=None, cmap="gray", file=None):
+    '''
+    Projection of minimum values.
+    '''
+    import matplotlib.pyplot as plt
+
+    img = array.min(dim)
+
+    # There is a bug in plt. It doesn't like float16
+    img = numpy.float32(img)
+
+    plt.figure()
+
+    plt.imshow(img[::-1, :], cmap=cmap)
+    plt.colorbar()
+    _after_plot_(title, file)
