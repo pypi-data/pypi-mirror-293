468bad86a524dc759388379f8383385db6a5d5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:03:52 +0000

    Add package types-aiobotocore-bedrock/2.14.0/types_aiobotocore_bedrock-2.14.0.tar.gz

---
+++ packages/types-aiobotocore-bedrock/types_aiobotocore_bedrock-2.14.0.tar.gz/types_aiobotocore_bedrock-2.14.0/types_aiobotocore_bedrock/paginator.py
@@ -0,0 +1,315 @@
+"""
+Type annotations for bedrock service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_bedrock.client import BedrockClient
+    from types_aiobotocore_bedrock.paginator import (
+        ListCustomModelsPaginator,
+        ListEvaluationJobsPaginator,
+        ListGuardrailsPaginator,
+        ListImportedModelsPaginator,
+        ListInferenceProfilesPaginator,
+        ListModelCopyJobsPaginator,
+        ListModelCustomizationJobsPaginator,
+        ListModelImportJobsPaginator,
+        ListModelInvocationJobsPaginator,
+        ListProvisionedModelThroughputsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("bedrock") as client:
+        client: BedrockClient
+
+        list_custom_models_paginator: ListCustomModelsPaginator = client.get_paginator("list_custom_models")
+        list_evaluation_jobs_paginator: ListEvaluationJobsPaginator = client.get_paginator("list_evaluation_jobs")
+        list_guardrails_paginator: ListGuardrailsPaginator = client.get_paginator("list_guardrails")
+        list_imported_models_paginator: ListImportedModelsPaginator = client.get_paginator("list_imported_models")
+        list_inference_profiles_paginator: ListInferenceProfilesPaginator = client.get_paginator("list_inference_profiles")
+        list_model_copy_jobs_paginator: ListModelCopyJobsPaginator = client.get_paginator("list_model_copy_jobs")
+        list_model_customization_jobs_paginator: ListModelCustomizationJobsPaginator = client.get_paginator("list_model_customization_jobs")
+        list_model_import_jobs_paginator: ListModelImportJobsPaginator = client.get_paginator("list_model_import_jobs")
+        list_model_invocation_jobs_paginator: ListModelInvocationJobsPaginator = client.get_paginator("list_model_invocation_jobs")
+        list_provisioned_model_throughputs_paginator: ListProvisionedModelThroughputsPaginator = client.get_paginator("list_provisioned_model_throughputs")
+    ```
+"""
+
+import sys
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .literals import (
+    EvaluationJobStatusType,
+    FineTuningJobStatusType,
+    ModelCopyJobStatusType,
+    ModelImportJobStatusType,
+    ModelInvocationJobStatusType,
+    ProvisionedModelStatusType,
+    SortOrderType,
+)
+from .type_defs import (
+    ListCustomModelsResponseTypeDef,
+    ListEvaluationJobsResponseTypeDef,
+    ListGuardrailsResponseTypeDef,
+    ListImportedModelsResponseTypeDef,
+    ListInferenceProfilesResponseTypeDef,
+    ListModelCopyJobsResponseTypeDef,
+    ListModelCustomizationJobsResponseTypeDef,
+    ListModelImportJobsResponseTypeDef,
+    ListModelInvocationJobsResponseTypeDef,
+    ListProvisionedModelThroughputsResponseTypeDef,
+    PaginatorConfigTypeDef,
+    TimestampTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = (
+    "ListCustomModelsPaginator",
+    "ListEvaluationJobsPaginator",
+    "ListGuardrailsPaginator",
+    "ListImportedModelsPaginator",
+    "ListInferenceProfilesPaginator",
+    "ListModelCopyJobsPaginator",
+    "ListModelCustomizationJobsPaginator",
+    "ListModelImportJobsPaginator",
+    "ListModelInvocationJobsPaginator",
+    "ListProvisionedModelThroughputsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListCustomModelsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListCustomModels)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listcustommodelspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeBefore: TimestampTypeDef = ...,
+        creationTimeAfter: TimestampTypeDef = ...,
+        nameContains: str = ...,
+        baseModelArnEquals: str = ...,
+        foundationModelArnEquals: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        isOwned: bool = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListCustomModelsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListCustomModels.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listcustommodelspaginator)
+        """
+
+
+class ListEvaluationJobsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListEvaluationJobs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listevaluationjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: EvaluationJobStatusType = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListEvaluationJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListEvaluationJobs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listevaluationjobspaginator)
+        """
+
+
+class ListGuardrailsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListGuardrails)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listguardrailspaginator)
+    """
+
+    def paginate(
+        self, *, guardrailIdentifier: str = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListGuardrailsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListGuardrails.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listguardrailspaginator)
+        """
+
+
+class ListImportedModelsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListImportedModels)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listimportedmodelspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeBefore: TimestampTypeDef = ...,
+        creationTimeAfter: TimestampTypeDef = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListImportedModelsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListImportedModels.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listimportedmodelspaginator)
+        """
+
+
+class ListInferenceProfilesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListInferenceProfiles)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listinferenceprofilespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListInferenceProfilesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListInferenceProfiles.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listinferenceprofilespaginator)
+        """
+
+
+class ListModelCopyJobsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelCopyJobs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listmodelcopyjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: ModelCopyJobStatusType = ...,
+        sourceAccountEquals: str = ...,
+        sourceModelArnEquals: str = ...,
+        targetModelNameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListModelCopyJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelCopyJobs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listmodelcopyjobspaginator)
+        """
+
+
+class ListModelCustomizationJobsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelCustomizationJobs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listmodelcustomizationjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: FineTuningJobStatusType = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListModelCustomizationJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelCustomizationJobs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listmodelcustomizationjobspaginator)
+        """
+
+
+class ListModelImportJobsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelImportJobs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listmodelimportjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: ModelImportJobStatusType = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListModelImportJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelImportJobs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listmodelimportjobspaginator)
+        """
+
+
+class ListModelInvocationJobsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelInvocationJobs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listmodelinvocationjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        submitTimeAfter: TimestampTypeDef = ...,
+        submitTimeBefore: TimestampTypeDef = ...,
+        statusEquals: ModelInvocationJobStatusType = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListModelInvocationJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelInvocationJobs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listmodelinvocationjobspaginator)
+        """
+
+
+class ListProvisionedModelThroughputsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListProvisionedModelThroughputs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listprovisionedmodelthroughputspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: ProvisionedModelStatusType = ...,
+        modelArnEquals: str = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListProvisionedModelThroughputsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListProvisionedModelThroughputs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bedrock/paginators/#listprovisionedmodelthroughputspaginator)
+        """
