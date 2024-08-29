f5ff7dc36f9a43affbc01c33fd41cd82e70739c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:23 +0000

    Add package vdk-lineage/0.3.1431637373/vdk_lineage-0.3.1431637373.tar.gz

---
+++ packages/vdk-lineage/vdk_lineage-0.3.1431637373.tar.gz/vdk_lineage-0.3.1431637373/setup.py
@@ -0,0 +1,48 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+"""
+Builds a package with the help of setuptools in order for this package to be imported in other projects
+"""
+
+__version__ = "0.3.1431637373"
+
+setuptools.setup(
+    name="vdk-lineage",
+    version=__version__,
+    url="https://github.com/vmware/versatile-data-kit",
+    description="VDK Lineage plugin collects lineage (input -> job -> output) information "
+    "and send it to a pre-configured destination.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "vdk-core",
+        "vdk-lineage-model",
+        "sqlparse",
+        "sqllineage==1.3.8",
+        "openlineage-integration-common",
+        "openlineage-python",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    include_package_data=True,
+    entry_points={
+        "vdk.plugin.run": [
+            "vdk-lineage = vdk.plugin.lineage.plugin_lineage",
+            "vdk-marquez = vdk.plugin.marquez.marquez_plugin",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
