aaac08d7fcc28634783676653f305776a20b36ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 06:05:11 +0000

    Add package py3-tts-wrapper/0.9.9/py3_tts_wrapper-0.9.9-py3-none-any.whl

---
+++ packages/py3-tts-wrapper/py3_tts_wrapper-0.9.9-py3-none-any.whl/tts_wrapper/engines/__init__.py
@@ -0,0 +1,12 @@
+import sys
+from .google import *
+from .microsoft import *
+from .pico import *
+from .polly import *
+from .sapi import *
+from .watson import *
+from .elevenlabs import *
+from .uwp import *
+from .witai import *
+from .sherpaonnx import *
+from .googletrans import * 
