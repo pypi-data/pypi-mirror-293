7914b6ed53fb4e6ff44963741a71dea2eeecd482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:05 +0000

    Add package mipx/0.2.16/mipx-0.2.16-py2.py3-none-any.whl

---
+++ packages/mipx/mipx-0.2.16-py2.py3-none-any.whl/mipx/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/31 22:18
+# @Author  : luyi
+from .constants import *
+from .lineExpr import LineExpr
+from .solver import Model
+from .tupledict import tupledict, multidict
+from .tuplelist import tuplelist
+from .utilx import name_str
+from .func import debugVar
+from .variable import Var, IntervalVar
+from ._version import __version__
+BINARY, CONTINUOUS, INTEGER = Vtype
+MINIMIZE, MAXIMIZE = ObjType
+LESS_EQUAL, EQUAL, GREATER_EQUAL = CmpType
+name = 'mipx'
---
+++ packages/mipx/mipx-0.2.16-py2.py3-none-any.whl/mipx/_version.py
@@ -0,0 +1,13 @@
+#         .__
+#   _____ |__|________  ___
+#  /     \|  \____ \  \/  /
+# |  Y Y  \  |  |_> >    <
+# |__|_|  /__|   __/__/\_ \
+#       \/   |__|        \/
+__title__ = "mipx"
+__description__ = "mipx"
+__version__ = "0.2.16"
+__author__ = "luyi"
+__author_email__ = "yi.lu@cisdi.com.cn"
+
+__copyright__ = "Copyright CISDI"
