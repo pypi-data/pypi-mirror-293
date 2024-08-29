bca60af1e738280e3df15997d2dab300008292e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:40 +0000

    Add package shancx/1.8.37/shancx-1.8.37-py3-none-any.whl

---
+++ packages/shancx/shancx-1.8.37-py3-none-any.whl/shancx/radar_nmc.py
@@ -0,0 +1,81 @@
+
+import matplotlib.pyplot as plt
+import datetime
+from shancx.wtx.colormap import cmp_hjnwtx
+import os
+import numpy as np
+ 
+ 
+
+def mkDir(path):
+    if "." in path:
+        os.makedirs(os.path.dirname(path),exist_ok=True)
+    else:
+        os.makedirs(path, exist_ok=True)
+
+def drawimg(array_dt,temp = "temp"):
+    now_str = datetime.datetime.now().strftime("%Y%m%d%H%M")
+    if len(array_dt.shape)==3:
+        for i , img_ch_nel in enumerate(array_dt): 
+            plt.imshow(img_ch_nel,vmin=0,vmax=100,cmap=cmp_hjnwtx["radar_nmc"])
+            plt.colorbar()
+            outpath = f"./radar_nmc/{temp}_{now_str}.png"
+            mkDir(outpath)
+            plt.savefig(outpath)
+            plt.close()
+    if len(array_dt.shape)==2:
+            plt.imshow(array_dt,vmin=0,vmax=100,cmap=cmp_hjnwtx["radar_nmc"])
+            plt.colorbar()
+            outpath = f"./radar_nmc/{temp}_{now_str}.png"
+            mkDir(outpath)
+            plt.savefig(outpath)
+            plt.close() 
+ 
+
+def mkDir(path):
+    directory = os.path.dirname(path)
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+
+def drawimg_coor(array_dt, temp="temp"):
+    now_str = datetime.datetime.now().strftime("%Y%m%d%H%M")
+    y_coords2, x_coords2 = np.where(array_dt > 0)
+    
+    def plot_and_save(image, path):
+        plt.imshow(image, vmin=0, vmax=10, cmap=cmp_hjnwtx["radar_nmc"])
+        for (x, y) in zip(x_coords2, y_coords2):
+            plt.plot(x, y, 'ro', markersize=25)  # Increase point size
+            plt.text(x, y, f'{(image[y, x] * 6):.1f}', color='white', fontsize=12, ha='center', va='center')  # Label the corresponding value
+        plt.colorbar()
+        mkDir(path)
+        plt.savefig(path)
+        plt.close()    
+    if len(array_dt.shape) == 3:
+        for i, img_ch_nel in enumerate(array_dt): 
+            plot_and_save(img_ch_nel, f"./radar_nmc/{temp}_{now_str}.png")
+    elif len(array_dt.shape) == 2:
+        plt.imshow(array_dt, vmin=0, vmax=100, cmap=cmp_hjnwtx["radar_nmc"])
+        plot_and_save(array_dt, f"./radar_nmc/{temp}_{now_str}.png")
+
+def drawimg_com(array_dt,temp = "temp"):
+    now_str = datetime.datetime.now().strftime("%Y%m%d%H%M")
+    now_str = datetime.datetime.now().strftime("%Y%m%d%H%M")
+    y_coords2, x_coords2 = np.where(array_dt > 0.1)
+    if len(array_dt.shape)==3:
+        for i , img_ch_nel in enumerate(array_dt): 
+            plt.imshow(img_ch_nel,vmin=0,vmax=10,cmap=cmp_hjnwtx["radar_nmc"])
+            plt.plot(x_coords2, y_coords2, 'ro', markersize=2)  # Adjust markersize here
+            plt.colorbar()
+            outpath = f"./radar_nmc/{temp}_{now_str}.png"
+            mkDir(outpath)
+            plt.savefig(outpath)
+            plt.close()
+    if len(array_dt.shape)==2: 
+            plt.imshow(array_dt, vmax=10, cmap=cmp_hjnwtx["pre_tqw"])
+            plt.plot(x_coords2, y_coords2, 'ro', markersize=0.5)  # Adjust markersize here
+            plt.colorbar()
+            outpath = f"./radar_nmc/{temp}_{now_str}.png"
+            mkDir(outpath)
+            plt.savefig(outpath)
+            plt.close() 
+ 
