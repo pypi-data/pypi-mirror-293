1ebc2c76d5b377480a13ffcd2076104faee6e0b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:26 +0000

    Add package packg/0.18.4/packg-0.18.4.tar.gz

---
+++ packages/packg/packg-0.18.4.tar.gz/packg-0.18.4/src/packg/strings/__init__.py
@@ -0,0 +1,11 @@
+from .abbreviations import create_nested_abbreviations, create_unique_abbreviations
+from .base64tools import (
+    b64_encode_from_str,
+    b64_encode_from_bytes,
+    b64_decode_to_str,
+    b64_decode_to_bytes,
+    get_random_b64_string,
+)
+from .hasher import hash_object
+from .quote_urlparse import quote_with_urlparse, unquote_with_urlparse
+from .tabul import format_pseudo_table
