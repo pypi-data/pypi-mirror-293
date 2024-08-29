9825743253858625d6a324112176d2ddfea3b14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 17:04:11 +0000

    Add package eth-typeshed-py/0.1.7.post7/eth_typeshed_py-0.1.7.post7.tar.gz

---
+++ packages/eth-typeshed-py/eth_typeshed_py-0.1.7.post7.tar.gz/eth_typeshed_py-0.1.7.post7/src/eth_typeshed/uniswap_v2/router/contract.py
@@ -0,0 +1,50 @@
+from typing import Annotated
+
+from eth_rpc import ContractFunc, ProtocolBase
+from eth_rpc.types import METHOD, Name
+
+from .types import (
+    AddLiquidityETHRequest,
+    AddLiquidityRequest,
+    AddLiquidityResponse,
+    RemoveLiquidityETHRequest,
+    RemoveLiquidityETHResponse,
+    RemoveLiquidityRequest,
+    RemoveLiquidityResponse,
+)
+
+ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+
+
+class UniswapV2Router(ProtocolBase):
+    add_liquidity: Annotated[
+        ContractFunc[
+            AddLiquidityRequest,
+            AddLiquidityResponse,
+        ],
+        Name("addLiquidity"),
+    ] = METHOD
+    add_liquidity_eth: Annotated[
+        ContractFunc[
+            AddLiquidityETHRequest,
+            AddLiquidityResponse,
+        ],
+        Name("addLiquidityETH"),
+    ] = METHOD
+
+    remove_liquidity: Annotated[
+        ContractFunc[
+            RemoveLiquidityRequest,
+            RemoveLiquidityResponse,
+        ],
+        Name("removeLiquidity"),
+    ]
+    remove_liquidity_eth: Annotated[
+        ContractFunc[
+            RemoveLiquidityETHRequest,
+            RemoveLiquidityETHResponse,
+        ],
+        Name("removeLiquidityETH"),
+    ]
+
+    # TODO: finish adding methods
