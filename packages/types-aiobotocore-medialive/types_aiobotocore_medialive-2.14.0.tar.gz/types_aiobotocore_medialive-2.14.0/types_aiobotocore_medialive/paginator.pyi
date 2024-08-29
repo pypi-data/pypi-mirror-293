ab1e4ccabd422117dedd158b105716ee1931a151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:04:18 +0000

    Add package types-aiobotocore-medialive/2.14.0/types_aiobotocore_medialive-2.14.0.tar.gz

---
+++ packages/types-aiobotocore-medialive/types_aiobotocore_medialive-2.14.0.tar.gz/types_aiobotocore_medialive-2.14.0/types_aiobotocore_medialive/paginator.pyi
@@ -0,0 +1,349 @@
+"""
+Type annotations for medialive service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_medialive.client import MediaLiveClient
+    from types_aiobotocore_medialive.paginator import (
+        DescribeSchedulePaginator,
+        ListChannelsPaginator,
+        ListCloudWatchAlarmTemplateGroupsPaginator,
+        ListCloudWatchAlarmTemplatesPaginator,
+        ListEventBridgeRuleTemplateGroupsPaginator,
+        ListEventBridgeRuleTemplatesPaginator,
+        ListInputDeviceTransfersPaginator,
+        ListInputDevicesPaginator,
+        ListInputSecurityGroupsPaginator,
+        ListInputsPaginator,
+        ListMultiplexProgramsPaginator,
+        ListMultiplexesPaginator,
+        ListOfferingsPaginator,
+        ListReservationsPaginator,
+        ListSignalMapsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("medialive") as client:
+        client: MediaLiveClient
+
+        describe_schedule_paginator: DescribeSchedulePaginator = client.get_paginator("describe_schedule")
+        list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+        list_cloud_watch_alarm_template_groups_paginator: ListCloudWatchAlarmTemplateGroupsPaginator = client.get_paginator("list_cloud_watch_alarm_template_groups")
+        list_cloud_watch_alarm_templates_paginator: ListCloudWatchAlarmTemplatesPaginator = client.get_paginator("list_cloud_watch_alarm_templates")
+        list_event_bridge_rule_template_groups_paginator: ListEventBridgeRuleTemplateGroupsPaginator = client.get_paginator("list_event_bridge_rule_template_groups")
+        list_event_bridge_rule_templates_paginator: ListEventBridgeRuleTemplatesPaginator = client.get_paginator("list_event_bridge_rule_templates")
+        list_input_device_transfers_paginator: ListInputDeviceTransfersPaginator = client.get_paginator("list_input_device_transfers")
+        list_input_devices_paginator: ListInputDevicesPaginator = client.get_paginator("list_input_devices")
+        list_input_security_groups_paginator: ListInputSecurityGroupsPaginator = client.get_paginator("list_input_security_groups")
+        list_inputs_paginator: ListInputsPaginator = client.get_paginator("list_inputs")
+        list_multiplex_programs_paginator: ListMultiplexProgramsPaginator = client.get_paginator("list_multiplex_programs")
+        list_multiplexes_paginator: ListMultiplexesPaginator = client.get_paginator("list_multiplexes")
+        list_offerings_paginator: ListOfferingsPaginator = client.get_paginator("list_offerings")
+        list_reservations_paginator: ListReservationsPaginator = client.get_paginator("list_reservations")
+        list_signal_maps_paginator: ListSignalMapsPaginator = client.get_paginator("list_signal_maps")
+    ```
+"""
+
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    DescribeScheduleResponseTypeDef,
+    ListChannelsResponseTypeDef,
+    ListCloudWatchAlarmTemplateGroupsResponseTypeDef,
+    ListCloudWatchAlarmTemplatesResponseTypeDef,
+    ListEventBridgeRuleTemplateGroupsResponseTypeDef,
+    ListEventBridgeRuleTemplatesResponseTypeDef,
+    ListInputDevicesResponseTypeDef,
+    ListInputDeviceTransfersResponseTypeDef,
+    ListInputSecurityGroupsResponseTypeDef,
+    ListInputsResponseTypeDef,
+    ListMultiplexesResponseTypeDef,
+    ListMultiplexProgramsResponseTypeDef,
+    ListOfferingsResponseTypeDef,
+    ListReservationsResponseTypeDef,
+    ListSignalMapsResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "DescribeSchedulePaginator",
+    "ListChannelsPaginator",
+    "ListCloudWatchAlarmTemplateGroupsPaginator",
+    "ListCloudWatchAlarmTemplatesPaginator",
+    "ListEventBridgeRuleTemplateGroupsPaginator",
+    "ListEventBridgeRuleTemplatesPaginator",
+    "ListInputDeviceTransfersPaginator",
+    "ListInputDevicesPaginator",
+    "ListInputSecurityGroupsPaginator",
+    "ListInputsPaginator",
+    "ListMultiplexProgramsPaginator",
+    "ListMultiplexesPaginator",
+    "ListOfferingsPaginator",
+    "ListReservationsPaginator",
+    "ListSignalMapsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class DescribeSchedulePaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.DescribeSchedule)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#describeschedulepaginator)
+    """
+
+    def paginate(
+        self, *, ChannelId: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[DescribeScheduleResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.DescribeSchedule.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#describeschedulepaginator)
+        """
+
+class ListChannelsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListChannels)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listchannelspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListChannelsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListChannels.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listchannelspaginator)
+        """
+
+class ListCloudWatchAlarmTemplateGroupsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListCloudWatchAlarmTemplateGroups)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listcloudwatchalarmtemplategroupspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        Scope: str = ...,
+        SignalMapIdentifier: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListCloudWatchAlarmTemplateGroupsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListCloudWatchAlarmTemplateGroups.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listcloudwatchalarmtemplategroupspaginator)
+        """
+
+class ListCloudWatchAlarmTemplatesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListCloudWatchAlarmTemplates)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listcloudwatchalarmtemplatespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        GroupIdentifier: str = ...,
+        Scope: str = ...,
+        SignalMapIdentifier: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListCloudWatchAlarmTemplatesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListCloudWatchAlarmTemplates.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listcloudwatchalarmtemplatespaginator)
+        """
+
+class ListEventBridgeRuleTemplateGroupsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListEventBridgeRuleTemplateGroups)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listeventbridgeruletemplategroupspaginator)
+    """
+
+    def paginate(
+        self, *, SignalMapIdentifier: str = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListEventBridgeRuleTemplateGroupsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListEventBridgeRuleTemplateGroups.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listeventbridgeruletemplategroupspaginator)
+        """
+
+class ListEventBridgeRuleTemplatesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListEventBridgeRuleTemplates)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listeventbridgeruletemplatespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        GroupIdentifier: str = ...,
+        SignalMapIdentifier: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListEventBridgeRuleTemplatesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListEventBridgeRuleTemplates.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listeventbridgeruletemplatespaginator)
+        """
+
+class ListInputDeviceTransfersPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListInputDeviceTransfers)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listinputdevicetransferspaginator)
+    """
+
+    def paginate(
+        self, *, TransferType: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListInputDeviceTransfersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListInputDeviceTransfers.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listinputdevicetransferspaginator)
+        """
+
+class ListInputDevicesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListInputDevices)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listinputdevicespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListInputDevicesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListInputDevices.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listinputdevicespaginator)
+        """
+
+class ListInputSecurityGroupsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListInputSecurityGroups)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listinputsecuritygroupspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListInputSecurityGroupsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListInputSecurityGroups.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listinputsecuritygroupspaginator)
+        """
+
+class ListInputsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListInputs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listinputspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListInputsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListInputs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listinputspaginator)
+        """
+
+class ListMultiplexProgramsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListMultiplexPrograms)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listmultiplexprogramspaginator)
+    """
+
+    def paginate(
+        self, *, MultiplexId: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListMultiplexProgramsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListMultiplexPrograms.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listmultiplexprogramspaginator)
+        """
+
+class ListMultiplexesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListMultiplexes)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listmultiplexespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListMultiplexesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListMultiplexes.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listmultiplexespaginator)
+        """
+
+class ListOfferingsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListOfferings)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listofferingspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ChannelClass: str = ...,
+        ChannelConfiguration: str = ...,
+        Codec: str = ...,
+        Duration: str = ...,
+        MaximumBitrate: str = ...,
+        MaximumFramerate: str = ...,
+        Resolution: str = ...,
+        ResourceType: str = ...,
+        SpecialFeature: str = ...,
+        VideoQuality: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListOfferingsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListOfferings.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listofferingspaginator)
+        """
+
+class ListReservationsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListReservations)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listreservationspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        ChannelClass: str = ...,
+        Codec: str = ...,
+        MaximumBitrate: str = ...,
+        MaximumFramerate: str = ...,
+        Resolution: str = ...,
+        ResourceType: str = ...,
+        SpecialFeature: str = ...,
+        VideoQuality: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListReservationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListReservations.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listreservationspaginator)
+        """
+
+class ListSignalMapsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListSignalMaps)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listsignalmapspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        CloudWatchAlarmTemplateGroupIdentifier: str = ...,
+        EventBridgeRuleTemplateGroupIdentifier: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListSignalMapsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/medialive.html#MediaLive.Paginator.ListSignalMaps.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_medialive/paginators/#listsignalmapspaginator)
+        """
