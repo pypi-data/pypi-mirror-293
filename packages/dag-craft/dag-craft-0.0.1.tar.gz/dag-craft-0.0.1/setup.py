7f8e88d7a574c66cfe05fea9194e52764f8b61a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:03:39 +0000

    Add package dag-craft/0.0.1/dag-craft-0.0.1.tar.gz

---
+++ packages/dag-craft/dag-craft-0.0.1.tar.gz/dag-craft-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name="dag-craft",
+    version="0.0.1",
+    url="https://github.com/MurilloSSJ/airflow-dag-factory",
+    license="MIT License",
+    author="Murillo Jacob",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email="murillostore@gmail.com",
+    keywords="Airflow, DAG, Factory",
+    description="Dag Factory para o Airflow",
+    packages=["dag_craft"],
+    install_requires=["airflow"],
+)
