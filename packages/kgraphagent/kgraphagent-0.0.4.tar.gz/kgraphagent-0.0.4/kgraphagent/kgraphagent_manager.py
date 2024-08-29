cc4584bea7461767b47b098c26e8786e8fab25cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:32 +0000

    Add package kgraphagent/0.0.4/kgraphagent-0.0.4.tar.gz

---
+++ packages/kgraphagent/kgraphagent-0.0.4.tar.gz/kgraphagent-0.0.4/kgraphagent/kgraphagent_manager.py
@@ -0,0 +1,15 @@
+
+class KGraphAgentManager:
+    pass
+
+# manager class for agents utilizing knowledge graphs
+# queries can be used with kgraphservice and kgraphmemory
+
+# this library is agnostic with regard to libraries used to implement agents
+# such as langchain and llamaindex
+
+# such a library may use kgraphagent as a dependency in its implementation or
+# an agent implementation may use such a library and wrap the functionality in
+# this library
+
+
