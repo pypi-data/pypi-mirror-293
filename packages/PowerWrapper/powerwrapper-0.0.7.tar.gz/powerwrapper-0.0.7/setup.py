82c56e1348cbde2ea155473b5de32e0bbf8b581c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:03:20 +0000

    Add package PowerWrapper/0.0.7/powerwrapper-0.0.7.tar.gz

---
+++ packages/PowerWrapper/powerwrapper-0.0.7.tar.gz/powerwrapper-0.0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='PowerWrapper',
+  version='0.0.7',
+  description='A powerful library that wraps advanced functions.',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type='text/markdown',
+  url='',  
+  author='SapphireKR',
+  author_email='sapphirekr11@protonmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['python', 'wrapper'], 
+  packages=find_packages(),
+  install_requires=[''] 
+)
