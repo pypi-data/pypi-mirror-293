ad6dcc1e1af0b71e7582fb900ba91f4407e39444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:04:11 +0000

    Add package types-aiobotocore-lex-runtime/2.14.0/types_aiobotocore_lex_runtime-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-lex-runtime/types_aiobotocore_lex_runtime-2.14.0-py3-none-any.whl/types_aiobotocore_lex_runtime/client.pyi
@@ -0,0 +1,192 @@
+"""
+Type annotations for lex-runtime service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_lex_runtime.client import LexRuntimeServiceClient
+
+    session = get_session()
+    async with session.create_client("lex-runtime") as client:
+        client: LexRuntimeServiceClient
+    ```
+"""
+
+from typing import Any, Dict, Mapping, Sequence, Type
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .type_defs import (
+    ActiveContextUnionTypeDef,
+    BlobTypeDef,
+    DeleteSessionResponseTypeDef,
+    DialogActionUnionTypeDef,
+    GetSessionResponseTypeDef,
+    IntentSummaryUnionTypeDef,
+    PostContentResponseTypeDef,
+    PostTextResponseTypeDef,
+    PutSessionResponseTypeDef,
+)
+
+__all__ = ("LexRuntimeServiceClient",)
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    BadGatewayException: Type[BotocoreClientError]
+    BadRequestException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    DependencyFailedException: Type[BotocoreClientError]
+    InternalFailureException: Type[BotocoreClientError]
+    LimitExceededException: Type[BotocoreClientError]
+    LoopDetectedException: Type[BotocoreClientError]
+    NotAcceptableException: Type[BotocoreClientError]
+    NotFoundException: Type[BotocoreClientError]
+    RequestTimeoutException: Type[BotocoreClientError]
+    UnsupportedMediaTypeException: Type[BotocoreClientError]
+
+class LexRuntimeServiceClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        LexRuntimeServiceClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client.exceptions)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client.can_paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/#can_paginate)
+        """
+
+    async def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client.close)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/#close)
+        """
+
+    async def delete_session(
+        self, *, botName: str, botAlias: str, userId: str
+    ) -> DeleteSessionResponseTypeDef:
+        """
+        Removes session information for a specified bot, alias, and user ID.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client.delete_session)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/#delete_session)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client.generate_presigned_url)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/#generate_presigned_url)
+        """
+
+    async def get_session(
+        self, *, botName: str, botAlias: str, userId: str, checkpointLabelFilter: str = ...
+    ) -> GetSessionResponseTypeDef:
+        """
+        Returns session information for a specified bot, alias, and user ID.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client.get_session)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/#get_session)
+        """
+
+    async def post_content(
+        self,
+        *,
+        botName: str,
+        botAlias: str,
+        userId: str,
+        contentType: str,
+        inputStream: BlobTypeDef,
+        sessionAttributes: str = ...,
+        requestAttributes: str = ...,
+        accept: str = ...,
+        activeContexts: str = ...,
+    ) -> PostContentResponseTypeDef:
+        """
+        Sends user input (text or speech) to Amazon Lex.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client.post_content)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/#post_content)
+        """
+
+    async def post_text(
+        self,
+        *,
+        botName: str,
+        botAlias: str,
+        userId: str,
+        inputText: str,
+        sessionAttributes: Mapping[str, str] = ...,
+        requestAttributes: Mapping[str, str] = ...,
+        activeContexts: Sequence[ActiveContextUnionTypeDef] = ...,
+    ) -> PostTextResponseTypeDef:
+        """
+        Sends user input to Amazon Lex.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client.post_text)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/#post_text)
+        """
+
+    async def put_session(
+        self,
+        *,
+        botName: str,
+        botAlias: str,
+        userId: str,
+        sessionAttributes: Mapping[str, str] = ...,
+        dialogAction: DialogActionUnionTypeDef = ...,
+        recentIntentSummaryView: Sequence[IntentSummaryUnionTypeDef] = ...,
+        accept: str = ...,
+        activeContexts: Sequence[ActiveContextUnionTypeDef] = ...,
+    ) -> PutSessionResponseTypeDef:
+        """
+        Creates a new session or modifies an existing session with an Amazon Lex bot.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client.put_session)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/#put_session)
+        """
+
+    async def __aenter__(self) -> "LexRuntimeServiceClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lex-runtime.html#LexRuntimeService.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_lex_runtime/client/)
+        """
