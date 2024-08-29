242ecd77950153d4524e351c3c05b1339462a252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:04:08 +0000

    Add package types-aiobotocore-ivs/2.14.0/types_aiobotocore_ivs-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-ivs/types_aiobotocore_ivs-2.14.0-py3-none-any.whl/types_aiobotocore_ivs/paginator.pyi
@@ -0,0 +1,139 @@
+"""
+Type annotations for ivs service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_ivs.client import IVSClient
+    from types_aiobotocore_ivs.paginator import (
+        ListChannelsPaginator,
+        ListPlaybackKeyPairsPaginator,
+        ListRecordingConfigurationsPaginator,
+        ListStreamKeysPaginator,
+        ListStreamsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("ivs") as client:
+        client: IVSClient
+
+        list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+        list_playback_key_pairs_paginator: ListPlaybackKeyPairsPaginator = client.get_paginator("list_playback_key_pairs")
+        list_recording_configurations_paginator: ListRecordingConfigurationsPaginator = client.get_paginator("list_recording_configurations")
+        list_stream_keys_paginator: ListStreamKeysPaginator = client.get_paginator("list_stream_keys")
+        list_streams_paginator: ListStreamsPaginator = client.get_paginator("list_streams")
+    ```
+"""
+
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    ListChannelsResponseTypeDef,
+    ListPlaybackKeyPairsResponseTypeDef,
+    ListRecordingConfigurationsResponseTypeDef,
+    ListStreamKeysResponseTypeDef,
+    ListStreamsResponseTypeDef,
+    PaginatorConfigTypeDef,
+    StreamFiltersTypeDef,
+)
+
+__all__ = (
+    "ListChannelsPaginator",
+    "ListPlaybackKeyPairsPaginator",
+    "ListRecordingConfigurationsPaginator",
+    "ListStreamKeysPaginator",
+    "ListStreamsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListChannelsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListChannels)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#listchannelspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        filterByName: str = ...,
+        filterByRecordingConfigurationArn: str = ...,
+        filterByPlaybackRestrictionPolicyArn: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListChannelsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListChannels.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#listchannelspaginator)
+        """
+
+class ListPlaybackKeyPairsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListPlaybackKeyPairs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#listplaybackkeypairspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListPlaybackKeyPairsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListPlaybackKeyPairs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#listplaybackkeypairspaginator)
+        """
+
+class ListRecordingConfigurationsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListRecordingConfigurations)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#listrecordingconfigurationspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListRecordingConfigurationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListRecordingConfigurations.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#listrecordingconfigurationspaginator)
+        """
+
+class ListStreamKeysPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListStreamKeys)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#liststreamkeyspaginator)
+    """
+
+    def paginate(
+        self, *, channelArn: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListStreamKeysResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListStreamKeys.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#liststreamkeyspaginator)
+        """
+
+class ListStreamsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListStreams)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#liststreamspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        filterBy: StreamFiltersTypeDef = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListStreamsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ivs.html#IVS.Paginator.ListStreams.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ivs/paginators/#liststreamspaginator)
+        """
