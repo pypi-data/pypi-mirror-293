f3d01f54204bd485d080c17ac96fcecf5f4f221a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:04:15 +0000

    Add package types-aiobotocore-machinelearning/2.14.0/types_aiobotocore_machinelearning-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-machinelearning/types_aiobotocore_machinelearning-2.14.0-py3-none-any.whl/types_aiobotocore_machinelearning/waiter.pyi
@@ -0,0 +1,154 @@
+"""
+Type annotations for machinelearning service client waiters.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_machinelearning/waiters/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_machinelearning.client import MachineLearningClient
+    from types_aiobotocore_machinelearning.waiter import (
+        BatchPredictionAvailableWaiter,
+        DataSourceAvailableWaiter,
+        EvaluationAvailableWaiter,
+        MLModelAvailableWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("machinelearning") as client:
+        client: MachineLearningClient
+
+        batch_prediction_available_waiter: BatchPredictionAvailableWaiter = client.get_waiter("batch_prediction_available")
+        data_source_available_waiter: DataSourceAvailableWaiter = client.get_waiter("data_source_available")
+        evaluation_available_waiter: EvaluationAvailableWaiter = client.get_waiter("evaluation_available")
+        ml_model_available_waiter: MLModelAvailableWaiter = client.get_waiter("ml_model_available")
+    ```
+"""
+
+from aiobotocore.waiter import AIOWaiter
+
+from .literals import (
+    BatchPredictionFilterVariableType,
+    DataSourceFilterVariableType,
+    EvaluationFilterVariableType,
+    MLModelFilterVariableType,
+    SortOrderType,
+)
+from .type_defs import WaiterConfigTypeDef
+
+__all__ = (
+    "BatchPredictionAvailableWaiter",
+    "DataSourceAvailableWaiter",
+    "EvaluationAvailableWaiter",
+    "MLModelAvailableWaiter",
+)
+
+class BatchPredictionAvailableWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/machinelearning.html#MachineLearning.Waiter.BatchPredictionAvailable)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_machinelearning/waiters/#batchpredictionavailablewaiter)
+    """
+
+    async def wait(
+        self,
+        *,
+        FilterVariable: BatchPredictionFilterVariableType = ...,
+        EQ: str = ...,
+        GT: str = ...,
+        LT: str = ...,
+        GE: str = ...,
+        LE: str = ...,
+        NE: str = ...,
+        Prefix: str = ...,
+        SortOrder: SortOrderType = ...,
+        NextToken: str = ...,
+        Limit: int = ...,
+        WaiterConfig: WaiterConfigTypeDef = ...,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/machinelearning.html#MachineLearning.Waiter.BatchPredictionAvailable.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_machinelearning/waiters/#batchpredictionavailablewaiter)
+        """
+
+class DataSourceAvailableWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/machinelearning.html#MachineLearning.Waiter.DataSourceAvailable)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_machinelearning/waiters/#datasourceavailablewaiter)
+    """
+
+    async def wait(
+        self,
+        *,
+        FilterVariable: DataSourceFilterVariableType = ...,
+        EQ: str = ...,
+        GT: str = ...,
+        LT: str = ...,
+        GE: str = ...,
+        LE: str = ...,
+        NE: str = ...,
+        Prefix: str = ...,
+        SortOrder: SortOrderType = ...,
+        NextToken: str = ...,
+        Limit: int = ...,
+        WaiterConfig: WaiterConfigTypeDef = ...,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/machinelearning.html#MachineLearning.Waiter.DataSourceAvailable.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_machinelearning/waiters/#datasourceavailablewaiter)
+        """
+
+class EvaluationAvailableWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/machinelearning.html#MachineLearning.Waiter.EvaluationAvailable)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_machinelearning/waiters/#evaluationavailablewaiter)
+    """
+
+    async def wait(
+        self,
+        *,
+        FilterVariable: EvaluationFilterVariableType = ...,
+        EQ: str = ...,
+        GT: str = ...,
+        LT: str = ...,
+        GE: str = ...,
+        LE: str = ...,
+        NE: str = ...,
+        Prefix: str = ...,
+        SortOrder: SortOrderType = ...,
+        NextToken: str = ...,
+        Limit: int = ...,
+        WaiterConfig: WaiterConfigTypeDef = ...,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/machinelearning.html#MachineLearning.Waiter.EvaluationAvailable.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_machinelearning/waiters/#evaluationavailablewaiter)
+        """
+
+class MLModelAvailableWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/machinelearning.html#MachineLearning.Waiter.MLModelAvailable)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_machinelearning/waiters/#mlmodelavailablewaiter)
+    """
+
+    async def wait(
+        self,
+        *,
+        FilterVariable: MLModelFilterVariableType = ...,
+        EQ: str = ...,
+        GT: str = ...,
+        LT: str = ...,
+        GE: str = ...,
+        LE: str = ...,
+        NE: str = ...,
+        Prefix: str = ...,
+        SortOrder: SortOrderType = ...,
+        NextToken: str = ...,
+        Limit: int = ...,
+        WaiterConfig: WaiterConfigTypeDef = ...,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/machinelearning.html#MachineLearning.Waiter.MLModelAvailable.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_machinelearning/waiters/#mlmodelavailablewaiter)
+        """
