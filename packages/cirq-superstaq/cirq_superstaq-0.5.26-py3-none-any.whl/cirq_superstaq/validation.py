f5d126e49bb18f8d35fd20edc0efd4580c5a674e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:03:54 +0000

    Add package cirq-superstaq/0.5.26/cirq_superstaq-0.5.26-py3-none-any.whl

---
+++ packages/cirq-superstaq/cirq_superstaq-0.5.26-py3-none-any.whl/cirq_superstaq/validation.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+
+import cirq
+
+
+def validate_cirq_circuits(circuits: object, require_measurements: bool = False) -> None:
+    """Validates that the input is either a single `cirq.Circuit` or a list of `cirq.Circuit`
+    instances.
+
+    Args:
+        circuits: The circuit(s) to run.
+        require_measurements: An optional boolean flag to check if all of the circuits have
+            measurements.
+
+    Raises:
+        ValueError: If the input is not a `cirq.Circuit` or a list of `cirq.Circuit` instances.
+    """
+
+    if not (
+        isinstance(circuits, cirq.Circuit)
+        or (
+            isinstance(circuits, Sequence)
+            and all(isinstance(circuit, cirq.Circuit) for circuit in circuits)
+        )
+    ):
+        raise ValueError(
+            "Invalid 'circuits' input. Must be a `cirq.Circuit` or a "
+            "sequence of `cirq.Circuit` instances."
+        )
+
+    if require_measurements:
+        circuit_list = [circuits] if isinstance(circuits, cirq.Circuit) else circuits
+        for circuit in circuit_list:
+            if isinstance(circuit, cirq.Circuit) and not circuit.has_measurements():
+                # TODO: only raise if the run method actually requires samples (and not for e.g. a
+                # statevector simulation)
+                raise ValueError("Circuit has no measurements to sample.")
