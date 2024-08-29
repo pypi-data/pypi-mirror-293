5a8bb4c1a24c285487a12771ea3c5b52fab52a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:03:53 +0000

    Add package power-comparison/1.0.3/power_comparison-1.0.3.tar.gz

---
+++ packages/power-comparison/power_comparison-1.0.3.tar.gz/power_comparison-1.0.3/src/power_comparison/default_values_utility.py
@@ -0,0 +1,53 @@
+"""Define the default values utility class."""
+
+from pathlib import Path
+
+import platformdirs
+
+
+class DefaultValuesUtility:
+    """Define utility functions for default values."""
+
+    _PROFILES_DIR = "profiles"
+    _DB_FILE_PATH = "data/user_data.db"
+    _APP_NAME = "Power Comparison"
+    _ICON_ICO_PATH = "img/power_compare.ico"
+    _ICON_PNG_PATH = "img/power_compare.png"
+    _THEME_PATH = "theme/pc-yellow.json"
+
+    @staticmethod
+    def create_dirs(filepath: str) -> None:
+        """Ensure all directories in filepath are created."""
+        Path(filepath).parent.mkdir(parents=True, exist_ok=True)
+
+    @staticmethod
+    def get_profiles_dir() -> str:
+        """Return a path to the profiles directory."""
+        return str(Path(__file__).parent / DefaultValuesUtility._PROFILES_DIR)
+
+    @staticmethod
+    def get_icon_ico_path() -> str:
+        """Return a path to the program's icon."""
+        return str(Path(__file__).parent / DefaultValuesUtility._ICON_ICO_PATH)
+
+    @staticmethod
+    def get_icon_png_path() -> str:
+        """Return a path to the program's icon."""
+        return str(Path(__file__).parent / DefaultValuesUtility._ICON_PNG_PATH)
+
+    @staticmethod
+    def get_theme_path() -> str:
+        """Return the path to the main theme."""
+        return str(Path(__file__).parent / DefaultValuesUtility._THEME_PATH)
+
+    @staticmethod
+    def get_db_file_path() -> str:
+        """Return a path to the database file."""
+        return str(
+            Path(
+                platformdirs.user_data_dir(
+                    DefaultValuesUtility._APP_NAME, roaming=True
+                )
+            )
+            / DefaultValuesUtility._DB_FILE_PATH
+        )
