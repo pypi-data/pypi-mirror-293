f922e9c8cc46680057a5cadf2e228716968e00f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:04:20 +0000

    Add package types-aiobotocore-personalize-runtime/2.14.0/types_aiobotocore_personalize_runtime-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-personalize-runtime/types_aiobotocore_personalize_runtime-2.14.0-py3-none-any.whl/types_aiobotocore_personalize_runtime/client.py
@@ -0,0 +1,161 @@
+"""
+Type annotations for personalize-runtime service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_personalize_runtime.client import PersonalizeRuntimeClient
+
+    session = get_session()
+    async with session.create_client("personalize-runtime") as client:
+        client: PersonalizeRuntimeClient
+    ```
+"""
+
+from typing import Any, Dict, Mapping, Sequence, Type
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .type_defs import (
+    GetActionRecommendationsResponseTypeDef,
+    GetPersonalizedRankingResponseTypeDef,
+    GetRecommendationsResponseTypeDef,
+    PromotionTypeDef,
+)
+
+__all__ = ("PersonalizeRuntimeClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    ClientError: Type[BotocoreClientError]
+    InvalidInputException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+
+
+class PersonalizeRuntimeClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        PersonalizeRuntimeClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client.exceptions)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client.can_paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/#can_paginate)
+        """
+
+    async def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client.close)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/#close)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client.generate_presigned_url)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/#generate_presigned_url)
+        """
+
+    async def get_action_recommendations(
+        self,
+        *,
+        campaignArn: str = ...,
+        userId: str = ...,
+        numResults: int = ...,
+        filterArn: str = ...,
+        filterValues: Mapping[str, str] = ...,
+    ) -> GetActionRecommendationsResponseTypeDef:
+        """
+        Returns a list of recommended actions in sorted in descending order by
+        prediction
+        score.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client.get_action_recommendations)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/#get_action_recommendations)
+        """
+
+    async def get_personalized_ranking(
+        self,
+        *,
+        campaignArn: str,
+        inputList: Sequence[str],
+        userId: str,
+        context: Mapping[str, str] = ...,
+        filterArn: str = ...,
+        filterValues: Mapping[str, str] = ...,
+        metadataColumns: Mapping[str, Sequence[str]] = ...,
+    ) -> GetPersonalizedRankingResponseTypeDef:
+        """
+        Re-ranks a list of recommended items for the given user.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client.get_personalized_ranking)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/#get_personalized_ranking)
+        """
+
+    async def get_recommendations(
+        self,
+        *,
+        campaignArn: str = ...,
+        itemId: str = ...,
+        userId: str = ...,
+        numResults: int = ...,
+        context: Mapping[str, str] = ...,
+        filterArn: str = ...,
+        filterValues: Mapping[str, str] = ...,
+        recommenderArn: str = ...,
+        promotions: Sequence[PromotionTypeDef] = ...,
+        metadataColumns: Mapping[str, Sequence[str]] = ...,
+    ) -> GetRecommendationsResponseTypeDef:
+        """
+        Returns a list of recommended items.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client.get_recommendations)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/#get_recommendations)
+        """
+
+    async def __aenter__(self) -> "PersonalizeRuntimeClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/personalize-runtime.html#PersonalizeRuntime.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/client/)
+        """
