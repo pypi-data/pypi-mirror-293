b1720d7def471a7ed269fac15cb4295b6cfc1da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 06:05:04 +0000

    Add package smartdataai-test/1.3/smartdataai_test-1.3.tar.gz

---
+++ packages/smartdataai-test/smartdataai_test-1.3.tar.gz/smartdataai_test-1.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="smartdataai_test",
+    version="1.3",
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'scikit-learn',
+        'langchain',
+        'langchain-community',
+        'langchain-core',
+        'langchain-experimental',
+        'langchain-openai'
+    ],
+    include_package_data=True,
+    description='A package for SmartData management and operations.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Ensure this is correct for Markdown
+    author='Talent AI Now',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/smartdataai',  # Update with your repository URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
