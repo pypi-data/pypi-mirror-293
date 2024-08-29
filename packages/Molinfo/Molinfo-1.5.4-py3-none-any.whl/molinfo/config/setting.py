79bc2337de1801529a1863a265678bc91e32ade7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 06:04:10 +0000

    Add package Molinfo/1.5.4/Molinfo-1.5.4-py3-none-any.whl

---
+++ packages/Molinfo/Molinfo-1.5.4-py3-none-any.whl/molinfo/config/setting.py
@@ -0,0 +1,11 @@
+# PACKAGE INFO
+# --------------
+
+# package name
+packageName = 'Molecular Information'
+# package short name
+packageShortName = 'Molinfo'
+# package version
+__version__ = '1.5.4'
+# package description
+__description__ = 'A Python package for advanced molecular analysis by converting molecular structures into graph representations.'
