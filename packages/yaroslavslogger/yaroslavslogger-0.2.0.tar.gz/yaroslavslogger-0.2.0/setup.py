2f07dc518a68172cd7edd97eb50ab186525b77c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:27 +0000

    Add package yaroslavslogger/0.2.0/yaroslavslogger-0.2.0.tar.gz

---
+++ packages/yaroslavslogger/yaroslavslogger-0.2.0.tar.gz/yaroslavslogger-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yaroslavslogger',
+    version='0.2.0',
+    author='YaroslavS',
+    author_email='yarosmr@gmail.com',
+    description='A simple logging helper for Python projects',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
