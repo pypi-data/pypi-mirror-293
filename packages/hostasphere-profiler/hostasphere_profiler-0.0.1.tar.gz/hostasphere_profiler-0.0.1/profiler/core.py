3aa1fd9c780a9b0133a3b6d90b3724374d28807a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:04:01 +0000

    Add package hostasphere-profiler/0.0.1/hostasphere_profiler-0.0.1.tar.gz

---
+++ packages/hostasphere-profiler/hostasphere_profiler-0.0.1.tar.gz/hostasphere_profiler-0.0.1/profiler/core.py
@@ -0,0 +1,26 @@
+import time
+import requests
+from .utils import get_function_name, get_memory_usage
+from .config import ENDPOINT_URL
+from .logger import log_info
+
+def probe():
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+
+            data = {
+                'function_name': get_function_name(func),
+                'execution_time': end_time - start_time,
+                'memory_usage': get_memory_usage(),
+                'timestamp': time.time(),
+            }
+
+            log_info(f"Sending data for {data['function_name']}: {data}")
+            requests.post(ENDPOINT_URL, json=data)
+
+            return result
+        return wrapper
+    return decorator
