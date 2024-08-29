a70ec10c0ee7406c914c78d962457da26b74f06c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 07:04:00 +0000

    Add package dooGL/0.1.1/doogl-0.1.1.tar.gz

---
+++ packages/dooGL/doogl-0.1.1.tar.gz/doogl-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dooGL",  # The name of your package
+    version="0.1.1",  # Initial version
+    packages=find_packages(),  # Automatically discover all modules
+    python_requires='>=3.6',  # Minimum Python version requirement
+)
