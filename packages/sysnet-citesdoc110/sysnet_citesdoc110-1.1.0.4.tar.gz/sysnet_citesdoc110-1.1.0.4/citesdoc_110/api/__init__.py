32531d30fedb81b3c8cb3736268170767a9ad8f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 05:03:16 +0000

    Add package sysnet-citesdoc110/1.1.0.4/sysnet_citesdoc110-1.1.0.4.tar.gz

---
+++ packages/sysnet-citesdoc110/sysnet_citesdoc110-1.1.0.4.tar.gz/sysnet_citesdoc110-1.1.0.4/citesdoc_110/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from citesdoc_110.api.additional_api import AdditionalApi
+from citesdoc_110.api.admins_api import AdminsApi
+from citesdoc_110.api.attachments_api import AttachmentsApi
+from citesdoc_110.api.developers_api import DevelopersApi
+from citesdoc_110.api.permit_api import PermitApi
+from citesdoc_110.api.public_api import PublicApi
+from citesdoc_110.api.registration_cards_api import RegistrationCardsApi
+from citesdoc_110.api.statement_api import StatementApi
