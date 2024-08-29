c3915e65c2d0752a72705efcaaf110cb540345d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:12 +0000

    Add package importio-client/0.1.2/importio_client-0.1.2.tar.gz

---
+++ packages/importio-client/importio_client-0.1.2.tar.gz/importio_client-0.1.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="importio-client",
+    version="0.1.2",
+    author="Nick Dekker",
+    author_email="",
+    description="A client library for interacting with the ImportIO API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nickdkr/import-io-hook",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+    include_package_data=True,
+    package_data={
+        "importio_client": ["LICENSE"],
+    },
+)
---
+++ packages/importio-client/importio_client-0.1.2.tar.gz/importio_client-0.1.2/src/importio_client/__init__.py
@@ -0,0 +1,27 @@
+"""
+ImportIO Client
+
+This module provides a client for interacting with the ImportIO API.
+"""
+
+from client import (
+    ImportIOClient,
+    ExtractorEndpoint,
+    CrawlRunEndpoint,
+    ImportIOError,
+    ExtractorNotFound,
+    StatusCodeError,
+    FileType
+)
+
+__all__ = [
+    'ImportIOClient',
+    'ExtractorEndpoint',
+    'CrawlRunEndpoint',
+    'ImportIOError',
+    'ExtractorNotFound',
+    'StatusCodeError',
+    'FileType'
+]
+
+__version__ = "0.1.2"
