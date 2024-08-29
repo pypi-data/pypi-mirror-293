3f0574cfee05f994b53cf6f003d272210f49f48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:05:08 +0000

    Add package annofab-3dpc-editor-cli/0.2.2/annofab_3dpc_editor_cli-0.2.2-py3-none-any.whl

---
+++ packages/annofab-3dpc-editor-cli/annofab_3dpc_editor_cli-0.2.2-py3-none-any.whl/anno3d/annofab/client.py
@@ -0,0 +1,32 @@
+from contextlib import contextmanager
+from typing import Generator, Optional
+
+import annofabapi
+from annofabapi import AnnofabApi, Resource
+
+
+class ClientLoader:
+    _annofab_id: str
+    _annofab_pass: str
+    _annofab_endpoint: Optional[str]
+
+    def __init__(self, annofab_id: str, annofab_pass: str, endpoint: Optional[str]) -> None:
+        self._annofab_id = annofab_id
+        self._annofab_pass = annofab_pass
+        self._annofab_endpoint = endpoint
+
+    @contextmanager
+    def open_resource(self) -> Generator[Resource, None, None]:
+        endpoint = (
+            self._annofab_endpoint if self._annofab_endpoint is not None else annofabapi.resource.DEFAULT_ENDPOINT_URL
+        )
+        resource = annofabapi.build(self._annofab_id, self._annofab_pass, endpoint_url=endpoint)
+        try:
+            yield resource
+        finally:
+            resource.api.session.close()
+
+    @contextmanager
+    def open_api(self) -> Generator[AnnofabApi, None, None]:
+        with self.open_resource() as r:
+            yield r.api
