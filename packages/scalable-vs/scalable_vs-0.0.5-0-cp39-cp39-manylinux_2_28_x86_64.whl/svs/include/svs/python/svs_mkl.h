04cc88ca6fabee02f5c3ad60a0e681f8220d419d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:24 +0000

    Add package scalable-vs/0.0.5/scalable_vs-0.0.5-0-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/scalable-vs/scalable_vs-0.0.5-0-cp39-cp39-manylinux_2_28_x86_64.whl/svs/include/svs/python/svs_mkl.h
@@ -0,0 +1,25 @@
+/**
+ *    Copyright (C) 2023, Intel Corporation
+ *
+ *    You can redistribute and/or modify this software under the terms of the
+ *    GNU Affero General Public License version 3.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    version 3 along with this software. If not, see
+ *    <https://www.gnu.org/licenses/agpl-3.0.en.html>.
+ */
+
+#include <optional>
+
+namespace svs::python {
+
+/// Return `true` if svs was build to link with Intel(R) MKL. Otherwise, return `false`.
+bool have_mkl();
+
+///
+/// @brief Return the number of threads used by Intel(R) MKL.
+///
+/// If ``have_mkl()`` returns false, return an empty optional.
+std::optional<size_t> mkl_num_threads();
+
+} // namespace svs::python
