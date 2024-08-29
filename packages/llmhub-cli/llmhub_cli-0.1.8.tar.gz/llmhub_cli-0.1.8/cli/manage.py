4a4fe71e11b75098a0a8b324c564969cb545939b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:04:15 +0000

    Add package llmhub-cli/0.1.8/llmhub_cli-0.1.8.tar.gz

---
+++ packages/llmhub-cli/llmhub_cli-0.1.8.tar.gz/llmhub_cli-0.1.8/cli/manage.py
@@ -0,0 +1,47 @@
+import click
+from llmhub_lib.app_dependency_container import AppDependencyContainer
+from llmhub_lib.helpers import start_proxy_process, stop_proxy_process
+
+service_manager = AppDependencyContainer.get("service_manager")
+
+@click.command()
+@click.argument('model_name', required=False)
+def start(model_name=None):
+    """Start a specific model process or all processes if no model_name is provided."""
+    if model_name and model_name.startswith("proxy-"):
+        start_proxy_process(service_manager)
+    elif model_name:
+        service_manager.start_service(model_name, quant="Q5_K_M")  # Example quant value
+    else:
+        service_manager.update_services()
+
+@click.command()
+@click.argument('model_name', required=False)
+def stop(model_name=None):
+    """Stop a specific process or all processes if no model_name is provided."""
+    # Stop the proxy process if the model_name starts with "proxy-"
+    if model_name and model_name.startswith("proxy-"):
+        stop_proxy_process(service_manager)
+    elif model_name:
+        service_manager.stop_service(model_name)
+    else:
+        service_manager.stop_all_services()
+
+@click.command()
+@click.argument('model_name', required=False)
+def restart(model_name=None):
+    """Restart a specific process or all processes if no model_name is provided."""
+    if model_name and model_name.startswith("proxy-"):
+        stop_proxy_process(service_manager)
+        start_proxy_process(service_manager)
+    elif model_name:
+        service_manager.stop_service(model_name)
+        service_manager.start_service(model_name, quant="Q5_K_M")  # Example quant value
+    else:
+        service_manager.stop_all_services()
+        service_manager.update_services()
+
+@click.command()
+def update():
+    """Update and restart all configured processes."""
+    service_manager.update_services()
---
+++ packages/llmhub-cli/llmhub_cli-0.1.8.tar.gz/llmhub_cli-0.1.8/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llmhub-cli",
+    version="0.1.8",
+    author="Jacob Mather",
+    author_email="jmather@jmather.com",
+    description="LLMHub is a lightweight management platform designed to streamline working with LLMs.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jmather/llmhub",  # Replace with your project's URL
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "flask",  # Add any other dependencies your project has
+        "psutil",
+        "requests",
+        "click",
+        "PyYAML"
+    ],
+    entry_points={
+        'console_scripts': [
+            'llmhub=cli:cli',
+        ],
+    },
+    package_data={
+        'llmhub_lib': ['web_server.py'],  # Include the web_server.py file in the package
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
