eb876fed257f5f157762bebcd94f85d0a69b76fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:08 +0000

    Add package emcie-common/0.2.0a2/emcie_common-0.2.0a2-py3-none-any.whl

---
+++ packages/emcie-common/emcie_common-0.2.0a2-py3-none-any.whl/emcie/common/base_models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class DefaultBaseModel(BaseModel):
+    """
+    Base class for all Emcie Pydantic models.
+    """
+
+    model_config = ConfigDict(
+        extra="forbid",
+        validate_default=True,
+        use_enum_values=True,
+    )
---
+++ packages/emcie-common/emcie_common-0.2.0a2-py3-none-any.whl/emcie/common/tools.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Literal, Mapping, NewType, TypedDict, Union
+from typing_extensions import NotRequired
+
+from emcie.common.types import JSONSerializable
+
+
+ToolId = NewType("ToolId", str)
+
+ToolParameterType = Literal[
+    "string",
+    "number",
+    "integer",
+    "boolean",
+]
+
+
+class ToolParameter(TypedDict):
+    type: ToolParameterType
+    description: NotRequired[str]
+    enum: NotRequired[list[Union[str, int, float, bool]]]
+
+
+class ToolContext:
+    pass
+
+
+@dataclass(frozen=True)
+class ToolResult:
+    data: JSONSerializable
+    metadata: Mapping[str, JSONSerializable] = field(default_factory=dict)
+
+
+@dataclass(frozen=True)
+class Tool:
+    id: ToolId
+    creation_utc: datetime
+    name: str
+    description: str
+    parameters: dict[str, ToolParameter]
+    required: list[str]
+    consequential: bool
+
+    def __hash__(self) -> int:
+        return hash(self.id)
