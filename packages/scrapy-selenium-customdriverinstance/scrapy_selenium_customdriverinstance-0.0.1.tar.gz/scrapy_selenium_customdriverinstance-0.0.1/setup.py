cd484d055960bf68a79c9763a6f5c7fe21a097a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:03:58 +0000

    Add package scrapy-selenium-customdriverinstance/0.0.1/scrapy_selenium_customdriverinstance-0.0.1.tar.gz

---
+++ packages/scrapy-selenium-customdriverinstance/scrapy_selenium_customdriverinstance-0.0.1.tar.gz/scrapy_selenium_customdriverinstance-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from os import path
+
+# with open('requirements.txt') as reqh:
+#     install_reqs = reqh.readlines()
+
+# with open(path.join(path.dirname(__file__), 'VERSION')) as v:
+#     VERSION = v.readline().strip()
+
+# with open('README.md') as f:
+#     long_description = f.read()
+
+setup(
+    # packages=find_packages(),
+    # install_requires=install_reqs,
+)
+
+
