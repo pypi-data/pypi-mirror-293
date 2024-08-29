793fd036ae2156c9c972b2ec23131279c617af76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:22 +0000

    Add package jax-relax/0.2.8/jax_relax-0.2.8-py3-none-any.whl

---
+++ packages/jax-relax/jax_relax-0.2.8-py3-none-any.whl/relax/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.2.8"
+
+from .data_module import DataModule, DataModuleConfig, load_data
+from .data_utils import (
+    Feature, FeaturesList
+)
+from .ml_model import MLModule, MLModuleConfig, load_ml_module
+from .explain import generate_cf_explanations
+from .evaluate import evaluate_cfs, benchmark_cfs
---
+++ packages/jax-relax/jax_relax-0.2.8-py3-none-any.whl/relax/methods/__init__.py
@@ -0,0 +1,10 @@
+from .base import CFModule, ParametricCFModule
+from .vanilla import VanillaCFConfig, VanillaCF
+from .dice import DiverseCF, DiverseCFConfig
+from .proto import ProtoCF, ProtoCFConfig
+from .counternet import CounterNet, CounterNetConfig
+from .sphere import GrowingSphere, GSConfig
+from .vaecf import VAECF, VAECFConfig
+from .cchvae import CCHVAE, CCHVAEConfig
+from .clue import CLUEConfig, CLUE
+from .l2c import L2C, L2CConfig
