f816336fd5b11c6d26aabccd1e42d5f664bee6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:30 +0000

    Add package tq42-grpc-client/1.0.185/tq42_grpc_client-1.0.185-py3-none-any.whl

---
+++ packages/tq42-grpc-client/tq42_grpc_client-1.0.185-py3-none-any.whl/com/terraquantum/role/v1/role/role_service_pb2.pyi
@@ -0,0 +1,5 @@
+from com.terraquantum.role.v1.role import check_permissions_pb2 as _check_permissions_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
