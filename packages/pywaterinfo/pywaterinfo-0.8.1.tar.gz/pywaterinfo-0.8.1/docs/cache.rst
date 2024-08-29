bc5c03e12f115dc58b8ba92e1a56c373cb7faf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:12 +0000

    Add package pywaterinfo/0.8.1/pywaterinfo-0.8.1.tar.gz

---
+++ packages/pywaterinfo/pywaterinfo-0.8.1.tar.gz/pywaterinfo-0.8.1/docs/cache.rst
@@ -0,0 +1,44 @@
+
+.. _cache:
+
+Caching
+=======
+
+To overcome redundant requests of the same data to the waterinfo endpoints, the package relies on the `requests-cache <https://pypi.org/project/requests-cache/>`_ package to cache the :code:`GET` requests. The
+cache feature is opt-in (not installed/activated by default), but can be used after installing the cache dependency packages:
+
+::
+
+    pip install pywaterinfo[cache]
+
+To use the cache, activate it on the initialization of the ``Waterinfo`` class:
+
+::
+
+    from pywaterinfo import Waterinfo
+    vmm = Waterinfo("vmm", cache=True)
+
+The cache is stored in a sqlite database called :code:`pywaterinfo_cache.sqlite` and the default retention time of the cache is 7 days.
+When you want to make sure to not use a cached requests, make sure to empty the cache first using the :func:`~pywaterinfo.Waterinfo.clear_cache` method:
+
+::
+
+    from pywaterinfo import Waterinfo
+    vmm = Waterinfo("vmm")
+    vmm.clear_cache()
+    vmm.get_timeseries_list(station_no="ME09_012")
+
+
+To see if the response is retrieved from cache or not, activate the logging, see `page on logging <logging>`_.
+
+.. warning::
+
+    When downloading a lot of (small) data with many requests in a short amount of time the cache can grow quickly. This increases the computational time to check if a request already has been made and can be retrieved from the cache. Clearing the cache can overcome increasing initialization time:
+
+    ::
+
+        from pywaterinfo import Waterinfo
+        vmm = Waterinfo("vmm")
+        vmm.clear_cache()
+
+    When the initialization time (Creating the ``Waterinfo`` class) goes up to more than a minute without a network timeout error, clearing the cache is worthwhile to test.
---
+++ packages/pywaterinfo/pywaterinfo-0.8.1.tar.gz/pywaterinfo-0.8.1/tests/test_cache.py
@@ -0,0 +1,82 @@
+import time
+
+from pywaterinfo import Waterinfo
+
+
+def test_cache_vmm(vmm_cached_connection):
+    """Second call of the same request is retrieved from cache for VMM requests."""
+    vmm_cached_connection.clear_cache()
+    data, res = vmm_cached_connection.request_kiwis({"request": "getRequestInfo"})
+    assert not res.from_cache
+    data, res = vmm_cached_connection.request_kiwis({"request": "getRequestInfo"})
+    assert res.from_cache
+
+
+def test_cache_hic(hic_cached_connection):
+    """Second call of the same request is retrieved from cache for HIC requests."""
+    hic_cached_connection.clear_cache()
+    # patch/exclude cache control to use retention time only
+    hic_cached_connection._request.settings.cache_control = False
+    data, res = hic_cached_connection.request_kiwis(
+        query={"request": "getTimeseriesValueLayer", "timeseriesgroup_id": "156207"}
+    )
+    assert not res.from_cache
+    data, res = hic_cached_connection.request_kiwis(
+        query={"request": "getTimeseriesValueLayer", "timeseriesgroup_id": "156207"}
+    )
+    assert res.from_cache
+
+
+def test_clear_cache(vmm_cached_connection):
+    """Cache is cleared."""
+    data, res = vmm_cached_connection.request_kiwis({"request": "getRequestInfo"})
+    assert not res.from_cache
+    vmm_cached_connection.clear_cache()
+    data, res = vmm_cached_connection.request_kiwis({"request": "getRequestInfo"})
+    assert not res.from_cache
+
+
+def test_cache_retention_between_sessions():
+    """Requests are cached in between two sessions."""
+    vmm = Waterinfo("vmm", cache=True)
+    vmm.clear_cache()
+    _, res = vmm.request_kiwis({"request": "getRequestInfo"})
+    assert not res.from_cache
+
+    # New session reuses the same database
+    vmm = Waterinfo("vmm", cache=True)
+    _, res = vmm.request_kiwis({"request": "getRequestInfo"})
+    assert res.from_cache
+
+
+def test_cache_retention(patch_retention):
+    """Request is not from cache after expiration date.
+
+    Uses monkeypatch version of the CACHE_RETENTION timing for unit testing.
+
+    Notes
+    -----
+    See requests-cache.readthedocs.io/en/stable/user_guide/headers.html,
+    conditional requests are automatically sent for any servers that support them. Once
+    a cached response expires, it will only be updated if the
+    remote content has changed. Hence, we check here for expiration first, remove the
+    expired cache and check for `from_cache` in a new request.
+    """
+    vmm = Waterinfo("vmm", cache=True)
+    vmm.clear_cache()
+    _, res = vmm.request_kiwis({"request": "getRequestInfo"})
+    assert not res.from_cache
+
+    time.sleep(1)
+    _, res = vmm.request_kiwis({"request": "getRequestInfo"})
+    assert vmm._request.cache.contains(  # noqa
+        url="https://download.waterinfo.be/tsmdownload/KiWIS/KiWIS?"
+        "request=getRequestInfo&service=kisters&type=QueryServices&"
+        "format=json&datasource=1&timezone=UTC"
+    )
+    time.sleep(1)
+    assert res.is_expired
+
+    vmm._request.cache.delete(expired=True)
+    _, res = vmm.request_kiwis({"request": "getRequestInfo"})
+    assert not res.from_cache
