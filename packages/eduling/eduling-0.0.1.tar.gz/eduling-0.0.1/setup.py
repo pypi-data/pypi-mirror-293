26049f52399caecbdcd2c03f75d957a547ef6c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 07:04:06 +0000

    Add package eduling/0.0.1/eduling-0.0.1.tar.gz

---
+++ packages/eduling/eduling-0.0.1.tar.gz/eduling-0.0.1/setup.py
@@ -0,0 +1,18 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='eduling',
+    version='0.0.1',
+    packages=find_packages(),
+    description='Un módulo de enciclopedia y diccionario para las funciones de Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Lyonnelv20',
+    author_email='vidalnelson112@gmail.com',
+    url='https://gitlab.com/vidalnelson112/eduling',  # Reemplaza con la URL de tu repositorio
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
