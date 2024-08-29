a2edd2e1701d21ee30ae459decc6d61ad5a641c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 05:04:30 +0000

    Add package distlh/0.1/distlh-0.1.tar.gz

---
+++ packages/distlh/distlh-0.1.tar.gz/distlh-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='distlh',
+    version='0.1',
+    description='Gaussian and Binomial distributions',
+    packages=['distlh'],
+    author="Linh Hoang",
+    author_email="linhht.419@gmail.com",
+    zip_safe=False
+)
