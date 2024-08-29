82230f4b3333c5ee0aa46601831afa83f994af71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:03:55 +0000

    Add package types-aiobotocore-datapipeline/2.14.0/types_aiobotocore_datapipeline-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-datapipeline/types_aiobotocore_datapipeline-2.14.0-py3-none-any.whl/types_aiobotocore_datapipeline/client.pyi
@@ -0,0 +1,386 @@
+"""
+Type annotations for datapipeline service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_datapipeline.client import DataPipelineClient
+
+    session = get_session()
+    async with session.create_client("datapipeline") as client:
+        client: DataPipelineClient
+    ```
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Sequence, Type, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .literals import TaskStatusType
+from .paginator import DescribeObjectsPaginator, ListPipelinesPaginator, QueryObjectsPaginator
+from .type_defs import (
+    CreatePipelineOutputTypeDef,
+    DescribeObjectsOutputTypeDef,
+    DescribePipelinesOutputTypeDef,
+    EmptyResponseMetadataTypeDef,
+    EvaluateExpressionOutputTypeDef,
+    FieldTypeDef,
+    GetPipelineDefinitionOutputTypeDef,
+    InstanceIdentityTypeDef,
+    ListPipelinesOutputTypeDef,
+    ParameterObjectUnionTypeDef,
+    ParameterValueTypeDef,
+    PipelineObjectUnionTypeDef,
+    PollForTaskOutputTypeDef,
+    PutPipelineDefinitionOutputTypeDef,
+    QueryObjectsOutputTypeDef,
+    QueryTypeDef,
+    ReportTaskProgressOutputTypeDef,
+    ReportTaskRunnerHeartbeatOutputTypeDef,
+    TagTypeDef,
+    TimestampTypeDef,
+    ValidatePipelineDefinitionOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("DataPipelineClient",)
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    ClientError: Type[BotocoreClientError]
+    InternalServiceError: Type[BotocoreClientError]
+    InvalidRequestException: Type[BotocoreClientError]
+    PipelineDeletedException: Type[BotocoreClientError]
+    PipelineNotFoundException: Type[BotocoreClientError]
+    TaskNotFoundException: Type[BotocoreClientError]
+
+class DataPipelineClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        DataPipelineClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.exceptions)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#exceptions)
+        """
+
+    async def activate_pipeline(
+        self,
+        *,
+        pipelineId: str,
+        parameterValues: Sequence[ParameterValueTypeDef] = ...,
+        startTimestamp: TimestampTypeDef = ...,
+    ) -> Dict[str, Any]:
+        """
+        Validates the specified pipeline and starts processing pipeline tasks.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.activate_pipeline)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#activate_pipeline)
+        """
+
+    async def add_tags(self, *, pipelineId: str, tags: Sequence[TagTypeDef]) -> Dict[str, Any]:
+        """
+        Adds or modifies tags for the specified pipeline.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.add_tags)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#add_tags)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.can_paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#can_paginate)
+        """
+
+    async def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.close)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#close)
+        """
+
+    async def create_pipeline(
+        self, *, name: str, uniqueId: str, description: str = ..., tags: Sequence[TagTypeDef] = ...
+    ) -> CreatePipelineOutputTypeDef:
+        """
+        Creates a new, empty pipeline.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.create_pipeline)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#create_pipeline)
+        """
+
+    async def deactivate_pipeline(
+        self, *, pipelineId: str, cancelActive: bool = ...
+    ) -> Dict[str, Any]:
+        """
+        Deactivates the specified running pipeline.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.deactivate_pipeline)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#deactivate_pipeline)
+        """
+
+    async def delete_pipeline(self, *, pipelineId: str) -> EmptyResponseMetadataTypeDef:
+        """
+        Deletes a pipeline, its pipeline definition, and its run history.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.delete_pipeline)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#delete_pipeline)
+        """
+
+    async def describe_objects(
+        self,
+        *,
+        pipelineId: str,
+        objectIds: Sequence[str],
+        evaluateExpressions: bool = ...,
+        marker: str = ...,
+    ) -> DescribeObjectsOutputTypeDef:
+        """
+        Gets the object definitions for a set of objects associated with the pipeline.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.describe_objects)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#describe_objects)
+        """
+
+    async def describe_pipelines(
+        self, *, pipelineIds: Sequence[str]
+    ) -> DescribePipelinesOutputTypeDef:
+        """
+        Retrieves metadata about one or more pipelines.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.describe_pipelines)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#describe_pipelines)
+        """
+
+    async def evaluate_expression(
+        self, *, pipelineId: str, objectId: str, expression: str
+    ) -> EvaluateExpressionOutputTypeDef:
+        """
+        Task runners call `EvaluateExpression` to evaluate a string in the context of
+        the specified
+        object.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.evaluate_expression)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#evaluate_expression)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.generate_presigned_url)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#generate_presigned_url)
+        """
+
+    async def get_pipeline_definition(
+        self, *, pipelineId: str, version: str = ...
+    ) -> GetPipelineDefinitionOutputTypeDef:
+        """
+        Gets the definition of the specified pipeline.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.get_pipeline_definition)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#get_pipeline_definition)
+        """
+
+    async def list_pipelines(self, *, marker: str = ...) -> ListPipelinesOutputTypeDef:
+        """
+        Lists the pipeline identifiers for all active pipelines that you have
+        permission to
+        access.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.list_pipelines)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#list_pipelines)
+        """
+
+    async def poll_for_task(
+        self,
+        *,
+        workerGroup: str,
+        hostname: str = ...,
+        instanceIdentity: InstanceIdentityTypeDef = ...,
+    ) -> PollForTaskOutputTypeDef:
+        """
+        Task runners call `PollForTask` to receive a task to perform from AWS Data
+        Pipeline.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.poll_for_task)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#poll_for_task)
+        """
+
+    async def put_pipeline_definition(
+        self,
+        *,
+        pipelineId: str,
+        pipelineObjects: Sequence[PipelineObjectUnionTypeDef],
+        parameterObjects: Sequence[ParameterObjectUnionTypeDef] = ...,
+        parameterValues: Sequence[ParameterValueTypeDef] = ...,
+    ) -> PutPipelineDefinitionOutputTypeDef:
+        """
+        Adds tasks, schedules, and preconditions to the specified pipeline.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.put_pipeline_definition)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#put_pipeline_definition)
+        """
+
+    async def query_objects(
+        self,
+        *,
+        pipelineId: str,
+        sphere: str,
+        query: QueryTypeDef = ...,
+        marker: str = ...,
+        limit: int = ...,
+    ) -> QueryObjectsOutputTypeDef:
+        """
+        Queries the specified pipeline for the names of objects that match the
+        specified set of
+        conditions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.query_objects)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#query_objects)
+        """
+
+    async def remove_tags(self, *, pipelineId: str, tagKeys: Sequence[str]) -> Dict[str, Any]:
+        """
+        Removes existing tags from the specified pipeline.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.remove_tags)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#remove_tags)
+        """
+
+    async def report_task_progress(
+        self, *, taskId: str, fields: Sequence[FieldTypeDef] = ...
+    ) -> ReportTaskProgressOutputTypeDef:
+        """
+        Task runners call `ReportTaskProgress` when assigned a task to acknowledge that
+        it has the
+        task.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.report_task_progress)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#report_task_progress)
+        """
+
+    async def report_task_runner_heartbeat(
+        self, *, taskrunnerId: str, workerGroup: str = ..., hostname: str = ...
+    ) -> ReportTaskRunnerHeartbeatOutputTypeDef:
+        """
+        Task runners call `ReportTaskRunnerHeartbeat` every 15 minutes to indicate that
+        they are
+        operational.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.report_task_runner_heartbeat)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#report_task_runner_heartbeat)
+        """
+
+    async def set_status(
+        self, *, pipelineId: str, objectIds: Sequence[str], status: str
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Requests that the status of the specified physical or logical pipeline objects
+        be updated in the specified
+        pipeline.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.set_status)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#set_status)
+        """
+
+    async def set_task_status(
+        self,
+        *,
+        taskId: str,
+        taskStatus: TaskStatusType,
+        errorId: str = ...,
+        errorMessage: str = ...,
+        errorStackTrace: str = ...,
+    ) -> Dict[str, Any]:
+        """
+        Task runners call `SetTaskStatus` to notify AWS Data Pipeline that a task is
+        completed and provide information about the final
+        status.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.set_task_status)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#set_task_status)
+        """
+
+    async def validate_pipeline_definition(
+        self,
+        *,
+        pipelineId: str,
+        pipelineObjects: Sequence[PipelineObjectUnionTypeDef],
+        parameterObjects: Sequence[ParameterObjectUnionTypeDef] = ...,
+        parameterValues: Sequence[ParameterValueTypeDef] = ...,
+    ) -> ValidatePipelineDefinitionOutputTypeDef:
+        """
+        Validates the specified pipeline definition to ensure that it is well formed
+        and can be run without
+        error.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.validate_pipeline_definition)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#validate_pipeline_definition)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["describe_objects"]
+    ) -> DescribeObjectsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.get_paginator)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["list_pipelines"]) -> ListPipelinesPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.get_paginator)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#get_paginator)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["query_objects"]) -> QueryObjectsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client.get_paginator)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> "DataPipelineClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/datapipeline.html#DataPipeline.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_datapipeline/client/)
+        """
