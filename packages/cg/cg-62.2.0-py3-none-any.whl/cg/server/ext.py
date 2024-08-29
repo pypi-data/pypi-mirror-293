f48aa994aa054beac4b225fd08dd8e6ff72334ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:03:57 +0000

    Add package cg/62.2.0/cg-62.2.0-py3-none-any.whl

---
+++ packages/cg/cg-62.2.0-py3-none-any.whl/cg/server/ext.py
@@ -0,0 +1,97 @@
+from decimal import Decimal
+from json import JSONEncoder
+
+from flask_admin import Admin
+from flask_cors import CORS
+from flask_wtf.csrf import CSRFProtect
+
+from cg.apps.lims import LimsAPI
+from cg.apps.osticket import OsTicket
+from cg.apps.tb.api import TrailblazerAPI
+from cg.services.delivery_message.delivery_message_service import DeliveryMessageService
+from cg.services.sample_run_metrics_service.sample_run_metrics_service import (
+    SampleRunMetricsService,
+)
+from cg.services.orders.order_service.order_service import OrderService
+from cg.services.orders.order_summary_service.order_summary_service import (
+    OrderSummaryService,
+)
+from cg.services.orders.submitters.order_submitter_registry import (
+    OrderSubmitterRegistry,
+    setup_order_submitter_registry,
+)
+from cg.services.sample_service.sample_service import SampleService
+from cg.store.database import initialize_database
+from cg.store.store import Store
+
+
+class FlaskLims(LimsAPI):
+    def __init__(self, app=None):
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        config = {
+            "lims": {
+                "host": app.config["lims_host"],
+                "username": app.config["lims_username"],
+                "password": app.config["lims_password"],
+            }
+        }
+        super(FlaskLims, self).__init__(config)
+
+
+class FlaskStore(Store):
+    def __init__(self, app=None):
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        uri = app.config["cg_sql_database_uri"]
+        initialize_database(uri)
+        super(FlaskStore, self).__init__()
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super().default(obj)
+
+
+class AnalysisClient(TrailblazerAPI):
+    def __init__(self, app=None):
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        service_account: str = app.config["trailblazer_service_account"]
+        service_account_auth_file: str = app.config["trailblazer_service_account_auth_file"]
+        host: str = app.config["trailblazer_host"]
+        config = {
+            "trailblazer": {
+                "service_account": service_account,
+                "service_account_auth_file": service_account_auth_file,
+                "host": host,
+            }
+        }
+        super(AnalysisClient, self).__init__(config)
+
+
+cors = CORS(resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+csrf = CSRFProtect()
+db = FlaskStore()
+
+admin = Admin(name="Clinical Genomics")
+lims = FlaskLims()
+osticket = OsTicket()
+analysis_client = AnalysisClient()
+delivery_message_service = DeliveryMessageService(store=db, trailblazer_api=analysis_client)
+summary_service = OrderSummaryService(store=db, analysis_client=analysis_client)
+order_service = OrderService(store=db, status_service=summary_service)
+sample_service = SampleService(db)
+flow_cell_service = SampleRunMetricsService(db)
+order_submitter_registry: OrderSubmitterRegistry = setup_order_submitter_registry(
+    lims=lims,
+    status_db=db,
+)
