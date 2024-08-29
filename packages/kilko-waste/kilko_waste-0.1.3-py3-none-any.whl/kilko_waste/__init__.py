bbd89094a0c49e85109f93a9d82aed812bc11314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:28 +0000

    Add package kilko-waste/0.1.3/kilko_waste-0.1.3-py3-none-any.whl

---
+++ packages/kilko-waste/kilko_waste-0.1.3-py3-none-any.whl/kilko_waste/__init__.py
@@ -0,0 +1,7 @@
+from .requests import login_request, balance_request, config_request, events_request
+from .definitions import (
+    LoginRequest,
+    LoginResponse,
+    AuthenticatedRequest,
+    BalanceResponse,
+)
