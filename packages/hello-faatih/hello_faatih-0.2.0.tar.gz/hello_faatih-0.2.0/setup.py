d2b4a72f281fa2c232a28cf03f36a834fb133e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 17:04:21 +0000

    Add package hello-faatih/0.2.0/hello_faatih-0.2.0.tar.gz

---
+++ packages/hello-faatih/hello_faatih-0.2.0.tar.gz/hello_faatih-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md",'r') as f:
+    description = f.read()
+
+setup(
+    name='hello_faatih',
+    version='0.2.0',
+    packages=find_packages(),
+
+    long_description=description,
+    long_description_content_type='text/markdown'
+    
+
+
+)
