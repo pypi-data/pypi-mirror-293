e4537d5a5b9d90b6f8e9262e8526adfbbec85b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 05:04:37 +0000

    Add package tsst/0.0.10/tsst-0.0.10-py3-none-any.whl

---
+++ packages/tsst/tsst-0.0.10-py3-none-any.whl/tsst.py
@@ -0,0 +1,9 @@
+from modules.base import cal #line:1
+class stock ():#line:3
+    def test (O0O000O0O0000O000 ,O0000000O0OO0O00O ,O0O0OO0000OO000OO ):#line:4
+        O0O0OO0000OO000OO =123 #line:5
+        print (O0O0OO0000OO000OO )#line:6
+        return cal (O0000000O0OO0O00O ,O0O0OO0000OO000OO )#line:7
+if __name__ =='__main__':#line:9
+    stock ()#line:10
+    print ("123")
