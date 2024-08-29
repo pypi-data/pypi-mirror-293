6e5fa42cf0a4c69adc77f1a6b75b6712c16890fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:34 +0000

    Add package cuda-histogram/0.1.0/cuda_histogram-0.1.0-py3-none-any.whl

---
+++ packages/cuda-histogram/cuda_histogram-0.1.0-py3-none-any.whl/cuda_histogram/__init__.py
@@ -0,0 +1,20 @@
+"""
+`cuda-histogram` is a histogram filling package for GPUs.
+
+The package follows `UHI <https://uhi.readthedocs.io>`__ and keeps its API similar to
+`boost-histogram <https://github.com/scikit-hep/boost-histogram>`__
+`and hist <https://github.com/scikit-hep/hist>`__.
+"""
+
+from __future__ import annotations
+
+from cuda_histogram import axis
+from cuda_histogram.hist import Hist
+
+from ._version import version as __version__
+
+__all__: list[str] = [
+    "Hist",
+    "axis",
+    "__version__",
+]
