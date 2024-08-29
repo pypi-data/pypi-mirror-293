3e02e5a897e739ae4dbe3ba97764718d6a69c50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:46 +0000

    Add package adofaihelper/1.1.1/adofaihelper-1.1.1.tar.gz

---
+++ packages/adofaihelper/adofaihelper-1.1.1.tar.gz/adofaihelper-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adofaihelper',
+    version='1.1.1',
+    description='A module to process adofai file',
+    author='Lipcu',
+    author_email='li-p-cu@outlook.com',
+    packages=find_packages(),
+    install_requires=[],
+)
