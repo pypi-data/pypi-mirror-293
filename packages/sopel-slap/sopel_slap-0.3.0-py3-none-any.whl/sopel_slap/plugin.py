3e0d1f955515f66269e0edca6d4ce60e0b5c8bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 06:04:42 +0000

    Add package sopel-slap/0.3.0/sopel_slap-0.3.0-py3-none-any.whl

---
+++ packages/sopel-slap/sopel_slap-0.3.0-py3-none-any.whl/sopel_slap/plugin.py
@@ -0,0 +1,49 @@
+"""Sopel Slap plugin
+
+Plugin for Sopel that lets users slap each other in fun ways.
+
+Original slap.py copyright 2009, Michael Yanovich, yanovich.net.
+
+This reworked version for Sopel 7.1+ copyright 2024, dgw, technobabbl.es
+
+https://sopel.chat
+"""
+from __future__ import annotations
+
+from sopel import plugin
+
+from .config import SlapSection, do_configure
+from .util import slap
+
+
+def setup(bot):
+    bot.settings.define_section('slap', SlapSection)
+
+
+def configure(settings):
+    settings.define_section('slap', SlapSection)
+    do_configure(settings.slap)
+
+
+@plugin.commands('slap', 'slaps')
+def slap_command(bot, trigger):
+    """Slap a <target> (e.g. nickname)"""
+    target = trigger.group(3)
+
+    if target is None:
+        target = trigger.nick
+
+    return slap(bot, trigger, target)
+
+
+@plugin.ctcp('ACTION')
+@plugin.rule(r'^slaps (\w+)$')
+def slap_action(bot, trigger):
+    """Slap someone using the power of CTCP ACTION."""
+    target = trigger.group(1)
+
+    if target is None:
+        # no self-slaps via /me; just fail silently
+        return plugin.NOLIMIT
+
+    return slap(bot, trigger, target)
