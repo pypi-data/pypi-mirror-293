be4fee879ab7e3a357519add4faa2506862a57bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:03:52 +0000

    Add package refnx/0.1.48/refnx-0.1.48-cp310-cp310-win_amd64.whl

---
+++ packages/refnx/refnx-0.1.48-cp310-cp310-win_amd64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.48'
+version = '0.1.48'
+full_version = '0.1.48'
+git_revision = '02ecf3128e6526dec71ab0c5c2e2ac089769be4c'
+release = True
+if not release:
+    version = full_version
