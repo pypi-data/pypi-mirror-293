123d8f9f189c9b8bace9aa42a66cadecd126e91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:03:59 +0000

    Add package types-aiobotocore-eks/2.14.0/types_aiobotocore_eks-2.14.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-eks/types_aiobotocore_eks-2.14.0-py3-none-any.whl/types_aiobotocore_eks/paginator.pyi
@@ -0,0 +1,304 @@
+"""
+Type annotations for eks service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_eks.client import EKSClient
+    from types_aiobotocore_eks.paginator import (
+        DescribeAddonVersionsPaginator,
+        ListAccessEntriesPaginator,
+        ListAccessPoliciesPaginator,
+        ListAddonsPaginator,
+        ListAssociatedAccessPoliciesPaginator,
+        ListClustersPaginator,
+        ListEksAnywhereSubscriptionsPaginator,
+        ListFargateProfilesPaginator,
+        ListIdentityProviderConfigsPaginator,
+        ListInsightsPaginator,
+        ListNodegroupsPaginator,
+        ListPodIdentityAssociationsPaginator,
+        ListUpdatesPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("eks") as client:
+        client: EKSClient
+
+        describe_addon_versions_paginator: DescribeAddonVersionsPaginator = client.get_paginator("describe_addon_versions")
+        list_access_entries_paginator: ListAccessEntriesPaginator = client.get_paginator("list_access_entries")
+        list_access_policies_paginator: ListAccessPoliciesPaginator = client.get_paginator("list_access_policies")
+        list_addons_paginator: ListAddonsPaginator = client.get_paginator("list_addons")
+        list_associated_access_policies_paginator: ListAssociatedAccessPoliciesPaginator = client.get_paginator("list_associated_access_policies")
+        list_clusters_paginator: ListClustersPaginator = client.get_paginator("list_clusters")
+        list_eks_anywhere_subscriptions_paginator: ListEksAnywhereSubscriptionsPaginator = client.get_paginator("list_eks_anywhere_subscriptions")
+        list_fargate_profiles_paginator: ListFargateProfilesPaginator = client.get_paginator("list_fargate_profiles")
+        list_identity_provider_configs_paginator: ListIdentityProviderConfigsPaginator = client.get_paginator("list_identity_provider_configs")
+        list_insights_paginator: ListInsightsPaginator = client.get_paginator("list_insights")
+        list_nodegroups_paginator: ListNodegroupsPaginator = client.get_paginator("list_nodegroups")
+        list_pod_identity_associations_paginator: ListPodIdentityAssociationsPaginator = client.get_paginator("list_pod_identity_associations")
+        list_updates_paginator: ListUpdatesPaginator = client.get_paginator("list_updates")
+    ```
+"""
+
+from typing import AsyncIterator, Generic, Iterator, Sequence, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .literals import EksAnywhereSubscriptionStatusType
+from .type_defs import (
+    DescribeAddonVersionsResponseTypeDef,
+    InsightsFilterTypeDef,
+    ListAccessEntriesResponseTypeDef,
+    ListAccessPoliciesResponseTypeDef,
+    ListAddonsResponseTypeDef,
+    ListAssociatedAccessPoliciesResponseTypeDef,
+    ListClustersResponseTypeDef,
+    ListEksAnywhereSubscriptionsResponseTypeDef,
+    ListFargateProfilesResponseTypeDef,
+    ListIdentityProviderConfigsResponseTypeDef,
+    ListInsightsResponseTypeDef,
+    ListNodegroupsResponseTypeDef,
+    ListPodIdentityAssociationsResponseTypeDef,
+    ListUpdatesResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "DescribeAddonVersionsPaginator",
+    "ListAccessEntriesPaginator",
+    "ListAccessPoliciesPaginator",
+    "ListAddonsPaginator",
+    "ListAssociatedAccessPoliciesPaginator",
+    "ListClustersPaginator",
+    "ListEksAnywhereSubscriptionsPaginator",
+    "ListFargateProfilesPaginator",
+    "ListIdentityProviderConfigsPaginator",
+    "ListInsightsPaginator",
+    "ListNodegroupsPaginator",
+    "ListPodIdentityAssociationsPaginator",
+    "ListUpdatesPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class DescribeAddonVersionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.DescribeAddonVersions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#describeaddonversionspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        kubernetesVersion: str = ...,
+        addonName: str = ...,
+        types: Sequence[str] = ...,
+        publishers: Sequence[str] = ...,
+        owners: Sequence[str] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[DescribeAddonVersionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.DescribeAddonVersions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#describeaddonversionspaginator)
+        """
+
+class ListAccessEntriesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListAccessEntries)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listaccessentriespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        clusterName: str,
+        associatedPolicyArn: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListAccessEntriesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListAccessEntries.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listaccessentriespaginator)
+        """
+
+class ListAccessPoliciesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListAccessPolicies)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listaccesspoliciespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListAccessPoliciesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListAccessPolicies.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listaccesspoliciespaginator)
+        """
+
+class ListAddonsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListAddons)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listaddonspaginator)
+    """
+
+    def paginate(
+        self, *, clusterName: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListAddonsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListAddons.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listaddonspaginator)
+        """
+
+class ListAssociatedAccessPoliciesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListAssociatedAccessPolicies)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listassociatedaccesspoliciespaginator)
+    """
+
+    def paginate(
+        self, *, clusterName: str, principalArn: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListAssociatedAccessPoliciesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListAssociatedAccessPolicies.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listassociatedaccesspoliciespaginator)
+        """
+
+class ListClustersPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListClusters)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listclusterspaginator)
+    """
+
+    def paginate(
+        self, *, include: Sequence[str] = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListClustersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListClusters.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listclusterspaginator)
+        """
+
+class ListEksAnywhereSubscriptionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListEksAnywhereSubscriptions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listeksanywheresubscriptionspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        includeStatus: Sequence[EksAnywhereSubscriptionStatusType] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListEksAnywhereSubscriptionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListEksAnywhereSubscriptions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listeksanywheresubscriptionspaginator)
+        """
+
+class ListFargateProfilesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListFargateProfiles)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listfargateprofilespaginator)
+    """
+
+    def paginate(
+        self, *, clusterName: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListFargateProfilesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListFargateProfiles.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listfargateprofilespaginator)
+        """
+
+class ListIdentityProviderConfigsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListIdentityProviderConfigs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listidentityproviderconfigspaginator)
+    """
+
+    def paginate(
+        self, *, clusterName: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListIdentityProviderConfigsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListIdentityProviderConfigs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listidentityproviderconfigspaginator)
+        """
+
+class ListInsightsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListInsights)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listinsightspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        clusterName: str,
+        filter: InsightsFilterTypeDef = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListInsightsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListInsights.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listinsightspaginator)
+        """
+
+class ListNodegroupsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListNodegroups)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listnodegroupspaginator)
+    """
+
+    def paginate(
+        self, *, clusterName: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListNodegroupsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListNodegroups.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listnodegroupspaginator)
+        """
+
+class ListPodIdentityAssociationsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListPodIdentityAssociations)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listpodidentityassociationspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        clusterName: str,
+        namespace: str = ...,
+        serviceAccount: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListPodIdentityAssociationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListPodIdentityAssociations.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listpodidentityassociationspaginator)
+        """
+
+class ListUpdatesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListUpdates)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listupdatespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        name: str,
+        nodegroupName: str = ...,
+        addonName: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListUpdatesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks.html#EKS.Paginator.ListUpdates.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/paginators/#listupdatespaginator)
+        """
