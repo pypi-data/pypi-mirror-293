75d7b798048f7209642d4a5a4f4255c034791e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:23 +0000

    Add package FENICE/0.1.9/FENICE-0.1.9-py3-none-any.whl

---
+++ packages/FENICE/FENICE-0.1.9-py3-none-any.whl/metric/__init__.py
@@ -0,0 +1,10 @@
+from metric.FENICE import FENICE
+import spacy
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
