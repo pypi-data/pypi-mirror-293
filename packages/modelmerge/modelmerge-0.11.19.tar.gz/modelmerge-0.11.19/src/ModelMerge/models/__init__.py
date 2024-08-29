21157c7ebecd314acf91fa31af894e1f5ca49710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 06:05:04 +0000

    Add package modelmerge/0.11.19/modelmerge-0.11.19.tar.gz

---
+++ packages/modelmerge/modelmerge-0.11.19.tar.gz/modelmerge-0.11.19/src/ModelMerge/models/__init__.py
@@ -0,0 +1,8 @@
+from .chatgpt import *
+from .claude import *
+from .genimi import *
+from .groq import *
+from .audio import *
+from .duckduckgo import *
+
+# __all__ = ["chatgpt", "claude", "claude3", "gemini", "groq"]
