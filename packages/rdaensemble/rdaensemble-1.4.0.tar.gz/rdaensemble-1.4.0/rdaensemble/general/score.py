fd0631b34daad46f4b44c45df5f17a2fe15706c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:04:04 +0000

    Add package rdaensemble/1.4.0/rdaensemble-1.4.0.tar.gz

---
+++ packages/rdaensemble/rdaensemble-1.4.0.tar.gz/rdaensemble-1.4.0/rdaensemble/general/score.py
@@ -0,0 +1,88 @@
+"""
+SCORE AN ENSEMBLE OF PLANS
+"""
+
+from typing import List, Dict, Tuple, Any
+
+from rdabase import (
+    mkPoints,
+    Point,
+    index_points,
+    IndexedPoint,
+    populations,
+    index_geoids,
+    index_assignments,
+    Assignment,
+    IndexedWeightedAssignment,
+    calc_energy,
+)
+from rdascore import analyze_plan
+from .compactness import cuts_and_boundaries
+from .utils import make_plan
+
+
+def score_ensemble(
+    plans: List[Dict[str, str | float | Dict[str, int | str]]],
+    data: Dict[str, Dict[str, int | str]],
+    shapes: Dict[str, Any],
+    graph: Dict[str, List[str]],
+    metadata: Dict[str, Any],
+    alt_minority: bool = False,
+) -> List[Dict]:
+    """Score an ensemble of maps."""
+
+    points: List[Point] = mkPoints(data, shapes)
+
+    indexed_geoids: Dict[str, int] = index_geoids(points)
+    indexed_points: List[IndexedPoint] = index_points(points)
+
+    pop_by_geoid: Dict[str, int] = populations(data)
+
+    scores: List[Dict] = list()
+
+    for i, p in enumerate(plans):
+        plan_name: str = str(p["name"])
+        print(f"... {i}: {plan_name} ...")
+
+        try:
+            plan_dict: Dict[str, int | str] = p["plan"]  # type: ignore
+            assignments: List[Assignment] = make_plan(plan_dict)
+            indexed_assignments: List[IndexedWeightedAssignment] = index_assignments(
+                assignments, indexed_geoids, pop_by_geoid
+            )
+
+            energy: float = calc_energy(indexed_assignments, indexed_points)
+
+            record: Dict[str, Any] = dict()
+            record["map"] = plan_name
+            record["energy"] = energy
+
+            cut_pct, boundary_pct = cuts_and_boundaries(plan_dict, graph)
+            record["cut_edges"] = cut_pct
+            record["boundary_nodes"] = boundary_pct
+
+            scorecard: Dict[str, Any] = analyze_plan(
+                assignments,
+                data,
+                shapes,
+                graph,
+                metadata,
+                alt_minority=alt_minority,
+            )
+
+            if alt_minority:
+                scorecard["minority"] = scorecard["minority_alt"]
+                scorecard.pop("minority_alt")
+
+            record.update(scorecard)
+            scores.append(record)
+            pass  # for break point
+
+        except Exception as e:
+            print(f"Failure: {e}")
+            pass
+
+    return scores
+
+
+### END ###
