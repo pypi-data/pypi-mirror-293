8f72e17083473cfbb75140eefd4d78facddd1183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 07:04:02 +0000

    Add package xync-scripts/0.2.2/xync_scripts-0.2.2.tar.gz

---
+++ packages/xync-scripts/xync_scripts-0.2.2.tar.gz/xync_scripts-0.2.2/xync_scripts/loader.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from os import getenv as env
+
+load_dotenv()
+
+dsn = env('DB_URL')
+HT = env('HT')
+BKEY = env('BKEY')
+BSEC = env('BSEC')
+OKXKEY = env('OKXKEY')
+OKXSEC = env('OKXSEC')
+OKXPSF = env('OKXPSF')
+BYT = env('BYT')
+BYKEY = env('BYKEY')
+BYSEC = env('BYSEC')
+KUKEY = env('KUKEY')
+KUSEC = env('KUSEC')
+CMXK = env('CMXK')
+CMXS = env('CMXS')
