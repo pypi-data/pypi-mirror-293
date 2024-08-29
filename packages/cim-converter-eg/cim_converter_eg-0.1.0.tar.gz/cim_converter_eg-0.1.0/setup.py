bc25f8fea47a2b50d2931bfe4db4f923efd2dce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:44 +0000

    Add package cim-converter-eg/0.1.0/cim_converter_eg-0.1.0.tar.gz

---
+++ packages/cim-converter-eg/cim_converter_eg-0.1.0.tar.gz/cim_converter_eg-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cim_converter_eg',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CIM converter',
+    author='Matej Oblak',
+)
