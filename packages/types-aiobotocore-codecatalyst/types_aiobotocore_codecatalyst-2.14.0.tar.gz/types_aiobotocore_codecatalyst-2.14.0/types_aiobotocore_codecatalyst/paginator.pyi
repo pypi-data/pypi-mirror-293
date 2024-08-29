0ad2281527bc54fd0614c161158981f714eda97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 14:03:51 +0000

    Add package types-aiobotocore-codecatalyst/2.14.0/types_aiobotocore_codecatalyst-2.14.0.tar.gz

---
+++ packages/types-aiobotocore-codecatalyst/types_aiobotocore_codecatalyst-2.14.0.tar.gz/types_aiobotocore_codecatalyst-2.14.0/types_aiobotocore_codecatalyst/paginator.pyi
@@ -0,0 +1,259 @@
+"""
+Type annotations for codecatalyst service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_codecatalyst.client import CodeCatalystClient
+    from types_aiobotocore_codecatalyst.paginator import (
+        ListAccessTokensPaginator,
+        ListDevEnvironmentSessionsPaginator,
+        ListDevEnvironmentsPaginator,
+        ListEventLogsPaginator,
+        ListProjectsPaginator,
+        ListSourceRepositoriesPaginator,
+        ListSourceRepositoryBranchesPaginator,
+        ListSpacesPaginator,
+        ListWorkflowRunsPaginator,
+        ListWorkflowsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("codecatalyst") as client:
+        client: CodeCatalystClient
+
+        list_access_tokens_paginator: ListAccessTokensPaginator = client.get_paginator("list_access_tokens")
+        list_dev_environment_sessions_paginator: ListDevEnvironmentSessionsPaginator = client.get_paginator("list_dev_environment_sessions")
+        list_dev_environments_paginator: ListDevEnvironmentsPaginator = client.get_paginator("list_dev_environments")
+        list_event_logs_paginator: ListEventLogsPaginator = client.get_paginator("list_event_logs")
+        list_projects_paginator: ListProjectsPaginator = client.get_paginator("list_projects")
+        list_source_repositories_paginator: ListSourceRepositoriesPaginator = client.get_paginator("list_source_repositories")
+        list_source_repository_branches_paginator: ListSourceRepositoryBranchesPaginator = client.get_paginator("list_source_repository_branches")
+        list_spaces_paginator: ListSpacesPaginator = client.get_paginator("list_spaces")
+        list_workflow_runs_paginator: ListWorkflowRunsPaginator = client.get_paginator("list_workflow_runs")
+        list_workflows_paginator: ListWorkflowsPaginator = client.get_paginator("list_workflows")
+    ```
+"""
+
+from typing import Any, AsyncIterator, Generic, Iterator, Mapping, Sequence, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    FilterTypeDef,
+    ListAccessTokensResponseTypeDef,
+    ListDevEnvironmentSessionsResponseTypeDef,
+    ListDevEnvironmentsResponseTypeDef,
+    ListEventLogsResponseTypeDef,
+    ListProjectsResponseTypeDef,
+    ListSourceRepositoriesResponseTypeDef,
+    ListSourceRepositoryBranchesResponseTypeDef,
+    ListSpacesResponseTypeDef,
+    ListWorkflowRunsResponseTypeDef,
+    ListWorkflowsResponseTypeDef,
+    PaginatorConfigTypeDef,
+    ProjectListFilterTypeDef,
+    TimestampTypeDef,
+)
+
+__all__ = (
+    "ListAccessTokensPaginator",
+    "ListDevEnvironmentSessionsPaginator",
+    "ListDevEnvironmentsPaginator",
+    "ListEventLogsPaginator",
+    "ListProjectsPaginator",
+    "ListSourceRepositoriesPaginator",
+    "ListSourceRepositoryBranchesPaginator",
+    "ListSpacesPaginator",
+    "ListWorkflowRunsPaginator",
+    "ListWorkflowsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListAccessTokensPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListAccessTokens)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listaccesstokenspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListAccessTokensResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListAccessTokens.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listaccesstokenspaginator)
+        """
+
+class ListDevEnvironmentSessionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListDevEnvironmentSessions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listdevenvironmentsessionspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        spaceName: str,
+        projectName: str,
+        devEnvironmentId: str,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListDevEnvironmentSessionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListDevEnvironmentSessions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listdevenvironmentsessionspaginator)
+        """
+
+class ListDevEnvironmentsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListDevEnvironments)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listdevenvironmentspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        spaceName: str,
+        projectName: str = ...,
+        filters: Sequence[FilterTypeDef] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListDevEnvironmentsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListDevEnvironments.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listdevenvironmentspaginator)
+        """
+
+class ListEventLogsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListEventLogs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listeventlogspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        spaceName: str,
+        startTime: TimestampTypeDef,
+        endTime: TimestampTypeDef,
+        eventName: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListEventLogsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListEventLogs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listeventlogspaginator)
+        """
+
+class ListProjectsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListProjects)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listprojectspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        spaceName: str,
+        filters: Sequence[ProjectListFilterTypeDef] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListProjectsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListProjects.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listprojectspaginator)
+        """
+
+class ListSourceRepositoriesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListSourceRepositories)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listsourcerepositoriespaginator)
+    """
+
+    def paginate(
+        self, *, spaceName: str, projectName: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListSourceRepositoriesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListSourceRepositories.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listsourcerepositoriespaginator)
+        """
+
+class ListSourceRepositoryBranchesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListSourceRepositoryBranches)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listsourcerepositorybranchespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        spaceName: str,
+        projectName: str,
+        sourceRepositoryName: str,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListSourceRepositoryBranchesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListSourceRepositoryBranches.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listsourcerepositorybranchespaginator)
+        """
+
+class ListSpacesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListSpaces)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listspacespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListSpacesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListSpaces.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listspacespaginator)
+        """
+
+class ListWorkflowRunsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListWorkflowRuns)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listworkflowrunspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        spaceName: str,
+        projectName: str,
+        workflowId: str = ...,
+        sortBy: Sequence[Mapping[str, Any]] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListWorkflowRunsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListWorkflowRuns.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listworkflowrunspaginator)
+        """
+
+class ListWorkflowsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListWorkflows)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listworkflowspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        spaceName: str,
+        projectName: str,
+        sortBy: Sequence[Mapping[str, Any]] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListWorkflowsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codecatalyst.html#CodeCatalyst.Paginator.ListWorkflows.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codecatalyst/paginators/#listworkflowspaginator)
+        """
