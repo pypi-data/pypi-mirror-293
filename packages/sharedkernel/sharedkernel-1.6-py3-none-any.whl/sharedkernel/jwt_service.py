bf47cc2e2a30db1c8509dc31b61fb56879ffe17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 04:03:23 +0000

    Add package sharedkernel/1.6/sharedkernel-1.6-py3-none-any.whl

---
+++ packages/sharedkernel/sharedkernel-1.6-py3-none-any.whl/sharedkernel/jwt_service.py
@@ -0,0 +1,38 @@
+import jwt
+import time
+from fastapi import Request
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from sharedkernel.exception.exception import UnAuthorizedException
+from sharedkernel.objects import JwtModel
+
+
+class JWTBearer(HTTPBearer):
+
+    def __init__(self,jwt_config:JwtModel, auto_error: bool = True):
+        self.jwt_config=jwt_config
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        try:
+            credentials: HTTPAuthorizationCredentials = await super(
+                JWTBearer, self
+            ).__call__(request)
+
+            self.verify(credentials.credentials)
+
+        except:
+            raise UnAuthorizedException()       
+
+
+    def verify(self, token: str) -> bool:
+            decoded_token = jwt.decode(
+            jwt= token.replace("Bearer","").strip(),
+            key= self.jwt_config.secret_key,
+            algorithms= self.jwt_config.algorithms,
+            audience= self.jwt_config.audience,
+            issuer= self.jwt_config.issuer
+            )
+
+            if decoded_token["exp"] < time.time():
+                raise UnAuthorizedException()
