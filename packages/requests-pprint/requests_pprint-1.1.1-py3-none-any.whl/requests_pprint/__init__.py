05b8e53e0e93d5573ec6c0be2f66ac94118c0bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 06:04:02 +0000

    Add package requests-pprint/1.1.1/requests_pprint-1.1.1-py3-none-any.whl

---
+++ packages/requests-pprint/requests_pprint-1.1.1-py3-none-any.whl/requests_pprint/__init__.py
@@ -0,0 +1,10 @@
+"""Init file for the requests_pprint package."""
+
+from requests_pprint.main import (
+    pprint_http_request,
+    pprint_http_response,
+    print_response_summary,
+    print_async_response_summary,
+    pprint_async_http_request,
+    pprint_async_http_response,
+)
