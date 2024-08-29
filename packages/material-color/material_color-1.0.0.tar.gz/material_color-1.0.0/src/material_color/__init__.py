a70efc30d9ede178b52186ad6bca75f2b0baca08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:33 +0000

    Add package material-color/1.0.0/material_color-1.0.0.tar.gz

---
+++ packages/material-color/material_color-1.0.0.tar.gz/material_color-1.0.0/src/material_color/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pkg_resources import get_distribution
+
+__version__ = get_distribution("material-color").version
+__author__ = "Martanto"
+__author_email__ = "martanto@live.com"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2024, MAGMA Indonesia"
+__url__ = "https://github.com/martanto/material-color"
