f071156fbf76c950963101ba51c9b76cbe3f9f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 16:04:23 +0000

    Add package vdk-ingest-http/0.2.1431637373/vdk_ingest_http-0.2.1431637373.tar.gz

---
+++ packages/vdk-ingest-http/vdk_ingest_http-0.2.1431637373.tar.gz/vdk_ingest_http-0.2.1431637373/src/vdk/plugin/ingest_http/ingest_over_http.py
@@ -0,0 +1,232 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+import gzip
+import logging
+import sys
+from typing import Dict
+from typing import List
+from typing import NewType
+from typing import Optional
+
+import requests
+import simplejson as json
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+from vdk.internal.builtin_plugins.ingestion.ingester_base import IIngesterPlugin
+from vdk.internal.builtin_plugins.run.job_context import JobContext
+from vdk.internal.core import errors
+from vdk.internal.core.errors import PlatformServiceError
+from vdk.internal.core.errors import ResolvableBy
+from vdk.internal.core.errors import UserCodeError
+from vdk.internal.core.errors import VdkConfigurationError
+
+log = logging.getLogger(__name__)
+IngestionResult = NewType("IngestionResult", Dict)
+
+
+class IngestOverHttp(IIngesterPlugin):
+    """
+    Create a new ingestion mechanism
+    """
+
+    def __init__(self, context: JobContext):
+        self._connect_timeout_seconds = (
+            int(
+                context.core_context.configuration.get_value(
+                    "INGEST_OVER_HTTP_CONNECT_TIMEOUT_SECONDS"
+                )
+            )
+            if context.core_context.configuration.get_value(
+                "INGEST_OVER_HTTP_CONNECT_TIMEOUT_SECONDS"
+            )
+            else None
+        )
+        self._read_timeout_seconds = (
+            int(
+                context.core_context.configuration.get_value(
+                    "INGEST_OVER_HTTP_READ_TIMEOUT_SECONDS"
+                )
+            )
+            if context.core_context.configuration.get_value(
+                "INGEST_OVER_HTTP_READ_TIMEOUT_SECONDS"
+            )
+            else None
+        )
+        self._verify = context.core_context.configuration.get_value(
+            "INGEST_OVER_HTTP_VERIFY"
+        )
+        self._cert_file_path = context.core_context.configuration.get_value(
+            "INGEST_OVER_HTTP_CERT_FILE_PATH"
+        )
+        self._compression_threshold_bytes = (
+            int(
+                context.core_context.configuration.get_value(
+                    "INGEST_OVER_HTTP_COMPRESSION_THRESHOLD_BYTES"
+                )
+            )
+            if context.core_context.configuration.get_value(
+                "INGEST_OVER_HTTP_COMPRESSION_THRESHOLD_BYTES"
+            )
+            else None
+        )
+        self._compression_encoding = context.core_context.configuration.get_value(
+            "INGEST_OVER_HTTP_COMPRESSION_ENCODING"
+        )
+        self._retry_total = (
+            int(
+                context.core_context.configuration.get_value(
+                    "INGEST_OVER_HTTP_RETRY_TOTAL"
+                )
+            )
+            if context.core_context.configuration.get_value(
+                "INGEST_OVER_HTTP_RETRY_TOTAL"
+            )
+            else None
+        )
+        self._retry_backoff_factor = (
+            float(
+                context.core_context.configuration.get_value(
+                    "INGEST_OVER_HTTP_RETRY_BACKOFF_FACTOR"
+                )
+            )
+            if context.core_context.configuration.get_value(
+                "INGEST_OVER_HTTP_RETRY_BACKOFF_FACTOR"
+            )
+            else None
+        )
+        self._retry_status_forcelist = (
+            [
+                int(s)
+                for s in context.core_context.configuration.get_value(
+                    "INGEST_OVER_HTTP_RETRY_STATUS_FORCELIST"
+                ).split(",")
+            ]
+            if context.core_context.configuration.get_value(
+                "INGEST_OVER_HTTP_RETRY_STATUS_FORCELIST"
+            )
+            else None
+        )
+        self._allow_nan = context.core_context.configuration.get_value(
+            "INGEST_OVER_HTTP_ALLOW_NAN"
+        )
+
+        adapter = HTTPAdapter(
+            max_retries=Retry(
+                total=self._retry_total,
+                backoff_factor=self._retry_backoff_factor,
+                allowed_methods=False,  # retry on all (including post)
+                status_forcelist=self._retry_status_forcelist,
+            )
+        )
+        self._session = requests.Session()
+        self._session.mount("https://", adapter)
+        self._session.mount("http://", adapter)  # nosec
+
+    def ingest_payload(
+        self,
+        payload: List[dict],
+        destination_table: Optional[str] = None,
+        target: Optional[str] = None,
+        collection_id: Optional[str] = None,
+        metadata: Optional[IIngesterPlugin.IngestionMetadata] = None,
+    ) -> Optional[IngestionResult]:
+        header = {"Content-Type": "application/octet-stream"}  # TODO: configurable
+
+        log.debug(
+            f"Ingesting payloads for target: {target}; "
+            f"collection_id: {collection_id}"
+        )
+
+        self.__verify_target(target)
+        self.__amend_payload(payload, destination_table)
+        return self.__send_data(payload, target, header)
+
+    @staticmethod
+    def __verify_target(target):
+        if not target:
+            errors.report_and_throw(
+                VdkConfigurationError(
+                    "Cannot send payload for ingestion over http.",
+                    "Target has not been provided to the plugin. "
+                    "Most likely it has been mis-configured",
+                    "Will not be able to send the payloads and will throw exception."
+                    "Likely the job would fail",
+                    "Make sure you have set correct target - "
+                    "either as VDK_INGEST_TARGET_DEFAULT configuration variable "
+                    "or passed target to send_**for_ingestion APIs",
+                )
+            )
+
+    @staticmethod
+    def __amend_payload(payload, destination_table):
+        for obj in payload:
+            # TODO: Move all ingestion formatting logic to a separate plugin.
+            if not ("@table" in obj):
+                if not destination_table:
+                    errors.report_and_throw(
+                        UserCodeError(
+                            "Corrupt payload",
+                            """destination_table argument is empty, or @table key is
+                            missing from payload.""",
+                            "Payload would not be ingested, and data job may fail.",
+                            "Re-send payload by including @table key/value pair, or pass a destination_table "
+                            "parameter to the ingestion method called.",
+                        )
+                    )
+                else:
+                    obj["@table"] = destination_table
+
+    def __send_data(self, data, http_url, headers) -> IngestionResult:
+        data = json.dumps(data, allow_nan=self._allow_nan)
+        uncompressed_size_in_bytes = sys.getsizeof(data)
+        compressed_size_in_bytes = None
+
+        if (
+            self._compression_threshold_bytes
+            and uncompressed_size_in_bytes >= self._compression_threshold_bytes
+        ):
+            headers["Content-encoding"] = "gzip"
+            data = gzip.compress(data.encode(self._compression_encoding))
+            compressed_size_in_bytes = sys.getsizeof(data)
+
+        try:
+            req = self._session.post(
+                url=http_url,
+                data=data,
+                headers=headers,
+                timeout=(self._connect_timeout_seconds, self._read_timeout_seconds),
+                cert=self._cert_file_path,
+                verify=self._verify,
+            )
+            if 400 <= req.status_code < 500:
+                errors.report_and_throw(
+                    UserCodeError(
+                        "Failed to sent payload",
+                        f"HTTP Client error. status is {req.status_code} and message was : {req.text}",
+                        "Will not be able to send the payload for ingestion",
+                        "Fix the error and try again ",
+                    )
+                )
+            if req.status_code >= 500:
+                errors.report_and_throw(
+                    PlatformServiceError(
+                        "Failed to sent payload",
+                        f"HTTP Server error. status is {req.status_code} and message was : {req.text}",
+                        "Will not be able to send the payload for ingestion",
+                        "Re-try the operation again. If error persist contact support team. ",
+                    )
+                )
+            log.debug(
+                "Payload was ingested. Request Details: "
+                f"Status Code: {req.status_code}, \nPayload: {req.text}"
+            )
+            return IngestionResult(
+                {
+                    "uncompressed_size_in_bytes": uncompressed_size_in_bytes,
+                    "compressed_size_in_bytes": compressed_size_in_bytes,
+                    "http_status": req.status_code,
+                }
+            )
+        except Exception as e:
+            errors.report(ResolvableBy.PLATFORM_ERROR, e)
+            raise e
---
+++ packages/vdk-ingest-http/vdk_ingest_http-0.2.1431637373.tar.gz/vdk_ingest_http-0.2.1431637373/tests/test_ingest_over_http.py
@@ -0,0 +1,286 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+import gzip
+import sys
+from decimal import Decimal
+from unittest import mock
+from unittest.mock import MagicMock
+
+import pytest
+import simplejson as json
+from vdk.internal.core.errors import PlatformServiceError
+from vdk.internal.core.errors import UserCodeError
+from vdk.internal.core.errors import VdkConfigurationError
+from vdk.plugin.ingest_http.ingest_over_http import IngestOverHttp
+
+payload: dict = {
+    "@id": "test_id",
+    "some_data": "some_test_data",
+}
+job_context = MagicMock()
+job_context.core_context.configuration.get_value.return_value = None
+encoding = "utf-8"
+
+
+def mocked_requests_post(url, *args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+            self.text = ""
+
+        def json(self):
+            return self.json_data
+
+    if url == "http://example.com/data-source":
+        return MockResponse(payload, 200)
+    elif url == "http://example.com/wrong/data-source":
+        return MockResponse(None, 500)
+
+    return MockResponse(None, 404)
+
+
+@mock.patch("requests.Session.post", side_effect=mocked_requests_post)
+def test_ingest_over_http(mock_post):
+    http_ingester: IngestOverHttp = IngestOverHttp(job_context)
+    http_ingester.ingest_payload(
+        payload=[payload],
+        destination_table="test_table",
+        target="http://example.com/data-source",
+    )
+
+    assert mock_post.call_count == 1
+
+    payload["@table"] = "test_table"
+
+    mock_post.assert_called_with(
+        url="http://example.com/data-source",
+        data=json.dumps([payload]),
+        headers={"Content-Type": "application/octet-stream"},
+        timeout=(None, None),
+        cert=None,
+        verify=None,
+    )
+
+
+@mock.patch("requests.Session.post", side_effect=mocked_requests_post)
+def test_ingest_over_http_compression(mock_post):
+    def configuration_side_effect(*args, **kwargs):
+        if args[0] == "INGEST_OVER_HTTP_COMPRESSION_THRESHOLD_BYTES":
+            return 3
+        if args[0] == "INGEST_OVER_HTTP_COMPRESSION_ENCODING":
+            return encoding
+        return None
+
+    job_context = MagicMock()
+    job_context.core_context.configuration.get_value.side_effect = (
+        configuration_side_effect
+    )
+    http_ingester: IngestOverHttp = IngestOverHttp(job_context)
+    http_ingester.ingest_payload(
+        payload=[payload],
+        destination_table="test_table",
+        target="http://example.com/data-source",
+    )
+
+    assert mock_post.call_count == 1
+    payload["@table"] = "test_table"
+
+    mock_post.assert_called_with(
+        headers={
+            "Content-Type": "application/octet-stream",
+            "Content-encoding": "gzip",
+        },
+        data=gzip.compress(json.dumps([payload]).encode(encoding)),
+        url="http://example.com/data-source",
+        timeout=(None, None),
+        cert=None,
+        verify=None,
+    )
+
+
+@mock.patch("requests.Session.post", side_effect=mocked_requests_post)
+def test_ingest_over_http_result(mock_post):
+    def configuration_side_effect(*args, **kwargs):
+        if args[0] == "INGEST_OVER_HTTP_COMPRESSION_THRESHOLD_BYTES":
+            return 3
+        if args[0] == "INGEST_OVER_HTTP_COMPRESSION_ENCODING":
+            return encoding
+        return None
+
+    job_context = MagicMock()
+    job_context.core_context.configuration.get_value.side_effect = (
+        configuration_side_effect
+    )
+    http_ingester: IngestOverHttp = IngestOverHttp(job_context)
+    ingestion_result = http_ingester.ingest_payload(
+        payload=[payload],
+        destination_table="test_table",
+        target="http://example.com/data-source",
+    )
+
+    assert ingestion_result["uncompressed_size_in_bytes"] == sys.getsizeof(
+        json.dumps([payload])
+    )
+    assert ingestion_result["compressed_size_in_bytes"] == sys.getsizeof(
+        gzip.compress(json.dumps([payload], allow_nan=False).encode(encoding))
+    )
+    assert ingestion_result["http_status"] == 200
+
+
+@mock.patch("requests.Session.post", side_effect=mocked_requests_post)
+def test_ingest_over_http_missing_target(mock_post):
+    http_ingester: IngestOverHttp = IngestOverHttp(job_context)
+    with pytest.raises(VdkConfigurationError):
+        http_ingester.ingest_payload(
+            payload=[payload],
+            destination_table="test_table",
+        )
+
+
+@mock.patch("requests.Session.post", side_effect=mocked_requests_post)
+def test_ingest_over_http_missing_destination_table(mock_post):
+    test_payload = [{"key1": 42, "key2": True}]
+    http_ingester: IngestOverHttp = IngestOverHttp(job_context)
+
+    with pytest.raises(UserCodeError):
+        http_ingester.ingest_payload(
+            payload=test_payload, target="http://example.com/data-source"
+        )
+
+
+@mock.patch("requests.Session.post", side_effect=mocked_requests_post)
+def test_ingest_over_http_request_errors(mock_post):
+    http_ingester: IngestOverHttp = IngestOverHttp(job_context)
+    with pytest.raises(UserCodeError):
+        http_ingester.ingest_payload(
+            payload=[payload],
+            destination_table="test_table",
+            target="http://example.com/",
+        )
+
+    with pytest.raises(PlatformServiceError):
+        http_ingester.ingest_payload(
+            payload=[payload],
+            destination_table="test_table",
+            target="http://example.com/wrong/data-source",
+        )
+
+
+@mock.patch("requests.Session.mount")
+@mock.patch("requests.Session.post", side_effect=mocked_requests_post)
+def test_ingest_over_http_request_parameters_propagation(mock_post, mock_mount):
+    retry_total = 10
+    retry_backoff_factor = 30
+    retry_status_forcelist = "500,502,503,504"
+    allow_nan = False
+
+    def configuration_side_effect(*args, **kwargs):
+        def arg(a):
+            return args[0] == a
+
+        if arg("INGEST_OVER_HTTP_CONNECT_TIMEOUT_SECONDS"):
+            return 10
+        if arg("INGEST_OVER_HTTP_READ_TIMEOUT_SECONDS"):
+            return 300
+        if arg("INGEST_OVER_HTTP_VERIFY"):
+            return True
+        if arg("INGEST_OVER_HTTP_CERT_FILE_PATH"):
+            return "cert.pem"
+        if arg("INGEST_OVER_HTTP_COMPRESSION_THRESHOLD_BYTES"):
+            return 3
+        if arg("INGEST_OVER_HTTP_COMPRESSION_ENCODING"):
+            return encoding
+        if arg("INGEST_OVER_HTTP_RETRY_TOTAL"):
+            return retry_total
+        if arg("INGEST_OVER_HTTP_RETRY_BACKOFF_FACTOR"):
+            return retry_backoff_factor
+        if arg("INGEST_OVER_HTTP_RETRY_STATUS_FORCELIST"):
+            return retry_status_forcelist
+        if arg("INGEST_OVER_HTTP_ALLOW_NAN"):
+            return allow_nan
+        return None
+
+    job_context = MagicMock()
+    job_context.core_context.configuration.get_value.side_effect = (
+        configuration_side_effect
+    )
+    IngestOverHttp(job_context).ingest_payload(
+        payload=[payload],
+        destination_table="test_table",
+        target="http://example.com/data-source",
+    )
+
+    retries_called = mock_mount.call_args[0][1].max_retries
+    assert retries_called.total == retry_total
+    assert retries_called.backoff_factor == retry_backoff_factor
+    assert (
+        ",".join(
+            [str(s) for s in mock_mount.call_args[0][1].max_retries.status_forcelist]
+        )
+        == retry_status_forcelist
+    )
+
+    mock_post.assert_called_with(
+        url="http://example.com/data-source",
+        data=gzip.compress(json.dumps([payload], allow_nan=allow_nan).encode(encoding)),
+        headers={
+            "Content-Type": "application/octet-stream",
+            "Content-encoding": "gzip",
+        },
+        timeout=(10, 300),
+        cert="cert.pem",
+        verify=True,
+    )
+
+
+@mock.patch("requests.Session.post", side_effect=mocked_requests_post)
+def test_ingest_over_http_special_type(mock_post):
+    http_ingester: IngestOverHttp = IngestOverHttp(job_context)
+    payload["decimal"] = Decimal(15.5)
+    http_ingester.ingest_payload(
+        payload=[payload],
+        destination_table="test_table",
+        target="http://example.com/data-source",
+    )
+
+    assert mock_post.call_count == 1
+
+    payload["@table"] = "test_table"
+
+    mock_post.assert_called_with(
+        url="http://example.com/data-source",
+        data=json.dumps([payload]),
+        headers={"Content-Type": "application/octet-stream"},
+        timeout=(None, None),
+        cert=None,
+        verify=None,
+    )
+
+
+@mock.patch("simplejson.dumps")
+def test_ingest_over_http_json_dumps_parameters_propagation(mock_jsondumps):
+    allow_nan = True
+
+    def configuration_side_effect(*args, **kwargs):
+        def arg(a):
+            return args[0] == a
+
+        if arg("INGEST_OVER_HTTP_ALLOW_NAN"):
+            return allow_nan
+        return None
+
+    job_context = MagicMock()
+    job_context.core_context.configuration.get_value.side_effect = (
+        configuration_side_effect
+    )
+    try:
+        IngestOverHttp(job_context).ingest_payload(
+            payload=[payload],
+            destination_table="test_table",
+            target="http://example.com/data-source",
+        )
+    except:
+        pass
+
+    mock_jsondumps.assert_called_with([payload], allow_nan=allow_nan)
