624f2518ee1b3a86ba34c68eb8bca1e83f3868fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 17:04:21 +0000

    Add package netbox-device-view/0.1.7/netbox_device_view-0.1.7.tar.gz

---
+++ packages/netbox-device-view/netbox_device_view-0.1.7.tar.gz/netbox_device_view-0.1.7/netbox_device_view/utils.py
@@ -0,0 +1,85 @@
+from dcim.models import ConsolePort
+from .models import DeviceView
+from django.core.exceptions import ObjectDoesNotExist
+
+import re
+
+
+def process_interfaces(interfaces, ports_chassis, dev):
+    if interfaces is not None:
+        for itf in interfaces:
+            if itf.type == "virtual" or itf.type == "lag":
+                continue
+            regex = r"^(?P<type>([a-zA-Z\-_]*))(\/|(?P<dev>[0-9]+).|\s)?((?P<module>[0-9]+).|\s)?((?P<port>[0-9]+))$"
+            matches = re.search(regex, itf.name.lower())
+            if matches:
+                itf.stylename = (
+                    (matches["type"] or "")
+                    + (matches["module"] or "")
+                    + "-"
+                    + matches["port"]
+                )
+            else:
+                itf.stylename = re.sub(r"[^.a-zA-Z\d]", "-", itf.name.lower())
+            if itf.stylename.isdigit():
+                itf.stylename = f"p{itf.stylename}"
+            if dev not in ports_chassis:
+                ports_chassis[dev] = []
+            ports_chassis[dev].append(itf)
+    return ports_chassis
+
+
+def process_ports(ports, ports_chassis, dev):
+    if ports is not None:
+        for port in ports:
+            if port.type == "virtual":
+                continue
+            port.is_port = True
+            port.stylename = re.sub(r"[^.a-zA-Z\d]", "-", port.name.lower())
+            if port.stylename.isdigit():
+                port.stylename = f"p{port.stylename}"
+            if dev not in ports_chassis:
+                ports_chassis[dev] = []
+            ports_chassis[dev].append(port)
+    return ports_chassis
+
+
+def prepare(obj):
+    ports_chassis = {}
+    dv = {}
+    modules = {}
+
+    try:
+        if obj.virtual_chassis is None:
+            dv[1] = DeviceView.objects.get(
+                device_type=obj.device_type
+            ).grid_template_area
+            modules[1] = obj.modules.all()
+            ports_chassis = process_interfaces(obj.interfaces.all(), ports_chassis, 1)
+            ports_chassis = process_ports(obj.frontports.all(), ports_chassis, "Front")
+            ports_chassis = process_ports(obj.rearports.all(), ports_chassis, "Rear")
+            ports_chassis = process_ports(
+                ConsolePort.objects.filter(device_id=obj.id),
+                ports_chassis,
+                list(ports_chassis.keys())[0],
+            )
+        else:
+            for member in obj.virtual_chassis.members.all():
+                dv[member.vc_position] = DeviceView.objects.get(
+                    device_type=member.device_type
+                ).grid_template_area.replace(
+                    ".area", ".area.d" + str(member.vc_position)
+                )
+                modules[member.vc_position] = member.modules.all()
+                ports_chassis = process_interfaces(
+                    member.interfaces.all(), ports_chassis, member.vc_position
+                )
+                ports_chassis = process_ports(
+                    ConsolePort.objects.filter(device_id=member.id),
+                    ports_chassis,
+                    member.vc_position,
+                )
+    except ObjectDoesNotExist:
+        return None, None, None
+
+    return dv, modules, ports_chassis
