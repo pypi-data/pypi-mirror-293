29535cec5bf9876b840055b14d18050e64ba1f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 15:04:10 +0000

    Add package pdf-watermark-remover/0.5.0/pdf_watermark_remover-0.5.0.tar.gz

---
+++ packages/pdf-watermark-remover/pdf_watermark_remover-0.5.0.tar.gz/pdf_watermark_remover-0.5.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pdf_watermark_remover',
+    version='0.5.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'Pillow',
+        'PyMuPDF',
+        'reportlab',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pdf_watermark_remover=pdf_watermark_remover.remover:process_pdf',
+        ],
+    },
+    author='huapohen',
+    author_email='694450321@qq.com',
+    description='A package to remove watermarks from PDF files',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/huapohen/pdf_watermark_remover',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
