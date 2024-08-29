ced558992706d209a569c93cf24bc2d85e236c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-29 03:03:52 +0000

    Add package dbt-odps-winwin/1.0.18/dbt_odps_winwin-1.0.18.tar.gz

---
+++ packages/dbt-odps-winwin/dbt_odps_winwin-1.0.18.tar.gz/dbt_odps_winwin-1.0.18/dbt/adapters/odps/relation.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass, field
+from dbt.adapters.base.relation import BaseRelation
+from dbt.contracts.relation import Policy, RelationType
+from odps.models.table import Table
+from dbt.adapters.odps.utils import print_method_call, logger
+
+
+@dataclass
+class OdpsIncludePolicy(Policy):
+    database: bool = True
+    schema: bool = False
+    identifier: bool = True
+
+
+@dataclass(frozen=True, eq=False, repr=False)
+class OdpsRelation(BaseRelation):
+    include_policy: Policy = field(default_factory=lambda: OdpsIncludePolicy())
+    quote_character: str = "`"
+
+    def without_quote(self):
+        return self.quote(False, False, False)
+
+    @classmethod
+    @print_method_call
+    def create(cls, database=None, schema=None, identifier=None, type=None, **kwargs):
+        #logger.error(f"OdpsRelation.create  database {database} . schema {schema} . identifier {identifier} . type {type} . kwargs {kwargs}")
+
+        kwargs.update(
+            {
+                "include_policy": OdpsIncludePolicy(schema=(schema != 'default'))
+            }
+        )
+        return super().create(database, schema, identifier, type, **kwargs)
+
+    @classmethod
+    @print_method_call
+    def from_odps_table(cls, table: Table):
+        identifier = table.name
+        schema = table.get_schema()
+        schema = schema.name if schema else "default"
+
+        return cls.create(
+            database=table.project.name,
+            schema=schema,
+            identifier=identifier,
+            type=RelationType.View if table.is_virtual_view else RelationType.Table,
+        )
