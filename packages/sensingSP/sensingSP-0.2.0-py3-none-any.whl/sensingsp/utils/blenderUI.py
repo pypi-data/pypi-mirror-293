2b3eb3b1fe7ff1522c014aaa6920a76858468e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 04:04:13 +0000

    Add package sensingSP/0.2.0/sensingSP-0.2.0-py3-none-any.whl

---
+++ packages/sensingSP/sensingSP-0.2.0-py3-none-any.whl/sensingsp/utils/blenderUI.py
@@ -0,0 +1,35 @@
+import bpy
+import os
+import tempfile
+def define_settings():
+    if not("Simulation Settings" in bpy.data.objects):
+        bpy.ops.object.empty_add(type='PLAIN_AXES', align='WORLD', location=(0, 0, 0), rotation=(0, 0, 0), scale=(.01, .01, .01))
+        sim_axes = bpy.context.object
+        sim_axes.name = f'Simulation Settings'
+        sim_axes["RF Frequency (GHz)"] = 70
+        sim_axes["Velocity Calc. Jump"] = 1
+        # sim_axes["Velocity Calc. Jump"] = bpy.props.IntProperty(name="Velocity Calc. Jump", default=1, min=1)
+        sim_axes["Bounce Number"] = 2
+        sim_axes["Render Blender Frames"] = True#False
+        sim_axes["Open Output Folder"] = True
+        sim_axes["do RayTracing LOS"] = True
+        sim_axes["do RayTracing Simple"] = False
+        sim_axes["CUDA SignalGeneration Enabled"] = False
+        
+        
+        # bpy.data.objects["Simulation Settings"]["do RayTracing LOS"]=False
+       
+        
+        # bpy.types.Object.velocity_calc_jump = bpy.props.IntProperty(name="Velocity Calc. Jump", default=1, min=1)
+        # bpy.types.Object.bounce_number = bpy.props.IntProperty(name="Bounce Number", default=2, min=2)
+        # bpy.types.Object.render_blender_frames = bpy.props.BoolProperty(name="Render Blender Frames", default=True)
+        # bpy.types.Object.open_output_folder = bpy.props.BoolProperty(name="Open Output Folder", default=True)
+
+        
+        temp_dir = tempfile.gettempdir()
+        radarsim_dir = os.path.join(temp_dir, 'SensingSP')
+        os.makedirs(radarsim_dir, exist_ok=True)
+        sim_axes["Video Directory"]=radarsim_dir
+        sim_axes["Add Ris"] = f"0,8,8,{os.path.join(radarsim_dir, 'Ris.mat')},2"
+        sim_axes["Add Probe"] = f"1,.1,20,20,db1,range_effect1,add_color1,add_open3d1,colormap1,0.05"
+        
