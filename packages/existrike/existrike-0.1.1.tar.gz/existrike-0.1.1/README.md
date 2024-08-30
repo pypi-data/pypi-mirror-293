4a9cbe0003430ea83522c62a725eb5d64d53514b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:04:17 +0000

    Add package existrike/0.1.1/existrike-0.1.1.tar.gz

---
+++ packages/existrike/existrike-0.1.1.tar.gz/existrike-0.1.1/README.md
@@ -0,0 +1,14 @@
+# ExiStrike
+
+ExiStrike is a beta version of a security framework. This is not the final version. We are not responsible for any misuse of this software.
+
+## Features
+
+- Scanning capabilities
+- Auxiliary scans, auxiliaries, exploits
+## Installation
+
+You can install ExiStrike using pip:
+
+```bash
+pip install .
---
+++ packages/existrike/existrike-0.1.1.tar.gz/existrike-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="existrike",
+    version="0.1.1",
+    packages=find_packages(),  # Inclui todos os pacotes e subpacotes
+    install_requires=[
+        'aiohttp',
+        'colorama',
+        'requests',
+        'tqdm',
+    ],
+    entry_points={
+        'console_scripts': [
+            'existrike=existrike:main',  # Atualize o módulo e função
+        ],
+    },
+    author="RADNET64",
+    author_email="radnet@tutamail.com",
+    description="Beta version of the ExiStrike framework, this is not the final version, remembering that we are not responsible for your actions!",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/RADNET64/Exploiter-alphatest",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
