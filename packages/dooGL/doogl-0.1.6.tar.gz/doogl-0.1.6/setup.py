db740fe9de183628397cb0a81bff88e84b0f7294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:04:32 +0000

    Add package dooGL/0.1.6/doogl-0.1.6.tar.gz

---
+++ packages/dooGL/doogl-0.1.6.tar.gz/doogl-0.1.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dooGL",  # The name of your package
+    version="0.1.6",  # Initial version
+    packages=find_packages(),  # Automatically discover all modules
+    python_requires='>=3.6',  # Minimum Python version requirement
+    include_package_data=True,  # Automatically include files specified in package_data
+    package_data={
+        'dooGL.core._2d': ['doGl_logo.png'],  # Specify the path to your image
+    },
+)
