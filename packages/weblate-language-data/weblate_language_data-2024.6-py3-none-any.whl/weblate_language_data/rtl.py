123a46be4287235eab5827dbbad2721626d2db0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:05:10 +0000

    Add package weblate-language-data/2024.6/weblate_language_data-2024.6-py3-none-any.whl

---
+++ packages/weblate-language-data/weblate_language_data-2024.6-py3-none-any.whl/weblate_language_data/rtl.py
@@ -0,0 +1,40 @@
+# Copyright © Michal Čihař <michal@weblate.org>
+#
+# SPDX-License-Identifier: MIT
+
+# List of RTL languages
+RTL_LANGS = {
+    "ajp",
+    "apc",
+    "ar",
+    "ara",
+    "arc",
+    "ae",
+    "aii",
+    "ave",
+    "ckb",
+    "dv",
+    "egy",
+    "fa",
+    "fas",
+    "ha",
+    "he",
+    "heb",
+    "khw",
+    "ks",
+    "ms_Arab",
+    "nqo",
+    "pal",
+    "per",
+    "phn",
+    "ps",
+    "sam",
+    "sd",
+    "skr",
+    "syc",
+    "syr",
+    "ug",
+    "ur",
+    "urd",
+    "yi",
+}
