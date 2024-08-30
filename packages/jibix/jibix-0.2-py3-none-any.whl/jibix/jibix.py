16bebed05ac789121a760a581297deb6391ecf27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:05:03 +0000

    Add package jibix/0.2/jibix-0.2-py3-none-any.whl

---
+++ packages/jibix/jibix-0.2-py3-none-any.whl/jibix/jibix.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+class Jibix(dict):
+    def __init__(self, name):
+        self.name = name
+        super().__init__()
+        self.make_file()
+        self.reload()
+    
+    def make_file(self):
+        if not os.path.exists(self.name) or not os.path.getsize(self.name):
+            with open(self.name, '+w') as fp:
+                fp.write('{}')
+    
+    def reload(self):
+        with open(self.name, 'rb') as fp:
+            super().update(json.load(fp))
+            
+    def commit(self):
+        with open(self.name, '+w') as fp:
+            fp.write(json.dumps(self))
+    
+    def __setitem__(self, key, value):
+        super().update({key:value})
+        self.commit()
+    
+    def __getattr__(self, name: str) :
+        return super().get(name)
+    
+    def pop(self, key):
+        super().pop(key)
+
+
+        
