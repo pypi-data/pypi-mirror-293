2099c0e47b8e02477e042fa285b03205c72a3a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:06:08 +0000

    Add package allianceauth-corptools-pinger/0.5.0/allianceauth-corptools-pinger-0.5.0.tar.gz

---
+++ packages/allianceauth-corptools-pinger/allianceauth-corptools-pinger-0.5.0.tar.gz/allianceauth-corptools-pinger-0.5.0/pinger/notifications/__init__.py
@@ -0,0 +1,8 @@
+from .character import *  # noqa Flake8(F401)
+from .corporate import *  # noqa Flake8(F401)
+from .moons import *  # noqa Flake8(F401)
+from .orbital import *  # noqa Flake8(F401)
+from .sov import *  # noqa Flake8(F401)
+from .structure import *  # noqa Flake8(F401)
+from .towers import *  # noqa Flake8(F401)
+from .war import *  # noqa Flake8(F401)
