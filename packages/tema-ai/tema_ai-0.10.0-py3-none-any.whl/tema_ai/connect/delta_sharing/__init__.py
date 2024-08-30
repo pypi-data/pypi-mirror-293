66033a3525705394c2a28e79945e6ee839caf910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:06:57 +0000

    Add package tema-ai/0.10.0/tema_ai-0.10.0-py3-none-any.whl

---
+++ packages/tema-ai/tema_ai-0.10.0-py3-none-any.whl/tema_ai/connect/delta_sharing/__init__.py
@@ -0,0 +1,1 @@
+from .patch import Column, File, get_files_in_table, get_table_schema, load_as_pandas
---
+++ packages/tema-ai/tema_ai-0.10.0-py3-none-any.whl/tema_ai/connect/types.py
@@ -0,0 +1,47 @@
+from typing import Callable, Dict, List, TypedDict
+
+
+class Token(TypedDict):
+    id: str
+    created_at: int
+    created_by: str
+    activation_url: str
+    expiration_time: int
+    updated_at: int
+    updated_by: str
+
+
+class Properties(TypedDict):
+    properties: Dict[str, str]
+
+
+class Recipient(TypedDict):
+    name: str
+    authentication_type: str
+    owner: str
+    properties_kvpairs: Properties
+    created_at: int
+    created_by: str
+    tokens: List[Token]
+    updated_at: int
+    updated_by: str
+    full_name: str
+    securable_type: str
+    securable_kind: str
+
+
+class Credentials(TypedDict):
+    client_id: str
+    secret_id: str
+
+
+CredentialsFunction = Callable[[], Credentials]
+
+
+class TableReport(TypedDict):
+    num_files: int
+    partition_columns: List[str]
+    total_records: int
+    avg_records_per_file: int
+    size: int
+    avg_size_per_file: int
