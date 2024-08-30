800f1f47cf4a286da638ad7f5791dbf8a9d8fd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 04:04:19 +0000

    Add package py-cz-api/0.2.4/py_cz_api-0.2.4.tar.gz

---
+++ packages/py-cz-api/py_cz_api-0.2.4.tar.gz/py_cz_api-0.2.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from py_cz_api.config import app_name, app_version
+
+# Условие для определения платформы
+import sys
+is_windows = sys.platform.startswith('win')
+
+# Основные зависимости
+install_requires = [
+    'aiohttp',
+    'pandas',
+    'PyJWT',
+    'requests'
+]
+
+if is_windows:
+    install_requires.append('pywin32')
+
+setup(
+    name=app_name,
+    version=app_version,
+    description='Библиотека для автоматизации работы с Честным Знаком через True API',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jqxl/py_cz_api',
+    download_url='https://codeload.github.com/jqxl/py_cz_api/zip/refs/heads/main',
+    author='jqxl',
+    author_email='jqxl+git@ya.ru',
+    license='GNU General Public License v3.0',
+    packages=['py_cz_api'],
+    install_requires=install_requires,
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3.10',
+)
