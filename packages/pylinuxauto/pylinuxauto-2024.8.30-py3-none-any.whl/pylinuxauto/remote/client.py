93e27292d9621c67a0acd176e648472e6dd03698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 15:04:36 +0000

    Add package pylinuxauto/2024.8.30/pylinuxauto-2024.8.30-py3-none-any.whl

---
+++ packages/pylinuxauto/pylinuxauto-2024.8.30-py3-none-any.whl/pylinuxauto/remote/client.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+# SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+# SPDX-License-Identifier: GPL-2.0-only
+from xmlrpc.client import ServerProxy
+
+
+def client(ip, port):
+    return ServerProxy(f"http://{ip}:{port}", allow_none=True)
