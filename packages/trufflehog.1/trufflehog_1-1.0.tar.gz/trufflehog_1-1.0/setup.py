29be890827434c5d3cb9b0494bec8bda5a5502c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:05:51 +0000

    Add package trufflehog.1/1.0/trufflehog_1-1.0.tar.gz

---
+++ packages/trufflehog.1/trufflehog_1-1.0.tar.gz/trufflehog_1-1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trufflehog.1',
+    version='1.0',  # Update to the desired version
+    description='TruffleHog - Find secrets in your code',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # Add other dependencies as required by TruffleHog
+    ],
+    entry_points={
+        'console_scripts': [
+            'install-trufflehog=trufflehog_installer.trufflehog:main',  # Update this if the entry point differs
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
