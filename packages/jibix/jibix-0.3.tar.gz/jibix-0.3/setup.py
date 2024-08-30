ae0e0e332c1831a16346e53e8e053ba1806fad53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:05:18 +0000

    Add package jibix/0.3/jibix-0.3.tar.gz

---
+++ packages/jibix/jibix-0.3.tar.gz/jibix-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jibix',
+    version='0.3',
+    packages=find_packages(),
+    description='Easy to handle json database manager',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Dr3xxine',
+    author_email='dr3xxine@gmail.com',
+    install_requires=[],  # List any dependencies
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
