1954a5d06e20376551050670acc8ccbedf9afa3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:05:37 +0000

    Add package shreyaspurankarmypackage/0.1.0/shreyaspurankarmypackage-0.1.0.tar.gz

---
+++ packages/shreyaspurankarmypackage/shreyaspurankarmypackage-0.1.0.tar.gz/shreyaspurankarmypackage-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='shreyaspurankarmypackage',
+version='0.1.0',
+author='Shreyas Purankar',
+author_email='shreyas.purankar@jashds.com',
+description='Testing sorting package on PyPI',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
