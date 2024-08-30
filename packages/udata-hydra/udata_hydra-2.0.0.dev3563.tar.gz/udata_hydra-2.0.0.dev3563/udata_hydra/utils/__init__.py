7d062668b03e9bdd673d5082adf7f5ff998c409c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:06:52 +0000

    Add package udata-hydra/2.0.0.dev3563/udata_hydra-2.0.0.dev3563.tar.gz

---
+++ packages/udata-hydra/udata_hydra-2.0.0.dev3563.tar.gz/udata_hydra-2.0.0.dev3563/udata_hydra/utils/__init__.py
@@ -0,0 +1,7 @@
+from .auth import token_auth_middleware
+from .csv import detect_tabular_from_headers
+from .file import compute_checksum_from_file, download_resource, read_csv_gz
+from .http import get_request_params, send
+from .queue import enqueue
+from .reader import Reader, generate_dialect
+from .timer import Timer
