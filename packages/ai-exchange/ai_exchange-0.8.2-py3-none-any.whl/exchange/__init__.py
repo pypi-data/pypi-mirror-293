e2fd81de1802ae57b728142af4836edf97598aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 14:04:18 +0000

    Add package ai-exchange/0.8.2/ai_exchange-0.8.2-py3-none-any.whl

---
+++ packages/ai-exchange/ai_exchange-0.8.2-py3-none-any.whl/exchange/__init__.py
@@ -0,0 +1,8 @@
+"""Classes for interacting with the exchange API."""
+
+from exchange.tool import Tool  # noqa
+from exchange.content import Text, ToolResult, ToolUse  # noqa
+from exchange.message import Message  # noqa
+from exchange.exchange import Exchange  # noqa
+
+module_name = "ai-exchange"
