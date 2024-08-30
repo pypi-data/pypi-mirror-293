2a3dace5492ef1c602b1dc477962be6edcb1f281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:05:19 +0000

    Add package zhihubackup/0.2.3/zhihubackup-0.2.3.tar.gz

---
+++ packages/zhihubackup/zhihubackup-0.2.3.tar.gz/zhihubackup-0.2.3/README.md
@@ -0,0 +1,47 @@
+# 退乎前备份知乎回答：zhihubackup
+
+每个知乎答主都有退乎的梦想，但退乎前如果删光回答，则十分可惜。因此，我用Python写了60行的脚本，可以在退乎前备份自己的所有回答和文章，以免事后后悔。
+
+## 安装
+
+用[pipx](https://github.com/pypa/pipx)安装zhihubackup到独立环境中：
+
+```sh
+pip install pipx
+pipx install zhihubackup
+```
+
+## 使用
+
+2024年7月起，知乎添加了如下所示的验证操作：
+
+```json
+{"error": {"need_login": true, "redirect": "https://www.zhihu.com/account/unhuman?type=S6E3V1&need_login=true", "code": 40352, "message": "系统监测到您的网络环境存在异常，为保证您的正常访问，请点击下方验证按钮进行验证。在您验证完成前，该提示将多次出现。"}}
+```
+
+因此需要从浏览器中手动获取zhihu.com的cookie：
+[Chrome获取Cookie](https://developer.chrome.com/docs/devtools/application/cookies?hl=zh-cn)
+[Edge获取Cookie](https://learn.microsoft.com/en-us/microsoft-edge/devtools-guide-chromium/storage/cookies)
+
+将获取的cookie设置为环境变量：
+
+```sh
+export ZHUHU_COOKIE="q_c1=...; ...; osd=..."
+```
+
+假如你是@贱贱，你的id是`splitter`，那么可以执行命令：
+
+```sh
+zhihubackup splitter
+```
+
+静等一段时间。运行结束后，可以看到产生了名为`splitter`的文件夹：
+```
+- splitter
+|- answer   (842 files)
+|- article  (101 files)
+|- pin      (3214 files)
+|- question (57 files)
+
+```
+备份已经成功，现在可以删光回答和文章了。
---
+++ packages/zhihubackup/zhihubackup-0.2.3.tar.gz/zhihubackup-0.2.3/zhihubackup/backup.py
@@ -0,0 +1,109 @@
+import json
+import os
+import subprocess
+
+import requests
+from nodejs_wheel import (
+    node,
+)
+from tqdm import tqdm
+
+
+def xzse96(path: str, d_c0: str):
+    completed_process = node(
+        [os.path.join(__file__, "..", "xzse96.js"), path, d_c0],
+        return_completed_process=True,
+        check=True,
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    return completed_process.stdout
+
+
+def act_api(username):
+    return f"https://www.zhihu.com/api/v3/moments/{username}/activities?desktop=true"
+
+
+def get_cookie(username):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75",
+    }
+    r = requests.get(f"https://www.zhihu.com/people/{username}", headers=headers)
+    return r.headers["Set-Cookie"]
+
+
+def get_json(url, cookie):
+    d_c0 = cookie.split("d_c0=")[1].split(";")[0]
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75",
+        "x-api-version": "3.0.91",
+        "x-zse-93": "101_3_3.0",
+        "x-zse-96": xzse96(url[len("https://www.zhihu.com") :], d_c0=d_c0),
+        "cookie": cookie.encode("utf-8"),
+    }
+    r = requests.get(url, headers=headers)
+    return json.loads(r.text)
+
+
+def makedirs(username, target_type):
+    try:
+        os.makedirs(os.path.join(username, target_type))
+    except OSError:
+        pass
+
+
+def loop(username):
+    cookie = os.environ.get("ZHIHU_COOKIE", get_cookie(username))
+    record_file = "record." + username
+    api = read_record(record_file=record_file)
+    if api is None:
+        api = act_api(username)
+    t = tqdm()
+    while True:
+        save_record(api, record_file=record_file)
+        jdata = get_json(api, cookie)
+        # data: ['target', 'action_text', 'is_sticky', 'actor', 'verb', 'created_time', 'type', 'id']
+        for dd in jdata["data"]:
+            target = dd["target"]
+            if "author" not in target or target["author"]["url_token"] != username:
+                # skip others' content
+                continue
+            target_type = target["type"]
+            makedirs(username, target_type)
+            saved = []
+            if "question" in target and "title" in target["question"]:
+                saved.append(target["question"]["title"])
+            for saved_type in ("title", "content", "updated_time"):
+                if saved_type in target:
+                    if not isinstance(target[saved_type], list):
+                        saved.append(str(target[saved_type]))
+                    else:
+                        for tt in target[saved_type]:
+                            for saved_type2 in ("content", "url"):
+                                if saved_type2 in tt:
+                                    saved.append(tt[saved_type2])
+            with open(
+                os.path.join(username, target_type, f"{target['id']}.txt"),
+                "w",
+                encoding="utf-8",
+            ) as f:
+                f.write("\n".join(saved))
+            t.update(1)
+        # paging: is_end next previous
+        paging = jdata["paging"]
+        if paging["is_end"]:
+            break
+        api = paging["next"]
+
+
+def save_record(current_api, record_file="record"):
+    with open(record_file, "w") as f:
+        f.write(current_api)
+
+
+def read_record(record_file="record"):
+    if os.path.isfile(record_file):
+        with open(record_file) as f:
+            return f.read().strip()
+    else:
+        return None
