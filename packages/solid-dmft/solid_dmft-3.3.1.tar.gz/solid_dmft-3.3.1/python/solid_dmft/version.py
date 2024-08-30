4af19c53d9d3464c2e8f9c518c2ab4dbff2e67b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:04:44 +0000

    Add package solid-dmft/3.3.1/solid_dmft-3.3.1.tar.gz

---
+++ packages/solid-dmft/solid_dmft-3.3.1.tar.gz/solid_dmft-3.3.1/python/solid_dmft/version.py
@@ -0,0 +1,33 @@
+################################################################################
+#
+# solid_dmft - A versatile python wrapper to perform DFT+DMFT calculations
+#              utilizing the TRIQS software library
+#
+# Copyright (C) 2018-2020, ETH Zurich
+# Copyright (C) 2021, The Simons Foundation
+#      authors: A. Hampel, M. Merkel, and S. Beck
+#
+# solid_dmft is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# solid_dmft is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with
+# solid_dmft (in the file COPYING.txt in this directory). If not, see
+# <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+version = "3.3.1"
+triqs_hash = "988fe982553f28027e3748720da5e94a3890bf9e"
+solid_dmft_hash = "5bac43d904ce46f7aa8e09ed6d72ddfa3a96305c"
+
+def show_version():
+  print("\nYou are using solid_dmft version %s\n"%version)
+
+def show_git_hash():
+  print("\nYou are using solid_dmft git hash %s based on triqs git hash %s\n"%("5bac43d904ce46f7aa8e09ed6d72ddfa3a96305c", triqs_hash))
