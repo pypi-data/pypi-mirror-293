6a119fbc714ce3d2312e1b5d4364fb0fa7564065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 03:03:52 +0000

    Add package picodingchallenge-rb/0.1.1/picodingchallenge_rb-0.1.1.tar.gz

---
+++ packages/picodingchallenge-rb/picodingchallenge_rb-0.1.1.tar.gz/picodingchallenge_rb-0.1.1/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='picodingchallenge_rb',
+    version='0.1.1',
+    author='Ridwan Basith',
+    author_email='ridwanbasith93@gmail.com',
+    description='a Python package designed to complete a coding challenge I was set.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'beautifulsoup4',
+        'requests',
+        'regex',
+        'io',
+        'datetime',
+        'csv',
+        'json',
+        'logging'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
