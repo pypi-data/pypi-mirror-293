d0696d6147dc06e431c23250899e2a6cf0577f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:05:22 +0000

    Add package codeforge/0.0.13/codeforge-0.0.13-py3-none-any.whl

---
+++ packages/codeforge/codeforge-0.0.13-py3-none-any.whl/codeforge/__init__.py
@@ -0,0 +1,73 @@
+__version__ = "0.541"
+from .logger import *
+from .loader import *
+from .paths import *
+from .markup import *
+
+# from .inspector import *
+from .load_defaults import *
+from .pdf_loader import PDF
+from .markup2 import AD
+from .registry import *
+from .ipython import *
+from .decorators import *
+from .misc import *
+from .dates import *
+from .s3_loader import *
+from .zen import *
+from .torch_loader import (
+    torch,
+    th,
+    torchvision,
+    T,
+    transforms,
+    nn,
+    np,
+    F,
+    Dataset,
+    DataLoader,
+    optim,
+    Report,
+    Reshape,
+    Permute,
+    device,
+    save_torch_model_weights_from,
+    load_torch_model_weights_to,
+    detach,
+    cat_with_padding,
+)
+
+
+# def init_torch():
+#     from .torch_loader import (
+#         torch,
+#         th,
+#         torchvision,
+#         T,
+#         transforms,
+#         nn,
+#         np,
+#         F,
+#         Dataset,
+#         DataLoader,
+#         optim,
+#         Report,
+#         Reshape,
+#         Permute,
+#         device,
+#         save_torch_model_weights_from,
+#         load_torch_model_weights_to,
+#         detach,
+#         cat_with_padding,
+#     )
+
+#     try:
+#         import lovely_tensors
+
+#         lovely_tensors.monkey_patch()
+#     except ModuleNotFoundError:
+#         raise ModuleNotFoundError(
+#             "Unable to import `lovely-tensors`. `pip install lovely-tensors` for a prettier experience"
+#         )
+
+#     globals().update(locals())
