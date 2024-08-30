2339db90fb2262091cd0ac544c0598c7f46adc98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 15:04:38 +0000

    Add package pyqwe/1.6.1/pyqwe-1.6.1-py3-none-any.whl

---
+++ packages/pyqwe/pyqwe-1.6.1-py3-none-any.whl/pyqwe/exceptions.py
@@ -0,0 +1,14 @@
+class NotAModuleOrPackage(Exception):
+    pass
+
+
+class FunctionNotFound(Exception):
+    pass
+
+
+class NotAFunction(Exception):
+    pass
+
+
+class EnvVarNotFound(Exception):
+    pass
