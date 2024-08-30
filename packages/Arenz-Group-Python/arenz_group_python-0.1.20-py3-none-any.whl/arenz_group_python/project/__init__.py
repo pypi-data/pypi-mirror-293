b54f77e3ac9e719f5d47f8e0cc1977e213d2eb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:04:59 +0000

    Add package Arenz-Group-Python/0.1.20/arenz_group_python-0.1.20-py3-none-any.whl

---
+++ packages/Arenz-Group-Python/arenz_group_python-0.1.20-py3-none-any.whl/arenz_group_python/project/__init__.py
@@ -0,0 +1,13 @@
+"""_summary_
+Module for creating projects\n
+. 
+
+"""
+
+from .util_paths import Project_Paths 
+
+__all__ = ["util_paths","Project_Paths"]
+
+
+#Import the submodules
+#from . import ec_data
