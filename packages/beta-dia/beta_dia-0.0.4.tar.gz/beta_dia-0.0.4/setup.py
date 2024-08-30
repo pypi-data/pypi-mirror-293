38f43ee424a576916230460d1159f3f67465b7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:05:13 +0000

    Add package beta-dia/0.0.4/beta_dia-0.0.4.tar.gz

---
+++ packages/beta-dia/beta_dia-0.0.4.tar.gz/beta_dia-0.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='beta_dia',
+    version='0.0.4',
+    packages=find_packages(),
+    description='A cool project that does something awesome on diaPASEF data.',
+    include_package_data=True,
+    author='Song Jian',
+    license='MIT',
+    install_requires=[
+        'h5py',
+        'matplotlib',
+        'networkx',
+        'numba',
+        'pandas',
+        'pyzstd',
+        'scikit-learn',
+        'scipy',
+        'statsmodels',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+    entry_points={
+        'console_scripts': [
+            'beta_dia_test=beta_dia.main:main',
+        ],
+    },
+)
