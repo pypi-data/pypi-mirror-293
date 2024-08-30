87e6d6b64be76be32cfd7f87a49b8006af40feab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 14:04:15 +0000

    Add package curvesimplify/0.1.1/curvesimplify-0.1.1-py3-none-any.whl

---
+++ packages/curvesimplify/curvesimplify-0.1.1-py3-none-any.whl/curvesimplify/__init__.py
@@ -0,0 +1,11 @@
+"""CurveSimplify is a collection of polyline simplifying functions.
+
+Each module defines the following functions:
+
+* ``min_err()`` : Minimize the simplification error with given number of vertices.
+* ``min_num()`` : Minimize the number of vertices with given  simplificationerror.
+
+The following types of errors are supported:
+
+1. ``frechet`` : Fréchet distance
+"""
