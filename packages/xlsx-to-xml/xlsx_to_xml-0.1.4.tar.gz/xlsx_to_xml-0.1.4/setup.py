cd97b60284161bad615b77a8f52a86feb4b6901e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:05:29 +0000

    Add package xlsx-to-xml/0.1.4/xlsx_to_xml-0.1.4.tar.gz

---
+++ packages/xlsx-to-xml/xlsx_to_xml-0.1.4.tar.gz/xlsx_to_xml-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools_rust import RustExtension
+
+setup(
+    name="xlsx_to_xml",
+    version="0.1.4",
+    rust_extensions=[RustExtension("xlsx_to_xml", "src/lib.rs")],
+    packages=["xlsx_to_xml"],
+    package_dir={"": "src"},
+    zip_safe=False,
+    install_requires=[
+        "setuptools>=42",
+        "setuptools-rust>=0.11",
+        "wheel",
+    ],
+)
