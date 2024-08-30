cb6981e4bdaaa68672335a14f4362268b96293e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 04:04:43 +0000

    Add package nicegui/1.4.37/nicegui-1.4.37.tar.gz

---
+++ packages/nicegui/nicegui-1.4.37.tar.gz/nicegui-1.4.37/nicegui/elements/joystick.vue
@@ -0,0 +1,30 @@
+<template>
+  <div><div></div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const joystick = nipplejs.create({
+      zone: this.$el.children[0],
+      position: { left: "50%", top: "50%" },
+      dynamicPage: true,
+      ...this.options,
+    });
+    joystick.on("start", (e) => this.$emit("start", e));
+    joystick.on("move", (_, data) => this.$emit("move", { data }));
+    joystick.on("end", (e) => this.$emit("end", e));
+  },
+  props: {
+    options: Object,
+  },
+};
+</script>
+
+<style scoped>
+:scope > div {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+</style>
