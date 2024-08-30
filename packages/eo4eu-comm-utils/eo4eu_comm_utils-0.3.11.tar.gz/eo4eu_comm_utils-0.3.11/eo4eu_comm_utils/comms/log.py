f8cb7e4ce173ca43259919eda91fef3e68a83fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 03:03:49 +0000

    Add package eo4eu-comm-utils/0.3.11/eo4eu_comm_utils-0.3.11.tar.gz

---
+++ packages/eo4eu-comm-utils/eo4eu_comm_utils-0.3.11.tar.gz/eo4eu_comm_utils-0.3.11/eo4eu_comm_utils/comms/log.py
@@ -0,0 +1,12 @@
+import traceback
+import logging
+from .interface import Comm, LogLevel
+
+
+class LogComm(Comm):
+    def __init__(self, logger: logging.Logger, print_traceback: bool = False):
+        self.logger = logger
+        self.print_traceback = print_traceback
+
+    def send(level: LogLevel, msg: str = "", *args, **kwargs):
+        self.logger.log(level.to_logging_level(), msg, exc_info = self.print_traceback)
