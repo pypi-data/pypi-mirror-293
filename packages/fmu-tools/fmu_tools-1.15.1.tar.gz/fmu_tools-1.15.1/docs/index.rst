438fbd7088be399f6c87eef6b0d4c2fd65f43d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 03:04:23 +0000

    Add package fmu-tools/1.15.1/fmu_tools-1.15.1.tar.gz

---
+++ packages/fmu-tools/fmu_tools-1.15.1.tar.gz/fmu_tools-1.15.1/docs/index.rst
@@ -0,0 +1,28 @@
+Welcome to fmu-tools' documentation!
+======================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   readme
+   fmudesign
+   qcforward
+   qcproperties
+   qcreset
+   properties
+   create_rft_ertobs
+   rename_rms_scripts
+   rms_import_localmodule
+   rmsvolumetrics2csv
+   ensembles
+   contributing
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/fmu-tools/fmu_tools-1.15.1.tar.gz/fmu_tools-1.15.1/src/fmu/tools/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for fmu-tools"""
+
+from fmu.tools.extract_grid_zone_tops import extract_grid_zone_tops  # noqa
+from fmu.tools.qcforward.qcforward import wellzonation_vs_grid  # noqa
+from fmu.tools.qcproperties.qcproperties import QCProperties  # noqa
+
+try:
+    import roxar  # noqa
+
+    ROXAR = True
+except ImportError:
+    ROXAR = False
+
+if not ROXAR:
+    from .rms import volumetrics  # noqa
+    from .sensitivities import DesignMatrix  # noqa
+    from .sensitivities import calc_tornadoinput  # noqa
+    from .sensitivities import excel2dict_design  # noqa
+    from .sensitivities import summarize_design  # noqa
+
+try:
+    from .version import version
+
+    __version__ = version
+except ImportError:
+    __version__ = "0.0.0"
