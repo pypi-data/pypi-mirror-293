8984311f8c19140f7f8033b5330573c278ed1a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:04:42 +0000

    Add package zetta-backend-toolkit/0.1.3/zetta-backend-toolkit-0.1.3.tar.gz

---
+++ packages/zetta-backend-toolkit/zetta-backend-toolkit-0.1.3.tar.gz/zetta-backend-toolkit-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zetta-backend-toolkit",
+    version="0.1.3",
+    author="Gonzalo Geraci",
+    author_email="gonza.geraci@gmail.com",
+    description="Un toolkit Backend para FastAPI",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gonzageraci/Zetta-Backend-Toolkit",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        "fastapi",
+        "starlette",
+    ],
+)
