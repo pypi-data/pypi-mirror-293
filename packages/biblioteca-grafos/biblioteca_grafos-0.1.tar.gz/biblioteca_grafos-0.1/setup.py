9988b9cc7cb9e80c2fa03b4d1816044778962dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 14:04:28 +0000

    Add package biblioteca-grafos/0.1/biblioteca_grafos-0.1.tar.gz

---
+++ packages/biblioteca-grafos/biblioteca_grafos-0.1.tar.gz/biblioteca_grafos-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='biblioteca_grafos',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Dependências que sua biblioteca precisa
+    description='Uma biblioteca de exemplo em Python',
+    author='Lucas Firmino Batista',
+    author_email='seuemail@example.com',
+    url='https://github.com/lucasfbb/biblioteca_grafos',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
