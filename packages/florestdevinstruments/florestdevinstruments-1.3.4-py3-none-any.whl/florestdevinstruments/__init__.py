1233443b69adf59edec9ca9bdc20e4a82a3c0ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:05:21 +0000

    Add package florestdevinstruments/1.3.4/florestdevinstruments-1.3.4-py3-none-any.whl

---
+++ packages/florestdevinstruments/florestdevinstruments-1.3.4-py3-none-any.whl/florestdevinstruments/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Волшебная библиотека Флореста для самых разных задач! <3\nFlorestDev, 2024.\n[Мои социальные сети](https://telegra.ph/Socseti-Floresta-06-28)"""
+
+from .florest_utilits import *
+from .ready_games import *
+from .working_api import *
+from .working_document import *
