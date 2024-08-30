57d6e95a07591e16152516b91c43940e58c7e9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:04:39 +0000

    Add package flexible-requirements/0.1.3/flexible_requirements-0.1.3.tar.gz

---
+++ packages/flexible-requirements/flexible_requirements-0.1.3.tar.gz/flexible_requirements-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flexible_requirements',
+    version='0.1.3',
+    author='benny-png',
+    author_email='mazikuben2@gmail.com',
+    description='A library for generating flexible Python requirements',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/benny-png/Flexible-Requirements-python-LIBRARY',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=open('own_requirements.txt').read().splitlines(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
