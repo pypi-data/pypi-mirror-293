3f7e1b239b93a147dbcf9a4cf687d7548450f8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:06:19 +0000

    Add package prefect/3.0.0rc20/prefect-3.0.0rc20-py3-none-any.whl

---
+++ packages/prefect/prefect-3.0.0rc20-py3-none-any.whl/prefect/server/ui/assets/404-DWgQjy-3.js
@@ -0,0 +1,2 @@
+import{d as e,c as a,n as o,o as t,de as n}from"./index-CKtbewUP.js";import{u as r}from"./usePageTitle-BJBko9bD.js";const m=e({__name:"404",setup(s){return r("Page Not Found"),(c,p)=>(t(),a(o(n)))}});export{m as default};
+//# sourceMappingURL=404-DWgQjy-3.js.map
---
+++ packages/prefect/prefect-3.0.0rc20-py3-none-any.whl/prefect/server/ui/assets/usePrefectApi-80PqJ8bJ.js
@@ -0,0 +1,2 @@
+import{cH as e,cI as r}from"./index-CKtbewUP.js";function c(){return e(r)}export{c as u};
+//# sourceMappingURL=usePrefectApi-80PqJ8bJ.js.map
