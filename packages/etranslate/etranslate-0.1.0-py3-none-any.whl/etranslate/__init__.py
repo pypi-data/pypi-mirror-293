f6e17a8b4f60fe65b6fb52e588b295ddb50b11ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:06:12 +0000

    Add package etranslate/0.1.0/etranslate-0.1.0-py3-none-any.whl

---
+++ packages/etranslate/etranslate-0.1.0-py3-none-any.whl/etranslate/__init__.py
@@ -0,0 +1,11 @@
+from .translate import translate
+from .translate import detect_laguage
+
+
+__author__      = 'Abolfazl Khalili'
+__copyright__   = 'Copyright 2024, etranslate'
+__license__     = 'MIT'
+__version__     = 'etranslate v0.1.0'
+__maintainer__  = 'Abolfazl Khalili'
+__email__       = 'kabolfazl39@gmail.com'
+__status__      = 'Stable'
