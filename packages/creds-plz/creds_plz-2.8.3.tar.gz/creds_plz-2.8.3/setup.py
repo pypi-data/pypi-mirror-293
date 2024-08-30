e5174bddd39d4d780e8611c4a32681cce63a730f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 14:04:16 +0000

    Add package creds-plz/2.8.3/creds_plz-2.8.3.tar.gz

---
+++ packages/creds-plz/creds_plz-2.8.3.tar.gz/creds_plz-2.8.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='creds-plz',
+    version='2.8.3',
+    install_requires=requirements,
+    author='Eric Pierce',
+    author_email='eric.pierce@nike.com',
+    description="A CLI to get temporary AWS credentials from Okta",
+    url='https://github.com/Nike-Inc/gimme-aws-creds',
+    long_description=open("LONG_DESCRIPTION.md").read(),
+    python_requires=">=3.7",
+    license='Apache License, v2.0',
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite="tests",
+    entry_points={
+        'console_scripts': [
+            'creds-plz=gimme_aws_creds.main:main',
+        ],
+    },
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: Apache Software License'
+    ]
+)
