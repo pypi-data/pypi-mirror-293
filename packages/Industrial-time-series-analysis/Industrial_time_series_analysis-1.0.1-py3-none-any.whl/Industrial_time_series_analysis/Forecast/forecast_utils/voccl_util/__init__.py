f94cca68d30721b6546828ad07670b8efea6e136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 15:04:42 +0000

    Add package Industrial-time-series-analysis/1.0.1/Industrial_time_series_analysis-1.0.1-py3-none-any.whl

---
+++ packages/Industrial-time-series-analysis/Industrial_time_series_analysis-1.0.1-py3-none-any.whl/Industrial_time_series_analysis/Forecast/forecast_utils/voccl_util/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""
+Name：PanYunJie
+Date：2024-07-20
+"""
+from Industrial_time_series_analysis.Forecast.forecast_utils.voccl_util import datasets
+from Industrial_time_series_analysis.Forecast.forecast_utils.voccl_util import models
+from Industrial_time_series_analysis.Forecast.forecast_utils.voccl_util import util
+from Industrial_time_series_analysis.Forecast.forecast_utils.voccl_util import evaluate
+from Industrial_time_series_analysis.Forecast.forecast_utils.voccl_util import test
+from Industrial_time_series_analysis.Forecast.forecast_utils.voccl_util import train
+
+__all__ = ['datasets', 'models','util', 'evaluate', 'test','train']
