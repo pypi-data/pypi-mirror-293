d7a686211cbf0d8b6ca664fb67eb90d7f26b4cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 04:04:39 +0000

    Add package aenox/0.0.3/aenox-0.0.3.tar.gz

---
+++ packages/aenox/aenox-0.0.3.tar.gz/aenox-0.0.3/aenox/models.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass
+class UserStats:
+    user_id: int
+    coins: int
+    claimed: datetime
+    streak: int
+    max_streak: int
+    pickaxe_level: int
+    smelter_level: int
+    luck: int
+    shields: int
+    cooldown_pickaxe: date
+    cooldown_smelter: date
+    maxInventorySize: int
+    inventory: []
+
+
+@dataclass
+class GuildStats:
+    guild_id: int
+    language: str
+    in_guild: bool
+    autoroles: []
+    ticket_transcript: bool
+    ticket_categorys: []
+    ticket_embed_channel: str
+    ticket_embed_id: str
+    ticket_category_id: str
+    enabled_modules: []
