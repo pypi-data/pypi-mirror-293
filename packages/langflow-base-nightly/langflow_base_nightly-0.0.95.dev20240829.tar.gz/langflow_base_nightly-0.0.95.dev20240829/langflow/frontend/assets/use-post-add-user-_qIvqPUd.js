9111e93ca4ff1ffea6ee9c534115fbaf6684c0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:06:18 +0000

    Add package langflow-base-nightly/0.0.95.dev20240829/langflow_base_nightly-0.0.95.dev20240829.tar.gz

---
+++ packages/langflow-base-nightly/langflow_base_nightly-0.0.95.dev20240829.tar.gz/langflow_base_nightly-0.0.95.dev20240829/langflow/frontend/assets/use-post-add-user-_qIvqPUd.js
@@ -0,0 +1,1 @@
+import{U as n,a as o,g as u}from"./index-CjF2qeUq.js";const i=t=>{const{mutate:e}=n(),a=async s=>(await o.post(`${u("USERS")}/`,s)).data;return e(["useAddUser"],async s=>await a(s),t)};export{i as u};
