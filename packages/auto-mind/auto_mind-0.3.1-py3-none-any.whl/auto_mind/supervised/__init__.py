86f71feda396bba87bc031c342f8b159415d2d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:04:53 +0000

    Add package auto-mind/0.3.1/auto_mind-0.3.1-py3-none-any.whl

---
+++ packages/auto-mind/auto_mind-0.3.1-py3-none-any.whl/auto_mind/supervised/__init__.py
@@ -0,0 +1,21 @@
+from ._manager import (
+    Manager,
+    DefaultManager,
+    ManagerDataParams,
+    ManagerModelParams,
+    ManagerOptimizerParams,
+    ManagerMetricsParams,
+    ManagerConfig,
+    ManagerTrainResult,
+)
+
+__all__ = [
+    'Manager',
+    'DefaultManager',
+    'ManagerDataParams',
+    'ManagerModelParams',
+    'ManagerOptimizerParams',
+    'ManagerMetricsParams',
+    'ManagerConfig',
+    'ManagerTrainResult',
+]
