62c37637a9610cb9ca754a9fa21c5623e7047526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:05:54 +0000

    Add package grid3d-maps/1.5.10/grid3d_maps-1.5.10.tar.gz

---
+++ packages/grid3d-maps/grid3d_maps-1.5.10.tar.gz/grid3d_maps-1.5.10/tests/test_vs_ert/test_ert_hooks.py
@@ -0,0 +1,78 @@
+"""Suite for testing through ERT."""
+
+import subprocess
+from pathlib import Path
+
+import pytest
+import yaml
+
+XTGEOTESTDATA = "tests/data/reek"
+
+
+@pytest.mark.requires_ert
+def test_grid3d_maps_through_ert(datatree):
+    """Test through ERT."""
+    print(f"ERT run on {datatree}")
+
+    eclbase = "REEK"
+
+    # Configuration file for grid3d_average_map,
+    # based on tests/yaml/avg1f.yml
+    avg_conf = {
+        "title": "Reek",
+        "input": {
+            "folderroot": XTGEOTESTDATA,
+            "grid": "$folderroot/reek2_grid_w_zerolayer.roff",
+            "dz": "$folderroot/reek2_grid_w_zerolayer_dz.roff",
+        },
+        "zonation": {"zranges": [{"ZERO": [15, 15]}]},
+        "plotsettings": {
+            "faultpolygons": str(Path(XTGEOTESTDATA) / "top_upper_reek_faultpoly.xyz"),
+            "dz": {"valuerange": [0, 3]},
+        },
+        "computesettings": {
+            "zone": True,
+            "all": False,
+            "tuning": {"zone_avg": True, "coarsen": 2},
+        },
+        "output": {"tag": "avg1f", "mapfolder": ".", "plotfolder": "."},
+    }
+    Path("avg_conf.yml").write_text(yaml.dump(avg_conf), encoding="utf8")
+
+    # Configuration file for grid3d_thickness,
+    # based on tests/yaml/avg1f.yml
+    eclroot = str(Path(XTGEOTESTDATA) / "REEK")
+    # (eclroot cannot be only in yaml, it is overriden by the forward model
+    # default argument)
+    hc_conf = {
+        "title": "Reek",
+        "input": {
+            "grid": str(Path(XTGEOTESTDATA) / "reek_grid_fromegrid.roff"),
+            "dates": ["19991201"],
+        },
+        "output": {"tag": "hc1f", "mapfolder": ".", "plotfolder": "."},
+    }
+    Path("hc_conf.yml").write_text(yaml.dump(hc_conf), encoding="utf8")
+
+    ert_config = [
+        "ECLBASE " + eclbase + ".DATA",
+        "QUEUE_SYSTEM LOCAL",
+        "NUM_REALIZATIONS 1",
+        "RUNPATH .",
+    ]
+
+    ert_config.append("FORWARD_MODEL GRID3D_AVERAGE_MAP(<CONFIG_AVGMAP>=avg_conf.yml)")
+    ert_config.append(
+        "FORWARD_MODEL GRID3D_HC_THICKNESS(<CONFIG_HCMAP>=hc_conf.yml, <ECLROOT>="
+        + eclroot
+        + ")"
+    )
+
+    ert_config_filename = Path(datatree) / "grid3d_maps_test.ert"
+    ert_config_filename.write_text("\n".join(ert_config))
+
+    subprocess.call(["ert", "test_run", ert_config_filename])
+
+    assert Path("zero--avg1f_average_dz.gri").is_file()
+    assert Path("all--hc1f_oilthickness--19991201.png").is_file()
+    assert Path("OK").is_file()
---
+++ packages/grid3d-maps/grid3d_maps-1.5.10.tar.gz/grid3d_maps-1.5.10/tests/test_vs_ert/test_hook_implementations.py
@@ -0,0 +1,92 @@
+"""Test suite for hook implementation."""
+
+import os
+import shutil
+from pathlib import Path
+
+import pytest
+
+try:
+    import ert  # noqa
+except ImportError:
+    try:
+        import ert_shared  # noqa
+    except ImportError:
+        pytest.skip(
+            "Not testing ERT hooks when ERT is not installed", allow_module_level=True
+        )
+
+import grid3d_maps.hook_implementations.jobs as jobs
+
+try:
+    from ert.shared.plugins.plugin_manager import ErtPluginManager
+except ModuleNotFoundError:
+    from ert_shared.plugins.plugin_manager import ErtPluginManager
+
+EXPECTED_JOBS = {
+    "GRID3D_AVERAGE_MAP": "grid3d_maps/config_jobs/GRID3D_AVERAGE_MAP",
+    "GRID3D_HC_THICKNESS": "grid3d_maps/config_jobs/GRID3D_HC_THICKNESS",
+    "GRID3D_AGGREGATE_MAP": "grid3d_maps/config_jobs/GRID3D_AGGREGATE_MAP",
+    "GRID3D_MIGRATION_TIME": "grid3d_maps/config_jobs/GRID3D_MIGRATION_TIME",
+}
+
+SRC_PATH = Path(__file__).absolute().parent.parent.parent / "src"
+
+
+@pytest.mark.requires_ert
+def test_hook_implementations():
+    """Test hook implementation."""
+    pma = ErtPluginManager(plugins=[jobs])
+
+    installable_jobs = pma.get_installable_jobs()
+    for wf_name, wf_location in EXPECTED_JOBS.items():
+        assert wf_name in installable_jobs
+        assert installable_jobs[wf_name].endswith(wf_location)
+        assert os.path.isfile(installable_jobs[wf_name])
+
+    assert set(installable_jobs.keys()) == set(EXPECTED_JOBS.keys())
+
+    expected_workflow_jobs = {}
+    installable_workflow_jobs = pma.get_installable_workflow_jobs()
+    for wf_name, wf_location in expected_workflow_jobs.items():
+        assert wf_name in installable_workflow_jobs
+        assert installable_workflow_jobs[wf_name].endswith(wf_location)
+
+    assert set(installable_workflow_jobs.keys()) == set(expected_workflow_jobs.keys())
+
+
+@pytest.mark.requires_ert
+def test_job_config_syntax():
+    """Check for syntax errors made in job configuration files"""
+    for _, job_config in EXPECTED_JOBS.items():
+        # Check (loosely) that double-dashes are enclosed in quotes:
+        with open(os.path.join(SRC_PATH, job_config), encoding="utf8") as f_handle:
+            for line in f_handle.readlines():
+                if not line.strip().startswith("--") and "--" in line:
+                    assert '"--' in line and " --" not in line
+
+
+@pytest.mark.requires_ert
+@pytest.mark.integration
+def test_executables():
+    """Test executables listed in job configurations exist in $PATH"""
+    for _, job_config in EXPECTED_JOBS.items():
+        with open(os.path.join(SRC_PATH, job_config), encoding="utf8") as f_handle:
+            executable = f_handle.readlines()[0].split()[1]
+            assert shutil.which(executable)
+
+
+@pytest.mark.requires_ert
+def test_hook_implementations_job_docs():
+    """Testing hook job docs."""
+    pma = ErtPluginManager(plugins=[jobs])
+
+    installable_jobs = pma.get_installable_jobs()
+
+    docs = pma.get_documentation_for_jobs()
+
+    assert set(docs.keys()) == set(installable_jobs.keys())
+
+    for job_name in installable_jobs:
+        assert docs[job_name]["description"] != ""
+        assert docs[job_name]["category"] != "other"
