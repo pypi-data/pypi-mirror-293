01863b1eefcd30d3b25c7839e20a43265905a865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:06:57 +0000

    Add package simapy/4.8.0/simapy-4.8.0-py3-none-any.whl

---
+++ packages/simapy/simapy-4.8.0-py3-none-any.whl/simapy/sima/package_info.py
@@ -0,0 +1,3 @@
+
+version = "4.8.0"
+packages =  {'command': 2, 'condition': 20, 'custom': 13, 'doc': 12, 'environment': 23, 'graph': 5, 'hla': 25, 'hydro': 29, 'metocean': 11, 'optimization': 1, 'post': 28, 'report': 12, 'riflex': 44, 'signals': 2, 'sima': 25, 'simo': 46, 'wamit': 15, 'windpark': 1, 'windturbine': 11, 'workflow': 20}
