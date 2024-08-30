9c4366de88c56794d47c64bb5b3d2776055e2966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:07:00 +0000

    Add package jollyjack/0.8.0/jollyjack-0.8.0-cp312-cp312-win_amd64.whl

---
+++ packages/jollyjack/jollyjack-0.8.0-cp312-cp312-win_amd64.whl/jollyjack/jollyjack.h
@@ -0,0 +1,16 @@
+#include "parquet/arrow/reader.h"
+#include "parquet/arrow/writer.h"
+#include "parquet/arrow/schema.h"
+
+void ReadIntoMemory (std::shared_ptr<arrow::io::RandomAccessFile> source
+    , std::shared_ptr<parquet::FileMetaData> file_metadata
+    , void* buffer
+    , size_t buffer_size
+    , size_t stride0_size
+    , size_t stride1_size
+    , const std::vector<int> &row_groups
+    , const std::vector<int> &column_indices
+    , const std::vector<std::string> &column_names
+    , bool pre_buffer
+    , bool use_threads
+    , int64_t expected_rows);
