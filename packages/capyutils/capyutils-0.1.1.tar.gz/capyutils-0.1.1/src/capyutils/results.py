9f5782ad44e54d8ad3f0fa0150e0cfd9b2429a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:04:46 +0000

    Add package capyutils/0.1.1/capyutils-0.1.1.tar.gz

---
+++ packages/capyutils/capyutils-0.1.1.tar.gz/capyutils-0.1.1/src/capyutils/results.py
@@ -0,0 +1,83 @@
+import typing as _t
+
+from capyutils.exceptions import UnwrappedException
+
+T = _t.TypeVar("T")
+
+
+class Result(_t.Generic[T]):
+    """
+    Util class to wrap around fallible values instead of raising Exceptions. Do
+    not instantiate direcly, instead, use `Err` and `Ok` util functions from
+    this package
+    """
+
+    def __init__(self, value: _t.Union[T, BaseException]):
+        self._value = value
+
+    def is_ok(self) -> bool:
+        """
+        Returns `True` if this `Result` is of the `Ok` type
+        """
+        return not self.is_ok()
+
+    def is_err(self) -> bool:
+        """
+        Returns `True` if this `Result` is of the `Err` type
+        """
+        return isinstance(self._value, BaseException)
+
+    def unwrap(self) -> T:
+        """
+        Returns the wrapped value if this `Result` is `Ok`, else, if this
+        `Result` is `Err`, then raises the UnwrappedException, instead.
+        """
+        if isinstance(self._value, BaseException):
+            raise UnwrappedException() from self._value
+        else:
+            return self._value
+
+    def try_unwrap(self) -> _t.Union[T, None]:
+        """
+        Returns the wrapped value if this `Result` is `Ok`, else, if this
+        `Result` is `Err`, returns `None`.
+        """
+        if not isinstance(self._value, BaseException):
+            return self._value
+        else:
+            return None
+
+    def unwrap_err(self) -> BaseException:
+        """
+        Returns the wrapped exception if this `Result` is `Err`, else, if this
+        `Result` is `Ok`, then raises a `UnwrapedException`.
+        """
+        if isinstance(self._value, BaseException):
+            return self._value
+        else:
+            raise TypeError("Can't unwrap error type in instance of Ok result.")
+
+    @staticmethod
+    def Err(error: _t.Union[BaseException, str]) -> "Result[T]":
+        """
+        Instantiate Error Result which wraps around exception. If `error` is a
+        string, will instantiate an `Exception` with `error` as error message.
+
+        If the wrapped type is not important for type checkers, use
+        `Result.Err(error)`. If it is, explictly provide the type (e.g.
+        `Result[str].Err(error)`)
+        """
+        if isinstance(error, str):
+            return Result(Exception(error))
+        return Result(error)
+
+    @staticmethod
+    def Ok(value: T) -> "Result[T]":
+        """
+        Instantiate Ok Result which wraps around a `value` (use `None` if value
+        is irrelevant)
+
+        Explictly provide the type (e.g. `Result[str].Ok("A string.")`) to let
+        type checkers know the type of the wrapped value
+        """
+        return Result(value)
