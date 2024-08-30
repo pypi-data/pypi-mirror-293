8cd6f697ab9e330575d6d6797a40813e3b96402d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:04:48 +0000

    Add package python-hddb/0.2.6/python_hddb-0.2.6-py3-none-any.whl

---
+++ packages/python-hddb/python_hddb-0.2.6-py3-none-any.whl/python_hddb/exceptions.py
@@ -0,0 +1,19 @@
+class HdDBClientError(Exception):
+    """Base exception for HdDBClient errors."""
+    pass
+
+class ConnectionError(HdDBClientError):
+    """Raised when there's an issue with the database connection."""
+    pass
+
+class QueryError(HdDBClientError):
+    """Raised when there's an error executing a query."""
+    pass
+
+class TableError(HdDBClientError):
+    """Raised when there's an issue with table operations."""
+    pass
+
+class TransactionError(HdDBClientError):
+    """Raised when there's an issue with transaction management."""
+    pass
