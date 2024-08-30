1c059c6936f23126bf6b0bb4bd06c11c01f8ff25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:04:17 +0000

    Add package simple-python-test/0.3.8/simple-python-test-0.3.8.tar.gz

---
+++ packages/simple-python-test/simple-python-test-0.3.8.tar.gz/simple-python-test-0.3.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import sys
+
+__version__ = "0.3.8"
+
+setup(
+    name="simple-python-test",
+    version=__version__,
+    url="https://github.com/SimpleDataLabsInc/dummy",
+    packages=find_packages(exclude=["test.*", "test"]),
+    description="Helper library for prophecy generated code",
+    long_description=open("README.md").read(),
+    install_requires=[],
+    keywords=["python", "prophecy"],
+    classifiers=[],
+    zip_safe=False,
+    license="GPL-3.0",
+)
