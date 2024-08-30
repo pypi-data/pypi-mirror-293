02519f7336e875dbb09de61c5834e31e5d02c669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 04:04:01 +0000

    Add package pytonapi/0.3.7/pytonapi-0.3.7.tar.gz

---
+++ packages/pytonapi/pytonapi-0.3.7.tar.gz/pytonapi-0.3.7/pytonapi/async_tonapi/methods/traces.py
@@ -0,0 +1,23 @@
+import base64
+import binascii
+
+from pytonapi.async_tonapi.client import AsyncTonapiClientBase
+from pytonapi.schema.traces import Trace
+
+
+class TracesMethod(AsyncTonapiClientBase):
+
+    async def get_trace(self, trace_id: str) -> Trace:
+        """
+        Get the trace by trace ID or hash of any transaction in trace.
+
+        :param trace_id: trace ID or transaction hash in hex (without 0x) or base64url format
+        :return: :class:`Trace`
+        """
+        if len(trace_id) == 44:
+            decoded = base64.urlsafe_b64decode(trace_id + "=" * (-len(trace_id) % 4))
+            trace_id = binascii.hexlify(decoded).decode("utf-8")
+        method = f"v2/traces/{trace_id}"
+        response_data = await self._get(method=method)
+
+        return Trace(**response_data)
---
+++ packages/pytonapi/pytonapi-0.3.7.tar.gz/pytonapi-0.3.7/pytonapi/tonapi/methods/traces.py
@@ -0,0 +1,23 @@
+import base64
+import binascii
+
+from pytonapi.schema.traces import Trace
+from pytonapi.tonapi.client import TonapiClientBase
+
+
+class TracesMethod(TonapiClientBase):
+
+    def get_trace(self, trace_id: str) -> Trace:
+        """
+        Get the trace by trace ID or hash of any transaction in trace.
+
+        :param trace_id: trace ID or transaction hash in hex (without 0x) or base64url format
+        :return: :class:`Trace`
+        """
+        if len(trace_id) == 44:
+            decoded = base64.urlsafe_b64decode(trace_id + "=" * (-len(trace_id) % 4))
+            trace_id = binascii.hexlify(decoded).decode("utf-8")
+        method = f"v2/traces/{trace_id}"
+        response = self._get(method=method)
+
+        return Trace(**response)
