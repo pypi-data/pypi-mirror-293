7249568361b5a4cbcd3a857f841bcfbd7f14b61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:04:46 +0000

    Add package optika/0.5.0/optika-0.5.0-py3-none-any.whl

---
+++ packages/optika/optika-0.5.0-py3-none-any.whl/optika/sensors/_materials/__init__.py
@@ -0,0 +1,37 @@
+from ._materials import (
+    energy_bandgap,
+    energy_electron_hole,
+    quantum_yield_ideal,
+    absorbance,
+    charge_collection_efficiency,
+    quantum_efficiency_effective,
+    probability_measurement,
+    electrons_measured,
+    AbstractImagingSensorMaterial,
+    IdealImagingSensorMaterial,
+    AbstractCCDMaterial,
+    AbstractBackilluminatedCCDMaterial,
+    AbstractStern1994BackilluminatedCCDMaterial,
+)
+from ._tektronix_tk512cb import TektronixTK512CBMaterial
+from ._e2v_ccd97 import E2VCCD97Material
+from ._e2v_ccd_aia import E2VCCDAIAMaterial
+
+__all__ = [
+    "energy_bandgap",
+    "energy_electron_hole",
+    "quantum_yield_ideal",
+    "absorbance",
+    "charge_collection_efficiency",
+    "quantum_efficiency_effective",
+    "probability_measurement",
+    "electrons_measured",
+    "AbstractImagingSensorMaterial",
+    "IdealImagingSensorMaterial",
+    "AbstractCCDMaterial",
+    "AbstractBackilluminatedCCDMaterial",
+    "AbstractStern1994BackilluminatedCCDMaterial",
+    "TektronixTK512CBMaterial",
+    "E2VCCD97Material",
+    "E2VCCDAIAMaterial",
+]
