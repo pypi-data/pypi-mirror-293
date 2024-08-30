d4480956392b7c682724c69a7733d954383f7b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:06:02 +0000

    Add package mtmai/0.3.546/mtmai-0.3.546.tar.gz

---
+++ packages/mtmai/mtmai-0.3.546.tar.gz/mtmai-0.3.546/mtmai/mtlibs/aisdk.py
@@ -0,0 +1,28 @@
+"""
+doc: https://sdk.vercel.ai/docs/ai-sdk-ui/stream-protocol
+"""
+
+import json
+
+
+def text(word: str):
+    return f"0:{json.dumps(word)}\n"
+
+
+def data(items):
+    if isinstance(items, list):
+        return f"2:{json.dumps(items)}\n"
+    else:
+        return f"2:{json.dumps([items])}\n"
+
+
+def error(error_message: str):
+    return f"3:{json.dumps(error_message)}\n"
+
+
+def finish(reason: str = "stop", prompt_tokens: int = 0, completion_tokens: int = 0):
+    data = {
+        "finishReason": reason,
+        "usage": {"promptTokens": prompt_tokens, "completionTokens": completion_tokens},
+    }
+    return f"d:{json.dumps(data)}\n"
