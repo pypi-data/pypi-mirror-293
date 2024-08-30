387cd70b961feac744fa0e978432f36b45d59c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 04:04:21 +0000

    Add package certbot-dns-ispconfig/0.3.0/certbot_dns_ispconfig-0.3.0.tar.gz

---
+++ packages/certbot-dns-ispconfig/certbot_dns_ispconfig-0.3.0.tar.gz/certbot_dns_ispconfig-0.3.0/setup.py
@@ -0,0 +1,60 @@
+from os import path
+from setuptools import setup
+from setuptools import find_packages
+
+version = "0.3.0"
+
+install_requires = [
+    "acme>=0.29.0",
+    "certbot>=0.34.0",
+    "setuptools",
+    "requests",
+    "mock",
+    "requests-mock",
+]
+
+# read the contents of your README file
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.rst")) as f:
+    long_description = f.read()
+
+setup(
+    name="certbot-dns-ispconfig",
+    version=version,
+    description="ispconfig DNS Authenticator plugin for Certbot",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/m42e/certbot-dns-ispconfig",
+    author="Matthias Bilger",
+    author_email="matthias@bilger.info",
+    license="Apache License 2.0",
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Plugins",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Security",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        "certbot.plugins": [
+            "dns-ispconfig = certbot_dns_ispconfig.dns_ispconfig:Authenticator"
+        ]
+    },
+    test_suite="certbot_dns_ispconfig",
+)
