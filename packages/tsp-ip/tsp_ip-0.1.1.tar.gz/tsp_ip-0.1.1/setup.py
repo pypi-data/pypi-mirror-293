7c72dca9a970722ecc71eb66721f4aadef07ebbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:06:49 +0000

    Add package tsp-ip/0.1.1/tsp_ip-0.1.1.tar.gz

---
+++ packages/tsp-ip/tsp_ip-0.1.1.tar.gz/tsp_ip-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+    name='tsp_ip',
+    version='0.1.1',
+    description='TSP-IP (Travelling Salesman Problem integer programing)',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    packages=['tsp_ip'],
+    author='rebui1der',
+    author_email='vlad_wizard@mail.ru',
+    zip_safe=False,
+    url='https://github.com/rebui1der/tsp-ip',
+    install_requires=[
+        'networkx>=3.3',
+        'numpy>=1.26.4',
+        'PuLP>=2.7.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+	'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    python_requires='>=3.11',
+    license_files = ('LICENSE.txt',),
+)
