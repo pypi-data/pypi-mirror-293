502fa1d8f507c51a06122200c10cd47a60e08a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 14:04:53 +0000

    Add package endstone/0.5.2/endstone-0.5.2-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/endstone/endstone-0.5.2-cp312-cp312-manylinux_2_31_x86_64.whl/endstone/form.py
@@ -0,0 +1,13 @@
+from endstone._internal.endstone_python import (
+    ActionForm,
+    MessageForm,
+    ModalForm,
+    Dropdown,
+    Label,
+    Slider,
+    StepSlider,
+    TextInput,
+    Toggle,
+)
+
+__all__ = ["ActionForm", "MessageForm", "ModalForm", "Dropdown", "Label", "Slider", "StepSlider", "TextInput", "Toggle"]
