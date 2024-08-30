a3d4014fc7f9d6263ecc6cea9980c2b21df7fd1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 14:04:17 +0000

    Add package d2dcnWidget/0.2.0/d2dcnWidget-0.2.0.tar.gz

---
+++ packages/d2dcnWidget/d2dcnWidget-0.2.0.tar.gz/d2dcnWidget-0.2.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+import d2dcnWidget
+import setuptools
+
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name = 'd2dcnWidget',
+        version = d2dcnWidget.version,
+        packages = ["d2dcnWidget"],
+        entry_points={
+            'console_scripts': [
+                'd2dcnGUI=d2dcnWidget.d2dcnGUI:main'
+            ],
+        },
+        install_requires = [
+            "d2dcn>=0.5.1",
+            "PyQt5"
+        ],
+        author = "Javier Moreno Garcia",
+        author_email = "jgmore@gmail.com",
+        description = "",
+        long_description_content_type = "text/markdown",
+        long_description = "",
+        url = "https://github.com/qeyup/d2dcnWidget-python"
+    )
