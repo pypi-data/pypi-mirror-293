fa060270cc9b6e981b96970079d248f870bf02be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 14:04:33 +0000

    Add package ok-script/0.0.185/ok-script-0.0.185.tar.gz

---
+++ packages/ok-script/ok-script-0.0.185.tar.gz/ok-script-0.0.185/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ok-script",
+    version="0.0.185",
+    author="ok-oldking",
+    author_email="firedcto@gmail.com",
+    description="Automation with Computer Vision for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ok-oldking/ok-script",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    install_requires=[
+        'adbutils>=2.7.2',
+        'numpy>=1.26.4',
+        'PyDirectInput>=1.0.4',
+        'pywin32>=306',
+        'typing-extensions>=4.11.0',
+        'PySide6>=6.7.0',
+        'PySide6-Fluent-Widgets>=1.5.6',
+        'psutil>=6.0.0',
+        'opencv-python>=4.9.0.80',
+        'ok-d3dshot>=0.1.5',
+        'py7zr>=0.21.0',
+        'wmi>=1.5.1',
+        'pycaw==20240210',
+    ],
+    python_requires='>=3.6',
+)
