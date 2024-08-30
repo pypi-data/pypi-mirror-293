3e95636a5f4a6c5fc918fd9a2b03fb70327e5fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 05:06:59 +0000

    Add package kadamapdf/1.0/kadamapdf-1.0.tar.gz

---
+++ packages/kadamapdf/kadamapdf-1.0.tar.gz/kadamapdf-1.0/setup.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+import setuptools
+
+
+setuptools.setup(
+    name='kadamapdf',
+    version=1.0,
+    long_description=Path('README.md').read_text(encoding='utf-8'),
+    packages=setuptools.find_packages(exclude=['tests', 'data']),
+)
