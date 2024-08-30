74ed1c668ba61f83964f71051afcacc451eadc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:04:37 +0000

    Add package launchflow/0.4.8/launchflow-0.4.8-py3-none-any.whl

---
+++ packages/launchflow/launchflow-0.4.8-py3-none-any.whl/launchflow/aws/service.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+from launchflow.models.enums import CloudProvider
+from launchflow.service import (
+    DockerService,
+    DockerServiceOutputs,
+    Service,
+    StaticService,
+    T,
+)
+
+
+class AWSService(Service[T]):
+    @classmethod
+    def cloud_provider(cls) -> CloudProvider:
+        return CloudProvider.AWS
+
+
+@dataclass
+class AWSDockerServiceOutputs(DockerServiceOutputs):
+    code_build_project_name: str
+
+
+class AWSDockerService(DockerService, AWSService):
+    def outputs(self) -> AWSDockerServiceOutputs:
+        raise NotImplementedError
+
+
+class AWSStaticService(StaticService, AWSService):
+    pass
