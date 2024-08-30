8cb35efee4c6b99d5fe13e0369c1b186398515fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 15:04:50 +0000

    Add package AppDT/1.1.0/appdt-1.1.0.tar.gz

---
+++ packages/AppDT/appdt-1.1.0.tar.gz/appdt-1.1.0/setup.py
@@ -0,0 +1,28 @@
+﻿from setuptools import setup
+ 
+setup(
+    name='AppDT',
+    version='1.1.0',
+    description='(This Is A Chinese Package)此工具箱可以帮您快速构建Windows™应用程序，也可以快速生成安装包',
+    long_description="""此工具箱可以帮您快速构建Windows™应用程序，也可以快速生成安装包。安装完毕后请在命令行中输入“adt”来运行它""",
+    author='xumouren225588',
+    author_email='xumouren225588@163.com',
+    url="https://www.douyin.com/user/MS4wLjABAAAARkk5zY-7-Et1347qxL_7rsHVzWHOh9NI1YbMAZZ8crI",
+    packages=['AppDT'],
+    license='MIT',
+    install_requires=[
+        'pyinstaller',
+        'PyQt5'
+    ],
+    python_requires='>=3.5',
+    entry_points={
+        'console_scripts': [
+            'adt=AppDT.whlcode:adt',
+        ],
+    },
+    classifiers=[
+        
+        "Programming Language :: Python",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
