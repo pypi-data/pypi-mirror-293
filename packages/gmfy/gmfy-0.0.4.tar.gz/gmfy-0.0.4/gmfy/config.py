95f4af399a4da85d8c483b8edbbaf18a286043ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 04:04:07 +0000

    Add package gmfy/0.0.4/gmfy-0.0.4.tar.gz

---
+++ packages/gmfy/gmfy-0.0.4.tar.gz/gmfy-0.0.4/gmfy/config.py
@@ -0,0 +1,9 @@
+import logging
+
+# Logging
+# --------------------------------------------------------------------------------------
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
