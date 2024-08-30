ef9c890444038c9260d945ca67b32213604d4a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 15:05:01 +0000

    Add package gitleak-package/0.1/gitleak_package-0.1.tar.gz

---
+++ packages/gitleak-package/gitleak_package-0.1.tar.gz/gitleak_package-0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import platform
+
+# Determine the operating system
+system = platform.system().lower()
+
+# Define entry points based on the OS
+if system == 'windows':
+    entry_points = {
+        'console_scripts': [
+            'install-gitleaks=gitleaks_installer.installer:install_gitleaks',
+            'add-gitleaks-path=gitleaks_installer.installer:add_gitleaks_to_path',
+        ],
+    }
+else:
+    entry_points = {
+        'console_scripts': [
+            'install-gitleaks=gitleaks_installer.installer:install_gitleaks',
+            'add-gitleaks-path=gitleaks_installer.installer:add_gitleaks_to_path',
+        ],
+    }
+
+setup(
+    name="gitleak-package",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # No external dependencies
+    entry_points=entry_points,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
