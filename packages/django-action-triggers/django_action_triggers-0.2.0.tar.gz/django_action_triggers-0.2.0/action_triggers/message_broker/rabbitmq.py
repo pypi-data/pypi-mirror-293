b35b6d53582e4a7bd296d1579631057812fa983d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 14:04:52 +0000

    Add package django-action-triggers/0.2.0/django_action_triggers-0.2.0.tar.gz

---
+++ packages/django-action-triggers/django_action_triggers-0.2.0.tar.gz/django_action_triggers-0.2.0/action_triggers/message_broker/rabbitmq.py
@@ -0,0 +1,75 @@
+import typing as _t
+
+from action_triggers.message_broker.base import BrokerBase, ConnectionBase
+from action_triggers.message_broker.enums import BrokerType
+from action_triggers.utils.module_import import MissingImportWrapper
+
+try:
+    import pika  # type: ignore[import-untyped]
+except ImportError:  # pragma: no cover
+    pika = MissingImportWrapper("pika")
+
+
+class RabbitMQConnection(ConnectionBase):
+    """Connection class for RabbitMQ."""
+
+    def validate_queue_exists(self):
+        """Validate the the queue exists either in the base configuration or
+        the user provided parameters.
+        """
+
+        if not self.config.get("queue") and not self.params.get("queue"):
+            self._errors.add_params_error(  # type: ignore[attr-defined]
+                "queue",
+                "Queue name must be provided.",
+            )
+
+    def validate(self) -> None:
+        self.validate_queue_exists()
+        super().validate()
+
+    def connect(self):
+        self.conn = pika.BlockingConnection(
+            pika.ConnectionParameters(**self.conn_details)
+        )
+
+
+class RabbitMQBroker(BrokerBase):
+    """Broker class for RabbitMQ.
+
+    :param broker_key: The key for the broker (must existing in
+        `settings.ACTION_TRIGGERS`).
+    :param conn_params: The connection parameters to use for establishing the
+        connection.
+    :param params: Additional parameters to use for the message broker.
+    :param kwargs: Additional keyword arguments to pass to the subclass.
+    """
+
+    broker_type = BrokerType.RABBITMQ
+    conn_class = RabbitMQConnection
+
+    def __init__(
+        self,
+        broker_key: str,
+        conn_params: _t.Union[dict, None],
+        params: _t.Union[dict, None],
+        **kwargs,
+    ):
+        super().__init__(broker_key, conn_params, params, **kwargs)
+        self.queue = self.params.get("queue")
+        self.exchange = self.params.get("exchange", "")
+
+    def _send_message_impl(self, conn: _t.Any, message: str) -> None:
+        """Send a message to the RabbitMQ broker.
+
+        :param conn: The connection to the broker.
+        :param message: The message to send.
+        """
+
+        channel = conn.conn.channel()
+        channel.queue_declare(queue=self.queue)
+        channel.basic_publish(
+            exchange=self.exchange,
+            routing_key=self.queue,
+            body=message.encode("utf-8"),
+        )
---
+++ packages/django-action-triggers/django_action_triggers-0.2.0.tar.gz/django_action_triggers-0.2.0/action_triggers/webhooks.py
@@ -0,0 +1,75 @@
+"""Module for processing webhook based actions."""
+
+import typing as _t
+
+import requests
+
+from action_triggers.dynamic_loading import replace_dict_values_with_results
+from action_triggers.exceptions import DisallowedWebhookEndpointError
+from action_triggers.models import Webhook
+
+
+class WebhookProcessor:
+    """Process an action which involves sending a webhook.
+
+    :param webhook: The webhook configuration to process.
+    :param payload: The payload to send with the webhook.
+    """
+
+    def __init__(self, webhook: Webhook, payload: _t.Union[str, dict]):
+        self.webhook = webhook
+        self.payload = payload
+        self.response: _t.Optional[requests.Response] = None
+
+    def __call__(self) -> requests.Response:
+        return self.process()
+
+    def process(self) -> requests.Response:
+        """Processes the webhook action."""
+
+        if not self.webhook.is_endpoint_whitelisted():
+            raise DisallowedWebhookEndpointError(self.webhook.url)
+
+        req_fn = self.get_request_fn()
+        fn_kwargs = self.get_fn_kwargs()
+
+        self.response = req_fn(**fn_kwargs)
+        self.response.raise_for_status()
+
+        return self.response
+
+    def get_request_fn(self) -> _t.Callable[..., requests.Response]:
+        """Returns the request function to use for the webhook.
+
+        :raises AttributeError: If the HTTP method is not supported.
+        :return: The request function to use for the webhook.
+        """
+
+        return getattr(requests, self.webhook.http_method.lower())
+
+    def get_fn_kwargs(self) -> dict:
+        """Returns the keyword arguments to pass to the request function.
+
+        :return: The keyword arguments to pass to the request function.
+        """
+
+        fn_kwargs: _t.Dict[str, _t.Any] = {"url": self.webhook.url}
+        headers = self.get_headers()
+        if headers:
+            fn_kwargs["headers"] = replace_dict_values_with_results(headers)
+
+        if isinstance(self.payload, dict):
+            fn_kwargs["json"] = self.payload
+        else:
+            fn_kwargs["data"] = self.payload
+
+        return fn_kwargs
+
+    def get_headers(self) -> dict:
+        """Returns the headers to use for the webhook.
+
+        :return: The headers to use for the webhook.
+        :rtype: dict
+        """
+
+        return self.webhook.headers
