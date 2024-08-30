a9be0391a0b91a0eb8ad192dfef65386ac5db827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 16:05:52 +0000

    Add package grid3d-maps/1.5.10/grid3d_maps-1.5.10-py3-none-any.whl

---
+++ packages/grid3d-maps/grid3d_maps-1.5.10-py3-none-any.whl/grid3d_maps/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for grid3d_maps"""
+
+try:
+    from .version import __version__
+
+except ImportError:
+    __version__ = "0.0.0"
---
+++ packages/grid3d-maps/grid3d_maps-1.5.10-py3-none-any.whl/grid3d_maps/contact/_compute_contact.py
@@ -0,0 +1,29 @@
+import numpy as np
+import xtgeo
+from xtgeo.common import XTGeoDialog, null_logger
+from xtgeo.surface import RegularSurface
+
+xtg = XTGeoDialog()
+logger = null_logger(__name__)
+
+
+def gridmap_contact(config, specd, propd, dates):
+    """Compute a contact as a gridded map surface"""
+
+    if "templatefile" in config["mapsettings"]:
+        xmap = xtgeo.surface_from_file(config["mapsettings"]["templatefile"])
+        xmap.values = 0.0
+    else:
+        ncol = config["mapsettings"].get("ncol")
+        nrow = config["mapsettings"].get("nrow")
+
+        xmap = RegularSurface(
+            xori=config["mapsettings"].get("xori"),
+            yori=config["mapsettings"].get("yori"),
+            ncol=ncol,
+            nrow=nrow,
+            xinc=config["mapsettings"].get("xinc"),
+            yinc=config["mapsettings"].get("yinc"),
+            values=np.zeros((ncol, nrow)),
+        )
+    print(propd)
