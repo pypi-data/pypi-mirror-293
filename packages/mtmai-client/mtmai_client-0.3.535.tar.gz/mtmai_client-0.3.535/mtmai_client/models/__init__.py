168acb0cb84a84222d4451003798774a46472c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 03:03:58 +0000

    Add package mtmai-client/0.3.535/mtmai_client-0.3.535.tar.gz

---
+++ packages/mtmai-client/mtmai_client-0.3.535.tar.gz/mtmai_client-0.3.535/mtmai_client/models/__init__.py
@@ -0,0 +1,129 @@
+"""Contains all the data models used in inputs/outputs"""
+
+from .agent_chat_config import AgentChatConfig
+from .blog_post_create_req import BlogPostCreateReq
+from .blog_post_create_res import BlogPostCreateRes
+from .blog_post_detail_res import BlogPostDetailRes
+from .blog_post_update_req import BlogPostUpdateReq
+from .blog_post_update_res import BlogPostUpdateRes
+from .blog_writer_agent_state import BlogWriterAgentState
+from .blog_writer_agent_state_messages_type_0_item import BlogWriterAgentStateMessagesType0Item
+from .body_auth_login_access_token import BodyAuthLoginAccessToken
+from .chat_input import ChatInput
+from .chat_message import ChatMessage
+from .config_response import ConfigResponse
+from .conversation_public import ConversationPublic
+from .doc_coll_create import DocCollCreate
+from .doc_coll_public import DocCollPublic
+from .doc_colls_public import DocCollsPublic
+from .editor import Editor
+from .http_validation_error import HTTPValidationError
+from .interview_state import InterviewState
+from .interview_state_references_type_0 import InterviewStateReferencesType0
+from .item_create import ItemCreate
+from .item_public import ItemPublic
+from .item_update import ItemUpdate
+from .items_public import ItemsPublic
+from .joke_agent_state import JokeAgentState
+from .joke_agent_state_messages_type_0_item import JokeAgentStateMessagesType0Item
+from .message import Message
+from .message_ack_request import MessageAckRequest
+from .message_public import MessagePublic
+from .message_public_message import MessagePublicMessage
+from .message_pull_item import MessagePullItem
+from .message_pull_req import MessagePullReq
+from .message_pull_response import MessagePullResponse
+from .message_pull_response_item import MessagePullResponseItem
+from .message_send_public import MessageSendPublic
+from .message_send_public_messages_item import MessageSendPublicMessagesItem
+from .mtm_editor_req import MtmEditorReq
+from .new_password import NewPassword
+from .outline import Outline
+from .post import Post
+from .rag_retrieval_req import RagRetrievalReq
+from .read_file_req import ReadFileReq
+from .research_state import ResearchState
+from .run_bash_req import RunBashReq
+from .runnable_config import RunnableConfig
+from .runnable_config_configurable import RunnableConfigConfigurable
+from .runnable_config_metadata import RunnableConfigMetadata
+from .section import Section
+from .sub_app import SubApp
+from .sub_web import SubWeb
+from .subsection import Subsection
+from .token import Token
+from .update_password import UpdatePassword
+from .user_create import UserCreate
+from .user_public import UserPublic
+from .user_register import UserRegister
+from .user_update import UserUpdate
+from .user_update_me import UserUpdateMe
+from .users_public import UsersPublic
+from .validation_error import ValidationError
+from .wiki_section import WikiSection
+from .workspace import Workspace
+
+__all__ = (
+    "AgentChatConfig",
+    "BlogPostCreateReq",
+    "BlogPostCreateRes",
+    "BlogPostDetailRes",
+    "BlogPostUpdateReq",
+    "BlogPostUpdateRes",
+    "BlogWriterAgentState",
+    "BlogWriterAgentStateMessagesType0Item",
+    "BodyAuthLoginAccessToken",
+    "ChatInput",
+    "ChatMessage",
+    "ConfigResponse",
+    "ConversationPublic",
+    "DocCollCreate",
+    "DocCollPublic",
+    "DocCollsPublic",
+    "Editor",
+    "HTTPValidationError",
+    "InterviewState",
+    "InterviewStateReferencesType0",
+    "ItemCreate",
+    "ItemPublic",
+    "ItemsPublic",
+    "ItemUpdate",
+    "JokeAgentState",
+    "JokeAgentStateMessagesType0Item",
+    "Message",
+    "MessageAckRequest",
+    "MessagePublic",
+    "MessagePublicMessage",
+    "MessagePullItem",
+    "MessagePullReq",
+    "MessagePullResponse",
+    "MessagePullResponseItem",
+    "MessageSendPublic",
+    "MessageSendPublicMessagesItem",
+    "MtmEditorReq",
+    "NewPassword",
+    "Outline",
+    "Post",
+    "RagRetrievalReq",
+    "ReadFileReq",
+    "ResearchState",
+    "RunBashReq",
+    "RunnableConfig",
+    "RunnableConfigConfigurable",
+    "RunnableConfigMetadata",
+    "Section",
+    "SubApp",
+    "Subsection",
+    "SubWeb",
+    "Token",
+    "UpdatePassword",
+    "UserCreate",
+    "UserPublic",
+    "UserRegister",
+    "UsersPublic",
+    "UserUpdate",
+    "UserUpdateMe",
+    "ValidationError",
+    "WikiSection",
+    "Workspace",
+)
