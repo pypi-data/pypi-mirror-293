729411818e0542e843ff56302d38a960eb3c3d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 06:05:09 +0000

    Add package irtorch/0.2.3/irtorch-0.2.3-py3-none-any.whl

---
+++ packages/irtorch/irtorch-0.2.3-py3-none-any.whl/irtorch/__init__.py
@@ -0,0 +1,16 @@
+import logging
+from .bit_scales import BitScales
+from .evaluation import Evaluation
+from .plotting import Plotting
+from . import load_dataset
+from . import config
+from . import utils
+from . import cross_validation
+
+__version__ = "0.2.3"
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(levelname)s: %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+
+logger = logging.getLogger("irtorch")
