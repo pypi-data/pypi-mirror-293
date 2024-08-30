a607e05664a99c80266fdc7b290bd288a227c429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-30 15:04:42 +0000

    Add package TextInParseX-test/0.0.1/TextInParseX-test-0.0.1.tar.gz

---
+++ packages/TextInParseX-test/TextInParseX-test-0.0.1.tar.gz/TextInParseX-test-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TextInParseX-test',
+    version='0.0.1',
+    author='bin_zhang',
+    description='A text-in parser engine for pdf to markdown processing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/intsig/x2md_tools/python/ParseGenius_engine',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python-headless',
+        'numpy',
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
