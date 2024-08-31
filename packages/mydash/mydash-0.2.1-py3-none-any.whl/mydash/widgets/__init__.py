0556b7a05832c8f7d3c6b4d92bda83db9650cf0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:33 +0000

    Add package mydash/0.2.1/mydash-0.2.1-py3-none-any.whl

---
+++ packages/mydash/mydash-0.2.1-py3-none-any.whl/mydash/widgets/__init__.py
@@ -0,0 +1,11 @@
+from .text import TextWidget
+from .bar_graph import BarGraphWidget
+from .base import BaseWidget
+from .image import ImageWidget
+
+__all__ = [
+    "TextWidget",
+    "BaseWidget",
+    "BarGraphWidget",
+    "ImageWidget",
+]
