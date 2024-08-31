cfdecfc3444978d7d3743a353432628967db272d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 04:05:35 +0000

    Add package vonage/3.99.3a0/vonage-3.99.3a0.tar.gz

---
+++ packages/vonage/vonage-3.99.3a0.tar.gz/vonage-3.99.3a0/src/vonage/__init__.py
@@ -0,0 +1,34 @@
+from vonage_utils import VonageError
+
+from .vonage import (
+    Account,
+    Application,
+    Auth,
+    HttpClientOptions,
+    Messages,
+    NumberInsight,
+    Sms,
+    Subaccounts,
+    Users,
+    Verify,
+    VerifyV2,
+    Voice,
+    Vonage,
+)
+
+__all__ = [
+    'Vonage',
+    'Auth',
+    'HttpClientOptions',
+    'Account',
+    'Application',
+    'Messages',
+    'NumberInsight',
+    'Sms',
+    'Subaccounts',
+    'Users',
+    'Verify',
+    'VerifyV2',
+    'Voice',
+    'VonageError',
+]
