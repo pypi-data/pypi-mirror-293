6185b16c7d0d958b75311d747d8aa57bbe05bbf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:32 +0000

    Add package torch-snippets/0.542/torch_snippets-0.542.tar.gz

---
+++ packages/torch-snippets/torch_snippets-0.542.tar.gz/torch_snippets-0.542/torch_snippets/__init__.py
@@ -0,0 +1,78 @@
+__version__ = "0.542"
+from .logger import *
+from .loader import *
+from .paths import *
+from .markup import *
+
+# from .inspector import *
+from .load_defaults import *
+from .pdf_loader import PDF
+from .markup2 import AD
+from .registry import *
+from .ipython import *
+from .decorators import *
+from .misc import *
+from .dates import *
+from .s3_loader import *
+from .zen import *
+
+
+def init_torch():
+    global torch
+    from .torch_loader import (
+        torch,
+        th,
+        torchvision,
+        T,
+        transforms,
+        nn,
+        np,
+        F,
+        Dataset,
+        DataLoader,
+        optim,
+        Report,
+        Reshape,
+        Permute,
+        device,
+        save_torch_model_weights_from,
+        load_torch_model_weights_to,
+        detach,
+        cat_with_padding,
+    )
+
+    try:
+        import lovely_tensors
+
+        lovely_tensors.monkey_patch()
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(
+            "Unable to import `lovely-tensors`. `pip install lovely-tensors` for a prettier experience"
+        )
+
+    globals().update(locals())
+
+
+"""
+def init_torch():
+    global torch
+    from .torch_loader import (
+        torch,
+        th,
+        torchvision,
+    )
+
+    try:
+        import lovely_tensors
+
+        lovely_tensors.monkey_patch()
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(
+            "Unable to import `lovely-tensors`. `pip install lovely-tensors` for a prettier experience"
+        )
+
+    print(AD(locals()))
+    globals().update(locals())
+    print(AD(globals()))
+
+"""
