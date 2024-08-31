77fc7e2c8b8c2309e5397d788e3970ffc5ea85c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:05:36 +0000

    Add package datamarkin/0.1.0/datamarkin-0.1.0.tar.gz

---
+++ packages/datamarkin/datamarkin-0.1.0.tar.gz/datamarkin-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='datamarkin',
+    version='0.1.0',
+    description='Reusable computer vision tools',
+    url='https://github.com/datamarkin/visionflow',
+    author='Datamarkin',
+    author_email='support@datamarkin.com',
+    license='MIT License',
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
