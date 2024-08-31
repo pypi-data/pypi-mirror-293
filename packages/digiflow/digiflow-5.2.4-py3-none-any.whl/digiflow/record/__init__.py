60e4524cc25480f395c75e407b3c288920f88f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:44 +0000

    Add package digiflow/5.2.4/digiflow-5.2.4-py3-none-any.whl

---
+++ packages/digiflow/digiflow-5.2.4-py3-none-any.whl/digiflow/record/__init__.py
@@ -0,0 +1,15 @@
+from .common import *
+from .record_handler import (
+    RecordHandler,
+    RecordHandlerException,
+)
+from .record_criteria import (
+    Text,
+    State,
+    Datetime,
+)
+from .record_service import (
+    Client, HandlerInformation, 
+	RecordsExhaustedException, RecordsServiceException,
+	run_server
+)
