ff5be8f504c7819c3f5639ce65de2443fed0967b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:05:14 +0000

    Add package kwcoco/0.8.3/kwcoco-0.8.3-py3-none-any.whl

---
+++ packages/kwcoco/kwcoco-0.8.3-py3-none-any.whl/kwcoco/sensorchan_spec.py
@@ -0,0 +1,8 @@
+"""
+This functionality has been moved to "delayed_image"
+"""
+from delayed_image.sensorchan_spec import SensorChanSpec, FusedSensorChanSpec
+from delayed_image.sensorchan_spec import sensorchan_concise_parts
+from delayed_image.sensorchan_spec import normalize_sensor_chan
+from delayed_image.sensorchan_spec import concise_sensor_chan
+from delayed_image.sensorchan_spec import sensorchan_normalized_parts
