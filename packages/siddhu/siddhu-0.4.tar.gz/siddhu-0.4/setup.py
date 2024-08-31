0bd51b7f62c19d731808fbea22815ecfeab10d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:46 +0000

    Add package siddhu/0.4/siddhu-0.4.tar.gz

---
+++ packages/siddhu/siddhu-0.4.tar.gz/siddhu-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='siddhu',
+    version='0.4',  # Incremented version number
+    packages=find_packages(),
+    install_requires=[],
+    description='A simple example library by Siddharth',
+    author='Siddharth',
+    author_email='siddharth09082001@gmail.com',
+    # url='https://github.com/yourusername/siddhu',  # Uncomment if you have a URL
+)
---
+++ packages/siddhu/siddhu-0.4.tar.gz/siddhu-0.4/siddhu/basic.py
@@ -0,0 +1,6 @@
+def greet(name,age):
+    return f'hello {name} ! you are {age} now !'
+
+
+def food(sweet,spicy):
+    return f'i want  {sweet} and {spicy} '
