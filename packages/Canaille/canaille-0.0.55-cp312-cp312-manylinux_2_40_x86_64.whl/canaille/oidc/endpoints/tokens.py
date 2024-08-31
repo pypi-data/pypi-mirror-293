ca3116b1492fd5f0de6c3c6217a97a57b86533f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:22 +0000

    Add package Canaille/0.0.55/canaille-0.0.55-cp312-cp312-manylinux_2_40_x86_64.whl

---
+++ packages/Canaille/canaille-0.0.55-cp312-cp312-manylinux_2_40_x86_64.whl/canaille/oidc/endpoints/tokens.py
@@ -0,0 +1,55 @@
+import datetime
+
+from flask import Blueprint
+from flask import abort
+from flask import flash
+from flask import request
+
+from canaille.app import models
+from canaille.app.flask import permissions_needed
+from canaille.app.flask import render_htmx_template
+from canaille.app.forms import TableForm
+from canaille.app.i18n import gettext as _
+from canaille.app.themes import render_template
+from canaille.backends import Backend
+
+from .forms import TokenRevokationForm
+
+bp = Blueprint("tokens", __name__, url_prefix="/admin/token")
+
+
+@bp.route("/", methods=["GET", "POST"])
+@permissions_needed("manage_oidc")
+def index(user):
+    table_form = TableForm(models.Token, formdata=request.form)
+    if request.form and request.form.get("page") and not table_form.validate():
+        abort(404)
+
+    return render_htmx_template(
+        "token_list.html", menuitem="admin", table_form=table_form
+    )
+
+
+@bp.route("/<token:token>", methods=["GET", "POST"])
+@permissions_needed("manage_oidc")
+def view(user, token):
+    form = TokenRevokationForm(request.form or None)
+
+    if request.form and form.validate():
+        if request.form.get("action") == "confirm-revoke":
+            return render_template("modals/revoke-token.html", token=token)
+
+        elif request.form.get("action") == "revoke":
+            token.revokation_date = datetime.datetime.now(datetime.timezone.utc)
+            Backend.instance.save(token)
+            flash(_("The token has successfully been revoked."), "success")
+
+        else:
+            abort(400, f"bad form action: {request.form.get('action')}")
+
+    return render_template(
+        "token_view.html",
+        token=token,
+        menuitem="admin",
+        form=form,
+    )
