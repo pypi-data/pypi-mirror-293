cfd1f3b423a95ae58b647aebd3faed132ce23447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 04:04:49 +0000

    Add package holosen-unit-converter/0.1.0/holosen_unit_converter-0.1.0-py3-none-any.whl

---
+++ packages/holosen-unit-converter/holosen_unit_converter-0.1.0-py3-none-any.whl/unit_converter/converter.py
@@ -0,0 +1,46 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+def kilometers_to_miles(kilometers):
+    return kilometers * 0.621371
+
+
+def miles_to_kilometers(miles):
+    return miles / 0.621371
+
+
+def kilograms_to_pounds(kilograms):
+    return kilograms * 2.20462
+
+
+def pounds_to_kilograms(pounds):
+    return pounds / 2.20462
+
+
+def meters_to_feet(meters):
+    return meters * 3.28084
+
+
+def feet_to_meters(feet):
+    return feet / 3.28084
+
+
+def liters_to_gallons(liters):
+    return liters * 0.264172
+
+
+def gallons_to_liters(gallons):
+    return gallons / 0.264172
+
+
+def grams_to_ounces(grams):
+    return grams * 0.035274
+
+
+def ounces_to_grams(ounces):
+    return ounces / 0.035274
