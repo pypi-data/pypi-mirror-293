520f72bd922e0441dedda1dfe4d6426c05a2c007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:27 +0000

    Add package ioxplugin/1.3.6/ioxplugin-1.3.6-py3-none-any.whl

---
+++ packages/ioxplugin/ioxplugin-1.3.6-py3-none-any.whl/tests/test_ioxplugin.py
@@ -0,0 +1,25 @@
+#test ioxplugin
+
+#from ioxplugin import Plugin, ModbusIoX
+import os
+from ioxplugin import Plugin
+from ioxplugin import PluginStoreOps
+#from ioxplugin import PG3WebsocketConnection
+from ioxplugin import PLUGIN_LOGGER
+import time
+
+
+curdir = os.curdir
+#pg3wss.await_completion()
+time.sleep(20000)
+
+plugin_path=(f"{curdir}/tests/dimmer.iox_plugin.json")
+storeOps=PluginStoreOps('Local')
+storeOps.addToStore(plugin_path, "tech@universal-devices.com", "admin", "/usr/home/admin/workspace/ioxplugin/tests")
+
+plugin=Plugin(plugin_path)
+s = plugin.areNodesStatic()
+plugin.toIoX()
+plugin.generateCode(path="/usr/home/admin/workspace/ioxplugin/tests")
+#ModbusIoX = ModbusIoX(plugin)
+#plugin.generateCode(".")
