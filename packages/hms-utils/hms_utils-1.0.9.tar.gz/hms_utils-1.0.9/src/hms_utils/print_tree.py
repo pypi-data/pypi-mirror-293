7a361073b0513505c3b260aca813088129f0c8f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 04:04:38 +0000

    Add package hms-utils/1.0.9/hms_utils-1.0.9.tar.gz

---
+++ packages/hms-utils/hms_utils-1.0.9.tar.gz/hms_utils-1.0.9/src/hms_utils/print_tree.py
@@ -0,0 +1,23 @@
+from typing import Optional, Callable
+
+
+def print_tree(data: dict, indent: Optional[int] = None,
+               hide_values: bool = False, printf: Optional[Callable] = None) -> None:
+    if not callable(printf):
+        printf = print
+    output = (lambda value: printf(f"{' ' * indent}{value}")) if isinstance(indent, int) and indent > 0 else printf
+    def traverse(data: dict, indent: str = "", first: bool = False, last: bool = True):  # noqa
+        nonlocal output
+        space = "    " if not first else "  "
+        for index, key in enumerate(keys := list(data.keys())):
+            last = (index == len(keys) - 1)
+            corner = "▷ " if first else ("└── " if last else "├── ")
+            if isinstance(value := data[key], dict):
+                output(indent + corner + str(key))
+                traverse(value, indent=indent + (space if last else f"{' ' if first else '│'}{space[1:]}"), last=last)
+            else:
+                if hide_values is not False:
+                    output(indent + corner + f"{key}: ********")
+                else:
+                    output(indent + corner + f"{key}: {value}")
+    traverse(data, first=True)
