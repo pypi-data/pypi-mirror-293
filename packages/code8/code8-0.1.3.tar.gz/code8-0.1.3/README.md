464c61217ae799ca7b5490beb5fa015a2cb647fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:11 +0000

    Add package code8/0.1.3/code8-0.1.3.tar.gz

---
+++ packages/code8/code8-0.1.3.tar.gz/code8-0.1.3/README.md
@@ -0,0 +1,29 @@
+# code8
+
+code8 is a dashboard to use LLMs to directly on your code files, save and load custom profiles with custom prompts and more.
+
+## Installation
+
+```python
+pip install code8
+```
+
+## Usage
+
+After installation, simply run the following command in your terminal:
+
+```python
+code8
+```
+
+This will open up a web browser with the code8 UI, allowing you configure settings
+and select files to run the LLMs on.
+
+After configuring the settings using "code8", you could continue using the tool
+through the UI with "code8", or you could use it in your terminal:
+
+```python
+code8 example.py
+```
+
+this will run the tool directly from the terminal without having to use the UI.
---
+++ packages/code8/code8-0.1.3.tar.gz/code8-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='code8',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        'openai',
+        'anthropic',
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'code8=code8:main',
+        ],
+    },
+    author='Naser Jamal',
+    author_email='naser.dll@hotmail.com',
+    description='a dashboard to use LLMs to directly on your code files, save and load custom profiles with custom prompts and more.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/naserjamal/code8',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
