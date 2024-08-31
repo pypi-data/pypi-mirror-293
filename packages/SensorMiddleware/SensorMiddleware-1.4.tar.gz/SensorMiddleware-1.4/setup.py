40f1f2345eb8efdc36fb6227f705935c984edf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:05:13 +0000

    Add package SensorMiddleware/1.4/SensorMiddleware-1.4.tar.gz

---
+++ packages/SensorMiddleware/SensorMiddleware-1.4.tar.gz/SensorMiddleware-1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='SensorMiddleware',
+    version='1.4',
+    author_name='Pablo Skubert',
+    author_email='pablo1920@protonmail.com',
+
+    packages=['app'],
+    install_requires=[
+        'boto3',
+        'requests'
+    ],
+    description='Middleware para os Sensores EcoTrust',
+    long_description=open('README.md', 'r').read(),
+    entry_points={
+        'console_scripts': [
+            'scontroler = app.main:scontroler_main'
+        ]
+    },
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+)
