91c5b3ccdcb09569ebdbcd0f4db291a284359f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:52 +0000

    Add package types-aiobotocore-full/2.14.0/types_aiobotocore_full-2.14.0.tar.gz

---
+++ packages/types-aiobotocore-full/types_aiobotocore_full-2.14.0.tar.gz/types_aiobotocore_full-2.14.0/types_aiobotocore_ivs_realtime/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for ivs-realtime service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_ivs_realtime import (
+        Client,
+        IvsrealtimeClient,
+        ListPublicKeysPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("ivs-realtime") as client:
+        client: IvsrealtimeClient
+        ...
+
+
+    list_public_keys_paginator: ListPublicKeysPaginator = client.get_paginator("list_public_keys")
+    ```
+"""
+
+from .client import IvsrealtimeClient
+from .paginator import ListPublicKeysPaginator
+
+Client = IvsrealtimeClient
+
+__all__ = ("Client", "IvsrealtimeClient", "ListPublicKeysPaginator")
