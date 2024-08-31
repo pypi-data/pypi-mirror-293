25df5bacd1c8861ed496c49d008b8c980697d0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 04:04:21 +0000

    Add package dataflow-core/1.0.0/dataflow-core-1.0.0.tar.gz

---
+++ packages/dataflow-core/dataflow-core-1.0.0.tar.gz/dataflow-core-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dataflow-core",
+    version="1.0.0",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    package_data={},
+    install_requires=[
+        'sqlalchemy',
+        'boto3',
+    ],
+    author="Dataflow",
+    author_email="",
+    description="Dataflow core package",
+    entry_points={
+        'jupyterhub.authenticators': [
+            'dataflow_authenticator = authenticator.dataflowhubauthenticator:DataflowHubAuthenticator',
+        ],
+    },
+)
