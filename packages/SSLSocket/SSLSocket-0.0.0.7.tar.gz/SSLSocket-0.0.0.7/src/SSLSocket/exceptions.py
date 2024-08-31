e12f185312c34ebc25309e066adb564b889d4b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 04:04:54 +0000

    Add package SSLSocket/0.0.0.7/SSLSocket-0.0.0.7.tar.gz

---
+++ packages/SSLSocket/SSLSocket-0.0.0.7.tar.gz/SSLSocket-0.0.0.7/src/SSLSocket/exceptions.py
@@ -0,0 +1,29 @@
+class DeviceNotSupported(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class AlreadyRunning(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class CannotBindPort(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+        
+class CannotInstall(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+        
+class PermissionDenied(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class UnknownOutput(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
