b6e559a0675a010ca99cfa548a3619392b6feca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:12 +0000

    Add package tgate/1.0.0/tgate-1.0.0-py3-none-any.whl

---
+++ packages/tgate/tgate-1.0.0-py3-none-any.whl/tgate/__init__.py
@@ -0,0 +1,7 @@
+from .PixArt_Alpha import TgatePixArtAlphaLoader
+from .PixArt_Sigma import TgatePixArtSigmaLoader
+from .SD import TgateSDLoader
+from .SD_DeepCache import TgateSDDeepCacheLoader
+from .SDXL import TgateSDXLLoader
+from .SDXL_DeepCache import TgateSDXLDeepCacheLoader
+from .SVD import TgateSVDLoader
