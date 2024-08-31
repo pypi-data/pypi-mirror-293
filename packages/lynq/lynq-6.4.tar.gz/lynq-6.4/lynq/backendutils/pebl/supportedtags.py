8ffeafbea9f9c0f61275f2531dab83c0f07c4156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 04:04:39 +0000

    Add package lynq/6.4/lynq-6.4.tar.gz

---
+++ packages/lynq/lynq-6.4.tar.gz/lynq-6.4/lynq/backendutils/pebl/supportedtags.py
@@ -0,0 +1,22 @@
+"""
+This file is part of Lynq (elemenom/lynq).
+
+Lynq is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Lynq is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Your Package Name. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+# separate with " "
+
+supported_tags: list[str] = (
+    "html head body button h1 h2 h3 h4 p img div meta title script style input"
+).split(" ")
---
+++ packages/lynq/lynq-6.4.tar.gz/lynq-6.4/lynq/dependencies/basin/getval.py
@@ -0,0 +1,1 @@
+from lynq.backendutils.basin.getval import getval
