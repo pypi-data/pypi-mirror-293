b8deb866f407de77cac75fcfcef79e53bb03ce69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:49 +0000

    Add package renovate-31075-sample/0.0.0/renovate_31075_sample-0.0.0.tar.gz

---
+++ packages/renovate-31075-sample/renovate_31075_sample-0.0.0.tar.gz/renovate_31075_sample-0.0.0/README.md
@@ -0,0 +1,17 @@
+# 31075
+
+git@github.com:Enkidu-Aururu/31075.git# minimal-reproduction-template
+
+Based on [Renovate minimal reproduction instructions](https://github.com/renovatebot/renovate/blob/main/docs/development/minimal-reproductions.md).
+
+## Problem
+
+TODO
+
+## Expectation
+
+TODO
+
+## References
+
+[Renovate discussion #31075](https://github.com/renovatebot/renovate/discussions/31075 "Could PYPI datasource accept also look into url field?")
---
+++ packages/renovate-31075-sample/renovate_31075_sample-0.0.0.tar.gz/renovate_31075_sample-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="renovate_31075_sample",
+    # install_requires=[],
+    description="Sample Python package for Renovate discussion https://github.com/renovatebot/renovate/discussions/31075.",
+    url="https://github.com/Enkidu-Aururu/31075",
+    author="Ivan Latka",
+    # license="MIT",
+    packages=find_packages(),
+    # zip_safe=False,
+    # setuptools_git_versioning={
+    #     "enabled": True,
+    # },
+    # setup_requires=["setuptools-git-versioning<2", "keyrings.google-artifactregistry-auth<2"],
+)
