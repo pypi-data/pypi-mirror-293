17e42488510bec2fdc6d0d09d8a3247d9091f186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 06:04:19 +0000

    Add package hmmCDR/0.1.1/hmmCDR-0.1.1.tar.gz

---
+++ packages/hmmCDR/hmmCDR-0.1.1.tar.gz/hmmCDR-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hmmCDR',
+    version='0.1.1',
+    description="Find CDR locations using bedmethyl file and CenSat annotations.(REQUIRES BEDTOOLS INSTALLED)",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/jmenendez98/hmmCDR",
+    author='Julian Menendez',
+    author_email='jmmenend@ucsc.edu',
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.21.5',
+        'pandas>=1.3.5',
+        'pybedtools>=0.8.1',
+        'hmmlearn>=0.3.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'hmmCDR=hmmCDR.hmmCDR:main',
+            'hmmCDRprior=hmmCDR.hmmCDRprior:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
