a791e68982e4fd06c61496e3655f3f01b04f1756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 15:05:07 +0000

    Add package tunan-ground/0.0.3/tunan_ground-0.0.3.tar.gz

---
+++ packages/tunan-ground/tunan_ground-0.0.3.tar.gz/tunan_ground-0.0.3/src/tunan_ground/say_hello.py
@@ -0,0 +1,11 @@
+"""
+这是一个方法，打印一个hello world 的字符串的方法
+"""
+
+
+def say_hello():
+    print("hello world!")
+
+
+if __name__ == '__main__':
+    say_hello()
---
+++ packages/tunan-ground/tunan_ground-0.0.3.tar.gz/tunan_ground-0.0.3/test/test01.py
@@ -0,0 +1,7 @@
+
+
+from src.tunan_ground import say_hello
+
+
+if __name__ == '__main__':
+    say_hello.say_hello()
