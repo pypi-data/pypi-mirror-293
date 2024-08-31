bee8e22915e8b801d0c138e8d085e01484ae550a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:05:06 +0000

    Add package kboat/0.0.2/kboat-0.0.2.tar.gz

---
+++ packages/kboat/kboat-0.0.2.tar.gz/kboat-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kboat',
+    version='0.0.2',
+    description='PYPI tutorial package creation written by TeddyNote',
+    author='somsaetnag',
+    author_email='somsaetang@naver.com',
+    url='https://github.com/somsaetang/kboat',
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=['kboat', '경정'],
+    python_requires='>=3.10',
+    package_data={},
+    zip_safe=False,
+    classifiers = [
+    "Programming Language :: Python :: 3"
+]
+)
