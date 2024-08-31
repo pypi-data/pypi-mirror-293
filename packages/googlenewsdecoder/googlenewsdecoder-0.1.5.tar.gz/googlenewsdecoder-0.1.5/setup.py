00913062d772d57fd107704b0f3c398fab8cab20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:17 +0000

    Add package googlenewsdecoder/0.1.5/googlenewsdecoder-0.1.5.tar.gz

---
+++ packages/googlenewsdecoder/googlenewsdecoder-0.1.5.tar.gz/googlenewsdecoder-0.1.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="googlenewsdecoder",
+    version="0.1.5",
+    description="A python package to decode Google News URLs.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SSujitX/google-news-url-decoder",
+    author="Sujit Biswas",
+    author_email="ssujitxx@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["requests", "selectolax"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
