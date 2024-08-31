69967b3ffe6a92f1abb7e11a127a424896bb3a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:05:09 +0000

    Add package python-worker/2.2.3/python-worker-2.2.3.tar.gz

---
+++ packages/python-worker/python-worker-2.2.3.tar.gz/python-worker-2.2.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = '2.2.3'
+DESCRIPTION = 'Simplify and master control (run and stop) the python threads (workers)'
+
+# Setting up
+setup(
+    name="python-worker",
+    version=VERSION,
+    author="danangjoyoo (Agus Danangjoyo)",
+    author_email="<agus.danangjoyo.blog@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'threading', 'worker', 'async worker', 'async thread', 'abort thread', 'thread stopper', "thread manager", 'simple thread', "thread monitor"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    url="https://github.com/Danangjoyoo/python-worker"
+)
