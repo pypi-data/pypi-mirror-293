50cc51822883dccf4f503ce476507dba95233a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 04:05:00 +0000

    Add package pyintelbras/0.0.10/pyintelbras-0.0.10-py3-none-any.whl

---
+++ packages/pyintelbras/pyintelbras-0.0.10-py3-none-any.whl/pyintelbras/api.py
@@ -0,0 +1,70 @@
+import logging
+import requests
+
+from requests.auth import HTTPDigestAuth
+from urllib.parse import urlencode, urlparse, parse_qsl, ParseResult
+from .exceptions import IntelbrasAPIException
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+class IntelbrasAPI:
+    def __init__(
+        self, server: str = 'http://localhost',
+        auth: HTTPDigestAuth = None,
+        verify_ssl: bool = False
+    ) -> None:
+        if not server.startswith('http'):
+            server = 'http://' + server
+
+        self.server = server.rstrip('/')
+        self.auth = auth
+
+        logger.info("API Server Endpoint: %s", self.server)
+
+    def login(
+            self,
+            user: str = "",
+            password: str = "") -> None:
+        if not user or not password:
+            raise IntelbrasAPIException('Empty user or password')
+        self.auth = HTTPDigestAuth(user, password)
+
+    def do_request(
+        self, method: str, path: str, params: dict, extra_path: str = ''
+    ):
+        url_parts = urlparse(self.server)
+
+        query = dict(parse_qsl(url_parts.params))
+        query.update(params)
+
+        url_path = (
+            f"{url_parts.path}"  # in case of proxy context path
+            f"/cgi-bin/"  # requirement of Intelbras API
+            f"{path.replace('.', '/')}"  # replacing dots with slashes
+            f"{'/' + urlencode(extra_path) if extra_path else ''}"  # add extra path is exists # noqa: E501
+            f".cgi"  # requirement of Intelbras API
+        )
+
+        res = ParseResult(
+            scheme=url_parts.scheme, netloc=url_parts.netloc,
+            path=url_path, params=url_parts.params,
+            query=urlencode(params), fragment=url_parts.fragment
+        )
+
+        url = res.geturl()
+
+        logger.debug(f'Requesting to URL {url}')
+
+        return requests.request(
+            method=method, url=url, auth=self.auth, verify=self.verify_ssl
+        )
+
+    def __getattr__(self, name):
+        def method(extra_path: str = '', *args, **kwargs):
+            logger.debug(
+                f"Call method '{name}' with arguments: {args} and {kwargs}"
+            )
+            return self.do_request('GET', name, kwargs, extra_path)
+        return method
