69a7d05f563fea5bca908968bf942f893627bb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:39 +0000

    Add package devsetgo-lib/0.14.2/devsetgo_lib-0.14.2-py3-none-any.whl

---
+++ packages/devsetgo-lib/devsetgo_lib-0.14.2-py3-none-any.whl/dsg_lib/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+DevSetGo Library
+=========
+
+DevSetGo Library is a Python library offering reusable functions for efficient coding. It includes file operations, calendar utilities, pattern matching, advanced logging with loguru, FastAPI endpoints, async database handling, and email validation. Designed for ease of use and versatility, it's a valuable tool for Python developers.
+
+Author: Mike Ryan <mikeryan56@gmail.com>
+License: MIT
+"""
+
+__version__ = "0.14.2"
