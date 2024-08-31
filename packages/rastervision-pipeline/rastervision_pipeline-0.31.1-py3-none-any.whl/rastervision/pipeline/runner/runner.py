c1653af1832209a3b131c51edfe9acf74b940303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:05:33 +0000

    Add package rastervision-pipeline/0.31.1/rastervision_pipeline-0.31.1-py3-none-any.whl

---
+++ packages/rastervision-pipeline/rastervision_pipeline-0.31.1-py3-none-any.whl/rastervision/pipeline/runner/runner.py
@@ -0,0 +1,47 @@
+from abc import abstractmethod
+
+from rastervision.pipeline.pipeline import Pipeline
+
+
+class Runner():
+    """A method for running a Pipeline.
+
+    This can be subclassed to provide the ability to run on different cloud
+    providers, etc.
+    """
+
+    @abstractmethod
+    def run(self,
+            cfg_json_uri: str,
+            pipeline: Pipeline,
+            commands: list[str],
+            num_splits: int = 1,
+            pipeline_run_name: str = 'raster-vision'):
+        """Run commands in a Pipeline using a serialized PipelineConfig.
+
+        Args:
+            cfg_json_uri: URI of a JSON file with a serialized PipelineConfig
+            pipeline: the Pipeline to run
+            commands: names of commands to run
+            num_splits: number of splits to use for splittable commands
+        """
+
+    @abstractmethod
+    def run_command(self, cmd: list[str]):
+        """Run a single command.
+
+        Args:
+            cmd: The command to run.
+        """
+
+    def get_split_ind(self) -> int | None:
+        """Get the split_ind for the process.
+
+        For split commands, the split_ind determines which split of work to perform
+        within the current OS process. The CLI has a --split-ind option, but some runners
+        may have their own means of communicating the split_ind, and this method should
+        be overridden in such cases. If this method returns None, then the --split-ind
+        option will be used. If both are null, then it won't be possible to run the
+        command.
+        """
+        return None
---
+++ packages/rastervision-pipeline/rastervision_pipeline-0.31.1-py3-none-any.whl/rastervision/pipeline/utils.py
@@ -0,0 +1,59 @@
+from typing import Any
+from collections.abc import Callable
+import os
+import atexit
+import logging
+from math import ceil
+
+log = logging.getLogger(__name__)
+
+
+def terminate_at_exit(process):
+    def terminate():
+        log.debug('Terminating {}...'.format(process.pid))
+        process.terminate()
+
+    atexit.register(terminate)
+
+
+def grouped(lst: list, size: int) -> list:
+    """Returns a list of lists of length 'size'.
+    The last list will have size <= 'size'.
+    """
+    return [lst[n:n + size] for n in range(0, len(lst), size)]
+
+
+def split_into_groups(lst: list, num_groups: int) -> list:
+    """Attempts to split a list into a given number of groups.
+
+    The number of groups will be at least 1 and at most
+    num_groups.
+
+    Args:
+       lst: The list to split.
+       num_groups: The number of groups to create.
+
+    Returns:
+       A list of size between 1 and num_groups containing lists of items of l.
+    """
+    group_sz = max(int(ceil((len(lst)) / num_groups)), 1)
+
+    return grouped(lst, group_sz)
+
+
+def repr_with_args(obj: Any, **kwargs) -> str:
+    """Builds a string of the form: <obj's class name>(k1=v1, k2=v2, ...)."""
+    cls = type(obj).__name__
+    arg_strs = [f'{k}={v!r}' for k, v in kwargs.items()]
+    arg_str = ', '.join(arg_strs)
+    return f'{cls}({arg_str})'
+
+
+def get_env_var(key: str,
+                default: Any | None = None,
+                out_type: type | Callable | None = None) -> Any | None:
+    val = os.environ.get(key, default)
+    if val is not None and out_type is not None:
+        if out_type == bool:
+            return val.lower() in ('1', 'true', 'y', 'yes')
+        return out_type(val)
