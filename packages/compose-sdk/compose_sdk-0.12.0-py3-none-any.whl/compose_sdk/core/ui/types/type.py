e8d92ecfcf209779979d822e1ad67029975bb28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:04:58 +0000

    Add package compose-sdk/0.12.0/compose_sdk-0.12.0-py3-none-any.whl

---
+++ packages/compose-sdk/compose_sdk-0.12.0-py3-none-any.whl/compose_sdk/core/ui/types/type.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+
+class TYPE(str, Enum):
+    # INPUT TYPES
+    INPUT_TEXT = "input-text"
+    INPUT_NUMBER = "input-number"
+    INPUT_EMAIL = "input-email"
+    INPUT_URL = "input-url"
+    INPUT_PASSWORD = "input-password"
+    INPUT_RADIO_GROUP = "input-radio-group"
+    INPUT_SELECT_DROPDOWN_SINGLE = "input-select-dropdown-single"
+    INPUT_SELECT_DROPDOWN_MULTI = "input-select-dropdown-multi"
+    INPUT_TABLE = "input-table"
+    INPUT_FILE_DROP = "input-file-drop"
+
+    # BUTTON TYPES
+    BUTTON_DEFAULT = "button-default"
+    BUTTON_FORM_SUBMIT = "button-form-submit"
+
+    # DISPLAY TYPES
+    DISPLAY_TEXT = "display-text"
+    DISPLAY_HEADER = "display-header"
+    DISPLAY_JSON = "display-json"
+    DISPLAY_SPINNER = "display-spinner"
+    DISPLAY_CODE = "display-code"
+    DISPLAY_IMAGE = "display-image"
+    DISPLAY_MARKDOWN = "display-markdown"
+    # A special type that's used to represent when a render returns None
+    DISPLAY_NONE = "display-none"
+
+    # LAYOUT TYPES
+    LAYOUT_STACK = "layout-stack"
+    LAYOUT_FORM = "layout-form"
