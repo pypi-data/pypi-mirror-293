9f7e9646a71b8858f0638b07e388689b859f7e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:34 +0000

    Add package nlxcaptcha/2.0/nlxcaptcha-2.0.tar.gz

---
+++ packages/nlxcaptcha/nlxcaptcha-2.0.tar.gz/nlxcaptcha-2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nlxcaptcha',
+    version='2.0',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'tk',
+        'sv-ttk',
+    ],
+    description='Tell "iPad Kids" and "Non-iPad Kids" apart using OpenAI API in this easy-to-integrate captcha.',
+    author='Nlcky Solutions',
+    author_email='nicky@nlcky.com',
+    url='https://github.com/nlckysolutions/NLX-Captcha',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
