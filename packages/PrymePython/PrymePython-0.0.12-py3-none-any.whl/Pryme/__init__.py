030ec6c230cb17613bf32c9e63d300e71f77c57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:04:58 +0000

    Add package PrymePython/0.0.12/PrymePython-0.0.12-py3-none-any.whl

---
+++ packages/PrymePython/PrymePython-0.0.12-py3-none-any.whl/Pryme/__init__.py
@@ -0,0 +1,7 @@
+from Pryme.prymepackage import (
+    list_primes, get_primes, is_prime, sum_primes, prime_factors,
+    next_prime, previous_prime, count_primes, prime_product,
+    prime_sum_pairs, mirror, twins, sophie, sexy, randprime,
+    cuban_first, cuban_second, mod_exp, lucas_test, prime_digit_sum,
+    prime_count_estimate, mersenne
+)
