a66668d4d56ad48bea86e9855149cb81888de8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 06:04:44 +0000

    Add package hal9/2.7.2/hal9-2.7.2-py3-none-any.whl

---
+++ packages/hal9/hal9-2.7.2-py3-none-any.whl/hal9/run.py
@@ -0,0 +1,24 @@
+import subprocess
+from pathlib import Path
+
+def run(path :str) -> str:
+  """Run an application
+
+  Parameters
+  ----------
+  path : str 
+          Path to the application.
+  """
+
+  app_path = Path(path) / 'app.py'
+
+  if not app_path.is_file():
+    print(f"Failed to run {app_path}")
+    return
+
+  try:
+    command = ['python3', str(app_path)]
+    with subprocess.Popen(command) as proc:
+      proc.wait()
+  except Exception as e:
+    print(f"An error occurred while running app.py: {e}")
