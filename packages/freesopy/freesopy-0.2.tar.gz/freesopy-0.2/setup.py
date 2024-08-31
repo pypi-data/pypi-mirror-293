0532f878088fbc9c8f650cf50fb89d45b05c404b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 15:04:41 +0000

    Add package freesopy/0.2/freesopy-0.2.tar.gz

---
+++ packages/freesopy/freesopy-0.2.tar.gz/freesopy-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="freesopy",
+    version="0.2",
+    description="This is a Python package for the implementation of various equations of Free Space Optical Communication",
+    author="Shoaib",
+    packages=["freesopy"],
+    install_requires=[],
+)
