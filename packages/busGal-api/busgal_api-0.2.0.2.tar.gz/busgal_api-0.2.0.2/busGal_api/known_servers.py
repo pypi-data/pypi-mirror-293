942cfa1d2d8e66c08812e640606a4daab05c146d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 15:05:07 +0000

    Add package busGal-api/0.2.0.2/busgal_api-0.2.0.2.tar.gz

---
+++ packages/busGal-api/busgal_api-0.2.0.2.tar.gz/busgal_api-0.2.0.2/busGal_api/known_servers.py
@@ -0,0 +1,14 @@
+XG_EXTERNOS = "https://tpgal-ws-externos.xunta.gal/tpgal_ws/rest"
+"""
+Public API for individuals
+"""
+
+XG_APP = "https://tpgal-ws.xunta.gal/tpgal_ws/rest"
+"""
+The API the Android app uses
+"""
+
+XG_XNQR_APP = "https://xentenovaqr.xunta.gal/api"
+"""
+The API the Android app uses for the Xente Nova QR functionality
+"""
