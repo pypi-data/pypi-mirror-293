8a7530997b4135bb5f26c7227c86d90690e4f661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:10 +0000

    Add package making-with-code-cli/2.0.5/making_with_code_cli-2.0.5-py3-none-any.whl

---
+++ packages/making-with-code-cli/making_with_code_cli-2.0.5-py3-none-any.whl/making_with_code_cli/teach/__init__.py
@@ -0,0 +1,16 @@
+import click
+from making_with_code_cli.teach.setup import setup
+from making_with_code_cli.teach.update import update
+from making_with_code_cli.teach.status import status
+from making_with_code_cli.teach.log import log
+from making_with_code_cli.teach.test import test
+
+@click.group()
+def teach():
+    "Commands for teachers"
+
+teach.add_command(setup)
+teach.add_command(update)
+teach.add_command(status)
+teach.add_command(log)
+teach.add_command(test)
