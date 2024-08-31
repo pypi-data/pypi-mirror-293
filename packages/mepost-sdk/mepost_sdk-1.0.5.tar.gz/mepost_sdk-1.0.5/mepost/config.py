57220adf85d3044b46fc99290ea97eb97c0b8705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 06:04:51 +0000

    Add package mepost-sdk/1.0.5/mepost_sdk-1.0.5.tar.gz

---
+++ packages/mepost-sdk/mepost_sdk-1.0.5.tar.gz/mepost_sdk-1.0.5/mepost/config.py
@@ -0,0 +1,4 @@
+class MepostConfig:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.base_url = "https://api.mepost.io/v1"
---
+++ packages/mepost-sdk/mepost_sdk-1.0.5.tar.gz/mepost_sdk-1.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mepost-sdk',
+    version='1.0.5',
+    packages=find_packages(),
+    description='A Python SDK for interacting with the Mepost API.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='mepost',
+    author_email='info@mepost.io',
+    license='MIT',
+    install_requires=[
+        'requests'
+    ],
+    url='https://github.com/mepost-io/python-sdk',
+    keywords='mepost, email, sdk, api',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
