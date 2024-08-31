7be6d9f56b906608f34e65c27bf3522d3a2e02e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 15:04:38 +0000

    Add package coherent.build/0.21.0/coherent_build-0.21.0-py3-none-any.whl

---
+++ packages/coherent.build/coherent_build-0.21.0-py3-none-any.whl/coherent/build/__init__.py
@@ -0,0 +1,37 @@
+__requires__ = [
+    'wheel',
+    'pip-run',
+    'setuptools_scm',
+    'build',
+    'git-fame',
+    'jaraco.context',
+    'requests',
+    'packaging',
+    'jaraco.functools',
+    'jaraco.compat >= 4.1',
+    'importlib_resources; python_version < "3.12"',
+    'jaraco.vcs',
+    'more-itertools',
+    'coherent.deps',
+]
+
+
+from .backend import (
+    build_editable,
+    build_sdist,
+    build_wheel,
+    prepare_metadata,
+)
+
+prepare_metadata_for_build_wheel = prepare_metadata_for_build_editable = (
+    prepare_metadata
+)
+
+
+__all__ = [
+    'build_sdist',
+    'prepare_metadata_for_build_wheel',
+    'prepare_metadata_for_build_editable',
+    'build_wheel',
+    'build_editable',
+]
