31c07f2361f71912dac34b6dff35bbdc9038c1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 04:05:24 +0000

    Add package neuroconv/0.6.1/neuroconv-0.6.1-py3-none-any.whl

---
+++ packages/neuroconv/neuroconv-0.6.1-py3-none-any.whl/neuroconv/datainterfaces/ecephys/kilosort/kilosortdatainterface.py
@@ -0,0 +1,38 @@
+from pydantic import DirectoryPath
+
+from ..basesortingextractorinterface import BaseSortingExtractorInterface
+
+
+class KiloSortSortingInterface(BaseSortingExtractorInterface):
+    """Primary data interface class for converting a KiloSortingExtractor from spikeinterface."""
+
+    display_name = "KiloSort Sorting"
+    associated_suffixes = (".npy",)
+    info = "Interface for KiloSort sorting data."
+
+    @classmethod
+    def get_source_schema(cls) -> dict:
+        source_schema = super().get_source_schema()
+        source_schema["properties"]["folder_path"][
+            "description"
+        ] = "Path to the output Phy folder (containing the params.py)"
+        return source_schema
+
+    def __init__(
+        self,
+        folder_path: DirectoryPath,
+        keep_good_only: bool = False,
+        verbose: bool = True,
+    ):
+        """
+        Load and prepare sorting data for kilosort
+
+        Parameters
+        ----------
+        folder_path: str or Path
+            Path to the output Phy folder (containing the params.py)
+        keep_good_only: bool, default: False
+            If True, only Kilosort-labeled 'good' units are returned
+        verbose: bool, default: True
+        """
+        super().__init__(folder_path=folder_path, keep_good_only=keep_good_only, verbose=verbose)
