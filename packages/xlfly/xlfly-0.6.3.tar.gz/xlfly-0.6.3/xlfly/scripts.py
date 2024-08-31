2f030f4969de38bc42ec0104d2a2895e3a31c24e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:28 +0000

    Add package xlfly/0.6.3/xlfly-0.6.3.tar.gz

---
+++ packages/xlfly/xlfly-0.6.3.tar.gz/xlfly-0.6.3/xlfly/scripts.py
@@ -0,0 +1,40 @@
+import xlfly.create_shortcut
+import os
+import xlfly.configs as configs
+import argparse
+import importlib
+
+
+def init_default(default_folder: str):
+    spec = importlib.util.spec_from_file_location("__init__", default_folder)
+    init = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(init)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run a Tkinter application.")
+    parser.add_argument("--init", action="store_true", help="Create Start Menu")
+    parser.add_argument("-t", "--tempfolder", type=str, help="Set Template Root Folder")
+    args = parser.parse_args()
+
+    if args.init:
+        xlfly.create_shortcut.create_shortcut()
+        print("Created Start Menu Item")
+
+    if args.tempfolder:
+        print(f"add {args.tempfolder} as template path")
+
+        settings = configs.load_settings()
+        folder_path = os.path.normpath(args.tempfolder)
+        if folder_path:
+            print(f"Selected folder: {folder_path}")
+            settings["tempfolder"] = folder_path
+            configs.save_settings(settings)
+            print("Initilizing default init...")
+            default_folder = os.path.join(folder_path, "default")
+            init_default(default_folder)
+            print("Done")
+
+
+if __name__ == "__main__":
+    main()
