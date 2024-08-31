873affab041290dff2bf9cd9bb1495f973d749a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:05:15 +0000

    Add package abstract-solcatcher/0.0.1.80/abstract_solcatcher-0.0.1.80.tar.gz

---
+++ packages/abstract-solcatcher/abstract_solcatcher-0.0.1.80.tar.gz/abstract_solcatcher-0.0.1.80/src/abstract_solcatcher/abstract_rate_limit.py
@@ -0,0 +1,31 @@
+from .utils import getEndpointUrl,get_async_response
+from abstract_apis import asyncPostRpcRequest,asyncPostRequest,asyncGetRequest
+
+async def asyncMakeLimitedCall(method=None, params=[]):
+    urls = await async_get_rate_limit_url(method)
+    response = await asyncPostRpcRequest(
+        url=urls.get('url'), method=method, params=params, status_code=True, response_result='result'
+    )
+    
+    if response[1] == 429:
+        response = await asyncPostRpcRequest(
+            url=urls.get('url2'), method=method, params=params, response_result='result', status_code=True
+        )
+    
+    await async_log_response(method, response[0])
+    return response[0]
+
+def makeLimitedCall(method=None, params=[]):
+    return get_async_response(asyncMakeLimitedCall, method, params)
+
+async def async_get_rate_limit_url(method='default_method'):
+    return await asyncGetRequest(url=getEndpointUrl("rate_limit"),data={"method":str(method)})
+
+def get_rate_limit_url(method_name, *args, **kwargs):
+    return get_async_response(async_get_rate_limit_url, method_name, *args, **kwargs)
+
+async def async_log_response(method='default_method', response_data={}):
+    return await asyncPostRequest(url=getEndpointUrl("log_response"),data={"method":str(method),"response_data":response_data})
+
+def log_response(method_name, response_data, *args, **kwargs):
+    return get_async_response(async_log_response, method_name, response_data, *args, **kwargs)
