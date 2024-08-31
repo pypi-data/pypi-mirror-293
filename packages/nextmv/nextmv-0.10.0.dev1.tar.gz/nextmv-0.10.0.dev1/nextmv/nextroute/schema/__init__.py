2aa29eb86a6aceca2ff4cddbeda21be2bd845b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 06:04:17 +0000

    Add package nextmv/0.10.0.dev1/nextmv-0.10.0.dev1.tar.gz

---
+++ packages/nextmv/nextmv-0.10.0.dev1.tar.gz/nextmv-0.10.0.dev1/nextmv/nextroute/schema/__init__.py
@@ -0,0 +1,25 @@
+"""Schema (class) definitions for the entities in Nextroute."""
+
+from .input import Defaults as Defaults
+from .input import DurationGroup as DurationGroup
+from .input import Input as Input
+from .location import Location as Location
+from .output import DataPoint as DataPoint
+from .output import ObjectiveOutput as ObjectiveOutput
+from .output import Output as Output
+from .output import PlannedStopOutput as PlannedStopOutput
+from .output import ResultStatistics as ResultStatistics
+from .output import RunStatistics as RunStatistics
+from .output import Series as Series
+from .output import SeriesData as SeriesData
+from .output import Solution as Solution
+from .output import Statistics as Statistics
+from .output import StopOutput as StopOutput
+from .output import VehicleOutput as VehicleOutput
+from .output import Version as Version
+from .stop import AlternateStop as AlternateStop
+from .stop import Stop as Stop
+from .stop import StopDefaults as StopDefaults
+from .vehicle import InitialStop as InitialStop
+from .vehicle import Vehicle as Vehicle
+from .vehicle import VehicleDefaults as VehicleDefaults
