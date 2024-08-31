1297b366619431a76c70a35b3f62edc2812c54a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:44 +0000

    Add package django-citext/1.0.2/django_citext-1.0.2-py3-none-any.whl

---
+++ packages/django-citext/django_citext-1.0.2-py3-none-any.whl/citext/__init__.py
@@ -0,0 +1,7 @@
+"""PostgreSQL CIText integration for Django."""
+
+from . import _version
+from .fields import *  # noqa
+
+__version__ = _version.version
+VERSION = _version.version_tuple
---
+++ packages/django-citext/django_citext-1.0.2-py3-none-any.whl/citext/fields.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+from . import lookups
+
+__all__ = ["CICharField", "CIEmailField", "CIText", "CITextField"]
+
+
+class CIText:
+    def get_internal_type(self):
+        return "CI" + super().get_internal_type()
+
+    def db_type(self, connection):
+        return "citext"
+
+
+class CICharField(CIText, models.CharField):
+    pass
+
+
+class CIEmailField(CIText, models.EmailField):
+    pass
+
+
+class CITextField(CIText, models.TextField):
+    pass
+
+
+field: models.Field
+
+for field in CIText.__subclasses__():
+    for lookup in lookups.CompareCITextLookupMixin.__subclasses__():
+        field.register_lookup(lookup)
+
+    for lookup in lookups.InsensitiveCompareCITextLookupMixin.__subclasses__():
+        field.register_lookup(lookup)
+
+    field.register_lookup(lookups.IsNull)
