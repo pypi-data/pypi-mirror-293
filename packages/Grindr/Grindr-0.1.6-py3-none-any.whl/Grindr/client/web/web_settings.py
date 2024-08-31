35a994083e29c7d052e404521ff1af0be62524f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 04:05:33 +0000

    Add package Grindr/0.1.6/Grindr-0.1.6-py3-none-any.whl

---
+++ packages/Grindr/Grindr-0.1.6-py3-none-any.whl/Grindr/client/web/web_settings.py
@@ -0,0 +1,31 @@
+import os
+from typing import Dict, Union
+
+GRINDR_HOST: str = os.getenv("GRINDR_HOST", "grindr.mobi")
+GRINDR_BASE: str = os.getenv("GRINDR_API_BASE", f"https://{GRINDR_HOST}")
+GRINDR_WS: str = os.getenv("GRINDR_API_WS", f"wss://{GRINDR_HOST}/v1/ws")
+
+GRINDR_V1: str = GRINDR_BASE + "/v1"
+GRINDR_V2: str = GRINDR_BASE + "/v2"
+GRINDR_V3: str = GRINDR_BASE + "/v3"
+GRINDR_V3_1: str = GRINDR_BASE + "/v3.1"
+GRINDR_V4: str = GRINDR_BASE + "/v4"
+GRINDR_V5: str = GRINDR_BASE + "/v5"
+GRINDR_V6: str = GRINDR_BASE + "/v6"
+
+DEFAULT_REQUEST_PARAMS: Dict[str, Union[int, bool, str]] = {}
+
+DEFAULT_REQUEST_HEADERS: Dict[str, str] = {
+    "requireRealDeviceInfo": "true",
+    "L-Time-Zone": "America/Toronto",
+    "L-Grindr-Roles": "[]",
+    "L-Device-Info": "",
+    "Accept": "application/json",
+    "User-Agent": "Grindr3/24.8.0.55320.047817240.99 (55320.047817240.99; iPhone8,1; iOS 15.8.2)",
+    "L-Locale": "en_US",
+    "Accept-language": "en-US",
+    "Content-Type": "application/json; charset=UTF-8",
+    "Host": GRINDR_HOST,
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+}
