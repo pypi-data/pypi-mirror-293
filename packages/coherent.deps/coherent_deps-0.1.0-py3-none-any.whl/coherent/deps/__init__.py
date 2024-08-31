a8e1e810e2a9b02416863e1f4bce6f3a3ced3ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:13 +0000

    Add package coherent.deps/0.1.0/coherent_deps-0.1.0-py3-none-any.whl

---
+++ packages/coherent.deps/coherent_deps-0.1.0-py3-none-any.whl/coherent/deps/__init__.py
@@ -0,0 +1,12 @@
+__requires__ = [
+    'requests_toolbelt',
+    'zipp>=3.20',
+    'jaraco.mongodb',
+    'keyring',
+    'tempora',
+    'requests-file',
+    'tqdm',
+    'retry-requests',
+    'jaraco.ui',
+    'jaraco.collections',
+]
