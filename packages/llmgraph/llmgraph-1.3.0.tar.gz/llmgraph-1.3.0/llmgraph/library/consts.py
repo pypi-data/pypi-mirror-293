0916798a5d3cd922d8e284b666fc0a6b4995c3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:24 +0000

    Add package llmgraph/1.3.0/llmgraph-1.3.0.tar.gz

---
+++ packages/llmgraph/llmgraph-1.3.0.tar.gz/llmgraph-1.3.0/llmgraph/library/consts.py
@@ -0,0 +1,7 @@
+package_name = "llmgraph"
+version = "1.3.0"
+prompts_yaml_location = "prompts.yaml"
+
+default_llm_model = "gpt-4o-mini"
+default_llm_temp = 0.0
+default_output_folder = "./_output/"
