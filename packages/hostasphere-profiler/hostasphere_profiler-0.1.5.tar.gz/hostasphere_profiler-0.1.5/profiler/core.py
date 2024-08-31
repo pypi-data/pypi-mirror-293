c108a0dc1aebe75c328f40c686bc9dbd4cfdfda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:24 +0000

    Add package hostasphere-profiler/0.1.5/hostasphere_profiler-0.1.5.tar.gz

---
+++ packages/hostasphere-profiler/hostasphere_profiler-0.1.5.tar.gz/hostasphere_profiler-0.1.5/profiler/core.py
@@ -0,0 +1,61 @@
+import time
+import grpc
+import threading
+
+from . import profiler_output_pb2_grpc as profiler_output_grpc
+from .utils import *
+
+class Profiler:
+    def __init__(self, address, token):
+        self._address = address
+        self._token = token
+        self._token_id = "66d1beef53cb6f370f775bf6" # todo: retrieve token id from the server
+
+    def sendProfilerOutput(self, profiler_data: profiler_output.ProfilerOutput):
+        try:
+            with grpc.insecure_channel(self._address) as channel:
+                stub = profiler_output_grpc.ProfilerStub(channel)
+                profiler_data.token = self._token
+                response = stub.SendProfilerOutput(profiler_data)
+            if not response.ok:
+                raise Exception(
+                    f"Error sending profiler output: {response.message}")
+        except grpc.RpcError as e:
+            raise Exception("Impossible to send profiler output check address, or check if hostaspere is running")
+
+    def sendProfilerOutputAsync(self, profiler_data: profiler_output.ProfilerOutput):
+        thread = threading.Thread(target=self.sendProfilerOutput, args=(profiler_data,))
+        thread.start()
+
+    def probe(self):
+        def decorator(func):
+            def wrapper(*args, **kwargs):
+                copied_args = deep_copy_args(args)
+                start_time = time.time()
+                result = func(*args, **kwargs)
+                end_time = time.time()
+
+                returned_value = profiler_output.ReturnedValue(
+                    value=str(result),
+                    type=type(result).__name__
+                )
+
+                profiler_data = profiler_output.ProfilerOutputRequest(
+                    profiler_output=profiler_output.ProfilerOutput(
+                        function_name=get_function_name(func),
+                        function_id=hash_function(func),
+                        function_caller=get_caller(),
+                        token_id=self._token_id,
+                        start_time=start_time,
+                        end_time=end_time,
+                        execution_time=(end_time - start_time) * 1000,  # in milliseconds
+                        memory_usage=get_memory_usage(),
+                        cpu_usage=get_cpu_usage(),
+                        func_params=get_func_params(copied_args, func),
+                        returned_value=returned_value
+                    )
+                )
+                self.sendProfilerOutputAsync(profiler_data)
+                return result
+            return wrapper
+        return decorator
