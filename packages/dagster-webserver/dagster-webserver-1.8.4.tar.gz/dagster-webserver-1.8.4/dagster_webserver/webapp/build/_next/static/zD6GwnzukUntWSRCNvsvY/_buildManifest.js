8c4ddf0c2c9c3eeaa1e0663e1976306316f09d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 05:05:07 +0000

    Add package dagster-webserver/1.8.4/dagster-webserver-1.8.4.tar.gz

---
+++ packages/dagster-webserver/dagster-webserver-1.8.4.tar.gz/dagster-webserver-1.8.4/dagster_webserver/webapp/build/_next/static/zD6GwnzukUntWSRCNvsvY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-c24eaeca74e97938.js"],"/_error":["static/chunks/pages/_error-e68810fc74d5d1a2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
