24a104f2cf39a86ad58dbbc6c86898f95d2fad6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 15:04:45 +0000

    Add package sticker-convert/2.10.2/sticker_convert-2.10.2-py3-none-any.whl

---
+++ packages/sticker-convert/sticker_convert-2.10.2-py3-none-any.whl/sticker_convert/__main__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+def main() -> None:
+    import multiprocessing
+    import sys
+
+    from sticker_convert.version import __version__
+
+    multiprocessing.freeze_support()
+    print(f"sticker-convert {__version__}")
+    if len(sys.argv) == 1:
+        print("Launching GUI...")
+        from sticker_convert.gui import GUI
+
+        GUI().gui()
+    else:
+        from sticker_convert.cli import CLI
+
+        CLI().cli()
+
+
+if __name__ == "__main__":
+    main()
