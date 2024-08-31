519b9d5f97b026725aec1059561aa4b6b35fbd63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:15 +0000

    Add package argus-server/1.22.0/argus_server-1.22.0.tar.gz

---
+++ packages/argus-server/argus_server-1.22.0.tar.gz/argus_server-1.22.0/src/argus/incident/constants.py
@@ -0,0 +1,7 @@
+# Prevent import loops
+# DO NOT import anything here, ever
+
+MIN_INCIDENT_LEVEL = 1  # Do not override
+MAX_INCIDENT_LEVEL = 5
+INCIDENT_LEVELS = tuple(range(MIN_INCIDENT_LEVEL, MAX_INCIDENT_LEVEL + 1))
+INCIDENT_LEVEL_CHOICES = tuple(zip(INCIDENT_LEVELS, map(str, INCIDENT_LEVELS)))
