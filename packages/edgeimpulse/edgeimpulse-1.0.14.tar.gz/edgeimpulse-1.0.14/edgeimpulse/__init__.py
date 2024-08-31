d8f4dc29659876c899a2089acc58bf8641661cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:51 +0000

    Add package edgeimpulse/1.0.14/edgeimpulse-1.0.14.tar.gz

---
+++ packages/edgeimpulse/edgeimpulse-1.0.14.tar.gz/edgeimpulse-1.0.14/edgeimpulse/__init__.py
@@ -0,0 +1,39 @@
+# Import the sub packages here to expose them to the user
+# ruff: noqa: F401,D104
+# mypy: ignore-errors
+import os
+import edgeimpulse.model
+import edgeimpulse.datasets
+import edgeimpulse.exceptions
+import edgeimpulse.experimental
+
+from edgeimpulse.util import configure_generic_client, default_project_id_for
+
+import edgeimpulse_api
+
+__version__ = "1.0.14"
+
+try:
+    API_KEY = os.environ["EI_API_KEY"]
+except KeyError:
+    API_KEY = None
+
+try:
+    EI_USERNAME = os.environ["EI_USERNAME"]
+except KeyError:
+    EI_USERNAME = None
+
+try:
+    EI_PASSWORD = os.environ["EI_PASSWORD"]
+except KeyError:
+    EI_PASSWORD = None
+
+try:
+    API_ENDPOINT = os.environ["EI_API_ENDPOINT"]
+except KeyError:
+    API_ENDPOINT = "https://studio.edgeimpulse.com/v1"
+
+try:
+    INGESTION_ENDPOINT = os.environ["EI_INGESTION_ENDPOINT"]
+except KeyError:
+    INGESTION_ENDPOINT = "https://ingestion.edgeimpulse.com"
