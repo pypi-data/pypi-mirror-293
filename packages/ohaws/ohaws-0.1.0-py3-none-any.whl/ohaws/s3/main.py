cf58efc8b3aecbab036ebef5ce0ec54dc0f7a402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 06:04:14 +0000

    Add package ohaws/0.1.0/ohaws-0.1.0-py3-none-any.whl

---
+++ packages/ohaws/ohaws-0.1.0-py3-none-any.whl/ohaws/s3/main.py
@@ -0,0 +1,83 @@
+import boto3
+from botocore.exceptions import ClientError
+from botocore.client import Config
+from typing import Optional
+
+
+class S3ObjectNotFoundError(Exception):
+    """Exception raised when the S3 object is not found."""
+
+    pass
+
+
+class S3AccessDeniedError(Exception):
+    """Exception raised when access to the S3 bucket is denied."""
+
+    pass
+
+
+class S3UnknownError(Exception):
+    """Exception raised for unknown S3 errors."""
+
+    pass
+
+
+def get_object_and_sign_url(
+    object_path: str,
+    bucket: str,
+    region: Optional[str] = "us-east-1",
+    signature_version: Optional[str] = "s3v4",
+) -> Optional[str]:
+    """
+    Generate a pre-signed URL for an S3 object if it exists.
+
+    Parameters:
+        object_path (str): The path (key) of the object in the S3 bucket.
+        bucket (str): The name of the S3 bucket.
+        region (Optional[str]): The AWS region where the bucket is located. Defaults to 'us-east-1'.
+        signature_version (Optional[str]): The signature version to use. Defaults to 's3v4'.
+
+    Returns:
+        Optional[str]: A pre-signed URL for the S3 object if it exists, or None if an error occurs.
+
+    Raises:
+        S3ObjectNotFoundError: If the object is not found.
+        S3AccessDeniedError: If access to the bucket is denied.
+        S3UnknownError: For any other unknown errors.
+
+    Example:
+        url = get_object_and_sign_url('myfolder/myfile.txt', 'mybucket')
+        if url:
+            print("Access your file here:", url)
+    """
+    s3_client = boto3.client(
+        "s3", config=Config(signature_version=signature_version), region_name=region
+    )
+    try:
+        # check if head exists
+        s3_client.head_object(Bucket=bucket, Key=object_path)
+
+        # generate presigned url
+        response = s3_client.generate_presigned_url(
+            "get_object", Params={"Bucket": bucket, "Key": object_path}
+        )
+        return response
+    except ClientError as e:
+        error_code = e.response["Error"]["Code"]
+        if error_code == "404":
+            raise S3ObjectNotFoundError(f"Item not found: {e}")
+        elif error_code == "403":
+            raise S3AccessDeniedError(f"Bucket doesn't exist or access denied: {e}")
+        else:
+            raise S3UnknownError(f"Unknown error occurred: {e}")
+
+
+def hello():
+    print("hello from ohaws")
+
+
+print(
+    get_object_and_sign_url(
+        object_path="test/test_svg.svg", bucket="bucket631", region="us-east-1"
+    )
+)
