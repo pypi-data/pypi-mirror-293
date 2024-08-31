b2e2c47829ef94489b5f7dd17d88dbc64371947b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:18 +0000

    Add package fun-things/0.31.1/fun_things-0.31.1-py3-none-any.whl

---
+++ packages/fun-things/fun_things-0.31.1-py3-none-any.whl/fun_things/asynchronous.py
@@ -0,0 +1,40 @@
+import inspect
+from typing import Any, Callable, Iterable
+
+
+def _subdivide_predicate(value):
+    if isinstance(value, str):
+        return False
+
+    if isinstance(value, bytes):
+        return False
+
+    return isinstance(value, Iterable)
+
+
+async def as_asyncgen(
+    fn: Callable,
+    subdivide_predicate: Callable[[Any], bool] = _subdivide_predicate,
+):
+    """
+    Calls a function as an async generator.
+    """
+    value = fn()
+
+    if inspect.isasyncgen(value):
+        # Already an async generator.
+        async for subvalue in value:
+            yield subvalue
+
+        return
+
+    if inspect.isawaitable(value):
+        value = await value
+
+    if subdivide_predicate(value):
+        for subvalue in value:
+            yield subvalue
+
+        return
+
+    yield value
