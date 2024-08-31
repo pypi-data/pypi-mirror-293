611f3de5367ec47d923a36d9f106648c3474d4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 14:04:11 +0000

    Add package coherent.cli/0.4.1/coherent_cli-0.4.1-py3-none-any.whl

---
+++ packages/coherent.cli/coherent_cli-0.4.1-py3-none-any.whl/coherent/cli/__init__.py
@@ -0,0 +1,15 @@
+"""
+A trivial test to allow the runner to pass.
+
+>>> __name__
+'coherent.cli'
+"""
+
+__requires__ = [
+    'coherent.build',
+    'coherent.test',
+    'jaraco.vcs',
+    'jaraco.versioning',
+    'typer',
+    'typing-extensions',
+]
