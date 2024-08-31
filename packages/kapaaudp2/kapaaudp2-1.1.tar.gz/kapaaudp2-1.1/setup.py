cb8f114ee050dcfb0c5b2b13055685de196f1bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-31 03:04:27 +0000

    Add package kapaaudp2/1.1/kapaaudp2-1.1.tar.gz

---
+++ packages/kapaaudp2/kapaaudp2-1.1.tar.gz/kapaaudp2-1.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import setuptools
+packages = ['kapaaudp2']
+setup(name='kapaaudp2',
+	version='1.1',
+	author='szblack',
+    packages=packages, 
+    package_dir={'requests': 'requests'},)
