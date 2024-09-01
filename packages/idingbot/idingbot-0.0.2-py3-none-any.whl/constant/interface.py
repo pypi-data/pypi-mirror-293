a612810a5d4a8e9ae35bb9488e31450755048af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 04:04:47 +0000

    Add package idingbot/0.0.2/idingbot-0.0.2-py3-none-any.whl

---
+++ packages/idingbot/idingbot-0.0.2-py3-none-any.whl/constant/interface.py
@@ -0,0 +1,59 @@
+# 全部类型消息
+MT_ALL_MSG = 11000
+
+# 发送文本消息
+MT_SEND_TEXT_MSG = 11029
+
+# 发送群 @ 消息
+MT_SEND_ROOM_AT_MSG = 11069
+
+# 发送名片消息
+MT_SEND_CARD_MSG = 11034
+
+# 发送链接卡片消息
+MT_SEND_LINK_MSG = 11033
+
+# 发送图片消息
+MT_SEND_IMAGE_MSG = 11030
+
+# 发送文件消息
+MT_SEND_FILE_MSG = 11031
+
+# 发送视频消息
+MT_SEND_VIDEO_MSG = 11067
+
+# 发送 GIF 消息
+MT_SEND_GIF_MSG = 11070
+
+# 接收文本消息
+MT_RECV_TEXT_MSG = 11041
+
+# 接收图片消息
+MT_RECV_IMAGE_MSG = 11042
+
+# 接收小程序消息
+MT_RECV_APP_MSG = 11066
+
+# 获取登陆企业微信信息
+MT_GET_SELF_MSG = 11035
+
+# 获取内部联系人列表
+MT_GET_INNER_CONTACTS_MSG = 11036
+
+# 获取外部联系人列表
+MT_GET_EXTERNAL_CONTACTS_MSG = 11037
+
+# 获取所有群
+MT_GET_ROOMS_MSG = 11038
+
+# 获取联系人详细信息
+MT_GET_CONTACT_DETAIL_MSG = 11039
+
+# 获取群成员详细信息
+MT_GET_ROOM_MEMBERS_MSG = 11040
+
+# WXCDN 下载
+MT_WXCDN_DOWNLOAD_MSG = 11171
+
+# C2CCDN 下载
+MT_C2CCDN_DOWNLOAD_MSG = 11170
