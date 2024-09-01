bc2334f8f68731767873d8ec9b242cd5a7c548c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 04:05:00 +0000

    Add package paralex/1.0.13/paralex-1.0.13-py2.py3-none-any.whl

---
+++ packages/paralex/paralex-1.0.13-py2.py3-none-any.whl/paralex/__init__.py
@@ -0,0 +1,6 @@
+from .paths import templates_path, standard_path, docs_path
+from .meta import paralex_factory
+from .markdown import to_markdown
+from .utils import segment_sounds, create_ids
+
+VERSION = "1.0.13"
