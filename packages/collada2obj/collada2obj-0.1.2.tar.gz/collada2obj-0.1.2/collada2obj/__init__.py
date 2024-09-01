5546143a85ba5cc593ed2b19c8361435f2506932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:19 +0000

    Add package collada2obj/0.1.2/collada2obj-0.1.2.tar.gz

---
+++ packages/collada2obj/collada2obj-0.1.2.tar.gz/collada2obj-0.1.2/collada2obj/__init__.py
@@ -0,0 +1,9 @@
+from .MeshConverter import MeshConverter
+from .TechniqueCommon import TechniqueCommon, ParameterDescription
+from .ColladaFileConverter import ColladaFileConverter
+
+__all__ = [
+    'MeshConverter',
+    'TechniqueCommon', 'ParameterDescription',
+    'ColladaFileConverter',
+]
