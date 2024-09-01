7dadb0d1a29bc42fa2d5d94e8a9b9ddc120e6f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:53 +0000

    Add package xeofs/2.4.1/xeofs-2.4.1-py3-none-any.whl

---
+++ packages/xeofs/xeofs-2.4.1-py3-none-any.whl/xeofs/utils/data_types.py
@@ -0,0 +1,32 @@
+from typing import (
+    List,
+    TypeAlias,
+    Sequence,
+    Tuple,
+    TypeVar,
+    Hashable,
+)
+
+import dask.array as da
+from xarray.core import dataarray as xr_dataarray
+from xarray.core import dataset as xr_dataset
+
+DataArray: TypeAlias = xr_dataarray.DataArray
+DataSet: TypeAlias = xr_dataset.Dataset
+Data: TypeAlias = DataArray | DataSet
+DataVar = TypeVar("DataVar", DataArray, DataSet)
+DataVarBound = TypeVar("DataVarBound", bound=Data)
+
+DataArrayList: TypeAlias = List[DataArray]
+DataSetList: TypeAlias = List[DataSet]
+DataList: TypeAlias = List[Data]
+DataVarList: TypeAlias = List[DataVar]
+
+
+DaskArray: TypeAlias = da.Array  # type: ignore
+DataObject: TypeAlias = DataArray | DataSet | DataList
+
+Dims: TypeAlias = Sequence[Hashable]
+DimsTuple: TypeAlias = Tuple[Dims, ...]
+DimsList: TypeAlias = List[Dims]
+DimsListTuple: TypeAlias = Tuple[DimsList, ...]
