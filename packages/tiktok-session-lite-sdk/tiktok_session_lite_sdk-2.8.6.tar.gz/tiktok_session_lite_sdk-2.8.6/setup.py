5ff7d6a82de42730ba4d1df60e323f9371c19792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:13 +0000

    Add package tiktok-session-lite-sdk/2.8.6/tiktok_session_lite_sdk-2.8.6.tar.gz

---
+++ packages/tiktok-session-lite-sdk/tiktok_session_lite_sdk-2.8.6.tar.gz/tiktok_session_lite_sdk-2.8.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '2.8.6'
+DESCRIPTION = 'Library for handling session metrics on TT'
+LONG_DESCRIPTION = 'Library for handling session metrics on TT'
+
+# Setting up
+setup(
+    name="tiktok_session_lite_sdk",
+    version=VERSION,
+    author="chickenfox23",
+    author_email="chickenfox23@byt.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=[]
+   )
