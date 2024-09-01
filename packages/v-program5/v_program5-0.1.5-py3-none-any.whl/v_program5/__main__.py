c378aa3423c73df13ba738c09081f91a4ba74657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:07 +0000

    Add package v-program5/0.1.5/v_program5-0.1.5-py3-none-any.whl

---
+++ packages/v-program5/v_program5-0.1.5-py3-none-any.whl/v_program5/__main__.py
@@ -0,0 +1,10 @@
+from .ml_model import train_model, predict_model
+
+def main():
+    # Placeholder main function
+    print("This is the main entry point.")
+    train_model()
+    predict_model()
+
+if __name__ == "__main__":
+    main()
---
+++ packages/v-program5/v_program5-0.1.5-py3-none-any.whl/v_program5/test.py
@@ -0,0 +1,9 @@
+import v_program5
+from .ml_model import run
+
+
+def main():
+    run()
+
+if __name__ == "__main__":
+    main()
