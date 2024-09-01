2c002c8911731d728133e6ab0392fbbdb74638ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 14:05:40 +0000

    Add package string-clean-util/1.3/string_clean_util-1.3.tar.gz

---
+++ packages/string-clean-util/string_clean_util-1.3.tar.gz/string_clean_util-1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="string-clean-util",
+    version="1.3",
+    description="A library that cleans up common string artifacts from strings",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="JB-102",
+    url="",
+    packages=find_packages()
+)
