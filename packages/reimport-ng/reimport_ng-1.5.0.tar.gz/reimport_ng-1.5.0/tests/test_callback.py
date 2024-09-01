aa3ab585a549f44575bde4875a1f86d7fab6b0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:45 +0000

    Add package reimport-ng/1.5.0/reimport_ng-1.5.0.tar.gz

---
+++ packages/reimport-ng/reimport_ng-1.5.0.tar.gz/reimport_ng-1.5.0/tests/test_callback.py
@@ -0,0 +1,26 @@
+import sys
+import shutil
+import time
+import reimport
+
+def trigger():
+    pass
+
+def test_callback():
+    shutil.copy("tests/callbacka.py", "tests/callback.py")
+
+    import callback
+    sys.setprofile(callback.TraceCallback)
+
+    print(callback.TraceCallback.__doc__)
+    trigger()
+
+    time.sleep(0.4)
+    shutil.copy("tests/callbackb.py", "tests/callback.py")
+    reimport.reimport(callback)
+
+    print(callback.TraceCallback.__doc__)
+    trigger()
+
+
+
