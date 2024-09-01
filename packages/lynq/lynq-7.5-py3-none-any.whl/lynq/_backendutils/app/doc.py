24e8294a0805effd5d44cdf602f0cd0af0ac697b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:15 +0000

    Add package lynq/7.5/lynq-7.5-py3-none-any.whl

---
+++ packages/lynq/lynq-7.5-py3-none-any.whl/lynq/_backendutils/app/doc.py
@@ -0,0 +1,30 @@
+from types import ModuleType
+from importlib import import_module
+
+from lynq._backendutils.app.app import app
+from lynq._backendutils.server.standard import LynqServer
+
+def generate_documentation(module_name: str, unique_port: int | None = None) -> None:
+    module: ModuleType = import_module(module_name)
+
+    @app(LynqServer(unique_port or 8000)) .export .standard
+    def index(self) -> None:
+
+        with self.head() as head:
+            with head.title() as title:
+                title.singular(f"Documentation for {module.__name__}")
+
+            with head.style() as style:
+                style.singular(r"body { font-family: arial; }")
+
+        with self.body() as body:
+            with body.h1() as h1:
+                h1.singular(module.__name__)
+
+            with body.h3() as h3:
+                h3.singular(module.__file__)
+
+            with body.p() as p:
+                p.singular(module.__doc__)
+
+    return index
