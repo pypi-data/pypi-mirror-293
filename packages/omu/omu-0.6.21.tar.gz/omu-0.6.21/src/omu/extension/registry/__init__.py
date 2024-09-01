fd2b86abdef3af9b016683cdac427536c02fcbc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 15:05:20 +0000

    Add package omu/0.6.21/omu-0.6.21.tar.gz

---
+++ packages/omu/omu-0.6.21.tar.gz/omu-0.6.21/src/omu/extension/registry/__init__.py
@@ -0,0 +1,9 @@
+from .registry import Registry, RegistryType
+from .registry_extension import REGISTRY_EXTENSION_TYPE, RegistryExtension
+
+__all__ = [
+    "Registry",
+    "RegistryType",
+    "REGISTRY_EXTENSION_TYPE",
+    "RegistryExtension",
+]
