3a64805a97ed9a27db3165f035958d0b6a6f1133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 04:04:56 +0000

    Add package 0040-NACE-clean/0.0.3/0040_nace_clean-0.0.3.tar.gz

---
+++ packages/0040-NACE-clean/0040_nace_clean-0.0.3.tar.gz/0040_nace_clean-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='0040_NACE_clean',
+version='0.0.3',
+author='ucabdv1',
+author_email='ucabdv1@ucl.ac.uk',
+description='A test to see how pypi packages work',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.10',
+)
+
