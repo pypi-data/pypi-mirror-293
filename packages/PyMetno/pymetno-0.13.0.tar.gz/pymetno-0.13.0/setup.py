870a262a3eaa83ab38b318fa626784056f812cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 04:04:48 +0000

    Add package PyMetno/0.13.0/pymetno-0.13.0.tar.gz

---
+++ packages/PyMetno/pymetno-0.13.0.tar.gz/pymetno-0.13.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'PyMetno',
+    packages = ['metno'],
+    install_requires=['xmltodict', "aiohttp>=3.0.6", "async_timeout>=3.0.0"],
+    version = '0.13.0',
+    description = 'A library to communicate with the met.no api',
+    author='Daniel Hjelseth Høyer',
+    url='https://github.com/Danielhiversen/pyMetno/',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
