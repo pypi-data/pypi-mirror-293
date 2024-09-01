051633432ba24e71d923638f4831839da4b271c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 15:05:18 +0000

    Add package hoangk7/0.0.0/hoangk7-0.0.0-py3-none-any.whl

---
+++ packages/hoangk7/hoangk7-0.0.0-py3-none-any.whl/hoangk7/hoang.py
@@ -0,0 +1,17 @@
+import requests
+
+class Hoang:
+  def __init__(self, a, b):
+    self.a = a
+    self.b = b
+  
+  def nhan(self):
+    print(int(self.a) * int(self.b))
+  
+  def info(self):
+    print("""
+    code test by : hoang
+    fb : https://www.facebook.com/hoanglailaptrinh123
+    """)
+
+  
