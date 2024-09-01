fed7419191563a70d1d81e57df69305a1e7ca7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:19 +0000

    Add package Arenz-Group-Python/0.2.2/arenz_group_python-0.2.2.tar.gz

---
+++ packages/Arenz-Group-Python/arenz_group_python-0.2.2.tar.gz/arenz_group_python-0.2.2/src/arenz_group_python/project/__init__.py
@@ -0,0 +1,11 @@
+""" ## Create Project Structure
+Module for creating default folder structure of a project.
+"""
+
+from .util_paths import Project_Paths 
+
+__all__ = ["util_paths","Project_Paths"]
+
+
+#Import the submodules
+#from . import ec_data
