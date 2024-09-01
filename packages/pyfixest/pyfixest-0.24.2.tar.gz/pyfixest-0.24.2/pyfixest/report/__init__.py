97fd60963ebf729c83ead353e0981f6830079976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:07 +0000

    Add package pyfixest/0.24.2/pyfixest-0.24.2.tar.gz

---
+++ packages/pyfixest/pyfixest-0.24.2.tar.gz/pyfixest-0.24.2/pyfixest/report/__init__.py
@@ -0,0 +1,12 @@
+from pyfixest.report.summarize import etable, summary
+from pyfixest.report.visualize import (
+    coefplot,
+    iplot,
+)
+
+__all__ = [
+    "summary",
+    "etable",
+    "iplot",
+    "coefplot",
+]
