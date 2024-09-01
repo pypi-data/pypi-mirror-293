06819d7cc921370091e7528cdf11fd8707234052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 14:05:10 +0000

    Add package aider-chat/0.54.9/aider_chat-0.54.9-py3-none-any.whl

---
+++ packages/aider-chat/aider_chat-0.54.9-py3-none-any.whl/aider/__init__.py
@@ -0,0 +1,11 @@
+from setuptools_scm import get_version
+
+try:
+    __version__ = get_version(root="..", relative_to=__file__)
+except Exception:
+    try:
+        from aider.__version__ import __version__
+    except Exception:
+        __version__ = "0.54.9"
+
+__all__ = [__version__]
