0c79a86047703bda0d9ce33a1b5e0d9094d50852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:38 +0000

    Add package converso-tempO-robert/0.1.0/converso_tempO_robert-0.1.0.tar.gz

---
+++ packages/converso-tempO-robert/converso_tempO_robert-0.1.0.tar.gz/converso_tempO_robert-0.1.0/README.md
@@ -0,0 +1,9 @@
+# temp_converter
+
+Um pacote simples para conversão de temperaturas entre Celsius, Fahrenheit e Kelvin.
+
+## Instalação
+
+```bash
+pip install temp_converter
+
---
+++ packages/converso-tempO-robert/converso_tempO_robert-0.1.0.tar.gz/converso_tempO_robert-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='converso_tempO_robert',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=['pytest'],
+    test_suite='tests',
+    description='Um pacote para conversão de temperaturas entre Celsius, Fahrenheit e Kelvin.',
+    author='Robert Maklyn',
+    author_email='robertmaklyns@gmail.com',
+    url="https://github.com/RobertMaklyn/Dio-pacote-python-converter-temp"
+)
