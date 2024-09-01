e6e6246f0ae0b15f938094510e77be6682a3fc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:45 +0000

    Add package v-program5/0.1.0/v_program5-0.1.0-py3-none-any.whl

---
+++ packages/v-program5/v_program5-0.1.0-py3-none-any.whl/v_program5/cli.py
@@ -0,0 +1,3 @@
+
+# In cli.py
+from v_program5.ml_model import train_model, predict_model
---
+++ packages/v-program5/v_program5-0.1.0-py3-none-any.whl/v_program5/ml_model.py
@@ -0,0 +1,9 @@
+# ml_model.py
+
+def train_model(data):
+    # Code to train a model
+    pass
+
+def predict_model(data):
+    # Code to make predictions
+    pass
