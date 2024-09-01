65032a2caf74c620246a01b6ee2a2f0c70b1992c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 15:05:09 +0000

    Add package geekros/0.0.3/geekros-0.0.3.tar.gz

---
+++ packages/geekros/geekros-0.0.3.tar.gz/geekros-0.0.3/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+with open("readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="geekros",
+    version="0.0.3",
+    author="MakerYang",
+    author_email="admin@wileho.com",
+    description="Python development framework for geekros.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geekros/geekros",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    python_requires=">=3.9.0",
+    license="Apache-2.0",
+    project_urls={
+        "Documentation": "https://www.geekros.com/docs",
+        "Website": "https://lgeekros.com",
+        "Source": "https://github.com/geekros/geekros",
+    },
+    keywords=["webrtc", "realtime", "audio", "video", "geekros", "ros", "ros2", "ubuntu"],
+    install_requires=[
+        "logging>=0.4.9.6",
+        "colorlog>=6.8.2",
+        "wave>=0.0.2",
+        "numpy>=2.1.0",
+        "pyaudio>=0.2.14",
+        "pyusb>=1.2.1",
+        "requests>=2.32.3",
+        "hidapi>=0.14.0.post2",
+        "webrtcvad>=2.0.10",
+        "pvporcupine==1.8.7",
+        "openai>=1.42.0",
+        "respeaker>=0.6.2"
+    ]
+)
