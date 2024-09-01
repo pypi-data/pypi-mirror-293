4f984e44ccae8340df9e00f00ba623c082939506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 14:05:14 +0000

    Add package mtmai/0.3.575/mtmai-0.3.575.tar.gz

---
+++ packages/mtmai/mtmai-0.3.575.tar.gz/mtmai-0.3.575/mtmai/agents/graphchatdemo/state.py
@@ -0,0 +1,63 @@
+import operator
+from collections.abc import Sequence
+from typing import Annotated, TypedDict
+
+from langchain_core.messages import BaseMessage
+
+# from langchain_core.pydantic_v1 import BaseModel as BaseModelV1
+from langgraph.channels.context import Context
+
+# from pydantic import BaseModel
+from mtmai.agents.graphchatdemo.context import AgentContext, make_agent_context
+
+
+class DemoAgentConfig(TypedDict):
+    name: str | None = None
+    default_llm: str | None = None
+    prompt_default: str | None = None
+
+
+class ChatBotUiState(TypedDict):
+    # class Config:
+    #     arbitrary_types_allowed = True
+
+    isOpenAgentRagView: bool | None = False
+    threadId: str | None = None
+    isDev: bool | None = False
+    isOpenRagUi: bool | None = False
+
+
+class UiCommandsItem(TypedDict):
+    name: str
+    args: dict
+
+
+class UiAppendComponentItem(TypedDict):
+    component: str
+
+
+class UiDelta(TypedDict):
+    class Config:
+        arbitrary_types_allowed = True
+
+    uiappendcomponents: list[UiAppendComponentItem] | None = None
+    commands: list[UiCommandsItem] | None = None
+    uiState: ChatBotUiState | None = None
+
+
+class MainState(TypedDict):
+    # emails: list[dict] | None
+    # action_required_emails: dict
+    # messages: Annotated[list, add_messages]
+    error: str | None = None
+    user_input: str | None = None
+    wait_user: bool | None = False
+    user_id: str | None = None
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+    context: Annotated[AgentContext, Context(make_agent_context)]
+    config: DemoAgentConfig
+    # uistate: UiState | None = None
+    uidelta: UiDelta | None = None
+
+    # 工具结果相关状态
+    # web_search_results: str | None = None
