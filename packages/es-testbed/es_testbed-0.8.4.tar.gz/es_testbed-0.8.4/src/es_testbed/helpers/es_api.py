f4b36ef4aae490dbfa617a1f7f9d61cbeb244216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:21 +0000

    Add package es-testbed/0.8.4/es_testbed-0.8.4.tar.gz

---
+++ packages/es-testbed/es_testbed-0.8.4.tar.gz/es_testbed-0.8.4/src/es_testbed/helpers/es_api.py
@@ -0,0 +1,502 @@
+"""Functions that make Elasticsearch API Calls"""
+
+import typing as t
+import logging
+from os import getenv
+from elasticsearch8.exceptions import NotFoundError
+from es_wait import Exists, Snapshot
+from ..defaults import MAPPING, PAUSE_DEFAULT, PAUSE_ENVVAR
+from ..exceptions import (
+    NameChanged,
+    ResultNotExpected,
+    TestbedFailure,
+    TestbedMisconfig,
+    TimeoutException,
+)
+from ..helpers.utils import (
+    get_routing,
+    mounted_name,
+    prettystr,
+    storage_type,
+)
+
+if t.TYPE_CHECKING:
+    from elasticsearch8 import Elasticsearch
+
+PAUSE_VALUE = float(getenv(PAUSE_ENVVAR, default=PAUSE_DEFAULT))
+
+logger = logging.getLogger(__name__)
+
+# pylint: disable=W0707
+
+
+def emap(kind: str, es: 'Elasticsearch', value=None) -> t.Dict[str, t.Any]:
+    """Return a value from a dictionary"""
+    _ = {
+        'alias': {
+            'delete': es.indices.delete_alias,
+            'exists': es.indices.exists_alias,
+            'get': es.indices.get_alias,
+            'kwargs': {'index': value, 'expand_wildcards': ['open', 'closed']},
+            'plural': 'alias(es)',
+        },
+        'data_stream': {
+            'delete': es.indices.delete_data_stream,
+            'exists': es.indices.exists,
+            'get': es.indices.get_data_stream,
+            'kwargs': {'name': value, 'expand_wildcards': ['open', 'closed']},
+            'plural': 'data_stream(s)',
+            'key': 'data_streams',
+        },
+        'index': {
+            'delete': es.indices.delete,
+            'exists': es.indices.exists,
+            'get': es.indices.get,
+            'kwargs': {'index': value, 'expand_wildcards': ['open', 'closed']},
+            'plural': 'index(es)',
+        },
+        'template': {
+            'delete': es.indices.delete_index_template,
+            'exists': es.indices.exists_index_template,
+            'get': es.indices.get_index_template,
+            'kwargs': {'name': value},
+            'plural': 'index template(s)',
+            'key': 'index_templates',
+        },
+        'ilm': {
+            'delete': es.ilm.delete_lifecycle,
+            'exists': es.ilm.get_lifecycle,
+            'get': es.ilm.get_lifecycle,
+            'kwargs': {'name': value},
+            'plural': 'ilm policy(ies)',
+        },
+        'component': {
+            'delete': es.cluster.delete_component_template,
+            'exists': es.cluster.exists_component_template,
+            'get': es.cluster.get_component_template,
+            'kwargs': {'name': value},
+            'plural': 'component template(s)',
+            'key': 'component_templates',
+        },
+        'snapshot': {
+            'delete': es.snapshot.delete,
+            'exists': es.snapshot.get,
+            'get': es.snapshot.get,
+            'kwargs': {'snapshot': value},
+            'plural': 'snapshot(s)',
+        },
+    }
+    return _[kind]
+
+
+def change_ds(client: 'Elasticsearch', actions: t.Union[str, None] = None) -> None:
+    """Change/Modify/Update a datastream"""
+    try:
+        client.indices.modify_data_stream(actions=actions, body=None)
+    except Exception as err:
+        raise ResultNotExpected(
+            f'Unable to modify datastreams. {prettystr(err)}'
+        ) from err
+
+
+def create_data_stream(client: 'Elasticsearch', name: str) -> None:
+    """Create a datastream"""
+    try:
+        client.indices.create_data_stream(name=name)
+        test = Exists(client, name=name, kind='data_stream', pause=PAUSE_VALUE)
+        test.wait()
+    except Exception as err:
+        raise TestbedFailure(
+            f'Unable to create datastream {name}. Error: {prettystr(err)}'
+        ) from err
+
+
+def create_index(
+    client: 'Elasticsearch',
+    name: str,
+    aliases: t.Union[t.Dict, None] = None,
+    settings: t.Union[t.Dict, None] = None,
+    tier: str = 'hot',
+) -> None:
+    """Create named index"""
+    if not settings:
+        settings = get_routing(tier=tier)
+    else:
+        settings.update(get_routing(tier=tier))
+    client.indices.create(
+        index=name, aliases=aliases, mappings=MAPPING, settings=settings
+    )
+    try:
+        test = Exists(client, name=name, kind='index', pause=PAUSE_VALUE)
+        test.wait()
+    except TimeoutException as err:
+        raise ResultNotExpected(f'Failed to create index {name}') from err
+    return exists(client, 'index', name)
+
+
+def verify(
+    client: 'Elasticsearch',
+    kind: str,
+    name: str,
+    repository: t.Union[str, None] = None,
+) -> bool:
+    """Verify that whatever was deleted is actually deleted"""
+    success = True
+    items = ','.split(name)
+    for item in items:
+        result = exists(client, kind, item, repository=repository)
+        if result:  # That means it's still in the cluster
+            success = False
+    return success
+
+
+def delete(
+    client: 'Elasticsearch',
+    kind: str,
+    name: str,
+    repository: t.Union[str, None] = None,
+) -> bool:
+    """Delete the named object of type kind"""
+    which = emap(kind, client)
+    func = which['delete']
+    success = False
+    if name is not None:  # Typically only with ilm
+        try:
+            if kind == 'snapshot':
+                res = func(snapshot=name, repository=repository)
+            elif kind == 'index':
+                res = func(index=name)
+            else:
+                res = func(name=name)
+        except NotFoundError as err:
+            logger.warning('%s named %s not found: %s', kind, name, prettystr(err))
+            success = True
+        except Exception as err:
+            raise ResultNotExpected(f'Unexpected result: {prettystr(err)}') from err
+        if 'acknowledged' in res and res['acknowledged']:
+            success = True
+            logger.info('Deleted %s: "%s"', which['plural'], name)
+        else:
+            success = verify(client, kind, name, repository=repository)
+    else:
+        logger.debug('"%s" has a None value for name', kind)
+    return success
+
+
+def do_snap(
+    client: 'Elasticsearch', repo: str, snap: str, idx: str, tier: str = 'cold'
+) -> None:
+    """Perform a snapshot"""
+    client.snapshot.create(repository=repo, snapshot=snap, indices=idx)
+    test = Snapshot(client, snapshot=snap, repository=repo, pause=1, timeout=60)
+    test.wait()
+
+    # Mount the index accordingly
+    client.searchable_snapshots.mount(
+        repository=repo,
+        snapshot=snap,
+        index=idx,
+        index_settings=get_routing(tier=tier),
+        renamed_index=mounted_name(idx, tier),
+        storage=storage_type(tier),
+        wait_for_completion=True,
+    )
+    # Fix aliases
+    fix_aliases(client, idx, mounted_name(idx, tier))
+
+
+def exists(
+    client: 'Elasticsearch', kind: str, name: str, repository: t.Union[str, None] = None
+) -> bool:
+    """Return boolean existence of the named kind of object"""
+    if name is None:
+        return False
+    retval = True
+    func = emap(kind, client)['exists']
+    try:
+        if kind == 'snapshot':
+            retval = func(snapshot=name, repository=repository)
+        elif kind == 'ilm':
+            retval = func(name=name)
+        elif kind in ['index', 'data_stream']:
+            retval = func(index=name)
+        else:
+            retval = func(name=name)
+    except NotFoundError:
+        retval = False
+    except Exception as err:
+        raise ResultNotExpected(f'Unexpected result: {prettystr(err)}') from err
+    return retval
+
+
+def fill_index(
+    client: 'Elasticsearch',
+    name: t.Union[str, None] = None,
+    doc_generator: t.Union[t.Generator[t.Dict, None, None], None] = None,
+    options: t.Union[t.Dict, None] = None,
+) -> None:
+    """
+    Create and fill the named index with mappings and settings as directed
+
+    :param client: ES client
+    :param name: Index name
+    :param doc_generator: The generator function
+
+    :returns: No return value
+    """
+    if not options:
+        options = {}
+    for doc in doc_generator(**options):
+        client.index(index=name, document=doc)
+    client.indices.flush(index=name)
+    client.indices.refresh(index=name)
+
+
+def find_write_index(client: 'Elasticsearch', name: str) -> t.AnyStr:
+    """Find the write_index for an alias by searching any index the alias points to"""
+    retval = None
+    for alias in get_aliases(client, name):
+        retval = get_write_index(client, alias)
+        if retval:
+            break
+    return retval
+
+
+def fix_aliases(client: 'Elasticsearch', oldidx: str, newidx: str) -> None:
+    """Fix aliases using the new and old index names as data"""
+    # Delete the original index
+    client.indices.delete(index=oldidx)
+    # Add the original index name as an alias to the mounted index
+    client.indices.put_alias(index=f'{newidx}', name=oldidx)
+
+
+def get(
+    client: 'Elasticsearch',
+    kind: str,
+    pattern: str,
+    repository: t.Union[str, None] = None,
+) -> t.Sequence[str]:
+    """get any/all objects of type kind matching pattern"""
+    if pattern is None:
+        msg = f'"{kind}" has a None value for pattern'
+        logger.error(msg)
+        raise TestbedMisconfig(msg)
+    which = emap(kind, client, value=pattern)
+    func = which['get']
+    kwargs = which['kwargs']
+    if kind == 'snapshot':
+        kwargs['repository'] = repository
+    try:
+        result = func(**kwargs)
+    except NotFoundError:
+        logger.debug('%s pattern "%s" had zero matches', kind, pattern)
+        return []
+    except Exception as err:
+        raise ResultNotExpected(f'Unexpected result: {prettystr(err)}') from err
+    if kind == 'snapshot':
+        retval = [x['snapshot'] for x in result['snapshots']]
+    elif kind in ['data_stream', 'template', 'component']:
+        retval = [x['name'] for x in result[which['key']]]
+    else:
+        # ['alias', 'ilm', 'index']
+        retval = list(result.keys())
+    return retval
+
+
+def get_aliases(client: 'Elasticsearch', name: str) -> t.Sequence[str]:
+    """Get aliases from index 'name'"""
+    res = client.indices.get(index=name)
+    try:
+        retval = list(res[name]['aliases'].keys())
+    except KeyError:
+        retval = None
+    return retval
+
+
+def get_backing_indices(client: 'Elasticsearch', name: str) -> t.Sequence[str]:
+    """Get the backing indices from the named data_stream"""
+    resp = resolver(client, name)
+    data_streams = resp['data_streams']
+    retval = []
+    if data_streams:
+        if len(data_streams) > 1:
+            raise ResultNotExpected(
+                f'Expected only a single data_stream matching {name}'
+            )
+        retval = data_streams[0]['backing_indices']
+    return retval
+
+
+def get_ds_current(client: 'Elasticsearch', name: str) -> str:
+    """
+    Find which index is the current 'write' index of the datastream
+    This is best accomplished by grabbing the last backing_index
+    """
+    backers = get_backing_indices(client, name)
+    retval = None
+    if backers:
+        retval = backers[-1]
+    return retval
+
+
+def get_ilm(client: 'Elasticsearch', pattern: str) -> t.Union[t.Dict[str, str], None]:
+    """Get any ILM entity in ES that matches pattern"""
+    try:
+        return client.ilm.get_lifecycle(name=pattern)
+    except Exception as err:
+        msg = f'Unable to get ILM lifecycle matching {pattern}. Error: {prettystr(err)}'
+        logger.critical(msg)
+        raise ResultNotExpected(msg) from err
+
+
+def get_ilm_phases(client: 'Elasticsearch', name: str) -> t.Dict:
+    """Return the policy/phases part of the ILM policy identified by 'name'"""
+    ilm = get_ilm(client, name)
+    try:
+        return ilm[name]['policy']['phases']
+    except KeyError as err:
+        msg = f'Unable to get ILM lifecycle named {name}. Error: {prettystr(err)}'
+        logger.critical(msg)
+        raise ResultNotExpected(msg) from err
+
+
+def get_write_index(client: 'Elasticsearch', name: str) -> str:
+    """
+    Calls :py:meth:`~.elasticsearch.client.IndicesClient.get_alias`
+
+    :param client: A client connection object
+    :param name: An alias name
+
+    :type client: :py:class:`~.elasticsearch.Elasticsearch`
+
+    :returns: The the index name associated with the alias that is designated
+    ``is_write_index``
+    """
+    response = client.indices.get_alias(index=name)
+    retval = None
+    for index in list(response.keys()):
+        try:
+            if response[index]['aliases'][name]['is_write_index']:
+                retval = index
+                break
+        except KeyError:
+            continue
+    return retval
+
+
+def ilm_explain(client: 'Elasticsearch', name: str) -> t.Union[t.Dict, None]:
+    """Return the results from the ILM Explain API call for the named index"""
+    try:
+        retval = client.ilm.explain_lifecycle(index=name)['indices'][name]
+    except KeyError:
+        logger.debug('Index name changed')
+        new = list(client.ilm.explain_lifecycle(index=name)['indices'].keys())[0]
+        retval = client.ilm.explain_lifecycle(index=new)['indices'][new]
+    except NotFoundError as err:
+        logger.warning('Datastream/Index Name changed. %s was not found', name)
+        raise NameChanged(f'{name} was not found, likely due to a name change') from err
+    except Exception as err:
+        msg = f'Unable to get ILM information for index {name}'
+        logger.critical(msg)
+        raise ResultNotExpected(f'{msg}. Exception: {prettystr(err)}') from err
+    return retval
+
+
+def ilm_move(
+    client: 'Elasticsearch', name: str, current_step: t.Dict, next_step: t.Dict
+) -> None:
+    """Move index 'name' from the current step to the next step"""
+    try:
+        client.ilm.move_to_step(
+            index=name, current_step=current_step, next_step=next_step
+        )
+    except Exception as err:
+        msg = (
+            f'Unable to move index {name} to ILM next step: {next_step}. '
+            f'Error: {prettystr(err)}'
+        )
+        logger.critical(msg)
+        raise ResultNotExpected(msg, err)
+
+
+def put_comp_tmpl(client: 'Elasticsearch', name: str, component: t.Dict) -> None:
+    """Publish a component template"""
+    try:
+        client.cluster.put_component_template(
+            name=name, template=component, create=True
+        )
+        test = Exists(client, name=name, kind='component', pause=PAUSE_VALUE)
+        test.wait()
+    except Exception as err:
+        raise TestbedFailure(
+            f'Unable to create component template {name}. Error: {prettystr(err)}'
+        ) from err
+
+
+def put_idx_tmpl(
+    client,
+    name: str,
+    index_patterns: list,
+    components: list,
+    data_stream: t.Union[t.Dict, None] = None,
+) -> None:
+    """Publish an index template"""
+    try:
+        client.indices.put_index_template(
+            name=name,
+            composed_of=components,
+            data_stream=data_stream,
+            index_patterns=index_patterns,
+            create=True,
+        )
+        test = Exists(client, name=name, kind='template', pause=PAUSE_VALUE)
+        test.wait()
+    except Exception as err:
+        raise TestbedFailure(
+            f'Unable to create index template {name}. Error: {prettystr(err)}'
+        ) from err
+
+
+def put_ilm(
+    client: 'Elasticsearch', name: str, policy: t.Union[t.Dict, None] = None
+) -> None:
+    """Publish an ILM Policy"""
+    try:
+        client.ilm.put_lifecycle(name=name, policy=policy)
+    except Exception as err:
+        raise TestbedFailure(
+            f'Unable to put index lifecycle policy {name}. Error: {prettystr(err)}'
+        ) from err
+
+
+def resolver(client: 'Elasticsearch', name: str) -> dict:
+    """
+    Resolve details about the entity, be it an index, alias, or data_stream
+
+    Because you can pass search patterns and aliases as name, each element comes back
+    as an array:
+
+    {'indices': [], 'aliases': [], 'data_streams': []}
+
+    If you only resolve a single index or data stream, you will still have a 1-element
+    list
+    """
+    return client.indices.resolve_index(name=name, expand_wildcards=['open', 'closed'])
+
+
+def rollover(client: 'Elasticsearch', name: str) -> None:
+    """Rollover alias or datastream identified by name"""
+    client.indices.rollover(alias=name, wait_for_active_shards='all')
+
+
+def snapshot_name(client: 'Elasticsearch', name: str) -> t.Union[t.AnyStr, None]:
+    """Get the name of the snapshot behind the mounted index data"""
+    res = {}
+    if exists(client, 'index', name):  # Can jump straight to nested keys if it exists
+        res = client.indices.get(index=name)[name]['settings']['index']
+    try:
+        retval = res['store']['snapshot']['snapshot_name']
+    except KeyError:
+        logger.error('%s is not a searchable snapshot')
+        retval = None
+    return retval
---
+++ packages/es-testbed/es_testbed-0.8.4.tar.gz/es_testbed-0.8.4/src/es_testbed/ilm.py
@@ -0,0 +1,265 @@
+"""ILM Defining Class"""
+
+import typing as t
+import logging
+from os import getenv
+from dotmap import DotMap
+from elasticsearch8.exceptions import BadRequestError
+from es_wait import IlmPhase, IlmStep
+from es_wait.exceptions import IlmWaitError
+from es_testbed.defaults import (
+    PAUSE_ENVVAR,
+    PAUSE_DEFAULT,
+    TIMEOUT_DEFAULT,
+    TIMEOUT_ENVVAR,
+)
+from es_testbed.exceptions import NameChanged, ResultNotExpected, TestbedMisconfig
+from es_testbed.helpers.es_api import get_ilm_phases, ilm_explain, ilm_move, resolver
+from es_testbed.helpers.utils import prettystr
+
+if t.TYPE_CHECKING:
+    from elasticsearch8 import Elasticsearch
+
+PAUSE_VALUE = float(getenv(PAUSE_ENVVAR, default=PAUSE_DEFAULT))
+TIMEOUT_VALUE = float(getenv(TIMEOUT_ENVVAR, default=TIMEOUT_DEFAULT))
+
+logger = logging.getLogger('es_testbed.IlmTracker')
+
+# ## Example ILM explain output
+# {
+#     'action': 'complete',
+#     'action_time_millis': 0,
+#     'age': '5.65m',
+#     'index': 'INDEX_NAME',
+#     'index_creation_date_millis': 0,
+#     'lifecycle_date_millis': 0,
+#     'managed': True,
+#     'phase': 'hot',
+#     'phase_execution': {
+#         'modified_date_in_millis': 0,
+#         'phase_definition': {
+#             'actions': {
+#                 'rollover': {
+#                     'max_age': 'MAX_AGE',
+#                     'max_primary_shard_docs': 1000,
+#                     'max_primary_shard_size': 'MAX_SIZE',
+#                     'min_docs': 1
+#                 }
+#             },
+#             'min_age': '0ms'
+#         },
+#         'policy': 'POLICY_NAME',
+#         'version': 1
+#     },
+#     'phase_time_millis': 0,
+#     'policy': 'POLICY_NAME',
+#     'step': 'complete',
+#     'step_time_millis': 0,
+#     'time_since_index_creation': '5.65m'
+# }
+
+
+class IlmTracker:
+    """ILM Phase Tracking Class"""
+
+    def __init__(self, client: 'Elasticsearch', name: str):
+        self.client = client
+        self.name = self.resolve(name)  # A single index name
+        self._explain = DotMap(self.get_explain_data())
+        self._phases = get_ilm_phases(self.client, self._explain.policy)
+
+    @property
+    def current_step(self) -> t.Dict:
+        """Return the current ILM step information"""
+        self.update()
+        return {
+            'phase': self._explain.phase,
+            'action': self._explain.action,
+            'name': self._explain.step,
+        }
+
+    @property
+    def explain(self) -> DotMap:
+        """Return the current stored value of ILM Explain"""
+        return self._explain
+
+    @property
+    def next_phase(self) -> str:
+        """Return the next phase in the index's ILM journey"""
+        retval = None
+        if self._explain.phase == 'delete':
+            logger.warning('Already on "delete" phase. No more phases to advance')
+        else:
+            curr = self.pnum(self._explain.phase)  # A numeric representation
+            # A list of any remaining phases in the policy with a higher number than
+            # the current
+            remaining = [
+                self.pnum(x) for x in self.policy_phases if self.pnum(x) > curr
+            ]
+            if remaining:  # If any:
+                retval = self.pname(remaining[0])
+                # Get the phase name from the number stored in the first element
+        return retval
+
+    @property
+    def policy_phases(self) -> t.Sequence[str]:
+        """Return a list of phases in the ILM policy"""
+        return list(self._phases.keys())
+
+    def _log_phase(self, phase: str) -> None:
+        logger.debug('ILM Explain Index: %s', self._explain.index)
+        logger.info('Index "%s" now on phase "%s"', self.name, phase)
+
+    def _phase_wait(
+        self, phase: str, pause: float = PAUSE_VALUE, timeout: float = TIMEOUT_VALUE
+    ) -> None:
+        """Wait until the new phase shows up in ILM Explain"""
+        kw = {'name': self.name, 'phase': phase, 'pause': pause, 'timeout': timeout}
+        phasechk = IlmPhase(self.client, **kw)
+        phasechk.wait()
+
+    def _ssphz(self, phase: str) -> bool:
+        return bool(self.pnum(phase) > self.pnum('warm'))
+
+    def advance(
+        self,
+        phase: t.Union[str, None] = None,
+        action: t.Union[str, None] = None,
+        name: t.Union[str, None] = None,
+    ) -> None:
+        """Advance index to next ILM phase"""
+        if self._explain.phase == 'delete':
+            logger.warning('Already on "delete" phase. No more phases to advance')
+        else:
+            logger.debug('current_step: %s', prettystr(self.current_step))
+            next_step = self.next_step(phase, action=action, name=name)
+            logger.debug('next_step: %s', prettystr(next_step))
+            if self._explain.phase == 'new' and phase == 'hot':
+                # It won't be for very long.
+                self._phase_wait('hot')
+
+            # Regardless of the remaining phases, the current phase steps must be
+            # complete before proceeding with ilm_move
+            self.update()
+            self.wait4complete()
+            self.update()
+
+            # We could have arrived with it hot, but incomplete
+            if phase == 'hot':
+                self._log_phase(phase)
+                # we've advanced to our target phase, and all steps are completed
+
+            # Remaining phases could be warm through frozen
+            elif self._explain.phase != phase:
+
+                # We will only wait for steps to complete for the hot and warm tiers
+                wait4steps = False if self._ssphz(phase) else False
+
+                ilm_move(self.client, self.name, self.current_step, next_step)
+                self._phase_wait(phase)
+                # If cold or frozen, we can return now. We let the calling function
+                # worry about the weird name changing behavior of searchable mounts
+
+                if wait4steps:
+                    self.update()
+                    logger.debug(
+                        'Waiting for "%s" phase steps to complete...',
+                        phase,
+                    )
+                    self.wait4complete()
+                    self.update()
+                self._log_phase(phase)
+            else:
+                logger.error('next_step is a None value')
+                logger.error('current_step: %s', prettystr(self.current_step))
+
+    def get_explain_data(self) -> t.Dict:
+        """Get the ILM explain data and return it"""
+        try:
+            return ilm_explain(self.client, self.name)
+        except NameChanged as err:
+            logger.debug('Passing along upstream exception...')
+            raise NameChanged from err
+        except ResultNotExpected as err:
+            msg = f'Unable to get ilm_explain results. Error: {prettystr(err)}'
+            logger.critical(msg)
+            raise ResultNotExpected(msg) from err
+
+    def next_step(
+        self,
+        phase: t.Union[str, None] = None,
+        action: t.Union[str, None] = None,
+        name: t.Union[str, None] = None,
+    ) -> t.Dict:
+        """Determine the next ILM step based on the current phase, action, and name"""
+        err1 = bool((action is not None) and (name is None))
+        err2 = bool((action is None) and (name is not None))
+        if err1 or err2:
+            msg = 'If either action or name is specified, both must be'
+            logger.critical(msg)
+            raise TestbedMisconfig(msg)
+        if not phase:
+            phase = self.next_phase
+        retval = {'phase': phase}
+        if action:
+            retval['action'] = action
+            retval['name'] = name
+        return retval
+
+    def pnum(self, phase: str) -> int:
+        """Map a phase name to a phase number"""
+        _ = {'new': 0, 'hot': 1, 'warm': 2, 'cold': 3, 'frozen': 4, 'delete': 5}
+        return _[phase]
+
+    def pname(self, num: int) -> str:
+        """Map a phase number to a phase name"""
+        _ = {0: 'new', 1: 'hot', 2: 'warm', 3: 'cold', 4: 'frozen', 5: 'delete'}
+        return _[num]
+
+    def resolve(self, name: str) -> str:
+        """Resolve that we have an index and NOT an alias or a datastream"""
+        res = resolver(self.client, name)
+        if len(res['aliases']) > 0 or len(res['data_streams']) > 0:
+            msg = f'{name} is not an index: {res}'
+            logger.critical(msg)
+            raise ResultNotExpected(msg)
+        if len(res['indices']) > 1:
+            msg = f'{name} resolved to multiple indices: {prettystr(res["indices"])}'
+            logger.critical(msg)
+            raise ResultNotExpected(msg)
+        return res['indices'][0]['name']
+
+    def update(self) -> None:
+        """Update self._explain with the latest from :py:meth:`get_explain_data`"""
+        try:
+            self._explain = DotMap(self.get_explain_data())
+        except NameChanged as err:
+            logger.debug('Passing along upstream exception...')
+            raise NameChanged from err
+
+    def wait4complete(self) -> None:
+        """Subroutine for waiting for an ILM step to complete"""
+        step_action = bool(self._explain.action == 'complete')
+        step_name = bool(self._explain.name == 'complete')
+        if bool(step_action and step_name):
+            logger.debug(
+                '%s: Current step complete: %s', self.name, prettystr(self.current_step)
+            )
+            return
+        logger.debug(
+            '%s: Current step not complete. %s', self.name, prettystr(self.current_step)
+        )
+        kw = {'name': self.name, 'pause': PAUSE_VALUE, 'timeout': TIMEOUT_VALUE}
+        step = IlmStep(self.client, **kw)
+        try:
+            step.wait()
+            logger.debug('ILM Step successful. The wait is over')
+        except KeyError as exc:
+            logger.error('KeyError: The index name has changed: %s', prettystr(exc))
+            raise exc
+        except BadRequestError as exc:
+            logger.error('Index not found')
+            raise exc
+        except IlmWaitError as exc:
+            logger.error('Other IlmWait error encountered: %s', prettystr(exc))
+            raise exc
