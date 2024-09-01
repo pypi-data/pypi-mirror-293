11722604ef0e44bd6969616098b3fc875a764215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:15 +0000

    Add package keras-spark/0.91/keras_spark-0.91-py3-none-any.whl

---
+++ packages/keras-spark/keras_spark-0.91-py3-none-any.whl/keras_spark/model.py
@@ -0,0 +1,29 @@
+from core import SparkDsTFDs,KerasOnSparkPredict
+import tensorflow as tf
+from pyspark.sql import DataFrame as SparkDataFrame
+
+class KerasSparkModel(tf.keras.Model):
+
+    def __init__(self, *args, **kwargs):
+        super(KerasSparkModel, self).__init__(*args, **kwargs)
+
+    def fit(self, x=None, y=None, partition_col="batch_id",nr_partitions=100,num_parallel_calls=1, **kwargs):
+
+        if isinstance(x, SparkDataFrame):
+
+            adapter = SparkDsTFDs(self)
+            dataset = adapter.convert(x, partition_col=partition_col, nr_partitions=nr_partitions, num_parallel_calls=num_parallel_calls, batch_size=kwargs['batch_size'])
+
+            super(KerasSparkModel, self).fit(dataset, **kwargs)
+
+        else:
+            super(KerasSparkModel, self).fit(x, y, **kwargs)
+
+    def predict(self, x=None, **kwargs):
+
+        if isinstance(x, SparkDataFrame):
+
+            return KerasOnSparkPredict().predict(x,self)
+
+        else:
+            return super(KerasSparkModel, self).predict(x, **kwargs)
