f9b239a30a03809508bb456d3315f49583447618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:52 +0000

    Add package graphics-planes/0.8.8/graphics_planes-0.8.8.tar.gz

---
+++ packages/graphics-planes/graphics_planes-0.8.8.tar.gz/graphics_planes-0.8.8/graphics_planes/PlaneModule.py
@@ -0,0 +1,27 @@
+import svgwrite
+from .ShapeModule import Shape
+
+class Plane:
+    def __init__(self, name="Drawing"):
+        self.name = name
+        self.shapes = []
+
+    def add_shape(self, shape):
+        """Añade una forma al dibujo."""
+        if not isinstance(shape, Shape):
+            raise TypeError("Expected an instance of Shape.")
+        self.shapes.append(shape)
+
+    def export_to_svg(self, filename):
+        """Exporta todo el dibujo a un archivo SVG."""
+        dwg = svgwrite.Drawing(filename, profile='tiny')
+
+        # Crear un grupo de elementos de dibujo
+        shape_group = dwg.add(dwg.g(id='shapes', fill='none', stroke='black'))
+
+        for shape in self.shapes:
+            # Usar el método _draw_to_svg de la forma para añadirla al SVG
+            shape._draw_to_svg(shape_group, dwg)
+
+        dwg.save()
+        print(f"Dibujo exportado a {filename}")
