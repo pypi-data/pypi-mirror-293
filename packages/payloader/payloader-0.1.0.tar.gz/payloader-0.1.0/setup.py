7f7877e18ba4f429ebc0f6b0dbaa1ec665d5b5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 14:05:24 +0000

    Add package payloader/0.1.0/payloader-0.1.0.tar.gz

---
+++ packages/payloader/payloader-0.1.0.tar.gz/payloader-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='payloader',
+    version='0.1.0',
+    description='A simple HTTP client for making GET and POST requests',
+    author='samhuelt',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
