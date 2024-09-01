dea5bfdcde07fe771c575b29bb797c330548d5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 04:04:44 +0000

    Add package mpnm/2024.8.31.16.31/mpnm-2024.8.31.16.31.tar.gz

---
+++ packages/mpnm/mpnm-2024.8.31.16.31.tar.gz/mpnm-2024.8.31.16.31/mpnm/__init__.py
@@ -0,0 +1,12 @@
+'''
+This package contains the classes and functions necessary to run the mpnm algorithm.
+'''
+
+import numpy as np
+from mpnm._algorithm import algorithm
+from mpnm._Base import Base
+from mpnm._network import network
+from mpnm._topotools import topotools
+from mpnm._phase import phase
+np.seterr(divide='ignore', invalid='ignore')
+# __all__=['_algorithm','_Base','_network','_phase','_topotools']
