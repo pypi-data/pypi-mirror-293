c5ff04ec6a604810b4e72fe51b2025500bdb32f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 14:05:50 +0000

    Add package jlm-tech-ds-ml-ba-toolkit/3.0.6/jlm-tech-ds-ml-ba-toolkit-3.0.6.tar.gz

---
+++ packages/jlm-tech-ds-ml-ba-toolkit/jlm-tech-ds-ml-ba-toolkit-3.0.6.tar.gz/jlm-tech-ds-ml-ba-toolkit-3.0.6/setup.py
@@ -0,0 +1,32 @@
+"""
+    Setup file for jlm-tech-ds-ml-ba-toolkit.
+    Use setup.cfg to configure your project.
+
+    This file was generated with PyScaffold 4.5.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    try:
+        setup(
+        name='jlm-tech-ds-ml-ba-toolkit',
+        version='3.0.6',
+        author='JLM-Tech LLC',
+        author_email='jlmtechconsultingsvc@gmail.com',
+        description='data science toolkit',
+        long_description='wraps and provides tools for data science',
+        license='MIT',
+        # packages=find_packages(where='src'),
+        # package_dir={'': 'src'},
+        # include_package_data=True,
+    )
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
---
+++ packages/jlm-tech-ds-ml-ba-toolkit/jlm-tech-ds-ml-ba-toolkit-3.0.6.tar.gz/jlm-tech-ds-ml-ba-toolkit-3.0.6/src/jlm_tech_ds_ml_ba_toolkit/stats_and_plots/__init__.py
@@ -0,0 +1,8 @@
+"""
+stats_and_plots package
+
+This package contains utility functions for data preprocessing.
+"""
+
+# allows importing from parent directory with specified name
+from .stats_and_plots import *
