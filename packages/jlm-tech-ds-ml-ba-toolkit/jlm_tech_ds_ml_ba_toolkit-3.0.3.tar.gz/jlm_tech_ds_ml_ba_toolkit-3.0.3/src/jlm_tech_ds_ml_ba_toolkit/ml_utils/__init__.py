8afe4e155f418c878eeda1076ac01f02c6cf083a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:12 +0000

    Add package jlm-tech-ds-ml-ba-toolkit/3.0.3/jlm_tech_ds_ml_ba_toolkit-3.0.3.tar.gz

---
+++ packages/jlm-tech-ds-ml-ba-toolkit/jlm_tech_ds_ml_ba_toolkit-3.0.3.tar.gz/jlm_tech_ds_ml_ba_toolkit-3.0.3/src/jlm_tech_ds_ml_ba_toolkit/ml_utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+ml_utils package
+
+This package contains utility functions for data preprocessing.
+"""
+
+# allows importing from parent directory with specified name
+from .ml_utils import *
