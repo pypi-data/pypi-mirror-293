300d755a7d665c1ea431f67734a0849fd6784791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:32 +0000

    Add package sfnx/0.1.1/sfnx-0.1.1.tar.gz

---
+++ packages/sfnx/sfnx-0.1.1.tar.gz/sfnx-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sfnx",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "typer[all]",
+        "rich",
+        "cryptography",
+        "argon2-cffi",
+        "sqlmodel"
+    ],
+    entry_points={
+        "console_scripts": [
+            "sfnx=sfnx.main:app",
+        ],
+    },
+    python_requires='>=3.7',  # Specify Python version requirements
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
