7197855baf3707367ef56d198248c0345013ce11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:58 +0000

    Add package playgolf-package/0.5/playgolf_package-0.5.tar.gz

---
+++ packages/playgolf-package/playgolf_package-0.5.tar.gz/playgolf_package-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='playgolf_package',
+    version='0.5',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'playgolf_package': ['playgolf_data.csv', 'playgolf_test.csv'],
+    },
+    install_requires=[
+        'pandas',
+    ],
+    description='A package for decision tree implementation with datasets included',
+)
