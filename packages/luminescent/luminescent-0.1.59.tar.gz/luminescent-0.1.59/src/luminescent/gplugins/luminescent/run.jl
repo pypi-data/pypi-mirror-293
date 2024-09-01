9662311caa45fad32bcb2028f6721267ecc707f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 14:05:28 +0000

    Add package luminescent/0.1.59/luminescent-0.1.59.tar.gz

---
+++ packages/luminescent/luminescent-0.1.59.tar.gz/luminescent-0.1.59/src/luminescent/gplugins/luminescent/run.jl
@@ -0,0 +1,20 @@
+
+using Luminescent, Random
+names(Luminescent)
+Random.seed!(1)
+if isempty(ARGS)
+    path = lastrun()
+    println("path: ", path)
+else
+    path = ARGS[1]
+end
+# gfrun(path; Courant=0.5)
+sol = gfrun(path)
+# @show sol.tparams
+# # Pkg.resolve()
+
+# using Pkg
+# pkg"add https://github.com/paulxshen/Luminescent.jl,Porcupine,Jello,ArrayPadding;up"
+
+# using Pkg
+# pkg"dev C:\Users\pxshe\OneDrive\Desktop\Porcupine.jl;dev C:\Users\pxshe\OneDrive\Desktop\ArrayPadding.jl; dev C:\Users\pxshe\OneDrive\Desktop\Jello.jl;dev C:\Users\pxshe\OneDrive\Desktop\Luminescent.jl;up"
