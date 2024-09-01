d452df131e5df164c1ed2a90ce6f62ce752d865a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 04:04:47 +0000

    Add package 0040-NACE-clean/0.0.1/0040_nace_clean-0.0.1.tar.gz

---
+++ packages/0040-NACE-clean/0040_nace_clean-0.0.1.tar.gz/0040_nace_clean-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+name='0040_NACE_clean',
+version='0.0.1',
+author='ucabdv1',
+author_email='ucabdv1@ucl.ac.uk',
+description='A test to see how pypi packages work',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.10',
+)
+
+
+# To build the package
+# python3 setup.py sdist
+
+# To upload the package
+# python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
