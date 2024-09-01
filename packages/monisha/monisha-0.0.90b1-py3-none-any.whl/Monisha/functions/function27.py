7760d710b61e37a427c20ce0d12ec2eff6a8d821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:43 +0000

    Add package monisha/0.0.90b1/monisha-0.0.90b1-py3-none-any.whl

---
+++ packages/monisha/monisha-0.0.90b1-py3-none-any.whl/Monisha/functions/function27.py
@@ -0,0 +1,15 @@
+import asyncio
+#====================================================================
+
+class Queue:
+
+    def __init__(self, wait=1, workers=1):
+        self.working = []
+        self.waiting = wait
+        self.workers = workers
+
+    async def queue(self, tasks):
+        while len(tasks) >= self.workers:
+            await asyncio.sleep(self.waiting)
+    
+#====================================================================
