399f18a936879fdec337e93da8ed89fe4378e34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:44 +0000

    Add package fastapi-query-parameter-model/0.1.0/fastapi_query_parameter_model-0.1.0-py3-none-any.whl

---
+++ packages/fastapi-query-parameter-model/fastapi_query_parameter_model-0.1.0-py3-none-any.whl/fastapi-query-parameter-model/__init__.py
@@ -0,0 +1,10 @@
+from query_parameter_model import QueryParameterModel
+
+
+__all__ = [
+    "QueryParameterModel"
+]
+
+__version__="0.1.0"
+__author__ = "Pierre DUVEAU"
+__credits__ = ["Pierre DUVEAU"]
