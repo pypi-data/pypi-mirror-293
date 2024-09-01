cfb821d0ecaa7fe55aa290b6b2728fb065c1870f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:40 +0000

    Add package indomaret/1.1.7/indomaret-1.1.7.tar.gz

---
+++ packages/indomaret/indomaret-1.1.7.tar.gz/indomaret-1.1.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='indomaret',  # Nama package di PyPI
+    version='1.1.7',  # Versi package kamu
+    packages=find_packages(),  # Secara otomatis menemukan semua packages
+    entry_points={
+        'console_scripts': [
+            'idf=script.idm:main',  # Nama command-line dan fungsi utama
+        ],
+    },
+    install_requires=[
+        # Daftar dependensi, jika ada
+    ],
+    description='hanya sebuah script biasa',
+    author='robbi 13',
+    author_email='email@example.com',
+    url='https://github.com/username/my_project',  # URL repositori atau website
+)
