24977a29b61e3b116a34d3d77cc63f5a4bd2d046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 14:05:41 +0000

    Add package scancopy/1.1.2/scancopy-1.1.2.tar.gz

---
+++ packages/scancopy/scancopy-1.1.2.tar.gz/scancopy-1.1.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+ 
+setup(
+    name="scancopy",
+    version="1.1.2",
+    author="jiaobenxiaozi",
+    author_email="183732521@qq.com",
+    description="scancopy",
+ 
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'tk' 
+    ],
+ 
+    python_requires='>=3.6',
+    # entry_points={
+    #     'console_scripts': [
+    #         'dp = DrissionPage.commons.cli:main',
+    #     ],
+    # },
+)
