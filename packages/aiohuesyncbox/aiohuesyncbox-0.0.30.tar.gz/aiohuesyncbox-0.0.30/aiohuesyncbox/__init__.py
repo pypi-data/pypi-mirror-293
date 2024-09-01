1c108849a31bd1b7ec6de432dd5d66e4ff6377d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 15:05:03 +0000

    Add package aiohuesyncbox/0.0.30/aiohuesyncbox-0.0.30.tar.gz

---
+++ packages/aiohuesyncbox/aiohuesyncbox-0.0.30.tar.gz/aiohuesyncbox-0.0.30/aiohuesyncbox/__init__.py
@@ -0,0 +1,6 @@
+from .errors import AiohuesyncboxException as AiohuesyncboxException
+from .errors import RequestError as RequestError
+from .errors import Unauthorized as Unauthorized
+from .errors import InvalidState as InvalidState
+
+from .huesyncbox import HueSyncBox as HueSyncBox
