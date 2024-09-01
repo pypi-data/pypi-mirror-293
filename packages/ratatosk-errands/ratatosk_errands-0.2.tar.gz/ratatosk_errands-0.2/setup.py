db4ecda079037ccfbd6f17b35b86345fd10a3f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 04:05:01 +0000

    Add package ratatosk-errands/0.2/ratatosk_errands-0.2.tar.gz

---
+++ packages/ratatosk-errands/ratatosk_errands-0.2.tar.gz/ratatosk_errands-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+installation_requirements = [
+    "pydantic==2.8.2",
+]
+
+setup(
+    name="ratatosk_errands",
+    description="errands for ratatosk",
+    version="0.2",
+    url="https://github.com/vagabond-systems/pasture",
+    author="(~)",
+    package_dir={"": "packages"},
+    packages=["ratatosk_errands"],
+    install_requires=installation_requirements
+)
