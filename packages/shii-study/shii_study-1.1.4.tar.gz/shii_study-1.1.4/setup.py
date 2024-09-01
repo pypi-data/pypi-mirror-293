21639a399ad087beb9f57a2dacc62791de506552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 15:05:16 +0000

    Add package shii-study/1.1.4/shii_study-1.1.4.tar.gz

---
+++ packages/shii-study/shii_study-1.1.4.tar.gz/shii_study-1.1.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="shii_study",
+  version="1.1.4",
+  author="boran",
+  author_email="boran@shii.me",
+  description="PYPI tutorial package creation written by Boran",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/boranloves/shii",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  install_requires=['setuptools>=61.0', 'wheel', "json", "logging"],
+  python_requires='>=3.8',
+)
