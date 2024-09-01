cd3e33e81a89623ec6f7f416a595fe9880f8eba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 03:10:41 +0000

    Add package uvtrick/0.3.0/uvtrick-0.3.0.tar.gz

---
+++ packages/uvtrick/uvtrick-0.3.0.tar.gz/uvtrick-0.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uvtrick',
+    version='0.3.0',
+    packages=find_packages(),
+    install_requires=[
+        'uv'
+    ],
+    python_requires='>=3.8',
+)
