6b236f3874cb90366e154e360f5855dd58bc614f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-01 14:05:08 +0000

    Add package edispatcher/0.1.1/edispatcher-0.1.1.tar.gz

---
+++ packages/edispatcher/edispatcher-0.1.1.tar.gz/edispatcher-0.1.1/README.md
@@ -0,0 +1,17 @@
+# Event Dispatcher
+
+Event Dispatcher is a simple implementation of an event-driven architecture in Python. It allows you to define events, register handlers, and dispatch events to those handlers.
+
+## Features
+
+- Define and dispatch custom events.
+- Register and manage event handlers.
+- Support for custom dispatchers.
+
+## Installation
+
+To install the package, use:
+
+```bash
+pip install edispatcher
+```
---
+++ packages/edispatcher/edispatcher-0.1.1.tar.gz/edispatcher-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="edispatcher",
+    version="0.1.1",
+    description="A simple event dispatcher implementation in Python",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dmtno/edispatcher",
+    author="dmtno",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+    install_requires=[],
+)
