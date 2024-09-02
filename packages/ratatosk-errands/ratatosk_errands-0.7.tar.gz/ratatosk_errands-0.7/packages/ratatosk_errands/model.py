cbde9cd245c30649aaf4f739451b1844179ce5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:18 +0000

    Add package ratatosk-errands/0.7/ratatosk_errands-0.7.tar.gz

---
+++ packages/ratatosk-errands/ratatosk_errands-0.7.tar.gz/ratatosk_errands-0.7/packages/ratatosk_errands/model.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class DiffusionInstructions(BaseModel):
+    prompt: str
+    negative_prompt: str = ""
+    num_inference_steps: int = 28
+    guidance_scale: float = 7.0
+
+
+class ImageToImageInstructions(DiffusionInstructions):
+    encoded_base_image: str
+
+
+class TextToImageInstructions(DiffusionInstructions):
+    width: int = 1024
+    height: int = 1024
+
+
+class ChatInstructions(BaseModel):
+    prompt: str
+    history: List[str]
+    max_new_tokens: int = 750
+    temperature: float = 0.8
+    repetition_penalty: float = 1.1
+
+
+class Errand(BaseModel):
+    instructions: (TextToImageInstructions |
+                   ImageToImageInstructions |
+                   ChatInstructions)
+    origin: str
+    destination: str
+    identifier: str
+    timestamp: float
---
+++ packages/ratatosk-errands/ratatosk_errands-0.7.tar.gz/ratatosk_errands-0.7/test/test_errand.py
@@ -0,0 +1,23 @@
+import time
+
+from dotenv import load_dotenv
+from starlette.testclient import TestClient
+
+from ratatosk_errands.model import Errand, TextToImageInstructions
+
+
+def test_errand():
+    load_dotenv("../ratatosk.env")
+    from ratatosk.main import app
+    with TestClient(app) as client:
+        errand = Errand(
+            instructions=TextToImageInstructions(
+                prompt="a textured oil painting of green pastures and still waters"
+            ),
+            origin="ratatosk",
+            destination="ratatosk",
+            identifier="ratatosk",
+            timestamp=time.time()
+        )
+        response = client.post("/give_errand", json=errand.model_dump())
+        assert response.status_code == 200
