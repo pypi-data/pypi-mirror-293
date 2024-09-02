3cea0ee887428f4c0f62d9f15a03d5d7d4018f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 03:06:21 +0000

    Add package ccrex/0.2.0/ccrex-0.2.0-py3-none-any.whl

---
+++ packages/ccrex/ccrex-0.2.0-py3-none-any.whl/ccrex/run_ccrex.py
@@ -0,0 +1,50 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import json
+import argparse
+
+from Bio import SeqIO
+import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
+
+from ccrex import CCREx
+from utils import convert_ndarray_to_list, plot_predictions
+
+BASE_LENGTH = 55
+
+def main():
+    
+    parser = argparse.ArgumentParser(description='Run CCREx')
+    parser.add_argument('-i', '--input', required=True, help='Input file path')
+    parser.add_argument('-o', '--output', type=str, default='.', help='Output directory')
+    parser.add_argument('-t', '--threads', type=int, default=1, help='Number of threads')
+    parser.add_argument('-p', '--plot', type=bool, default=False, help='Some boolean parameter')
+
+    args = parser.parse_args()
+
+    ccrex = CCREx(n_cpu=args.threads, verbose=False)
+
+    records = list(SeqIO.parse(args.input, 'fasta'))
+
+    results = {}
+
+    for record in records:
+        seq = str(record.seq.upper())
+
+        if len(seq) < BASE_LENGTH:
+            print(f'Sequence {record.id} is too short. Skipping...')
+            continue
+
+        results[record.id] = ccrex.predict(seq)
+
+    results = convert_ndarray_to_list(results)
+
+    results_path = os.path.join(args.output, 'results.json')
+    with open(results_path, 'w') as f:
+        json.dump(results, f)
+
+    if args.plot:
+        plot_predictions(results, args.output)
+
+if __name__ == '__main__':
+    main()
