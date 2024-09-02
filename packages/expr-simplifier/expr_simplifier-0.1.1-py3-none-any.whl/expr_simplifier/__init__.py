8a9cbe4c6fd0976aa6451efde8e5bdc08144ba6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:19 +0000

    Add package expr-simplifier/0.1.1/expr_simplifier-0.1.1-py3-none-any.whl

---
+++ packages/expr-simplifier/expr_simplifier-0.1.1-py3-none-any.whl/expr_simplifier/__init__.py
@@ -0,0 +1,14 @@
+# Meta information for the project.
+from __future__ import annotations
+
+from expr_simplifier.transforms.auto_simplify import auto_simplify as auto_simplify
+
+__version__ = "0.1.1"
+__author__ = "Nyakku Shigure"
+__year__ = "2024"
+__project_info__ = {
+    "name": __name__,
+    "version": __version__,
+    "copyright": f"{__year__}, {__author__}",
+    "author": __author__,
+}
---
+++ packages/expr-simplifier/expr_simplifier-0.1.1-py3-none-any.whl/expr_simplifier/transforms/inline_named_expr.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import ast
+import copy
+from collections.abc import Callable
+
+from typing_extensions import TypeAlias
+
+NamedExpressions: TypeAlias = dict[str, ast.expr]
+ShouldReplaceFn: TypeAlias = Callable[[str, NamedExpressions], bool]
+
+
+class InlineNamedExpr(ast.NodeTransformer):
+    def __init__(self, should_replace_fn: ShouldReplaceFn) -> None:
+        super().__init__()
+        self.should_replace_fn = should_replace_fn
+        self.named_expressions = dict[str, ast.expr]()
+
+    def visit_NamedExpr(self, node: ast.NamedExpr) -> ast.expr:
+        value = self.visit(node.value)
+        name = node.target.id
+        self.named_expressions[name] = value
+        if not self.should_replace_fn(name, self.named_expressions):
+            return node
+        return value
+
+    def visit_Name(self, node: ast.Name) -> ast.expr:
+        if node.id in self.named_expressions:
+            if not self.should_replace_fn(node.id, self.named_expressions):
+                return node
+            return copy.deepcopy(self.named_expressions[node.id])
+        return node
+
+
+def apply_inline_all_named_expr(expr: ast.AST) -> ast.AST:
+    inline_named_expr = InlineNamedExpr(lambda symbol, named_expressions: True)
+    return inline_named_expr.visit(expr)
+
+
+def apply_constant_propagation(expr: ast.AST) -> ast.AST:
+    inline_named_expr = InlineNamedExpr(
+        lambda symbol, named_expressions: isinstance(named_expressions[symbol], ast.Constant)
+    )
+    return inline_named_expr.visit(expr)
