ab0081e8196713dc6d3990bc0f4955dcb3f728e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 04:05:21 +0000

    Add package avocet/1.0.2/avocet-1.0.2.tar.gz

---
+++ packages/avocet/avocet-1.0.2.tar.gz/avocet-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="avocet",
+    version="1.0.2",
+    description="An easy to use Python package for encryption and decryption needs",
+    author="Siddharth Krishnan",
+    author_email="sid@sidkrishnan.com",
+    url="https://github.com/sidkris/avocet",
+    license="MIT",
+    install_requires=["pycryptodomex"],
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
