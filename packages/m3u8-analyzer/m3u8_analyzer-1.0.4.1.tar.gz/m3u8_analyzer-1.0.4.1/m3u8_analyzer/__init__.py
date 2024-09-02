33607c678dd1d137d80426979427e88ae02e8afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 15:05:56 +0000

    Add package m3u8-analyzer/1.0.4.1/m3u8_analyzer-1.0.4.1.tar.gz

---
+++ packages/m3u8-analyzer/m3u8_analyzer-1.0.4.1.tar.gz/m3u8_analyzer-1.0.4.1/m3u8_analyzer/__init__.py
@@ -0,0 +1,7 @@
+# m3u8_analyzer/__init__.py
+
+from .M3u8Analyzer import M3u8Analyzer, Wrapper,EncryptSuport,M3u8Downloader
+
+__all__ = ['M3u8Analyzer', 'Wrapper','EncryptSuport','M3u8Downloader']
+if __name__ == '__main__':
+    raise RuntimeError("no escope!")
