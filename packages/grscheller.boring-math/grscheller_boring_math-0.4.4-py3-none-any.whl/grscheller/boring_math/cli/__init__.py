d7a02796583b9457fb551da5e71f88cae8291adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:04:49 +0000

    Add package grscheller.boring-math/0.4.4/grscheller_boring_math-0.4.4-py3-none-any.whl

---
+++ packages/grscheller.boring-math/grscheller_boring_math-0.4.4-py3-none-any.whl/grscheller/boring_math/cli/__init__.py
@@ -0,0 +1,32 @@
+# Copyright 2016-2024 Geoffrey R. Scheller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+## Entry points for automatically generated CLI scripts.
+
+CGI scripts are automatically generated in an OS independent way by the package
+build backend. This is configured by the `[project.scripts]` section in the
+project's pyproject.toml file.
+
+### Ackermann's Function
+
+Ackermann's function is an example of a computable function that is not
+primitively recursive.
+
+* program **Ackermann_list** uses a Python list to implement the recursion
+
+### Pythagorean Triples
+
+* program **pythag3** outputs lists of Pythagorean triples in increasing order
+"""
