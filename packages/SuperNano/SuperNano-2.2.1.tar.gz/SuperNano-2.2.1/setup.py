fddae467d4646f36d0605e9f1e0f4a3598b73560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 04:05:38 +0000

    Add package SuperNano/2.2.1/SuperNano-2.2.1.tar.gz

---
+++ packages/SuperNano/SuperNano-2.2.1.tar.gz/SuperNano-2.2.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SuperNano",
+    version="2.2.1",
+    author="LcfherShell",
+    author_email="alfiandecker2@gmail.com",
+    description="A console-based text editor.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LcfherShell/SuperNano",
+    py_modules=["supernano/main", "supernano/install"],
+    packages=find_packages(),# This automatically finds both 'supernano' and 'utils'
+    include_package_data=True,
+    install_requires=[
+      # Add other dependencies here
+    ],
+    entry_points={
+        "console_scripts": [
+            "supernano=supernano.main:main",  # Entry point untuk __main__.py
+            "isupernano=supernano.install:install_script",  # Entry point untuk install.py
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
