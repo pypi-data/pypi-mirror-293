782c1c6f0bb6b7d3a9489254049de7e21e070e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:03 +0000

    Add package django-debug-toolbar/5.0.0a0/django_debug_toolbar-5.0.0a0.tar.gz

---
+++ packages/django-debug-toolbar/django_debug_toolbar-5.0.0a0.tar.gz/django_debug_toolbar-5.0.0a0/tests/base.py
@@ -0,0 +1,91 @@
+from typing import Optional
+
+import html5lib
+from asgiref.local import Local
+from django.http import HttpResponse
+from django.test import Client, RequestFactory, TestCase, TransactionTestCase
+
+from debug_toolbar.panels import Panel
+from debug_toolbar.toolbar import DebugToolbar
+
+
+class ToolbarTestClient(Client):
+    def request(self, **request):
+        # Use a thread/async task context-local variable to guard against a
+        # concurrent _created signal from a different thread/task.
+        data = Local()
+        data.toolbar = None
+
+        def handle_toolbar_created(sender, toolbar=None, **kwargs):
+            data.toolbar = toolbar
+
+        DebugToolbar._created.connect(handle_toolbar_created)
+        try:
+            response = super().request(**request)
+        finally:
+            DebugToolbar._created.disconnect(handle_toolbar_created)
+        response.toolbar = data.toolbar
+
+        return response
+
+
+rf = RequestFactory()
+
+
+class BaseMixin:
+    client_class = ToolbarTestClient
+
+    panel: Optional[Panel] = None
+    panel_id = None
+
+    def setUp(self):
+        super().setUp()
+        self._get_response = lambda request: HttpResponse()
+        self.request = rf.get("/")
+        self.toolbar = DebugToolbar(self.request, self.get_response)
+        self.toolbar.stats = {}
+
+        if self.panel_id:
+            self.panel = self.toolbar.get_panel_by_id(self.panel_id)
+            self.panel.enable_instrumentation()
+        else:
+            self.panel = None
+
+    def tearDown(self):
+        if self.panel:
+            self.panel.disable_instrumentation()
+        super().tearDown()
+
+    def get_response(self, request):
+        return self._get_response(request)
+
+    def assertValidHTML(self, content):
+        parser = html5lib.HTMLParser()
+        parser.parseFragment(content)
+        if parser.errors:
+            msg_parts = ["Invalid HTML:"]
+            lines = content.split("\n")
+            for position, errorcode, datavars in parser.errors:
+                msg_parts.append("  %s" % html5lib.constants.E[errorcode] % datavars)
+                msg_parts.append("    %s" % lines[position[0] - 1])
+            raise self.failureException("\n".join(msg_parts))
+
+
+class BaseTestCase(BaseMixin, TestCase):
+    pass
+
+
+class BaseMultiDBTestCase(BaseMixin, TransactionTestCase):
+    databases = {"default", "replica"}
+
+
+class IntegrationTestCase(TestCase):
+    """Base TestCase for tests involving clients making requests."""
+
+    def setUp(self):
+        # The HistoryPanel keeps track of previous stores in memory.
+        # This bleeds into other tests and violates their idempotency.
+        # Clear the store before each test.
+        for key in list(DebugToolbar._store.keys()):
+            del DebugToolbar._store[key]
+        super().setUp()
---
+++ packages/django-debug-toolbar/django_debug_toolbar-5.0.0a0.tar.gz/django_debug_toolbar-5.0.0a0/tests/views.py
@@ -0,0 +1,77 @@
+import asyncio
+
+from asgiref.sync import sync_to_async
+from django.contrib.auth.models import User
+from django.core.cache import cache
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import render
+from django.template.response import TemplateResponse
+from django.views.decorators.cache import cache_page
+
+
+def execute_sql(request):
+    list(User.objects.all())
+    return render(request, "base.html")
+
+
+async def async_execute_sql(request):
+    await sync_to_async(list)(User.objects.all())
+    return render(request, "base.html")
+
+
+async def async_execute_sql_concurrently(request):
+    await asyncio.gather(sync_to_async(list)(User.objects.all()), User.objects.acount())
+    return render(request, "base.html")
+
+
+def regular_view(request, title):
+    return render(request, "basic.html", {"title": title})
+
+
+def template_response_view(request, title):
+    return TemplateResponse(request, "basic.html", {"title": title})
+
+
+def new_user(request, username="joe"):
+    User.objects.create_user(username=username)
+    return render(request, "basic.html", {"title": "new user"})
+
+
+def resolving_view(request, arg1, arg2):
+    # see test_url_resolving in tests.py
+    return render(request, "base.html")
+
+
+@cache_page(60)
+def cached_view(request):
+    return render(request, "base.html")
+
+
+def cached_low_level_view(request):
+    key = "spam"
+    value = cache.get(key)
+    if not value:
+        value = "eggs"
+        cache.set(key, value, 60)
+    return render(request, "base.html")
+
+
+def json_view(request):
+    return JsonResponse({"foo": "bar"})
+
+
+def regular_jinjia_view(request, title):
+    return render(request, "basic.jinja", {"title": title}, using="jinja2")
+
+
+def listcomp_view(request):
+    lst = [i for i in range(50000) if i % 2 == 0]
+    return render(request, "basic.html", {"title": "List comprehension", "lst": lst})
+
+
+def redirect_view(request):
+    return HttpResponseRedirect("/regular/redirect/")
+
+
+def ajax_view(request):
+    return render(request, "ajax/ajax.html")
