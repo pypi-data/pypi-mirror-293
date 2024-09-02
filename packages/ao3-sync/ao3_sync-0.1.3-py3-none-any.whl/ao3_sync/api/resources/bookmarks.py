b818bba8fddf11424ac990a8a2589753bd72f6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:04:48 +0000

    Add package ao3-sync/0.1.3/ao3_sync-0.1.3-py3-none-any.whl

---
+++ packages/ao3-sync/ao3_sync-0.1.3-py3-none-any.whl/ao3_sync/api/resources/bookmarks.py
@@ -0,0 +1,254 @@
+from typing import Any, Callable
+
+import parsel
+from tqdm import tqdm
+from yaspin import yaspin
+
+import ao3_sync.api.exceptions
+from ao3_sync.api.client import AO3ApiClient
+from ao3_sync.api.enums import (
+    BOOKMARKS_SORT_QUERY_PARAM,
+    DEFAULT_BOOKMARKS_SORT_OPTION,
+    DEFAULT_DOWNLOAD_FORMATS,
+    BookmarksSortOption,
+    DownloadFormat,
+    ItemType,
+)
+from ao3_sync.api.models import Bookmark
+
+
+class BookmarksApi:
+    """
+    API for handling AO3 bookmarks
+
+    Args:
+        client (AO3ApiClient): AO3ApiClient instance
+
+    Attributes:
+        URL_PATH (str): URL path for bookmarks
+    """
+
+    URL_PATH: str = "/bookmarks"
+
+    def __init__(self, client: AO3ApiClient):
+        self._client = client
+
+    def sync(
+        self,
+        start_page=1,
+        end_page=None,
+        sort_by: BookmarksSortOption = DEFAULT_BOOKMARKS_SORT_OPTION,
+        query_params=None,
+        formats: list[DownloadFormat] = DEFAULT_DOWNLOAD_FORMATS,
+    ):
+        """
+        Downloads the user's bookmarks from AO3.
+
+        Args:
+            start_page (int): Starting page of bookmarks to download. Defaults to 1
+            end_page (int): Ending page of bookmarks to download. Defaults to None
+            query_params (dict): Query parameters for bookmarks
+            formats (list[DownloadFormat]): Formats to download. Defaults to DEFAULT_DOWNLOAD_FORMATS
+        """
+        bookmarks = self.fetch_pages(
+            start_page=start_page,
+            end_page=end_page,
+            sort_by=sort_by,
+            query_params=query_params,
+        )
+        bookmarks.reverse()
+
+        def update_history(bookmark_id):
+            history = self._client.get_history()
+            if sort_by == BookmarksSortOption.DATE_BOOKMARKED:
+                history.bookmarks.date_bookmarked_last_bookmark = bookmark_id
+            elif sort_by == BookmarksSortOption.DATE_UPDATED:
+                history.bookmarks.date_updated_last_bookmark = bookmark_id
+            self._client.update_history(history)
+
+        self.download(bookmarks, formats=formats, after_item_sync=update_history)
+
+    def fetch_pages(
+        self,
+        start_page=1,
+        end_page=None,
+        sort_by: BookmarksSortOption = DEFAULT_BOOKMARKS_SORT_OPTION,
+        query_params=None,
+    ) -> list[Bookmark]:
+        """
+        Gets a list of bookmarks for the user.
+
+        If `end_page` is not provided, it will download all bookmarks from `start_page` to the last page.
+
+        Args:
+            start_page (int): Starting page of bookmarks to download. Defaults to 1
+            end_page (int): Ending page of bookmarks to download. Defaults to None
+            query_params (dict): Query parameters for bookmarks
+
+        Returns:
+            bookmarks (Bookmark): List of bookmarks. Ordered from newest to oldest.
+        """
+
+        if query_params is None:
+            query_params = {}
+
+        with yaspin(text="Getting count of bookmark pages\r", color="yellow") as spinner:
+            try:
+                num_pages = self.fetch_page_count()
+                spinner.color = "green"
+                spinner.text = f"Found {num_pages} pages of bookmarks"
+                spinner.ok("✔")
+            except Exception:
+                spinner.color = "red"
+                spinner.fail("✘")
+                raise
+
+        if num_pages == 0 or start_page > num_pages:
+            return []
+
+        end_page = num_pages if end_page is None else end_page
+        num_pages_to_download = end_page - start_page + 1
+
+        if num_pages_to_download > 1:
+            self._client._log(f"Downloading {num_pages_to_download} pages, from page {start_page} to {end_page}")
+        else:
+            self._client._log(f"Downloading page {start_page}")
+
+        bookmark_list = []
+        for page_num in tqdm(range(start_page, end_page + 1), desc="Pages", unit="pg"):
+            bookmarks = self.fetch_page(page_num, sort_by=sort_by, query_params=query_params)
+            bookmark_list.extend(bookmarks)
+
+        return bookmark_list
+
+    def fetch_page(
+        self,
+        page: int,
+        sort_by: BookmarksSortOption = DEFAULT_BOOKMARKS_SORT_OPTION,
+        query_params=None,
+    ):
+        """
+        Gets a page of bookmarks for the user.
+
+        Args:
+            query_params (dict): Query parameters for bookmarks
+
+        Returns:
+            bookmarks (Bookmark): List of bookmarks. Ordered from newest to oldest.
+        """
+
+        if query_params is None:
+            query_params = {}
+
+        query_params["page"] = page
+        query_params["user_id"] = self._client.auth.username
+        query_params["bookmark_search[sort_column]"] = BOOKMARKS_SORT_QUERY_PARAM[sort_by]
+
+        if query_params["page"] < 1:
+            raise ao3_sync.api.exceptions.FailedRequest("Page number must be greater than 0")
+
+        history = self._client.get_history()
+
+        bookmarks_page: Any = self._client.get_or_fetch(
+            self.URL_PATH,
+            query_params=query_params,
+        )
+
+        bookmark_element_list = parsel.Selector(bookmarks_page).css("ol.bookmark > li")
+        bookmark_list: list[Bookmark] = []
+        for idx, bookmark_el in enumerate(bookmark_element_list, start=1):
+            bookmark_id = bookmark_el.css("::attr(id)").get()
+            if not bookmark_id:
+                self._client._debug_error(f"Skipping bookmark {idx} as it has no ID")
+                continue
+
+            if self._client.use_history and (
+                (
+                    sort_by == BookmarksSortOption.DATE_BOOKMARKED
+                    and history.bookmarks.date_bookmarked_last_bookmark == bookmark_id
+                )
+                or (
+                    sort_by == BookmarksSortOption.DATE_UPDATED
+                    and history.bookmarks.date_updated_last_bookmark == bookmark_id
+                )
+            ):
+                self._client._debug_log(f"Stopping at bookmark {idx} as it is already cached")
+                break
+
+            title_raw = bookmark_el.css("h4.heading a:not(rel)")
+            item_href = title_raw.css("::attr(href)").get()
+
+            if not item_href:
+                self._client._debug_error(f"Skipping bookmark {idx} as it has no item_href")
+                continue
+
+            _, item_path, item_id = item_href.split("/")
+
+            item_type = None
+            match f"/{item_path}":
+                case self._client.works.URL_PATH:
+                    item_type = ItemType.WORK
+                case self._client.series.URL_PATH:
+                    item_type = ItemType.SERIES
+                case _:
+                    self._client._debug_error(f"Skipping bookmark {idx} as it has an unknown type: {item_path}")
+                    continue
+
+            bookmark = Bookmark(id=bookmark_id, item_type=item_type, item_id=item_id)
+            bookmark_list.append(bookmark)
+
+        return bookmark_list
+
+    def fetch_page_count(self):
+        """
+        Gets the number of bookmark pages for the user.
+
+        Returns:
+            num_pages (int): Number of bookmark pages
+        """
+        first_page: Any = self._client.get_or_fetch(
+            self.URL_PATH,
+            query_params={"page": 1, "user_id": self._client.auth.username, "sort_column": "created_at"},
+        )
+        pagination = parsel.Selector(first_page).css("ol.pagination li").getall()
+
+        if len(pagination) < 3:
+            return 0
+
+        last_page_str = parsel.Selector(pagination[-2]).css("::text").get()
+        return int(last_page_str) if last_page_str else 0
+
+    def download(
+        self,
+        bookmarks: list[Bookmark],
+        formats: list[DownloadFormat] = DEFAULT_DOWNLOAD_FORMATS,
+        after_item_sync: Callable[[str], None] | None = None,
+    ):
+        """
+        Downloads the work download files for the given bookmarks.
+
+        Args:
+            bookmarks (list[Bookmark]): List of bookmarks to download
+            formats (list[DownloadFormat]): Formats to download. Defaults to DEFAULT_DOWNLOAD_FORMATS
+
+        """
+
+        if not bookmarks or len(bookmarks) == 0:
+            self._client._log("\nNo bookmarks to download")
+            return
+
+        format_values = [format.value for format in formats]
+        self._client._log(f"\nDownloading {', '.join(format_values)} files for {len(bookmarks)} bookmarks")
+        progress_bar = tqdm(total=len(bookmarks), desc="Bookmarks", unit="bkmk")
+        for bookmark in bookmarks:
+            if bookmark.item_type == ItemType.WORK:
+                self._client.works.sync(bookmark.item_id, formats=formats)
+            elif bookmark.item_type == ItemType.SERIES:
+                self._client.series.sync(bookmark.item_id, formats=formats)
+
+            if after_item_sync:
+                after_item_sync(bookmark.id)
+
+            progress_bar.update(1)
+
+        progress_bar.close()
---
+++ packages/ao3-sync/ao3_sync-0.1.3-py3-none-any.whl/ao3_sync/cli.py
@@ -0,0 +1,428 @@
+import functools
+
+import rich_click as click
+from pydantic import SecretStr
+from rich.console import Console
+from rich.table import Table
+from yaspin import yaspin
+
+import ao3_sync.api.exceptions
+from ao3_sync.api import AO3ApiClient
+from ao3_sync.api.enums import (
+    DEFAULT_BOOKMARKS_SORT_OPTION_VALUE,
+    DEFAULT_DOWNLOAD_FORMATS_VALUES,
+    BookmarksSortOption,
+    DownloadFormat,
+)
+from ao3_sync.utils import seralize_download_format, seralize_sort_by
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+console = Console()
+
+base_api = AO3ApiClient()
+
+
+def create_option_group(options):
+    return [
+        {
+            "name": ":lock: Authentication",
+            "options": [
+                "--username",
+                "--password",
+            ],
+            "panel_styles": {
+                "border_style": "yellow",
+            },
+        },
+        options,
+        {
+            "name": "Advanced Options",
+            "options": [
+                "--output-dir",
+                "--downloads-dir",
+                "--requests-delay-seconds",
+                "--history",
+                "--history-file",
+            ],
+        },
+        {
+            "name": "Debug Options",
+            "options": [
+                "--debug",
+                "--debug-cache",
+                "--debug-cache-dir",
+            ],
+        },
+    ]
+
+
+# https://archiveofourown.org/works/18462467
+
+click.rich_click.USE_RICH_MARKUP = True
+click.rich_click.OPTION_GROUPS = {
+    "ao3-sync bookmarks": create_option_group(
+        {
+            "name": "Sync Bookmarks Options",
+            "options": [
+                "--start-page",
+                "--end-page",
+                "--sort-by",
+                "--query-params",
+                "--format",
+            ],
+            "panel_styles": {
+                "border_style": "white",
+            },
+        }
+    ),
+    "ao3-sync series": create_option_group(
+        {
+            "name": "Sync Series Options",
+            "options": [
+                "--series",
+                "--format",
+            ],
+            "panel_styles": {
+                "border_style": "white",
+            },
+        }
+    ),
+    "ao3-sync work": create_option_group(
+        {
+            "name": "Sync Work Options",
+            "options": [
+                "--work",
+                "--format",
+            ],
+            "panel_styles": {
+                "border_style": "white",
+            },
+        }
+    ),
+}
+
+
+def api_command(func):
+    @cli.command(
+        epilog="""
+        [bold underline white]Resources[/]\n
+        \n
+        [link=https://github.com]User Guides[/] \n
+        [link=https://github.com]Developer Documentation[/] \n
+        """,
+    )
+    @click.pass_context
+    @click.option(
+        "-u",
+        "--username",
+        "username",
+        help="AO3 Username",
+        default=lambda: base_api.auth.username if base_api.auth.username else "",
+        required=True,
+    )
+    @click.option(
+        "-p",
+        "--password",
+        "password",
+        help="AO3 Password",
+        hide_input=True,
+        default=lambda: base_api.auth.password.get_secret_value() if base_api.auth.password else "",
+        required=True,
+    )
+    @click.option(
+        "--output-dir",
+        "output_dir",
+        type=click.Path(file_okay=False, writable=True),
+        default=base_api.output_dir,
+        show_default=True,
+        help="Directory to save output",
+    )
+    @click.option(
+        "--downloads-dir",
+        "downloads_dir",
+        type=click.Path(file_okay=False, writable=True),
+        default=base_api.downloads_dir,
+        show_default=True,
+        help="Directory to save downloads. Directory wil be nested under output-dir",
+    )
+    @click.option(
+        "--requests-delay-seconds",
+        "requests_delay_seconds",
+        type=float,
+        default=base_api.requests_delay_seconds,
+        show_default=True,
+        help="Number of requests per second",
+    )
+    @click.option(
+        "--history/--no-history",
+        "use_history",
+        default=base_api.use_history,
+        show_default=True,
+        help="Enable or disable history",
+    )
+    @click.option(
+        "--history-file",
+        "history_filepath",
+        type=click.Path(file_okay=True, writable=True),
+        default=base_api.history_filepath,
+        show_default=True,
+        help="Path to history file. File will be nested under output-dir",
+    )
+    @click.option(
+        "--debug/--no-debug",
+        "debug",
+        default=base_api.debug,
+        show_default=True,
+        help="Enable debug mode",
+    )
+    @click.option(
+        "--debug-cache/--no-debug-cache",
+        "use_debug_cache",
+        default=base_api.use_debug_cache,
+        show_default=True,
+        help="Enable or disable the debug cache",
+    )
+    @click.option(
+        "--debug-cache-dir",
+        "debug_cache_dir",
+        type=click.Path(file_okay=False, writable=True),
+        default=base_api.debug_cache_dir,
+        show_default=True,
+        help="Directory to save debug cache. Directory will be nested under output-dir",
+    )
+    @functools.wraps(func)
+    def wrapper(ctx, **kwargs):
+        api = AO3ApiClient(
+            username=kwargs.pop("username"),
+            password=kwargs.pop("password"),
+            output_dir=kwargs.pop("output_dir"),
+            downloads_dir=kwargs.pop("downloads_dir"),
+            requests_delay_seconds=kwargs.pop("requests_delay_seconds"),
+            use_history=kwargs.pop("use_history"),
+            history_filepath=kwargs.pop("history_filepath"),
+            debug=kwargs.pop("debug"),
+            use_debug_cache=kwargs.pop("use_debug_cache"),
+            debug_cache_dir=kwargs.pop("debug_cache_dir"),
+        )
+
+        username = api.auth.username
+        password = api.auth.password.get_secret_value() if api.auth.password else None
+
+        click.secho("AO3 Sync", bold=True, color=True)
+        click.secho("Press Ctrl+C to cancel \n", color=True)
+
+        table = Table(
+            title="Settings",
+            title_justify="left",
+            title_style="bold",
+            show_lines=False,
+            show_edge=True,
+            show_header=False,
+            expand=True,
+            pad_edge=True,
+        )
+        table.add_column("Setting")
+        table.add_column("Value")
+        table.add_row("Downloads Directory", str(api.get_downloads_dir().resolve()), end_section=True)
+
+        if api.requests_delay_seconds != base_api.requests_delay_seconds:
+            table.add_row("Requests Delay (seconds)", str(api.requests_delay_seconds), end_section=True)
+
+        if api.use_history:
+            table.add_row("History", "[green]Enabled")
+            table.add_row("History File", str(api.get_history_filepath().resolve()), end_section=True)
+
+        if api.debug:
+            table.add_row("Debug", "[green]Enabled")
+            table.add_row("Debug Cache", "[green]Enabled" if api.use_debug_cache else "[red]Disabled")
+            table.add_row("Debug Cache Directory", str(api.get_debug_cache_dir().resolve()), end_section=True)
+
+        console.print(table)
+        click.echo()
+
+        has_username = username is not None and len(username) > 0
+        has_password = password is not None and len(password) > 0
+
+        if not has_username or not has_password:
+            click.secho("Please provide your AO3 username and password", color=True)
+
+        if username is None or len(username) == 0:
+            username = click.prompt(click.style("AO3 username", fg="blue"), type=str)
+        else:
+            click.secho(f"AO3 username: {username}", color=True)
+
+        if password is None or len(password) == 0:
+            password = click.prompt(click.style("AO3 password", fg="blue"), type=str, hide_input=True)
+        else:
+            click.secho(f"AO3 password: {SecretStr(password)}", color=True)
+
+        with yaspin(text="Logging into AO3\r", color="yellow") as spinner:
+            try:
+                api.auth.login(username=username, password=password)
+                spinner.color = "green"
+                spinner.text = "Successfully logged in!"
+                spinner.ok("✔")
+            except Exception as e:
+                is_ao3_exception = isinstance(e, ao3_sync.api.exceptions.AO3Exception)
+                spinner.color = "red"
+                spinner.text = e.args[0] if is_ao3_exception else "An error occurred while logging in"
+                spinner.fail("✘")
+                api._debug_log(e)
+                return
+
+        return func(ctx, api, **kwargs)
+
+    return wrapper
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.pass_context
+def cli(ctx):
+    """
+    Archive your AO3 Account
+    """
+    ctx.ensure_object(dict)
+    return
+
+
+@api_command
+@click.option(
+    "--start-page",
+    "start_page",
+    type=int,
+    default=1,
+    show_default=True,
+    help="Start page number",
+)
+@click.option(
+    "--end-page",
+    "end_page",
+    type=int,
+    default=None,
+    show_default=True,
+    help="End page number",
+)
+@click.option(
+    "--sort-by",
+    "sort_by",
+    type=click.Choice([f.value for f in BookmarksSortOption]),
+    default=DEFAULT_BOOKMARKS_SORT_OPTION_VALUE,
+    show_default=True,
+    help="Sort bookmarks by",
+)
+@click.option(
+    "--query-params",
+    "query_params",
+    type=str,
+    default=None,
+    show_default=True,
+    help="Query parameters",
+)
+@click.option(
+    "--format",
+    "formats",
+    type=click.Choice([f.value for f in DownloadFormat]),
+    default=DEFAULT_DOWNLOAD_FORMATS_VALUES,
+    show_default=True,
+    help="Formats to download",
+    multiple=True,
+)
+def bookmarks(ctx, api, **kwargs):
+    """
+    Sync AO3 Bookmarks
+    """
+    click.secho("\nSyncing AO3 Bookmarks", bold=True, color=True)
+
+    kwargs["formats"] = seralize_download_format(kwargs.get("formats"))
+    kwargs["sort_by"] = seralize_sort_by(kwargs.get("sort_by"))
+
+    try:
+        api.bookmarks.sync(**kwargs)
+        click.secho("DONE!", bold=True, fg="green", color=True)
+    except Exception as e:
+        is_ao3_exception = isinstance(e, ao3_sync.api.exceptions.AO3Exception)
+        if is_ao3_exception:
+            click.secho(e.args[0], fg="red", color=True, bold=True)
+            api._debug_log(e)
+        else:
+            click.secho("An error occurred while syncing bookmarks", fg="red", color=True, bold=True)
+            api._debug_log(e)
+
+
+@api_command
+@click.option(
+    "--work",
+    "work_id",
+    type=str,
+    required=True,
+)
+@click.option(
+    "--format",
+    "formats",
+    type=click.Choice([f.value for f in DownloadFormat]),
+    default=DEFAULT_DOWNLOAD_FORMATS_VALUES,
+    show_default=True,
+    help="Formats to download",
+    multiple=True,
+)
+def work(ctx, api, **kwargs):
+    """
+    Sync AO3 Work
+    """
+    click.secho("\nSyncing AO3 Work", bold=True, color=True)
+
+    kwargs["formats"] = seralize_download_format(kwargs.get("formats"))
+
+    try:
+        api.works.sync(**kwargs)
+        click.secho("DONE!", bold=True, fg="green", color=True)
+    except Exception as e:
+        is_ao3_exception = isinstance(e, ao3_sync.api.exceptions.AO3Exception)
+        if is_ao3_exception:
+            click.secho(e.args[0], fg="red", color=True, bold=True)
+            api._debug_log(e)
+        else:
+            click.secho("An error occurred while syncing work", fg="red", color=True, bold=True)
+            api._debug_log(e)
+
+
+@api_command
+@click.option(
+    "--series",
+    "series_id",
+    type=str,
+    required=True,
+)
+@click.option(
+    "--format",
+    "formats",
+    type=click.Choice([f.value for f in DownloadFormat]),
+    default=DEFAULT_DOWNLOAD_FORMATS_VALUES,
+    show_default=True,
+    help="Formats to download",
+    multiple=True,
+)
+def series(ctx, api, **kwargs):
+    """
+    Sync AO3 Work
+    """
+    click.secho("\nSyncing AO3 Series", bold=True, color=True)
+
+    kwargs["formats"] = seralize_download_format(kwargs.get("formats"))
+
+    try:
+        api.series.sync(**kwargs)
+        click.secho("DONE!", bold=True, fg="green", color=True)
+    except Exception as e:
+        is_ao3_exception = isinstance(e, ao3_sync.api.exceptions.AO3Exception)
+        if is_ao3_exception:
+            click.secho(e.args[0], fg="red", color=True, bold=True)
+            api._debug_log(e)
+        else:
+            click.secho("An error occurred while syncing work", fg="red", color=True, bold=True)
+            api._debug_log(e)
+
+
+if __name__ == "__main__":
+    cli()
