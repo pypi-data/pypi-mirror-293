e154535b4a6b75e3a1039016e0959b7840c29292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:04:56 +0000

    Add package betty/0.4.0a9/betty-0.4.0a9-py3-none-any.whl

---
+++ packages/betty/betty-0.4.0a9-py3-none-any.whl/betty/extension/gramps/__init__.py
@@ -0,0 +1,65 @@
+"""
+Integrate Betty with `Gramps <https://gramps-project.org>`_.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from typing import final
+
+from typing_extensions import override
+
+from betty.extension.gramps.config import GrampsConfiguration
+from betty.gramps.loader import GrampsLoader
+from betty.load import LoadAncestryEvent
+from betty.locale.localizable import static, _, Localizable
+from betty.project.extension import ConfigurableExtension
+
+if TYPE_CHECKING:
+    from betty.event_dispatcher import EventHandlerRegistry
+    from betty.machine_name import MachineName
+
+
+async def _load_ancestry(event: LoadAncestryEvent) -> None:
+    gramps_configuration = event.project.configuration.extensions[
+        Gramps
+    ].extension_configuration
+    assert isinstance(gramps_configuration, GrampsConfiguration)
+    for family_tree in gramps_configuration.family_trees:
+        file_path = family_tree.file_path
+        if file_path:
+            await GrampsLoader(
+                event.project,
+                localizer=event.project.app.localizer,
+            ).load_file(file_path)
+
+
+@final
+class Gramps(ConfigurableExtension[GrampsConfiguration]):
+    """
+    Integrate Betty with `Gramps <https://gramps-project.org>`_.
+    """
+
+    @override
+    @classmethod
+    def plugin_id(cls) -> MachineName:
+        return "gramps"
+
+    @override
+    @classmethod
+    def default_configuration(cls) -> GrampsConfiguration:
+        return GrampsConfiguration()
+
+    @override
+    def register_event_handlers(self, registry: EventHandlerRegistry) -> None:
+        registry.add_handler(LoadAncestryEvent, _load_ancestry)
+
+    @override
+    @classmethod
+    def plugin_label(cls) -> Localizable:
+        return static("Gramps")
+
+    @override
+    @classmethod
+    def plugin_description(cls) -> Localizable:
+        return _('Load <a href="https://gramps-project.org/">Gramps</a> family trees.')
