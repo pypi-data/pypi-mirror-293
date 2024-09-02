141638152c250b9a86a90996e2ef57ffde5de3ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:00 +0000

    Add package rpyt/0.17/rpyt-0.17.tar.gz

---
+++ packages/rpyt/rpyt-0.17.tar.gz/rpyt-0.17/rpyt/l10n/processors/processor.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from io import TextIOWrapper
+from typing import Any
+
+from rpyt.l10n.common import TlString
+
+
+class Processor(ABC):
+    extension: str
+
+    @abstractmethod
+    def read(self, file: str) -> dict[str, str]:
+        pass
+
+    @abstractmethod
+    def write(
+        self, file: str | TextIOWrapper, content: dict[str, Any]
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def process(self, hashid: str, tlstring: TlString) -> Any:
+        pass
+
+    @abstractmethod
+    def update(self, old: Any, new: str | None) -> Any:
+        pass
