c0250b8d2671042506159c25940f44158a0b7b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 03:06:00 +0000

    Add package django-admin-plugin/0.1.5/django_admin_plugin-0.1.5-py3-none-any.whl

---
+++ packages/django-admin-plugin/django_admin_plugin-0.1.5-py3-none-any.whl/django_admin_plugin/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RestFrameworkApiAuthConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_admin_plugin"
+    label = "admin_plugin"
