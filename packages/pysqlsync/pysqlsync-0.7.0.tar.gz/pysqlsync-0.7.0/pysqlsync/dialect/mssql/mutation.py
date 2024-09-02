e375dcf50736810b66a8e9bb993d2f47043ce349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 15:05:44 +0000

    Add package pysqlsync/0.7.0/pysqlsync-0.7.0.tar.gz

---
+++ packages/pysqlsync/pysqlsync-0.7.0.tar.gz/pysqlsync-0.7.0/pysqlsync/dialect/mssql/mutation.py
@@ -0,0 +1,52 @@
+from typing import Optional
+
+from pysqlsync.formation.mutation import Mutator
+from pysqlsync.formation.object_types import (
+    Column,
+    ColumnFormationError,
+    StatementList,
+    Table,
+    join_or_none,
+)
+from pysqlsync.model.id_types import LocalId
+
+
+class MSSQLMutator(Mutator):
+    def mutate_column_stmt(self, source: Column, target: Column) -> Optional[str]:
+        if source.identity != target.identity:
+            raise ColumnFormationError(
+                "operation not permitted; cannot add or drop identity property",
+                source.name,
+            )
+
+        if source.data_type != target.data_type or source.nullable != target.nullable:
+            nullable = " NOT NULL" if not target.nullable else ""
+            return f"ALTER COLUMN {source.name} {target.data_type}{nullable}"
+        else:
+            return None
+
+    def mutate_table_stmt(self, source: Table, target: Table) -> Optional[str]:
+        statements = StatementList()
+
+        constraints: list[str] = []
+        common_columns = source.columns.intersection(target.columns)
+        for source_column, target_column in common_columns:
+            source_def = source_column.default
+            target_def = target_column.default
+
+            if source_def == target_def:
+                continue
+
+            name = LocalId(f"df_{source_column.name.local_id}")
+            if source_def is not None:
+                constraints.append(f"DROP CONSTRAINT {name}")
+            if target_def is not None:
+                constraints.append(
+                    f"ADD CONSTRAINT {name} DEFAULT {target_def} FOR {source_column.name}"
+                )
+
+        if constraints:
+            statements.append(source.alter_table_stmt(constraints))
+
+        statements.append(super().mutate_table_stmt(source, target))
+        return join_or_none(statements)
---
+++ packages/pysqlsync/pysqlsync-0.7.0.tar.gz/pysqlsync-0.7.0/pysqlsync/dialect/mssql/object_types.py
@@ -0,0 +1,102 @@
+from typing import Optional
+
+from pysqlsync.formation.object_types import (
+    Column,
+    EnumTable,
+    Namespace,
+    ObjectFactory,
+    Table,
+    join_or_none,
+)
+from pysqlsync.model.data_types import quote
+from pysqlsync.model.id_types import LocalId
+
+
+class MSSQLColumn(Column):
+    @property
+    def data_spec(self) -> str:
+        nullable = " NOT NULL" if not self.nullable else ""
+        identity = " IDENTITY" if self.identity else ""
+        return f"{self.data_type}{nullable}{identity}"
+
+
+class MSSQLTable(Table):
+    def alter_table_stmt(self, statements: list[str]) -> str:
+        return "\n".join(
+            f"ALTER TABLE {self.name} {statement};" for statement in statements
+        )
+
+    def add_constraints_stmt(self) -> Optional[str]:
+        statements: list[str] = []
+
+        constraints: list[str] = []
+        for column in self.columns.values():
+            if column.default is None:
+                continue
+            name = LocalId(f"df_{column.name.local_id}")
+            constraints.append(
+                f"ADD CONSTRAINT {name} DEFAULT {column.default} FOR {column.name}"
+            )
+        if constraints:
+            statements.append(self.alter_table_stmt(constraints))
+
+        if self.table_constraints:
+            statements.append(
+                f"ALTER TABLE {self.name} ADD\n"
+                + ",\n".join(f"CONSTRAINT {c.spec}" for c in self.table_constraints)
+                + ";"
+            )
+
+        return join_or_none(statements)
+
+    def drop_constraints_stmt(self) -> Optional[str]:
+        statements: list[str] = []
+
+        constraints: list[str] = []
+        for column in self.columns.values():
+            if column.default is None:
+                continue
+            name = LocalId(f"df_{column.name.local_id}")
+            constraints.append(f"DROP CONSTRAINT {name}")
+        if constraints:
+            statements.append(self.alter_table_stmt(constraints))
+
+        if self.table_constraints:
+            statements.append(
+                f"ALTER TABLE {self.name} DROP\n"
+                + ",\n".join(f"CONSTRAINT {c.name}" for c in self.table_constraints)
+                + "\n;"
+            )
+
+        return join_or_none(statements)
+
+
+class MSSQLEnumTable(EnumTable, MSSQLTable):
+    pass
+
+
+class MSSQLNamespace(Namespace):
+    def create_schema_stmt(self) -> Optional[str]:
+        if self.name.local_id:
+            # Microsoft SQL Server requires a separate batch for creating a schema
+            return f"IF NOT EXISTS ( SELECT * FROM sys.schemas WHERE name = N{quote(self.name.id)} ) EXEC('CREATE SCHEMA {self.name}');"
+        else:
+            return None
+
+
+class MSSQLObjectFactory(ObjectFactory):
+    @property
+    def column_class(self) -> type[Column]:
+        return MSSQLColumn
+
+    @property
+    def table_class(self) -> type[Table]:
+        return MSSQLTable
+
+    @property
+    def enum_table_class(self) -> type[EnumTable]:
+        return MSSQLEnumTable
+
+    @property
+    def namespace_class(self) -> type[Namespace]:
+        return MSSQLNamespace
