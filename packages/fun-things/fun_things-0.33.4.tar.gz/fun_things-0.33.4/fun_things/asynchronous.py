f45bcb4d47507b1a193f9d939b8b6bc1370ad74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:04:49 +0000

    Add package fun-things/0.33.4/fun_things-0.33.4.tar.gz

---
+++ packages/fun-things/fun_things-0.33.4.tar.gz/fun_things-0.33.4/fun_things/asynchronous.py
@@ -0,0 +1,65 @@
+import asyncio
+import inspect
+from typing import Any, AsyncGenerator, Callable, Generator, Iterable, TypeVar
+
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+
+
+def _subdivide_predicate(value):
+    if isinstance(value, str):
+        return False
+
+    if isinstance(value, bytes):
+        return False
+
+    return isinstance(value, Iterable)
+
+
+async def as_asyncgen(
+    value,
+    subdivide_predicate: Callable[[Any], bool] = _subdivide_predicate,
+):
+    """
+    Calls a function as an async generator.
+
+    Also awaits async functions.
+    """
+    if inspect.isasyncgen(value):
+        # Already an async generator.
+        async for subvalue in value:
+            yield subvalue
+
+        return
+
+    if inspect.isawaitable(value):
+        value = await value
+
+    if subdivide_predicate(value):
+        for subvalue in value:
+            yield subvalue
+
+        return
+
+    yield value
+
+
+async def as_async(value):
+    if inspect.isawaitable(value):
+        value = await value
+
+    return value
+
+
+def as_gen(
+    values: AsyncGenerator[T1, T2],
+) -> Generator[T1, T2, None]:
+    """
+    Converts an async generator to a non-async generator.
+    """
+    while True:
+        try:
+            yield asyncio.run(values.__anext__())  # type: ignore
+
+        except StopAsyncIteration:
+            break
