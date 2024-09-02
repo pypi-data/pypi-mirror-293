0d472a039ee57027731d928ea60f6b80a390a323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:12 +0000

    Add package VisualOP/0.0.5/VisualOP-0.0.5.tar.gz

---
+++ packages/VisualOP/VisualOP-0.0.5.tar.gz/VisualOP-0.0.5/setup.py
@@ -0,0 +1,40 @@
+import codecs
+import os
+import re
+import setuptools
+
+def read(*parts):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    " find version information in Prof.__init__.py "
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+setuptools.setup(
+    name="VisualOP",
+    version=find_version("Prof", "__init__.py"),
+    author="wangm",
+    author_email="2435403746@qq.com",
+    description="Operators Performance Visualization System ",
+    long_description= read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/warming423/pf",
+    packages=setuptools.find_packages(), 
+    license="MIT",
+    python_requires='>=3.8.0',
+    install_requires=["torch>=1.9.0,<2.3",
+                      'paddlepaddle-gpu >= 2.6.1',
+                      "tensorboard >= 2.14.0",
+                      "pandas==2.0.3"], 
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
