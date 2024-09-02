eafd170144d901a38d4f57d8b9ca2676df2e572a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 15:06:05 +0000

    Add package magma-converter/1.8.2/magma_converter-1.8.2-py3-none-any.whl

---
+++ packages/magma-converter/magma_converter-1.8.2-py3-none-any.whl/magma_converter/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from magma_converter.convert import Convert
+from pkg_resources import get_distribution
+
+__version__ = get_distribution("magma-converter").version
+__author__ = "Martanto"
+__author_email__ = "martanto@live.COM"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2024, MAGMA Indonesia"
+__url__ = "https://github.com/martanto/cvghm-multigas"
+
+__all__ = [
+    "Convert",
+]
