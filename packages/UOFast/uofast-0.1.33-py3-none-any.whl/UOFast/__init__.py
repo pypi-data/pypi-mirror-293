54f3e6af1b93bbe074677d442298a7f7ba75889a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 04:05:33 +0000

    Add package UOFast/0.1.33/uofast-0.1.33-py3-none-any.whl

---
+++ packages/UOFast/uofast-0.1.33-py3-none-any.whl/UOFast/__init__.py
@@ -0,0 +1,6 @@
+"""UOFast is a U2 UOPY based restful service which pools U2 UOPY connections and accepts GET/PUT type requests 
+to U2 databases. Its based on FastAPI, gsocketpool, pydantic and uopy. 
+This is an opensource sample, to demonstrate the capabilities of UOPY connectivity using a restful service.
+ #U2 #uopy #sample #python #Unidata #example."""
+__version__ = "0.1.33"
+from UOFast.main import *
