eccf48fc8834c7e70600464ec64f14c795c528f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:19 +0000

    Add package llmlib-amq/0.0.2/llmlib_amq-0.0.2-py3-none-any.whl

---
+++ packages/llmlib-amq/llmlib_amq-0.0.2-py3-none-any.whl/llmlib/__init__.py
@@ -0,0 +1,18 @@
+"""A simple library for making requests to LLMs."""
+
+__version__ = "0.0.2"  # Update this manually when you release a new version
+
+
+# ruff: noqa: F401
+from .core import (
+    LLMClient,
+    Provider,
+    OpenAIModel,
+    AnthropicModel,
+    Model,
+    Role,
+    Message,
+    LLMResponse,
+    process_and_collect_stream,
+    print_stream,
+)
