2a5b358659773bb96ac8d303f3264170e8b69482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 04:05:33 +0000

    Add package mrjsonstore/0.1.0/mrjsonstore-0.1.0.tar.gz

---
+++ packages/mrjsonstore/mrjsonstore-0.1.0.tar.gz/mrjsonstore-0.1.0/mrjsonstore/json_store.py
@@ -0,0 +1,33 @@
+# Copyright 2024 Ole Kliemann
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import json
+from result import as_result
+from mrjsonstore.handle import Handle
+
+class JsonStore:
+    @staticmethod
+    @as_result(
+        FileNotFoundError,
+        PermissionError,
+        IsADirectoryError,
+        OSError,
+        UnicodeDecodeError,
+        json.JSONDecodeError
+    )
+    def make(filename):
+        return JsonStore(filename)
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.content = {}
+        if os.path.exists(self.filename):
+          with open(self.filename) as f:
+              self.content = json.loads(f.read())
+
+    def __call__(self):
+        return Handle(self, False)
+
+    def transaction(self):
+        return Handle(self, True)
