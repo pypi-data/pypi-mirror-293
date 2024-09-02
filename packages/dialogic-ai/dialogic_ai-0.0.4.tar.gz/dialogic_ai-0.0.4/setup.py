a8b771ecc189353090bcf1577353a89af36a4533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:14 +0000

    Add package dialogic-ai/0.0.4/dialogic_ai-0.0.4.tar.gz

---
+++ packages/dialogic-ai/dialogic_ai-0.0.4.tar.gz/dialogic_ai-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dialogic-ai',
+    version='0.0.4',
+    packages=find_packages(include=['dialogic_ai', 'dialogic_ai.*']),
+    install_requires=[
+    ],
+    extras_require={
+        'dev': [
+            "pytest>=8.3.2",
+        "black>=24.8.0"
+        ],
+    },  
+    include_package_data=True,
+    license='MIT',
+    description="A Python package designed to manage complex, threaded conversations with branching capabilities, much like version control systems manage code branches.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/endybits/dialogic-ai",
+    author="Endy Bermudez Rodriguez",
+    author_email="endyb.dev@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    keywords="conversational AI, chat, chatbot, threaded conversations, AI, NLP, branching dialogues, dialogicai, dialogic-ai", 
+)
