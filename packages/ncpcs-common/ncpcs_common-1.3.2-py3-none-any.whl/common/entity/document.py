016fee6621a8e227cbf2eee26abfc8255cd2f27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:00 +0000

    Add package ncpcs-common/1.3.2/ncpcs_common-1.3.2-py3-none-any.whl

---
+++ packages/ncpcs-common/ncpcs_common-1.3.2-py3-none-any.whl/common/entity/document.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+from common.entity.timeline import Timeline
+
+
+@dataclass
+class Document:
+    uuid: str
+    medical_institution_code: str
+    medical_record_no: str
+    discharge_time: str
+    table_name: str
+    column_name: str
+    table_uuid: str
+    page: int
+    page_uuid: str
+    start_index: int
+    end_index: int
+    content: str
+    timeline: Timeline = None
