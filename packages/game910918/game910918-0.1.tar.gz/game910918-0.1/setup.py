a93f2edc37936bde4e3e56b324e9b7c1f87a8407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:06 +0000

    Add package game910918/0.1/game910918-0.1.tar.gz

---
+++ packages/game910918/game910918-0.1.tar.gz/game910918-0.1/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='game910918',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        '',
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A description of your library',
+    url='https://github.com/yourusername/game910918',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+    
