e4d15c86f56950b23e89e6e6603f273a2fe3f84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 14:05:21 +0000

    Add package empiricaldist/0.7.5/empiricaldist-0.7.5.tar.gz

---
+++ packages/empiricaldist/empiricaldist-0.7.5.tar.gz/empiricaldist-0.7.5/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+
+def readme():
+    try:
+        with open("README.rst") as f:
+            return f.read()
+    except IOError:
+        return ""
+
+
+setup(
+    name="empiricaldist",
+    version="0.7.5",
+    author="Allen B. Downey",
+    author_email="downey@allendowney.com",
+    packages=["empiricaldist"],
+    requires=["matplotlib", "numpy", "pandas", "scipy"],
+    url="https://github.com/AllenDowney/empiricaldist",
+    license="BSD-3-Clause",
+    description="Python library that represents empirical distributions.",
+    long_description=readme(),
+)
