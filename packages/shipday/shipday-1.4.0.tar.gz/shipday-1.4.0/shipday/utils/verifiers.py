e0dc356b72f30091e803bf674f330007301bdb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 04:05:33 +0000

    Add package shipday/1.4.0/shipday-1.4.0.tar.gz

---
+++ packages/shipday/shipday-1.4.0.tar.gz/shipday-1.4.0/shipday/utils/verifiers.py
@@ -0,0 +1,35 @@
+from shipday.exeptions import ShipdayException
+
+
+def verify_none_or_instance_of(obj_type, variable, error_message):
+    if type(obj_type) is not list:
+        obj_type = [obj_type]
+    if variable is None:
+        return
+    if type(variable) not in obj_type:
+        raise ShipdayException(error_message)
+
+
+def verify_instance_of(obj_type, variable, error_message):
+    if type(obj_type) is not list:
+        obj_type = [obj_type]
+    if type(variable) not in obj_type:
+        raise ShipdayException(error_message)
+
+
+def verify_not_negative(number, error_message):
+    verify_instance_of([int, float], number, error_message)
+    if number < 0:
+        raise ShipdayException(error_message)
+
+
+def verify_none_or_not_negative(number, error_message):
+    verify_none_or_instance_of([int, float], number, error_message)
+
+    if number is not None and number < 0:
+        raise ShipdayException(error_message)
+
+def verify_within_range(number, min_value, max_value, error_message):
+    verify_instance_of([int, float], number, error_message)
+    if number <= min_value or number >= max_value:
+        raise ShipdayException(error_message)
