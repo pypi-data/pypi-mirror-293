def95e0c01014d010992ea3a6ce459c5268c0f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 15:05:45 +0000

    Add package openmmla-audio/0.1.4.post5/openmmla_audio-0.1.4.post5-py3-none-any.whl

---
+++ packages/openmmla-audio/openmmla_audio-0.1.4.post5-py3-none-any.whl/openmmla_audio/utils/os_utils.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+
+def clear_directory(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
+
+
+def safe_delete_file(file_path):
+    """Safely deletes a file, ensuring it exists first."""
+    if os.path.exists(file_path):
+        os.remove(file_path)
