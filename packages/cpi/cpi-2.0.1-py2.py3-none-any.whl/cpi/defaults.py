8c8acc2a1792f57019ca44630e9d110617de331e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 04:05:17 +0000

    Add package cpi/2.0.1/cpi-2.0.1-py2.py3-none-any.whl

---
+++ packages/cpi/cpi-2.0.1-py2.py3-none-any.whl/cpi/defaults.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+"""
+Default values.
+"""
+
+DEFAULT_SERIES_ID = "CUUR0000SA0"
+DEFAULTS_SERIES_ATTRS = {
+    "survey": "All urban consumers",
+    "seasonally_adjusted": False,
+    "periodicity": "Monthly",
+    "area": "U.S. city average",
+    "items": "All items",
+}
