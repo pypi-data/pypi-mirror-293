3f8d34b047a17df058ab705a809728ffde5196a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 03:06:30 +0000

    Add package PyMemoryR-W/0.1.0.1/pymemoryr_w-0.1.0.1.tar.gz

---
+++ packages/PyMemoryR-W/pymemoryr_w-0.1.0.1.tar.gz/pymemoryr_w-0.1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PyMemoryR-W',
+    version='0.1.0.1',
+    author='Mahd Hasan Shuvo',
+    author_email='shuvobbhh@gmail.com',
+    description='A library for manipulating memory in a Windows process',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Ensure this matches your file format
+    url='https://github.com/Mahdi-hasan-shuvo/PyMemoryRW',
+    packages=find_packages(include=['mahdix', 'pymem', 'ctypes', 'pyinjector']),
+    install_requires=[
+        'pywin32',
+        'pyinjector',
+        'pymem',
+        'ctypes',
+        'mahdix',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
