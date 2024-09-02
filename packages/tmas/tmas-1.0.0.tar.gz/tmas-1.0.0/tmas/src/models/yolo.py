13e0d53fc8505aa6825b249dcd22d0efea624e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 15:06:01 +0000

    Add package tmas/1.0.0/tmas-1.0.0.tar.gz

---
+++ packages/tmas/tmas-1.0.0.tar.gz/tmas-1.0.0/tmas/src/models/yolo.py
@@ -0,0 +1,15 @@
+# tmas/tmas/models/yolo.py
+import os
+from ultralytics import YOLO
+
+class YOLOv8:
+    def __init__(self, weights="tmas/src/models/best_model_yolo.pt"):
+        if not os.path.exists(weights):
+            raise FileNotFoundError(f"Model file {weights} not found. Please ensure the model file is in the correct path.")
+        
+        # Load the YOLOv8 model
+        self.model = YOLO(weights)
+
+    def predict(self, img):
+        # Predict using the loaded model
+        return self.model(img)
