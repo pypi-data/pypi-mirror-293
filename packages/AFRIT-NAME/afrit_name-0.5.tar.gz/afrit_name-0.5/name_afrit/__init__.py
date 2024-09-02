5cc15c3590a74edaaa6007ddc51dbee9846ad9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-02 04:05:18 +0000

    Add package AFRIT-NAME/0.5/afrit_name-0.5.tar.gz

---
+++ packages/AFRIT-NAME/afrit_name-0.5.tar.gz/afrit_name-0.5/name_afrit/__init__.py
@@ -0,0 +1,19 @@
+
+#المشكله بعفريتون
+#مايكعد راحه
+#تمتع بلمكتبه
+#مكتبة اسماء عربيه
+#ويابانية
+from .OS import fers_fers_name
+from .OS import lsit_name
+from .OS import fers_name
+from .OS import fol2_name
+from .OS import fol_name
+from .OS import fers_fers_name_ja
+from .OS import lsit_name_ja
+from .OS import fers_name_ja
+from .OS import fol2_name_ja
+from .OS import fol_name_ja
+from .AFRI import AFREncrypt
+from .AFRI import get_x_bogus
+from .AFRI import Signer
