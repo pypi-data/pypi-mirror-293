165e464b88e9ea7b019136ff1f4822a73249e7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:07:08 +0000

    Add package payme-drf/1.0.0/payme_drf-1.0.0.tar.gz

---
+++ packages/payme-drf/payme_drf-1.0.0.tar.gz/payme_drf-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="payme-drf",
+    version="1.0.0",
+    author="Islombek Normamatov",
+    author_email="islomjon2702@gmail.com",
+    description="payme-drf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.5",
+    install_requires=['requests', 'django', 'djangorestframework'],
+    url="https://github.com/islombeknv/payme-drf",
+    packages=setuptools.find_packages(exclude=['settings']),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
