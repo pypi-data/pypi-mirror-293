76475bd4e340b8382a96882b6cb06124bbed4ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:05:55 +0000

    Add package mltrain/0.1.0/mltrain-0.1.0.tar.gz

---
+++ packages/mltrain/mltrain-0.1.0.tar.gz/mltrain-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    description = f.read()
+
+setup(
+    name='mltrain',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'tensorflow'
+    ],
+    long_description=description,
+    long_description_content_type='text/markdown'
+)
