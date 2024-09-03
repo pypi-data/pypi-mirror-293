c8edd485abf8866ca8efb35fb6b9fea6717cc39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:06:03 +0000

    Add package torch-attentions/0.1.4/torch_attentions-0.1.4.tar.gz

---
+++ packages/torch-attentions/torch_attentions-0.1.4.tar.gz/torch_attentions-0.1.4/torch_attentions/channel.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class ChannelAttention(nn.Module):
+    def __init__(self, channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        
+        self.fc = nn.Sequential(
+            nn.Linear(channels, channels // reduction_ratio, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channels // reduction_ratio, channels, bias=False)
+        )
+        
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        b, c, _, _ = x.size()
+        
+        avg_out = self.fc(self.avg_pool(x).view(b, c))
+        max_out = self.fc(self.max_pool(x).view(b, c))
+        
+        out = avg_out + max_out
+        return x * self.sigmoid(out).view(b, c, 1, 1)
