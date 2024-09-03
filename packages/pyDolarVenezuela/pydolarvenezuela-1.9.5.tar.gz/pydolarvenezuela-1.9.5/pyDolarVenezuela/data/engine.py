502010aae1e924d0c4b45920e37b7bb2ba440607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:06:54 +0000

    Add package pyDolarVenezuela/1.9.5/pydolarvenezuela-1.9.5.tar.gz

---
+++ packages/pyDolarVenezuela/pydolarvenezuela-1.9.5.tar.gz/pydolarvenezuela-1.9.5/pyDolarVenezuela/data/engine.py
@@ -0,0 +1,24 @@
+from typing import Union
+from sqlalchemy import create_engine
+from .models import Base
+from ..models import Database, LocalDatabase
+
+# https://github.com/orgs/supabase/discussions/27071 Using SQLAlchemy with Supabase
+def get_connection(connection: Union[Database, LocalDatabase]):
+    """
+    Obtiene un motor de base de datos (engine) de SQLAlchemy según el tipo de conexión proporcionada.
+    """
+    if isinstance(connection, Database):
+        return create_engine(f'{connection.motor}://{connection.user}:{connection.password}@{connection.host}:{connection.port}/{connection.database}',
+                            connect_args={'options': '-c timezone=utc'})
+    elif isinstance(connection, LocalDatabase):
+        return create_engine(f'{connection.motor}:///{connection.url}')
+
+def create_tables(engine):
+    """
+    Crea todas las tablas definidas en los modelos utilizando el motor de base de datos proporcionado.
+    """
+    try:
+        Base.metadata.create_all(engine)
+    except Exception as e:
+        raise e
