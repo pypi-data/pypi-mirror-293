603f622d108b0ff5aa797d33f6b11c03cad72118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:06:18 +0000

    Add package systemdcli/0.1.1/systemdcli-0.1.1.tar.gz

---
+++ packages/systemdcli/systemdcli-0.1.1.tar.gz/systemdcli-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='systemdcli',
+    version='0.1.1',
+    py_modules=['systemdcli'],
+    install_requires=['cleo', ],
+    package_dir={"": "src"},
+    entry_points={
+        'console_scripts': [
+            'systemdcli = systemdcli:main'
+        ]
+    }
+)
