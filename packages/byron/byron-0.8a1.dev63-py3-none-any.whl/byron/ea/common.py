d1600d5d53643d7348dad42d57557e3e461890c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:05:25 +0000

    Add package byron/0.8a1.dev63/byron-0.8a1.dev63-py3-none-any.whl

---
+++ packages/byron/byron-0.8a1.dev63-py3-none-any.whl/byron/ea/common.py
@@ -0,0 +1,39 @@
+###################################|###|####################################
+#   _____                          |   |                                   #
+#  |  __ \--.--.----.-----.-----.  |===|  This file is part of Byron, an   #
+#  |  __ <  |  |   _|  _  |     |  |___|  evolutionary source-code fuzzer. #
+#  |____/ ___  |__| |_____|__|__|   ).(   Version 0.8a1 "Don Juan"         #
+#        |_____|                    \|/                                    #
+#################################### ' #####################################
+
+# Copyright 2023-24 Giovanni Squillero and Alberto Tonda
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# =[ HISTORY ]===============================================================
+# v1 / April 2023 / Squillero (GX)
+
+__all__ = ["take_operators"]
+
+from typing import Callable
+
+from byron.sys import get_operators
+
+
+def take_operators(no_parents: bool, operators_list: list[Callable] = None) -> list[Callable]:
+    if operators_list is None or (flag := all((op.num_parents is not None) != no_parents for op in operators_list)):
+        ops = [op for op in get_operators() if (op.num_parents is None) == no_parents]
+    elif not flag:
+        ops = [op for op in operators_list if (op.num_parents is None) == no_parents]
+    return ops
