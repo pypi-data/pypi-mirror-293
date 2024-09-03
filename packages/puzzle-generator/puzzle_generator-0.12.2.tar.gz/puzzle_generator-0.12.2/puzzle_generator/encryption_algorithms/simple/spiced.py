a37475301394e33a61faab5935a36e31e074ebad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 05:05:24 +0000

    Add package puzzle-generator/0.12.2/puzzle_generator-0.12.2.tar.gz

---
+++ packages/puzzle-generator/puzzle_generator-0.12.2.tar.gz/puzzle_generator-0.12.2/puzzle_generator/encryption_algorithms/simple/spiced.py
@@ -0,0 +1,62 @@
+import typing
+import secrets
+
+from .common import (
+    derive_key,
+    xor_bytes,
+    sign_bytes,
+    digest_size,
+    merge_data_and_signature,
+    split_data_and_signature,
+)
+
+
+def get_encrypt(
+    proc_spices: typing.List[bytes],
+    signature_spices: typing.List[bytes],
+    scrypt_params,
+    signature_params,
+) -> typing.Callable[[bytes, bytes], bytes]:
+    assert proc_spices  # nosec B101
+    assert signature_spices  # nosec B101
+
+    def _encrypt(in_bytes: bytes, in_pass: bytes) -> bytes:
+        signature_spice = secrets.choice(signature_spices)
+        signature = sign_bytes(in_bytes + signature_spice, in_pass, signature_params)
+        merged = merge_data_and_signature(in_bytes, signature)
+        proc_spice = secrets.choice(proc_spices)
+        key = derive_key(
+            password=in_pass + proc_spice, dklen=len(merged), **scrypt_params
+        )
+        return xor_bytes(merged, key)
+
+    return _encrypt
+
+
+def get_decrypt(
+    proc_spices: typing.List[bytes],
+    signature_spices: typing.List[bytes],
+    scrypt_params,
+    signature_params,
+) -> typing.Callable[[bytes, bytes], bytes | None]:
+    assert proc_spices  # nosec B101
+    assert signature_spices  # nosec B101
+
+    def _decrypt(in_bytes: bytes, in_pass: bytes) -> bytes | None:
+        for proc_spice in proc_spices:
+            key = derive_key(
+                password=in_pass + proc_spice, dklen=len(in_bytes), **scrypt_params
+            )
+            data = xor_bytes(in_bytes, key)
+            decrypted, signature = split_data_and_signature(
+                data, digest_size(signature_params)
+            )
+
+            if any(
+                sign_bytes(decrypted + _, in_pass, signature_params) == signature
+                for _ in signature_spices
+            ):
+                return decrypted
+        return None
+
+    return _decrypt
