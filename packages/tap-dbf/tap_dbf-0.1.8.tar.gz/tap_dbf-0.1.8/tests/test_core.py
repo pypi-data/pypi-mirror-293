87c17c358b2ccc1f8c5b427a9c495e28867f683d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 05:05:49 +0000

    Add package tap-dbf/0.1.8/tap_dbf-0.1.8.tar.gz

---
+++ packages/tap-dbf/tap_dbf-0.1.8.tar.gz/tap_dbf-0.1.8/tests/test_core.py
@@ -0,0 +1,24 @@
+"""Test tap-dbf."""
+
+from __future__ import annotations
+
+from singer_sdk.testing import get_tap_test_class
+
+from tap_dbf.tap import TapDBF
+
+TestTapDBF = get_tap_test_class(
+    TapDBF,
+    config={
+        "path": "/files/*.dbf",
+        "fs_root": "file://tests/data",
+        "ignore_missing_memofile": True,
+    },
+)
+
+TestTapDBFNoRoot = get_tap_test_class(
+    TapDBF,
+    config={
+        "path": "tests/data/files/*.dbf",
+        "ignore_missing_memofile": True,
+    },
+)
