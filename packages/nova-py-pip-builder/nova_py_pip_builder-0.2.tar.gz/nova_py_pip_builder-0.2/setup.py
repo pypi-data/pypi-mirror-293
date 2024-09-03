e2ed0de3606202a58db14e00b400ea11e1f4be5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:05:47 +0000

    Add package nova-py-pip-builder/0.2/nova_py_pip_builder-0.2.tar.gz

---
+++ packages/nova-py-pip-builder/nova_py_pip_builder-0.2.tar.gz/nova_py_pip_builder-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='nova_py_pip_builder',
+    version='0.2',
+    packages=['PIPBUILDER'],
+    description='Module pour exécuter un fichier .exe',
+    author='By IMCSX BBY',
+    author_email='booking.biremont@gmail.com',
+)
