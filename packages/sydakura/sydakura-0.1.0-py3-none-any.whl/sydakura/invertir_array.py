33e27d600b95151120112e3b02f56a053510588e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:06:52 +0000

    Add package sydakura/0.1.0/sydakura-0.1.0-py3-none-any.whl

---
+++ packages/sydakura/sydakura-0.1.0-py3-none-any.whl/sydakura/invertir_array.py
@@ -0,0 +1,9 @@
+def reverse_array(arr):
+    return arr[::-1]
+
+if __name__ == "__main__":
+    # Ejemplo de uso
+    A = [1, 2, 3]
+    print(reverse_array(A))  # Debería imprimir: [3, 2, 1]
+
+print ("sydakura")
