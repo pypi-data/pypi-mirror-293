2ee268baf0cdebcf1a05f24bc24e3555fe41a3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 05:04:57 +0000

    Add package tessellation/0.0.5/tessellation-0.0.5-py3-none-any.whl

---
+++ packages/tessellation/tessellation-0.0.5-py3-none-any.whl/tessellation/procgen/action.py
@@ -0,0 +1,17 @@
+from enum import Enum, auto
+
+
+class Action(Enum):
+    """Enum representing the possible actions for the generator."""
+
+    UP = auto()
+    UP_RIGHT = auto()
+    # UP_LEFT = auto()
+    DOWN = auto()
+    DOWN_RIGHT = auto()
+    # DOWN_LEFT = auto()
+    RIGHT = auto()
+    # LEFT = auto()
+
+
+ALL_ACTIONS = [action for action in Action]
