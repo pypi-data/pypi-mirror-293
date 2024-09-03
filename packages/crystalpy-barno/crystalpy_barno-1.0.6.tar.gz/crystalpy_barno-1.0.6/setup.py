b621acf3d0ba98a9664839aaec9ce75d8a87d510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:05:35 +0000

    Add package crystalpy-barno/1.0.6/crystalpy_barno-1.0.6.tar.gz

---
+++ packages/crystalpy-barno/crystalpy_barno-1.0.6.tar.gz/crystalpy_barno-1.0.6/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="crystalpy_barno",
+    version="1.0.6",
+    description="Python integration with crystal report",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="barno1994",
+    author_email="barno.baptu@gmail.com",
+    maintainer="barno.baptu",
+    maintainer_email="barno.baptu@gmail.com",
+    url="https://github.com/barno1994/RPT_PY",
+    project_urls={
+        "Homepage": "https://github.com/barno1994/RPT_PY",
+        "Issues": "https://github.com/barno1994/RPT_PY/issues",
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords=["crystal report", "python integration"],
+    python_requires=">=3.6",
+    license="MIT",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),  # Adjusted to find all packages under 'src'
+    include_package_data=True,
+    package_data={
+        "crystalpy_barno_barno.baptu": ["ReportsClasses/CR/*", "ReportsClasses/Helper/*", 
+                                        "ReportsClasses/Sales/*", "ReportsClasses/Stock/*", 
+                                        "rpt/*"],  # Updated to reflect correct package data structure
+    },
+    install_requires=[],  # Add required dependencies here if any
+    extras_require={
+        "dev": [
+            "cffi==1.17.0",
+            "clr-loader==0.2.6",
+            "pycparser==2.22",
+            "pythonnet==3.0.3",
+        ],
+    },
+)
