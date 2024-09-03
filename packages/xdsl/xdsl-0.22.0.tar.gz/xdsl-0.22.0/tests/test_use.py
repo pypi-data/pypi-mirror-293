25c3e3037c6b1d10f09e620724879d9e1a3f8c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:06:34 +0000

    Add package xdsl/0.22.0/xdsl-0.22.0.tar.gz

---
+++ packages/xdsl/xdsl-0.22.0.tar.gz/xdsl-0.22.0/tests/test_use.py
@@ -0,0 +1,30 @@
+from xdsl.context import MLContext
+from xdsl.dialects.builtin import Builtin, ModuleOp
+from xdsl.dialects.test import Test
+from xdsl.ir import Use
+from xdsl.parser import Parser
+
+test_prog = """
+"builtin.module"() ({
+  %0 = "test.op"() : () -> i32
+  %1 = "test.op"(%0, %0) : (i32, i32) -> i32
+}) : () -> ()
+"""
+
+
+def test_main():
+    ctx = MLContext()
+    ctx.load_dialect(Builtin)
+    ctx.load_dialect(Test)
+
+    parser = Parser(ctx, test_prog)
+    module = parser.parse_op()
+
+    module.verify()
+    assert isinstance(module, ModuleOp)
+
+    op1, op2 = list(module.ops)
+    assert op1.results[0].uses == {Use(op2, 0), Use(op2, 1)}
+    assert op2.results[0].uses == set()
+
+    print("Done")
