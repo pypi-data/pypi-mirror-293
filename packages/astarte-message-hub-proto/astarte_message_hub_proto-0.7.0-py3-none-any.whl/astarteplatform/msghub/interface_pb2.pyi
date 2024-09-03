31fa0c85d2b07726648700539ca517fa867e01be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:05:43 +0000

    Add package astarte-message-hub-proto/0.7.0/astarte_message_hub_proto-0.7.0-py3-none-any.whl

---
+++ packages/astarte-message-hub-proto/astarte_message_hub_proto-0.7.0-py3-none-any.whl/astarteplatform/msghub/interface_pb2.pyi
@@ -0,0 +1,18 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class InterfacesJson(_message.Message):
+    __slots__ = ("interfaces_json",)
+    INTERFACES_JSON_FIELD_NUMBER: _ClassVar[int]
+    interfaces_json: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, interfaces_json: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class InterfacesName(_message.Message):
+    __slots__ = ("names",)
+    NAMES_FIELD_NUMBER: _ClassVar[int]
+    names: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, names: _Optional[_Iterable[str]] = ...) -> None: ...
