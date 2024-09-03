d4814eaae99b8c8da632095bfd4e4338c4d944a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 05:05:14 +0000

    Add package geospaitial-lab-aviary/0.2.0/geospaitial_lab_aviary-0.2.0.tar.gz

---
+++ packages/geospaitial-lab-aviary/geospaitial_lab_aviary-0.2.0.tar.gz/geospaitial_lab_aviary-0.2.0/aviary/_functional/data/dataset.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import numpy.typing as npt
+
+if TYPE_CHECKING:
+    # noinspection PyProtectedMember
+    from aviary._utils.types import (
+        Coordinate,
+        CoordinatesSet,
+    )
+    from aviary.data.data_fetcher import DataFetcher
+    from aviary.data.data_preprocessor import DataPreprocessor
+
+
+def get_item(
+    data_fetcher: DataFetcher,
+    data_preprocessor: DataPreprocessor,
+    coordinates: CoordinatesSet,
+    index: int,
+) -> tuple[npt.NDArray, Coordinate, Coordinate]:
+    """Returns the sample.
+
+    Parameters:
+        data_fetcher: data fetcher
+        data_preprocessor: data preprocessor
+        coordinates: coordinates (x_min, y_min) of each tile
+        index: index of the tile
+
+    Returns:
+        sample
+    """
+    x_min, y_min = coordinates[index]
+    data = data_fetcher(
+        x_min=x_min,
+        y_min=y_min,
+    )
+    data = data_preprocessor(
+        data=data,
+    )
+    return data, x_min, y_min
+
+
+def get_length(
+    coordinates: CoordinatesSet,
+) -> int:
+    """Computes the number of samples.
+
+    Parameters:
+        coordinates: coordinates (x_min, y_min) of each tile
+
+    Returns:
+        number of samples
+    """
+    return len(coordinates)
