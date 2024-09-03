1ff7533cedaa84d0a9c58506ff646ea57cff8cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:05:47 +0000

    Add package docx-python/0.1.0/docx_python-0.1.0-py3-none-any.whl

---
+++ packages/docx-python/docx_python-0.1.0-py3-none-any.whl/docx/__init__.py
@@ -0,0 +1,27 @@
+from docx import (
+    abstract,
+    client,
+    constants,
+    error,
+    resources,
+    utils,
+    types,
+)
+
+from docx.client import Docx, Client
+from docx.version import VERSION
+
+version = VERSION
+
+__all__ = [
+    "abstract",
+    "client",
+    "constants",
+    "error",
+    "resources",
+    "utils",
+    "types",
+    "Docx",
+    "Client"
+    "version"
+]
