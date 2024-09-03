67c75529b52cc1b9e4f73c796a2c0a23d8bbb6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:06:43 +0000

    Add package fsl-pipe/1.1.0/fsl_pipe-1.1.0-py3-none-any.whl

---
+++ packages/fsl-pipe/fsl_pipe-1.1.0-py3-none-any.whl/fsl_pipe/__init__.py
@@ -0,0 +1,18 @@
+"""
+Declarative pipeline definition based on filetrees.
+
+Typical usage:
+
+.. code-block:: python
+
+    from fsl_pipe import pipe, In, Out, Ref, Var
+
+    @pipe
+    def job(input_file: In, output_file: Out):
+        # code to convert `input_file` to `output_file`
+
+    pipe.cli()  # runs command line interface
+"""
+from .pipeline import Pipeline, pipe, In, Out, Ref, Var
+from .job import update_closure
+__version__ = '1.1.0'
