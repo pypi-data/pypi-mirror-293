c34cc7c595ccd4513f12cc49507a7d125478ffa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:05:15 +0000

    Add package tempuscator/0.3.0a4/tempuscator-0.3.0a4-py3-none-any.whl

---
+++ packages/tempuscator/tempuscator-0.3.0a4-py3-none-any.whl/tempuscator/exceptions.py
@@ -0,0 +1,40 @@
+class DirectoryNotEmpty(Exception):
+    """
+    Directory not empty exception
+    """
+
+
+class BackupFileCorrupt(Exception):
+    """
+    Backup file corrupt exception
+    """
+
+
+class MysqldNotRunning(Exception):
+    """
+    Mysqld process not running exception
+    """
+
+
+class MysqlAccessDeniend(Exception):
+    """
+    Mysql access deniend exception
+    """
+
+
+class BackupCreateError(Exception):
+    """
+    Exception for failed backup create
+    """
+
+
+class MyCnfConfigError(Exception):
+    """
+    Mysql user config exception
+    """
+
+
+class MissingConfigSection(Exception):
+    """
+    Exception for missing config section in ini file
+    """
