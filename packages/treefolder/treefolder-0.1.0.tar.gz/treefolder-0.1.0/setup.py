585685959a4f6a230c1c6c1377a7481626be3156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:05:32 +0000

    Add package treefolder/0.1.0/treefolder-0.1.0.tar.gz

---
+++ packages/treefolder/treefolder-0.1.0.tar.gz/treefolder-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='treefolder',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'tkinter',
+    ],
+    entry_points={
+        'console_scripts': [
+            'treefolder=treefolder.tree:main',
+        ],
+    },
+    author='Alberto Perez Ortega',
+    author_email='apo0106@gmail.com',
+    description='A simple Python program that generates an ASCII representation of a directory tree.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Aperezortega/Treefolder',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
