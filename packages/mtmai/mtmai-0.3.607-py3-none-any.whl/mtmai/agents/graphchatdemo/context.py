55b72987ca7bbade33025205f4c97e1cd9d4f9f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:04:56 +0000

    Add package mtmai/0.3.607/mtmai-0.3.607-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.607-py3-none-any.whl/mtmai/agents/graphchatdemo/context.py
@@ -0,0 +1,28 @@
+from contextlib import contextmanager
+
+import httpx
+from langchain_core.pydantic_v1 import BaseModel
+from langchain_core.runnables import RunnableConfig
+from sqlalchemy import Engine
+from sqlmodel import Session
+
+from mtmai.core.db import getdb
+
+
+class AgentContext(BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+
+    httpx_session: httpx.Client
+    db: Engine
+    session: Session
+
+
+@contextmanager
+def make_agent_context(config: RunnableConfig):
+    session = httpx.Client()
+    db = getdb()
+    try:
+        yield AgentContext(httpx_session=session, db=db, session=Session(db))
+    finally:
+        session.close()
