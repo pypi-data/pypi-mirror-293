95b860423b2a6fe7029943ec6c8c13f1464e13c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:06:18 +0000

    Add package ytnoti/2.1.0/ytnoti-2.1.0-py3-none-any.whl

---
+++ packages/ytnoti/ytnoti-2.1.0-py3-none-any.whl/ytnoti/enums.py
@@ -0,0 +1,28 @@
+"""Defines Enum classes used in the package."""
+
+__all__ = ["NotificationKind", "ServerMode"]
+
+from enum import Enum
+
+
+class NotificationKind(Enum):
+    """Enum for the kind of notification."""
+
+    UPLOAD = 0
+    """Upload notification"""
+
+    EDIT = 1
+    """Edit notification"""
+
+    ANY = 2
+    """Any notification"""
+
+
+class ServerMode(Enum):
+    """Enum for the server mode."""
+
+    RUN = "run"
+    """Create an event loop and run the server"""
+
+    SERVE = "serve"
+    """Serve the server in the existing event loop"""
