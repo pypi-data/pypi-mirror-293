bc8e0de2a9a4b681d7db3d782dc1c9458dc569c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:05:43 +0000

    Add package gzhlaker/0.0.4/gzhlaker-0.0.4.tar.gz

---
+++ packages/gzhlaker/gzhlaker-0.0.4.tar.gz/gzhlaker-0.0.4/gzhlaker/oss.py
@@ -0,0 +1,49 @@
+import os
+import pickle
+import oss2
+from oss2.credentials import EnvironmentVariableCredentialsProvider
+
+def check_and_upload(source_path, target_path, file_list):
+    if file_list is []:
+        return 
+    
+    auth = oss2.ProviderAuthV4(EnvironmentVariableCredentialsProvider())
+    bucket = oss2.Bucket(auth, 'https://oss-cn-huhehaote.aliyuncs.com', 'gzhlaker-experiment', region="cn-huhehaote")
+    
+    for file_name, file_description in file_list:
+        
+        oss_path = os.path.join(target_path, file_name)
+        local_path = os.path.join(source_path, file_name)
+        readme_path = os.path.join(target_path, "readme.txt")
+        
+        resule_position = 0
+        
+        oss_exists = bucket.object_exists(oss_path)
+        local_exists = os.path.exists(local_path)
+        
+        if local_exists and not oss_exists:
+            bucket.put_object_from_file(oss_path, local_path)
+            resule = bucket.append_object(readme_path, resule_position, f"{file_name}:{file_description}\n")
+            resule_position = resule.next_position
+        elif oss_exists and not local_exists:
+            bucket.get_object_to_file(oss_path, local_path)
+        elif not oss_exists and not local_exists:
+            print("无可用文件")
+
+def get_file_name(path):
+  """
+  获取一个文件夹下的所有名称
+  """
+  files = []
+  for file in os.listdir(path):
+      files.append(file)
+  return files
+
+def check_path(path):
+    if not os.path.exists(path):
+        os.system(f"mkdir -p {path}")
+
+
+def save_pickle(path, target):
+    with open("path", mode="wb") as f:
+        pickle.dump(target, f)
