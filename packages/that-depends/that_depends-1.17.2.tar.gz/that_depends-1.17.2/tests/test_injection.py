397cab81dfb4c6d026ef071f936e48bf7e29a56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 05:05:22 +0000

    Add package that-depends/1.17.2/that_depends-1.17.2.tar.gz

---
+++ packages/that-depends/that_depends-1.17.2.tar.gz/that_depends-1.17.2/tests/test_injection.py
@@ -0,0 +1,89 @@
+import asyncio
+import datetime
+import warnings
+
+import pytest
+
+from tests import container
+from that_depends import Provide, inject
+
+
+@pytest.fixture(name="fixture_one")
+def create_fixture_one() -> int:
+    return 1
+
+
+@inject
+async def test_injection(
+    fixture_one: int,
+    simple_factory: container.SimpleFactory = Provide[container.DIContainer.simple_factory],
+    dependent_factory: container.DependentFactory = Provide[container.DIContainer.dependent_factory],
+    default_zero: int = 0,
+) -> None:
+    assert simple_factory.dep1
+    assert isinstance(dependent_factory.async_resource, datetime.datetime)
+    assert default_zero == 0
+    assert fixture_one == 1
+
+
+async def test_wrong_injection() -> None:
+    @inject
+    async def inner(
+        _: container.SimpleFactory = Provide[container.DIContainer.simple_factory],
+    ) -> None:
+        """Do nothing."""
+
+    with pytest.raises(RuntimeError, match="Injected arguments must not be redefined"):
+        await inner(_=container.SimpleFactory(dep1="1", dep2=2))
+
+
+async def test_empty_injection() -> None:
+    @inject
+    async def inner(_: int) -> None:
+        """Do nothing."""
+
+    warnings.filterwarnings("error")
+
+    with pytest.raises(RuntimeWarning, match="Expected injection, but nothing found. Remove @inject decorator."):
+        await inner(1)
+
+
+@inject
+def test_sync_injection(
+    fixture_one: int,
+    simple_factory: container.SimpleFactory = Provide[container.DIContainer.simple_factory],
+    default_zero: int = 0,
+) -> None:
+    assert simple_factory.dep1
+    assert default_zero == 0
+    assert fixture_one == 1
+
+
+def test_wrong_sync_injection() -> None:
+    @inject
+    def inner(
+        _: container.SimpleFactory = Provide[container.DIContainer.simple_factory],
+    ) -> None:
+        """Do nothing."""
+
+    with pytest.raises(RuntimeError, match="Injected arguments must not be redefined"):
+        inner(_=container.SimpleFactory(dep1="1", dep2=2))
+
+
+def test_sync_empty_injection() -> None:
+    @inject
+    def inner(_: int) -> None:
+        """Do nothing."""
+
+    warnings.filterwarnings("error")
+
+    with pytest.raises(RuntimeWarning, match="Expected injection, but nothing found. Remove @inject decorator."):
+        inner(1)
+
+
+def test_type_check() -> None:
+    @inject
+    async def main(simple_factory: container.SimpleFactory = Provide[container.DIContainer.simple_factory]) -> None:
+        assert simple_factory
+
+    asyncio.run(main())
