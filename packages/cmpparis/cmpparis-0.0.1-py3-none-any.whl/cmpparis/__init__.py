f2a3f61209bcb460781640b087835bd427d37010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:05:54 +0000

    Add package cmpparis/0.0.1/cmpparis-0.0.1-py3-none-any.whl

---
+++ packages/cmpparis/cmpparis-0.0.1-py3-none-any.whl/cmpparis/__init__.py
@@ -0,0 +1,8 @@
+####################################################
+# __init__.py for the 'cmpparis' library
+# Created by: Sofiane Charrad
+####################################################
+
+from .utils import hello_paris
+
+__all__ = ["hello_paris"]
