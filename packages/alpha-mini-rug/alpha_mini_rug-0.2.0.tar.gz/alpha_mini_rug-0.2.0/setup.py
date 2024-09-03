2a43d9039539379182d378010f31048ca50ed241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:05:41 +0000

    Add package alpha-mini-rug/0.2.0/alpha_mini_rug-0.2.0.tar.gz

---
+++ packages/alpha-mini-rug/alpha_mini_rug-0.2.0.tar.gz/alpha_mini_rug-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alpha_mini_rug",
+    version="0.2.0",
+    description="Alpha Mini Robot wrapper for the RUG Social Robotics Lab",
+    author="RUG Social Robotics Lab",
+    packages=find_packages(),
+    install_requires=[
+        "txaio",
+        "autobahn",
+        "numpy",
+        "opencv-python",
+        "opencv-contrib-python",
+    ],
+)
