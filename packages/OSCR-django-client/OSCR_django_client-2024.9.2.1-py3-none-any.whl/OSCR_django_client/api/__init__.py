a96f176a77b6ab1183031a7b1371bd3aad5741fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:05:45 +0000

    Add package OSCR-django-client/2024.9.2.1/OSCR_django_client-2024.9.2.1-py3-none-any.whl

---
+++ packages/OSCR-django-client/OSCR_django_client-2024.9.2.1-py3-none-any.whl/OSCR_django_client/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from OSCR_django_client.api.combatlog_api import CombatlogApi
+from OSCR_django_client.api.ladder_api import LadderApi
+from OSCR_django_client.api.ladder_entries_api import LadderEntriesApi
+from OSCR_django_client.api.variant_api import VariantApi
+
