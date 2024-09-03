97112770a37071eb2c9adc5bf965f8bb3263c9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 05:05:37 +0000

    Add package pojang-resorter/2.32.22/pojang_resorter-2.32.22.tar.gz

---
+++ packages/pojang-resorter/pojang_resorter-2.32.22.tar.gz/pojang_resorter-2.32.22/pojang_resorter/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pojang Resorter Package
+
+This package provides functionality for the Pojang Resorter CLI tool.
+"""
+
+from .main import main
+
+__version__ = '2.32.22'
+__author__ = 'George Hammond'
