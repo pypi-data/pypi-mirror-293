6bd2dfe05bcbebc9d342a83841ea9c188185ea41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:05:32 +0000

    Add package pycolourslib/0.1.1/pycolourslib-0.1.1-py3-none-any.whl

---
+++ packages/pycolourslib/pycolourslib-0.1.1-py3-none-any.whl/pycolourslib/__init__.py
@@ -0,0 +1,28 @@
+from .colours import (
+    black, white, red, green, blue, yellow, cyan, magenta, gray, lightgray,
+    darkgray, orange, purple, brown, pink, lime, teal, navy, maroon, olive,
+    silver, gold, beige, coral, salmon, turquoise, indigo, crimson, darkblue,
+    darkgreen, darkred, firebrick, forestgreen, gainsboro, lightcoral, 
+    lightseagreen, lightsalmon, mediumaquamarine, mediumblue, mediumorchid, 
+    mediumslateblue, mediumpurple, mediumseagreen, mediumturquoise, 
+    mediumvioletred, midnightblue, mistyrose, oldlace, orangered, palegoldenrod, 
+    palegreen, paleturquoise, palevioletred, peachpuff, peru, plum, powderblue,
+    rosybrown, royalblue, saddlebrown, sandybrown, seashell, sienna, skyblue, 
+    slateblue, slategray, snow, springgreen, steelblue, tomato, wheat, yellowgreen
+)
+
+__all__ = [
+    'black', 'white', 'red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 
+    'gray', 'lightgray', 'darkgray', 'orange', 'purple', 'brown', 'pink', 
+    'lime', 'teal', 'navy', 'maroon', 'olive', 'silver', 'gold', 'beige', 
+    'coral', 'salmon', 'turquoise', 'indigo', 'crimson', 'darkblue', 
+    'darkgreen', 'darkred', 'firebrick', 'forestgreen', 'gainsboro', 
+    'lightcoral', 'lightseagreen', 'lightsalmon', 'mediumaquamarine', 
+    'mediumblue', 'mediumorchid', 'mediumslateblue', 'mediumpurple', 
+    'mediumseagreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 
+    'mistyrose', 'oldlace', 'orangered', 'palegoldenrod', 'palegreen', 
+    'paleturquoise', 'palevioletred', 'peachpuff', 'peru', 'plum', 
+    'powderblue', 'rosybrown', 'royalblue', 'saddlebrown', 'sandybrown', 
+    'seashell', 'sienna', 'skyblue', 'slateblue', 'slategray', 'snow', 
+    'springgreen', 'steelblue', 'tomato', 'wheat', 'yellowgreen'
+]
