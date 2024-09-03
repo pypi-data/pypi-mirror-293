23fac85e29c481c63a57a385b737fcbd4614e08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:06:17 +0000

    Add package mtmai/0.3.603/mtmai-0.3.603-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.603-py3-none-any.whl/mtmai/agents/graphchatdemo/entry_node.py
@@ -0,0 +1,62 @@
+import logging
+
+from langchain_core.messages import ChatMessage
+from langchain_core.runnables import RunnableConfig
+
+from mtmai.agents.graphchatdemo.prompts import Prompts
+from mtmai.agents.graphchatdemo.state import (
+    ChatBotUiState,
+    ExampleInputItem,
+    MainState,
+    UIChatMessageItem,
+    UiDelta,
+)
+from mtmai.core.config import settings
+
+from .state import agent_name
+
+logger = logging.getLogger()
+
+
+class EntryNode:
+    def __init__(self):
+        # self.runnable = runnable
+        pass
+
+    def __call__(self, state: MainState, config: RunnableConfig):
+        thread_id = config.get("configurable").get("thread_id")
+        db = state["context"].db
+        state_ret = {
+            "messages": [ChatMessage(role="system", content=Prompts.chatbot())],
+            "uidelta": UiDelta(
+                uiState=ChatBotUiState(
+                    threadId=thread_id,
+                    agent=agent_name,
+                    agent_url_base=settings.API_V1_STR + "/" + agent_name,
+                    graph_image_url=f"{settings.API_V1_STR}/{agent_name}/graph_image",
+                    example_input_items=[
+                        ExampleInputItem(
+                            title="例子1",
+                            description="ai介绍",
+                            content="请自我介绍,尽量详细.",
+                        ),
+                        ExampleInputItem(
+                            title="例子2", description="例子2描述", content="hello"
+                        ),
+                        ExampleInputItem(
+                            title="例子3", description="例子3描述", content="hello"
+                        ),
+                        ExampleInputItem(
+                            title="例子4", description="例子4描述", content="hello"
+                        ),
+                    ],
+                    uichatitems=[
+                        UIChatMessageItem(
+                            compType="UserInput", props={"content": "fake-user-input"}
+                        )
+                    ],
+                    # ui_messages=[UiChatItem(component="GraphFlowView", props={})],
+                )
+            ),
+        }
+        return state_ret
