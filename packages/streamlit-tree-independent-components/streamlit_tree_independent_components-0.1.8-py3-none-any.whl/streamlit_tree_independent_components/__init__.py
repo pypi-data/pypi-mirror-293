209c53a41ad7720ee6171f1c001ac30dca58d953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:07:06 +0000

    Add package streamlit-tree-independent-components/0.1.8/streamlit_tree_independent_components-0.1.8-py3-none-any.whl

---
+++ packages/streamlit-tree-independent-components/streamlit_tree_independent_components-0.1.8-py3-none-any.whl/streamlit_tree_independent_components/__init__.py
@@ -0,0 +1,26 @@
+import os
+import streamlit.components.v1 as components
+from typing import Callable, Optional
+
+_RELEASE = True
+
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "streamlit_tree_independent_components",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("streamlit_tree_independent_components", path=build_dir)
+
+
+def tree_independent_components(treeItems={},checkItems=[], disable=False, single_select=False, on_change: Optional[Callable[[], None]] = None, y_scroll=False, x_scroll=False,key=None, x_scroll_width=10, y_scroll_height=10):
+
+    component_value = _component_func(treeItems=treeItems,checkItems=checkItems, disable=disable,single_select=single_select, on_change=on_change, y_scroll=y_scroll,x_scroll=x_scroll, key=key, default=checkItems, x_scroll_width=x_scroll_width, y_scroll_height=y_scroll_height)
+    if on_change is not None:
+        on_change()
+    return component_value
+
+
