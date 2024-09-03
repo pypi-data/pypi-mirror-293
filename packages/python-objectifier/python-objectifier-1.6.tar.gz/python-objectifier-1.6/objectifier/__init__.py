3f301932f187ed292989ae49b56cfeeb85ea9907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 04:04:40 +0000

    Add package python-objectifier/1.6/python-objectifier-1.6.tar.gz

---
+++ packages/python-objectifier/python-objectifier-1.6.tar.gz/python-objectifier-1.6/objectifier/__init__.py
@@ -0,0 +1,39 @@
+import json
+dict_item = dict
+
+class convert_json_to_object:
+    def __new__(self, item):
+        if isinstance(item, str):
+            item = json.loads(item) # If json object string convert it first before objectifying
+
+        if isinstance(item, dict_item):
+            updated_dict = dict()
+            for key, value in item.items():
+                if isinstance(value, (dict_item, list)):
+                    value = convert_json_to_object(value)
+                setattr(updated_dict, key, value)
+                    
+            return updated_dict
+
+        elif isinstance(item, list):
+            for index, sub_item in enumerate(item):
+                if isinstance(sub_item, (dict_item, list)):
+                    item[index] = convert_json_to_object(sub_item)
+                
+            return item
+
+class dict(dict_item): # https://goodcode.io/articles/python-dict-object/
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError(f'No such attribute: {name}')
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError(f'No such attribute: {name}')
