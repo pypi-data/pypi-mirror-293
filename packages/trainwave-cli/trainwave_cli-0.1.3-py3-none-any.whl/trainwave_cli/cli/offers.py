7079e1ed2cab298724981b00526ad5b7b3752d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-03 03:05:43 +0000

    Add package trainwave-cli/0.1.3/trainwave_cli-0.1.3-py3-none-any.whl

---
+++ packages/trainwave-cli/trainwave_cli-0.1.3-py3-none-any.whl/trainwave_cli/cli/offers.py
@@ -0,0 +1,35 @@
+import typer
+from beaupy import confirm, prompt, select, select_multiple
+from rich.console import Console
+
+from trainwave_cli.api import Api
+from trainwave_cli.config.config import config
+from trainwave_cli.utils import async_command, ensure_api_key
+
+app = typer.Typer()
+
+
+HARDWARE_OPTIONS = [
+    "GPU",
+    "CPU",
+]
+CURSOR = "➡️"
+
+
+@app.command()
+@async_command
+@ensure_api_key
+async def list():
+    api_client = Api(config.api_key, config.endpoint)
+    offers = await api_client.offers()
+
+    console = Console()
+    console.print("Select Hardware")
+    hw = select(HARDWARE_OPTIONS, cursor=CURSOR)
+
+    if hw == "GPU":
+        
+    else:
+        pass
+
+
